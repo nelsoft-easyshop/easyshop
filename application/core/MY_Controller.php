f3be4e0322b026e77aa804fba9e58fdb260dae43
Sam Gavinio <samgavinio@easyshop.ph>
2014-02-19 10:01:44 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@100 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -144,17 +144,6 @@ class MY_Controller extends CI_Controller
 		return $data;
 	}
 
-	function clean($string){
-		$string = preg_replace("/\s+/", " ", $string);
-		$string = str_replace('-', ' ', trim($string)); 
-		$string = preg_replace("/\s+/", " ", $string);
-		$string = str_replace(' ', '-', trim($string));  
-		$string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);  
-
-		$string = str_replace('-', ' ', $string); 
-		$string = str_replace(' ', '-', $string);  
-		return preg_replace('/[^A-Za-z0-9\-]/', '', $string);
-	}
 
 }
 
