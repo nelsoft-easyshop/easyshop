8d4b816939de2b975d23c3787dfa3fb201ebd0af
ryan.vasquez <ryanannatuvasquez@easyshop.ph>
2014-02-19 06:43:28 +0000

    tipuedrop dont need anymore
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@89 2929c1f8-612a-3548-b275-2a983e7c7391

--- assets/JavaScript/js/tipuedrop_content.js
+++
@@ -1,5 +0,0 @@
-var tipuedrop = {"pages": [
-     {"title": "ssTipue Search", "thumb": "../../assets/images/img_girdear_pant2_thumb.jpg", "text": "Printed Thinck Winter Pants Floral Pants ", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"},
-     {"title": "ssTipue drop", "thumb": "../../assets/images/img_girdear_pant2_thumb.jpg", "text": "Tipue drop, a search suggestion box jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
-     {"title": "ssTipr", "thumb": "../../assets/images/img_girdear_pant2_thumb.jpg", "text": "Tipr, a small and simple tooltip jQuery plugin", "tags": "Tipue", "loc": "http://www.tipue.com/tipr"}
-]};
