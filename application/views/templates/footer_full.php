7d33215bd2a22eebfc18ce5c67acc015ed363185
paulmodino <paul.modino@easyshop.ph>
2014-02-20 08:02:06 +0000

    udpate layout and remove images replace with css sprite
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@138 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/footer_full.php
+++ application/views/templates/footer_full.php
@@ -1,6 +1,18 @@
 <footer>
       <div class="wrapper footer_panel">
-        <div>
+        <div class="social_media">
+          <h2>Stay in Touch</h2>
+          <span class="facebook"></span>
+          <span class="twitter"></span>
+          <span class="youtube"></span>
+          <span class="pinterest"></span>
+          <div class="signup">
+            <h2>Signup for Newsletter</h2>
+            <input type="text"><input type="submit" value="Submit">
+          </div>
+        </div>
+
+        <div class="about_footer">
           <h2>About</h2>
           <ul>
             <li><a href="<?=base_url()?>home/under_construction">About Us</a></li>
@@ -9,57 +21,23 @@
             <li><a href="<?=base_url()?>home/under_construction">User Services</a></li>
             <li><a href="<?=base_url()?>home/under_construction">Privacy Policy</a></li>
             <li><a href="<?=base_url()?>home/under_construction">Terms &amp; Conditions</a></li>
+            <li><a href="<?=base_url()?>home/under_construction">Contact Us</a></li>
           </ul>
+        </div>
 
-        </div>
-        <div>
+        <div class="footer_payment">
           <h2>Payment</h2>
-          <ul>
-            <li><img src="<?=base_url()?>assets/images/img_mastercard.png"></li>
-            <li><img src="<?=base_url()?>assets/images/img_visa.png"></li>
-            <li><img src="<?=base_url()?>assets/images/img_jcb.png"></li>
-          </ul>
-          
-        </div>
-        <div>
-          <h2>Connect</h2>
-          <ul>
-            <li>
-              <a href="<?=base_url()?>home/under_construction"><img src="<?=base_url()?>assets/images/icon_facebook.png"></a> 
-              <a href="<?=base_url()?>home/under_construction"><img src="<?=base_url()?>assets/images/icon_twitter.png"></a>
-            </li>
-            <li>
-              <a href="<?=base_url()?>home/under_construction"><img src="<?=base_url()?>assets/images/icon_youtube.png"></a> 
-              <a href="<?=base_url()?>home/under_construction"><img src="<?=base_url()?>assets/images/icon_googleplus.png"></a>
-            </li>
-            <li>
-              <a href="<?=base_url()?>home/under_construction"><img src="<?=base_url()?>assets/images/icon_pinterest.png"></a>
-            </li>
-          </ul>
-          
-        </div>
-        <div>
-          <h2>Contact</h2>
-          <div>
-            <address>
-              Unit 8C MARC 2000 TOWER 1973
-              <br /> Taft Avenue, Malate, Manila<br />Philippines
-            </address>
-            <br />
-            <p><strong>Telephone No.:&nbsp;</strong>(02)354-5973 or (02)353-6021</p>
-            <p><strong>Fax No.:&nbsp;</strong>(02)354-5973 or (02)353-6021</p>
-            <map></map>
-            
-          </div>
-          <div>
-		
-          </div>
-        </div>
-        <div class="clear"></div>
-        <div class="copyright">
-          <p>Copyright &copy; <?php echo date("Y"); ?> Easyshop.ph</p>
-        </div>
+            <span class="mastercard"></span>
+            <span class="visa"></span>
+            <span class="jcb"></span>
+            <span class="paypal"></span>
+        </div>   
       </div>
+      <div class="clear"></div>
+      <div class="copyright">
+          <p>Copyright &copy; <?php echo date("Y"); ?> Easyshop.ph</p>
+      </div>
+    </div>
     </footer>
 	</body>
 </html>
