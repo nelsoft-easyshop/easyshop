9bca5ab9ac1cd5f4efebc8c8f9278d9387c93223
paulmodino <paul.modino@easyshop.ph>
2014-02-20 06:55:46 +0000

    remove images and replace with css sprite
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@135 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -58,7 +58,7 @@
               <!-- <li><a href="<?=base_url()?>home/under_construction">Training Center</a></li> -->
             </ul>
           </li>
-          <li class="top_nav_main"><img src="<?=base_url()?>assets/images/img_cart.jpg">Shopping Cart <span class="cart_no"><?PHP echo isset($total_items)?$total_items:0; ?></span> items
+          <li class="top_nav_main"><span class="cart"></span>Shopping Cart <span class="cart_no"><?PHP echo isset($total_items)?$total_items:0; ?></span> items
             <ul>
               <li><a href="<?php echo base_url()."cart/"; ?>">View my Cart</a></li>
             </ul>
@@ -100,7 +100,7 @@
 <form action="<?php echo base_url(); ?>search/search.html" name="search_form" method="get">
   <section>
     <div class="wrapper search_wrapper">
-      <div class="logo"> <a href="<?=base_url()?>home"><img src="<?=base_url()?>assets/images/img_logo.png" alt="Logo"></a> </div>
+      <div class="logo"> <a href="<?=base_url()?>home"><span></span></a> </div>
       <div class="search_box prob_search_box">
         <div>
           <input name="q_str" type="text" id="main_search" value="<?php if(isset($_GET['q_str'])) echo str_replace('-', ' ', $_GET['q_str']); ?>" autocomplete="off">
