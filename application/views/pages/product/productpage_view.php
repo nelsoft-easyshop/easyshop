c8a6d1b6b250b355497e6a055026cb00eced6e14
paulmodino <paul.modino@easyshop.ph>
2014-03-06 03:35:57 +0000

    update thumbnail arrow
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@374 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/product/productpage_view.php
+++ application/views/pages/product/productpage_view.php
@@ -36,17 +36,19 @@
       <div  id="product_content_gallery">
         <div class="prod_con_gal"> <a href="<?=base_url()?><?php echo $product_images[0]['path']; ?><?php echo $product_images[0]['file']; ?>" class="jqzoom" rel='gal1'  title="Brand: <?php echo $product['brand_name'];?>" > <img src="<?=base_url()?><?php echo $product_images[0]['path']; ?>small/<?php echo $product_images[0]['file']; ?>"  title="product"> </a> </div>
         <br/>
-        <div class="jcarousel">
-          <ul id="thumblist">
-            <?php foreach($product_images as $image): ?>
-            <li> <a href="javascript:void(0);" rel="{gallery: 'gal1', smallimage: '<?=base_url()?><?php echo $image['path']; ?>small/<?php echo $image['file']; ?>',largeimage: '<?=base_url()?><?php echo $image['path']; ?><?php echo $image['file']; ?>'}"> <img src='<?=base_url()?><?php echo $image['path']; ?>thumbnail/<?php echo $image['file']; ?>'> </a> </li>
-            <?php endforeach;?>
-          </ul>
+        <div class="thumbnails_container">
+          <div class="jcarousel">
+            <ul id="thumblist">
+              <?php foreach($product_images as $image): ?>
+              <li> <a href="javascript:void(0);" rel="{gallery: 'gal1', smallimage: '<?=base_url()?><?php echo $image['path']; ?>small/<?php echo $image['file']; ?>',largeimage: '<?=base_url()?><?php echo $image['path']; ?><?php echo $image['file']; ?>'}"> <img src='<?=base_url()?><?php echo $image['path']; ?>thumbnail/<?php echo $image['file']; ?>'> </a> </li>
+              <?php endforeach;?>
+            </ul>
 
-        </div>
+          </div>
             <!-- Controls -->
             <a href="javascript:void(0)" class="jcarousel-control-prev inactive">&lsaquo;</a>
             <a href="javascript:void(0)" class="jcarousel-control-next inactive">&rsaquo;</a>
+        </div>
       </div>
       <div class="product_inner_content_info">
         <h1 class="id-class" id="<?php echo $product['id_product'];?>"> 
