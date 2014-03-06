95df1f9e64b7dfc9479b35b9719853138aa66202
Sam Gavinio <samgavinio@easyshop.ph>
2014-03-06 07:47:59 +0000

    Added data-default attribute for default qty
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@385 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/product/productpage_view.php
+++ application/views/pages/product/productpage_view.php
@@ -117,7 +117,7 @@
           </div>
           <div class="availability">
             <p> Availability <br />
-                <span class="quantity" data-qty="" id="p_availability"></span>
+                <span class="quantity" data-qty="" data-default="false" id="p_availability"></span>
             </p>
           </div>
           <div class="buy_box"> 
@@ -152,7 +152,7 @@
           <p> <strong>Description: </strong><?php echo html_purify($product['description']);?> </p>
           <ul>
             <li><strong>Brand: </strong><?php echo html_escape($product['brand_name']);?></li>
-            <li><strong>Style: </strong><?php echo html_escape($product['style_name']);?></li>
+            <!-- <li><strong>Style: </strong><?php echo html_escape($product['style_name']);?></li> -->
             <li><strong>Additional description: </strong><?php echo html_escape($product['brief']);?></li>
           </ul>
         </div>
