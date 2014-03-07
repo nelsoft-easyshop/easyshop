5ea73f313e47ead538f99e5ceadbb218cfc533a2
paulmodino <paul.modino@easyshop.ph>
2014-03-07 08:33:53 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@412 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/email_template.php
+++ application/views/templates/email_template.php
@@ -1,6 +1,6 @@
 <html>
   <body>
-    <table style="width:600px; text-align:center; margin:0px auto; border-top:5px solid #ff4400; border-left:1px solid #ffa382;border-right:1px solid #ffa382;border-bottom:1px solid #ffa382;">
+    <table style="width:600px; text-align:center; margin:0px auto; border-top:5px solid #ff4400; border-left:1px solid #ffa382;border-right:1px solid #ffa382;border-bottom:1px solid #ffa382; font-family:arial">
       <thead>
         <tr>
           <td style="height:60px;">
@@ -9,37 +9,36 @@
         </tr>
       </thead>
       <tbody>
-    <tr>
-      <td>
-      Dear {user},
-      </td>
-    </tr>
-        <tr>
-          <td>
-            <h2 style="color:#ff4400;">Thank you for registering to Easyshop.ph!</h2>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Please click below to verify your email address:<br />
-            <a href="{site_url}?h={hash}" style="background-color:#0191C8; color:#ffffff; display:inline-block; font-size:14px; padding:10px 20px; margin-top:20px;">Verify email</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-      By verifying your email address, you are made eligible to transact through Easyshop.ph! <br>
-      Easyshop.ph aims to provide its users a unique and user-friendly experience in online shopping. <br>
-      Easyshop.ph - Your store, made easy. 
-            &nbsp;
-          </td>
-        </tr>
-    <tr>
-       <td>
-       Contact Us: <br>
-       Unit 8-C 8th floor Marc Tower 2000, <br>
-       1973 Taft Avenue, San Andres Malate Manila <br>
-       </td>
-    </tr>
+      <tr>
+        <td style="text-align:left;padding-bottom:10px;">
+        Dear {user},
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h2 style="color:#ff4400;">Thank you for registering to Easyshop.ph!</h2>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Please click below to verify your email address:<br />
+          <a href="{site_url}?h={hash}" style="background-color:#0191C8; color:#ffffff; display:inline-block; font-size:14px; padding:10px 20px; margin-top:20px;">Verify email</a>
+        </td>
+      </tr>
+      <tr>
+        <td style="text-align:left;font-size:12px;padding-top:50px;">
+          By verifying your email address, you are made eligible to transact through <span style="color:#ff4400;font-weight:bold;">Easyshop.ph!</span> <br>
+          <span style="color:#ff4400;font-weight:bold;">Easyshop.ph</span> aims to provide its users a unique and user-friendly experience in online shopping. <br>
+          <span style="color:#ff4400;font-weight:bold;">Easyshop.ph</span> - Your store, made easy. 
+        </td>
+      </tr>
+      <tr>
+         <td style="text-align:left;padding-top:50px;font-size:12px;">
+           <strong>Contact Us:</strong> <br />
+           Unit 8-C 8th floor Marc Tower 2000, <br />
+           1973 Taft Avenue, San Andres Malate Manila 
+         </td>
+      </tr>
       </tbody>
     </table>
 
