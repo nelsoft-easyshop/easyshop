e0e61e892c145ada250208694a36372b694e4adb
stephenjanz <stephenjanz@easyshop.ph>
2014-03-11 10:14:29 +0000

    removed dev code
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@493 2929c1f8-612a-3548-b275-2a983e7c7391

--- assets/tinymce/plugins/jbimages/ci/application/controllers/uploader.php
+++ assets/tinymce/plugins/jbimages/ci/application/controllers/uploader.php
@@ -142,7 +142,6 @@ class Uploader extends CI_Controller {
 			// Output to user
 			$this->load->view('ajax_upload_result', $result);
 		}
-		sleep(10);
 	}
 	
 	/* Blank Page (default source for iframe) */
