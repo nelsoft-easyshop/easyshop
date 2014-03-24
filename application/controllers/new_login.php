88955c131ecb1783120a8ebdf81d41dee6d6e813
stephenjanz <stephenjanz@easyshop.ph>
2014-03-24 08:22:03 +0000

    added new login controller js and view for combined login and register page. added new form validation with no captcha check for new registration page
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@666 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/controllers/new_login.php
@@ -0,0 +1,197 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class New_login extends MY_Controller {
+	#Load the user model that will be used in the functions below
+    function __construct() {
+        parent::__construct();
+        $this->load->library('cart');
+		$this->load->model('register_model');
+		$this->load->library('encrypt');
+    }
+	
+	#Passing the data to login form and loads it.
+    function index() { 
+        $data = array(
+            'title' => 'Login | Easyshop.ph',
+            'page_javascript' => 'assets/JavaScript/new_login.js',
+			);
+		$data = array_merge($data, $this->fill_header());
+        $view = 'login_view';
+        $this->load->view('templates/header_plain', $data);
+        //$this->load->view('pages/user/login_view');
+		$this->load->view('pages/user/new_loginview');
+        $this->load->view('templates/footer');
+    }
+
+	#when login button is clicked, username pass will check if it exsits in the database
+	function authenticate() {
+		if(($this->input->post('login_form'))&&($this->form_validation->run('login_form'))){
+			$uname = $this->input->post('login_username');
+			$pass = $this->input->post('login_password');	
+			$dataval = array('login_username' => $uname, 'login_password' => $pass);
+			$row = $this->user_model->verify_member($dataval);               
+
+			if ($row['o_success'] >= 1) {///////////////////////////////////////////if exsist,member id and usersession(defined in the sql query) will be set in the session
+				$this->session->set_userdata('member_id', $row['o_memberid']);
+				$this->session->set_userdata('usersession', $row['o_session']);
+                $this->session->set_userdata('cart_contents', $this->cart_model->cartdata($row['o_memberid']));
+				if($this->input->post('keepmeloggedin') == 'on'){ //create cookie bound to memberid||ip||browser 
+					$temp = array(
+						'member_id' => $this->session->userdata('member_id'),
+						'ip' => $this->session->userdata('ip_address'),
+						'useragent' => $this->session->userdata('user_agent'),
+						'session' => $this->session->userdata('session_id'),
+					);
+					$cookieval = $this->user_model->dbsave_cookie_keeplogin($temp)['o_token'];
+					$this->user_model->create_cookie($cookieval);
+				}
+			}  
+			echo json_encode($row);//////////////////////////////////////////////////else return o_message(set in the query) to ajax and post it
+        }
+    }
+
+    function logout() {/////////////////////////////////////////////////////////when log out button is clicked,session will be set to empty and redirect to home
+        
+        $cart_items = serialize($this->session->userdata('cart_contents'));
+        $id = $this->session->userdata('member_id');
+        $this->cart_model->save_cartitems($cart_items,$id);
+        
+        $this->user_model->logout();
+        $temp = array(
+                'member_id' => $this->session->userdata('member_id'),
+                'ip' => $this->session->userdata('ip_address'),
+                'useragent' => $this->session->userdata('user_agent'),
+                'token' => get_cookie('es_usr')
+        );
+
+        $this->user_model->dbdelete_cookie_keeplogin($temp);
+        delete_cookie('es_usr');
+		
+        $this->session->sess_destroy();
+		$referrer = $this->input->get('referrer');
+		if(trim($referrer))
+			redirect(base_url().$referrer);
+		else
+			redirect(base_url().'login');		
+    }
+	
+	
+	
+	/**	SIGNUP in Login Page**/
+	function signup()
+	{
+		if(($this->input->post('register_page1'))&&($this->form_validation->run('register_form1_new'))){
+			$data['username'] = html_escape($this->input->post('username'));
+			$data['password'] = html_escape($this->input->post('password'));
+			$data['email'] = $this->input->post('email');
+			
+			
+			$this->sendVerificationCode($data);
+
+			$temp['member_username'] = $data['username'];
+			$temp['verification_msg'] = $this->lang->line('success_registration');
+			$temp = array_merge($temp, $this->fill_header());
+		
+			$this->load->view("pages/user/register_form3_view", $temp);
+		}
+	}
+	
+	function sendVerificationCode($data)
+	{
+		//GENERATE MOBILE CONFIRMATION CODE
+		$temp['mobilecode'] = $this->register_model->rand_alphanumeric(6);
+		//GENERATE HASH FOR EMAIL VERIFICATION
+		$temp['emailcode'] = sha1($this->session->userdata('session_id').time());
+		
+		$temp['member_id'] = $this->register_model->signupMember($data)['id_member'];
+		
+		$status = $this->register_model->send_email_msg($data['email'], $data['username'], $temp['emailcode']);
+		//$status = 'success';
+		
+		if($status === 'success'){
+			$temp['email'] = 1;
+		}else{
+			$temp['email'] = 0;
+		}
+		
+		$this->register_model->store_verifcode($temp);
+	}
+	/** End of Signup in Login Page **/
+	
+	
+	
+	function identify(){
+		
+        $data = array(
+            'title' => 'Forgot Password | Easyshop.ph',
+            'page_javascript' => 'assets/JavaScript/login.js',
+		);
+		$data = array_merge($data, $this->fill_header());
+        $this->load->view('templates/header_plain', $data);
+		
+		$temp['toggle_view'] = "";
+		if(($this->input->post('identify_btn'))&& ($this->form_validation->run('identify_form'))){
+			
+            $email = $this->input->post('email');
+            $result = $this->register_model->validate_email($email);
+         
+			if (isset($result['username'])){
+				// magsesend na siya ng email dito at magra-write sa db.
+				if ($this->register_model->forgotpass($email, $result['username'], $result['id_member']) == 1){
+					$temp['toggle_view'] = "1";
+				}else{
+					$temp['toggle_view'] = "3";		
+				}
+			}else{
+				$temp['toggle_view'] = "2";
+			}
+        }
+        $this->load->view('pages/user/forgotpass', $temp);
+        $this->load->view('templates/footer');	
+	}
+	
+	function resetconfirm(){
+
+		$data = array(
+			'title' => 'Reset Password | Easyshop.ph'
+			//'page_javascript' => 'assets/JavaScript/register_js.php',
+		);
+		$data = array_merge($data, $this->fill_header());
+		$this->load->view('templates/header_plain', $data);		
+        
+		$hash = html_escape($this->input->get('confirm'));
+        
+        $result = $this->register_model->forgotpass_email($hash);
+        
+        if (isset($result['username'])){
+            $user = $result['username'];
+            $curpass = $result['password'];
+            
+			if(($this->input->post('forgotpass_btn')) && ($this->form_validation->run('forgotpass'))){
+				$data = array(
+					'username' => $user,
+					'cur_password' => $curpass,
+					'password' => $this->input->post('password')
+				);
+				$temp['toggle_view'] = "";
+				$this->register_model->forgotpass_update($data); // pasok pa sa oras pero magsasave na.
+				$this->session->unset_userdata('user_cur_loc');
+			}
+			else{
+				$temp['toggle_view'] = "1";                      // pasok pa sa oras pero hindi pa magsasave ito.
+			}			
+		}else{
+			$temp['toggle_view'] = "2";                        
+		}
+        $this->load->view('pages/user/forgotpass_confirm', $temp);
+		$this->load->view('templates/footer');
+	}
+	
+}
+
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
---
+++ application/views/pages/user/new_loginview.php
@@ -0,0 +1,148 @@
+<div id="login_register">
+<div class="clear"></div>
+
+      <div class="wrapper login_content">
+	
+          <div class="login_left_content"><img src="<?=base_url()?>assets/images/img_login_banner.jpg" alt="banner"></div>
+			<?php if($logged_in): ?>
+				<div class="login_box">
+					<div style="text-align:center;">
+						<p>You are currently signed-in as <b><?php echo $uname; ?></b>. </p>
+						<p>If you wish to sign-in as another user, <a href='<?=base_url()?>login/logout' class="orange2">click here.</a> </p>
+					</div>
+				</div>
+			<?php else: ?>
+				<!--<form id="login_form">-->
+				<?php
+					$attr = array('id'=>'login_form');
+					echo form_open('', $attr);
+				?>
+					<div id="login_div" class="login_box fillup">
+						<div>
+                            <label for="login_username">Username:</label>
+                            <input type="text" id="login_username" name="login_username">
+							<span id="username_error" class="red error_cont" style="font-weight:bold;margin-left:8em;"></span>
+						</div>
+						<div>
+						  <label for="login_password">Password:</label>
+						  <input type="password" id="login_password" name="login_password"> 
+						  <span id="passw_error" class="red error_cont" style="font-weight:bold;margin-left:8em;"></span>
+						  <p><a href="<?=base_url()?>login/identify">Forgot your password?</a></p>
+						</div>
+						<div>
+						  <input type="checkbox" name="keepmeloggedin">
+						  <p>
+							Keep me signed in
+						  </p>
+						</div>
+
+						<div>
+						  <input id="login" type="submit" name="login_form" value="Login"/>
+						  <img src="<?=base_url()?>/assets/images/orange_loader_small.gif" id="loading_img" class="login_loading_img" style="display:none"/>
+						  <br>
+						  <p>
+							<!--<a href="<?=base_url()?>register">Don't have an account? Register now</a>-->
+							<a class="signup_register">Don't have an account? Register now</a>
+						  </p>
+						</div>
+					</div>
+					<div>
+						<?php
+							/*
+							echo 'SERVER: '.$_SERVER['HTTP_REFERER'];
+							echo '<br/>';
+							$redirect = isset($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:base_url().'home';
+							echo 'redirect: '.$redirect.' ';echo '<br/>';
+							echo 'currenturl: '.current_url().'<br/>';
+							echo 'uri: '.uri_string(); echo '<br/>';
+							$redirect = ($redirect!==$_SERVER['PHP_SELF'])?$redirect:base_url().'home'; 
+							<input id="login_redirect" type = "hidden" value="<?php echo $redirect;?>"/>
+							*/
+						?>
+					</div>
+                    <input id="redirect" type="hidden" value="<?php echo $user_cur_loc;?>"/> 
+                    
+				<?php echo form_close();?>
+				
+				
+				<!-- Registration Section -->
+					<div id="signup_div" class="login_box fillup" style="display:none;">
+					<?php
+						$attr2 = array('id'=>'register_form1');
+						echo form_open('', $attr2);
+					?>
+						<div class="field">
+							<label for="username">Username:</label>
+							<input id="username" name="username" type="text" maxlength="25" autocomplete="off" />
+							<input type="hidden" id="usernamecheck" value="">
+							<img class="check_loader" src="<?=base_url()?>/assets/images/orange_loader_small.gif" style="display:none;">
+							<div id="username_status" style="display:inline;">
+								<img src="<?=base_url()?>/assets/images/check_icon.png" id="username_check" style="position: relative;display:none;vertical-align:middle"/>
+								<img src="<?=base_url()?>/assets/images/x_icon.png" id="username_x" style="position: relative;display:none;vertical-align:middle"/>
+								<span class="username_availability"></span>
+							</div>
+							<span class="red ci_form_validation_error"><?php echo form_error('username'); ?></span>
+						</div>
+						<div class="username_info">
+							  The username must be 5-25 characters long.
+						</div>
+
+						<div class="field">
+							<div class="password_label">
+							  <label for="password">Password:</label>
+							  <input id="password" name="password" type="password" maxlength="25" />
+							</div>
+							<span class="red ci_form_validation_error"><?php echo form_error('password'); ?></span>
+						</div>
+						  
+						<div class="clear"></div>
+						<div class="password_info">
+							Passwords must be within 6-25 characters, and include numbers and upper-case and lower-case letters.
+						</div>
+						
+						<div class="field">
+							<label>Confirm Password:</label>
+							<input id="cpassword" name="cpassword" type="password" maxlength="100" disabled="true">
+							<img src="<?=base_url()?>/assets/images/check_icon.png" id="cpassword_check" style="position: relative;display:none;"/>
+							<img src="<?=base_url()?>/assets/images/x_icon.png" id="cpassword_x" style="position: relative;display:none; "/>
+							<span class="red ci_form_validation_error"><?php echo form_error('cpassword'); ?></span>
+						</div>
+						  
+						<div class="field">			
+							<label for="email">Email Address:</label>
+							<input type="text" name="email" class="contactinfo" id="email" maxlength="40">
+							<input type="hidden" id="emailcheck" value="">
+							<img class="check_loader" src="<?=base_url()?>/assets/images/orange_loader_small.gif" style="display:none;">
+							<div id="email_status" style="display:inline;">
+								<img src="<?=base_url()?>/assets/images/check_icon.png" id="email_check" style="position: relative;display:none;vertical-align:middle"/>
+								<img src="<?=base_url()?>/assets/images/x_icon.png" id="email_x" style="position: relative;display:none;vertical-align:middle"/>
+								<span class="email_availability"></span>
+							</div>
+							<span class="red ci_form_validation_error"><?php echo form_error('email'); ?></span>
+						</div>
+						
+						<div class="field" id="tc">
+							<input type="checkbox" name="terms_checkbox" id="terms_checkbox">
+							<label for="terms_checkbox" id="terms">I agree to Easyshop.ph's Terms and Conditions.</label>
+							<span class="red ci_form_validation_error"><?php echo form_error('terms_checkbox'); ?></span>
+						</div>
+						
+						<div class="field" style="padding-top:25px">
+							<input type="submit" id="register_page1" name="register_page1" value="Agree and Continue"/>
+							<img src="<?=base_url()?>/assets/images/orange_loader_small.gif" id="register_form1_loadingimg" style="position: relative; top:10px; left:20px; display:none"/>
+							<a class="signup_register">Already have an account? Sign In</a>
+						</div>
+						
+						<?php echo form_close();?>
+					</div>
+					<!-- END OF Registration Section -->
+
+			<?php endif; ?>
+      </div>
+
+    </section>
+   <div class="clear"></div>
+</div>
+   
+<script type='text/javascript' src='<?=base_url()?>assets/JavaScript/js/jquery.validate.js'></script>   
+<script src="<?=base_url().$page_javascript?>" type="text/javascript"></script>
