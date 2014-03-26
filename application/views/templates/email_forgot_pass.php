7d7c8f0bf863ad99e7e51eef9865685f24b30dd1
paulmodino <paul.modino@easyshop.ph>
2014-03-26 10:58:33 +0000

    apply css style
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@701 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/email_forgot_pass.php
+++ application/views/templates/email_forgot_pass.php
@@ -1,17 +1,31 @@
-<section>
-  	<div align="right"><a href="<?=base_url()?>home"><img src="<?=base_url()?>assets/images/img_logo.png" alt="Easyshop.ph"></a> </div>
-</section>
-<section>
-    <div>
-        Hi <?php echo $username; ?>,<br /><br />
-        We received a password reset request for your Easyshop.ph account. To reset your password, use the links below:<br />
-        <a href="<?php echo base_url() . "login/resetconfirm?&confirm=". $trigger; ?>"><?php echo base_url() . "login/resetconfirm?confirm=". $trigger; ?></a>
-        <br /><br />
-        If you didn't request a password reset, you can let us know here:<br />
-        <a href="https://Easyshop.ph/contact/">https://Easyshop.ph/contact/</a>. <br />
-        Either way, you can ignore this message and your password will not be changed -- someone probably typed in your username or email address by accident.
-        <br /><br />
-        <strong>- The Easyshop.ph Team</strong>    
-        <br /><br /><br /><br /><br /><br />
-    </div>
-</section>
+<table style="width:780px; margin:0px auto; border-top:5px solid #f18200; border-left:1px solid #f18200;border-right:1px solid #f18200;border-bottom:1px solid #f18200; font-family:arial">
+      <thead>
+        <tr>
+          <td style="height:60px;text-align:center;">
+            <a href="<?=base_url()?>home"><img src="<?=base_url()?>assets/images/img_logo.png" alt="Easyshop.ph"></a>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>
+                <div>
+                Hi <?php echo $username; ?>,<br /><br />
+                We received a password reset request for your Easyshop.ph account. To reset your password, use the links below:<br />
+                <a href="<?php echo base_url() . "login/resetconfirm?&confirm=". $trigger; ?>"><?php echo base_url() . "login/resetconfirm?confirm=". $trigger; ?></a>
+                <br /><br />
+                If you didn't request a password reset, you can let us know here:<br />
+                <a href="https://Easyshop.ph/contact/">https://Easyshop.ph/contact/</a>. <br />
+                Either way, you can ignore this message and your password will not be changed -- someone probably typed in your username or email address by accident.
+            </div>
+            </td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+         <td style="text-align:left;padding-top:50px;padding-bottom:50px;font-size:12px;">
+           <strong>- The Easyshop.ph Team</strong>   
+         </td>
+      </tr>
+    </tfoot>
+</table>
