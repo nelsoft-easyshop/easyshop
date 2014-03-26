1bbf234a9a90b26d0b410791d00fbe8dce996324
paulmodino <paul.modino@easyshop.ph>
2014-03-26 11:00:52 +0000

    update css style of reset password button apply css to message when reset pword is click
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@702 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/user/forgotpass.php
+++ application/views/pages/user/forgotpass.php
@@ -3,12 +3,18 @@
 	switch ($toggle_view){
 		case "1":
 			$msg = $this->lang->line('msg1');
+			$class = "error1";
+			$imgclass = "img_error1";
 			break;
 		case "2":
 			$msg = $this->lang->line('msg2');
+			$class = "error2";
+			$imgclass = "img_error2";
 			break;
 		case "3":
 			$msg = $this->lang->line('msg3');
+			$class = "error3";
+			$imgclass = "img_error3";
 			break;							
 		default:
 			$msg = form_error('email');
@@ -22,15 +28,17 @@
 	$attr = array('id'=>'identify_form');
 	echo form_open('',$attr);
   ?>
-    <div class="login_box">
+    <div class="login_box reset_pword">
       <div>
         <label for="email">Email Address: </label>
         <input type="text" id="email" name="email">
       </div>
-      <div>
+      <div class="reset_pword_btn_con">
         <input id="identify_btn" type="submit" name="identify_btn" value="Reset Password"/>
       </div>
-      <span class="red" id="login_error"><center><?php echo $msg; ?></center></span>
+      <div class="reset_pword_error_con <?php echo $class; ?>">
+      <span class="span_bg img_error_con <?php echo $imgclass; ?>"></span><span id="login_error"><?php echo $msg; ?></span>
+      </div>
     </div>
   <?php echo form_close();?>
 </div>
