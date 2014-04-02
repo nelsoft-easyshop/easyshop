70fd816d1557cca7a8740417806cf43add7e278d
stephenjanz <stephenjanz@easyshop.ph>
2014-04-02 11:12:38 +0000

    Update
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@786 2929c1f8-612a-3548-b275-2a983e7c7391

--- assets/JavaScript/landingpage/landingpage.js
+++ assets/JavaScript/landingpage/landingpage.js
@@ -1,4 +1,3 @@
-
 /**********************************************************************/
 /**************************	SIGNUP/REGISTER ***************************/
 /**********************************************************************/
@@ -12,7 +11,7 @@ $(document).ready(function(){
 		  });
 	  }).on('blur', function(){
 		var fieldlength = $.trim($('#username').val()).length;
-		if(fieldlength >= 5 && $(this).hasClass('forSearch')){
+		if(fieldlength >= 5 && $(this).hasClass('forSearch') && $(this).hasClass('valid')){
 			setTimeout(username_check,500);
 		}
 		else if(fieldlength < 5){
@@ -47,7 +46,7 @@ $(document).ready(function(){
 			       $('#cpassword').val("");
 				   hidecheckx($('#cpassword'));
 			   }
-			   
+
 				if($(this).val() !== $('#cpassword').val() && !$('#cpassword')[0].disabled)
 					showx($('#cpassword'));
 				else if($(this).val() == $('#cpassword').val() && !$('#cpassword')[0].disabled)
@@ -65,7 +64,7 @@ $(document).ready(function(){
 	  
 	  $('#email').on('blur', function(){
 		var fieldlength = $.trim($('#email').val()).length;
-		if(fieldlength >= 6 && $(this).hasClass('forSearch')){
+		if(fieldlength >= 6 && $(this).hasClass('forSearch') && $(this).hasClass('valid')){
 			setTimeout(email_check,500);
 		}
 		else if(fieldlength < 6){
@@ -93,11 +92,7 @@ $(document).ready(function(){
 
 	 jQuery.validator.addMethod("alphanumeric", function(value, element) {
         return this.optional(element) || (/[a-zA-Z]/.test(value) && /\d/.test(value));
-     }, "Must contain numbers and letters"); 
-	 
-	 jQuery.validator.addMethod("case_all", function(value, element) {
-        return this.optional(element) || (/[a-z]/.test(value) && /[A-Z]/.test(value));
-     }, "Must contain upper-case and lower-case letters"); 
+     }, "Must contain numbers and letters");
 	 
 	 jQuery.validator.addMethod("alphanumeric_underscore", function(value, element) {
         return this.optional(element) || /^\w+$/i.test(value);
@@ -109,15 +104,13 @@ $(document).ready(function(){
                 required: true,
                 minlength: 5,
                 maxlength:25,
-				alphanumeric_underscore: true,
-				equalTo: '#usernamecheck'
+				alphanumeric_underscore: true
 				},
 			password: {
 				required: true,
                 minlength: 6,
                 maxlength:25,
-				alphanumeric: true,
-				//case_all: true
+				alphanumeric: true
 				},
 			cpassword: {
 				required: true,
@@ -128,62 +121,69 @@ $(document).ready(function(){
 			email: {
 				required: true,
 				email: true,
-				minlength: 6,
-				equalTo: '#emailcheck',
+				minlength: 6
 				}
 		 },
 		 messages:{
-		 	username:{
-				equalTo: ''
-			},
 			cpassword:{
 				equalTo: ''
 			},
 			email:{
 				required: "Please enter a valid email address",
 				email: 'Please enter a valid email address',
-				minlength: '*Email too short',
-				equalTo: ''
+				minlength: '*Email too short'
 			}
 		 },
 		 errorElement: "span",
 		 errorPlacement: function(error, element) {
 				error.addClass('red');
+				/*
 				if(element.attr('name') == 'password'){
 					var added_span = $('<span/>',{'class':"red"});
 					error.insertBefore(element.next());
 					added_span.insertBefore(element.next());}
 				else
 					error.appendTo(element.parent());
+				*/
+				if(element.attr('name') !== 'cpassword'){
+					error.insertAfter(element);
+				}
 		 },
 		 submitHandler: function(form){
-			$('#register_form1_loadingimg').show();
-			$('#register_form1_btn').attr('disabled', true);
-		 	$.post(config.base_url + 'landingpage/signup', $(form).serializeArray(), function(data){
-				$('#register_form1_loadingimg').hide();
-				$('#register_form1_btn').attr('disabled', false);
-				if(data == 1){
-					$('#result_desc').html("Thank you for registering to Easyshop.ph!");
-					var title = "Registration Complete";
-				}
-				else{
-					$('#result_desc').html("An error was encountered. Try again later.");
-					var title= "Failed to Register";
-				}
-				$('#register_result').dialog({
-					autoOpen: false,
-					title: title,
-					modal: true,
-					closeOnEscape: false,
-					close: function(event,ui){window.location.replace(config.base_url + "landingpage");},
-					buttons:{
-						OK: function(){
-							$(this).dialog("close");
-						}
+			if( $('#username').hasClass('pass') && $('#email').hasClass('pass') ){
+				$('#register_form1_loadingimg').show();
+				$('#register_form1_btn').attr('disabled', true);
+
+			 	$.post(config.base_url + 'landingpage/signup', $(form).serializeArray(), function(data){
+					$('#register_form1_loadingimg').hide();
+					$('#register_form1_btn').attr('disabled', false);
+					if(data == 1){
+						$('#result_desc').html("Thank you for registering to Easyshop.ph!");
+						var title = "Registration Complete";
+
+						$('div.pass-container, .fieldstatus').hide();
+						$(form).find('input.reqfield').each(function(){
+							$(this).prop('value', '');
+						});
 					}
+					else{
+						$('#result_desc').html("An error was encountered. Try again later.");
+						var title= "Failed to Register";
+					}
+					$('#register_result').dialog({
+						autoOpen: false,
+						title: title,
+						modal: true,
+						closeOnEscape: false,
+						buttons:{
+							OK: function(){
+								$(this).dialog("close");
+							}
+						}
+					});
+					$('#register_result').dialog('open');
 				});
-				$('#register_result').dialog('open');
-			});
+			}
 			return false;
 		 }
 	 });
@@ -191,7 +191,6 @@ $(document).ready(function(){
 	 $('.field input').on('click', function(){
 		$('.ci_form_validation_error').text('');
 	 });
-	 
 	 
 	 $('#subscription_form').validate({
 		rules: {           
@@ -216,12 +215,16 @@ $(document).ready(function(){
 		 submitHandler: function(form){
 			$('#subscribe_loadingimg').show();
 			$('#subscribe_btn').attr('disabled', true);
-		 	$.post(config.base_url + 'landingpage/subscribe', $(form).serializeArray(), function(data){
+
+			$.post(config.base_url + 'landingpage/subscribe', $(form).serializeArray(), function(data){
 				$('#subscribe_loadingimg').hide();
 				$('#subscribe_btn').attr('disabled', false);
 				if(data == 1){
 					$('#result_desc').html("Thank you for subscribing to Easyshop.ph!");
 					var title = "Subscription Complete";
+					$(form).find('input[type="text"]').each(function(){
+						$(this).prop('value', '');
+					});
 				}
 				else{
 					$('#result_desc').html("An error was encountered. Try again later.");
@@ -232,7 +235,6 @@ $(document).ready(function(){
 					title: title,
 					modal: true,
 					closeOnEscape: false,
-					close: function(event,ui){window.location.replace(config.base_url + "landingpage");},
 					buttons:{
 						OK: function(){
 							$(this).dialog("close");
@@ -242,8 +244,7 @@ $(document).ready(function(){
 				$('#register_result').dialog('open');
 			});
 			return false;
-		 }
-		
+		}
 	});
 });
 
@@ -251,38 +252,44 @@ $(document).ready(function(){
 function username_check(){
 	var username = $('#username').val();
 	var csrftoken = $('#register_form1').find('input[name^="es_csrf"]').val();
-	$('#username').siblings('img.check_loader').show().css('display','inline-block');
+	var field = $('#username');
+	$('#username_loader').show().css('display','inline-block');
 	$.post(config.base_url+'landingpage/username_check', {username: username, es_csrf_token : csrftoken}, function(result){
 		if(result == 1){
 			showcheck($('#username'));
 			$('.username_availability').html('Username available');
 			$('#usernamecheck').attr('value', $('#username').val());
+			field.addClass('pass');
 		}
 		else{
 			showx($('#username'));
 			$('.username_availability').html('Username already exists.');
+			field.removeClass('pass');
 		}
-		$('#username').removeClass('forSearch');
-		$('#username').siblings('img.check_loader').hide();
+		field.removeClass('forSearch');
+		$('#username_loader').hide();
 	});
 }
 
 function email_check(){
 	var email = $('#email').val();
 	var csrftoken = $('#register_form1').find('input[name^="es_csrf"]').val();
-	$('#email').siblings('img.check_loader').show().css('display','inline-block');
+	var field = $('#email');
+	$('#email_loader').show().css('display','inline-block');
 	$.post(config.base_url+'landingpage/email_check', {email: email, es_csrf_token : csrftoken}, function(result){
 		if(result == 1){
 			showcheck($('#email'));
 			$('.email_availability').html('Email available');
 			$('#emailcheck').attr('value', $('#email').val());
+			field.addClass('pass');
 		}
 		else{
 			showx($('#email'));
 			$('.email_availability').html('Email already used.');
+			field.removeClass('pass');
 		}
-		$('#email').removeClass('forSearch');
-		$('#email').siblings('img.check_loader').hide();
+		field.removeClass('forSearch');
+		$('#email_loader').hide();
 	});
 }
 
