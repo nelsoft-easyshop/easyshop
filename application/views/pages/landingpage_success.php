fc8b4bd11d3987fcd7e1f989f3e4e8657fc79b6c
paulmodino <paul.modino@easyshop.ph>
2014-04-11 06:34:18 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@947 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/landingpage_success.php
+++ application/views/pages/landingpage_success.php
@@ -1,1 +1,59 @@
-easyshop
+<!DOCTYPE html>
+    <head>
+
+<meta property="og:title" content="EasyShop.ph" />
+<meta property="og:description" content="Sample Description" />
+<meta property="og:image" content="https://easyshop.ph/assets/images/img_logo.png" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="<?=base_url()?>assets/css/landingpage/bootstrap.css" rel="stylesheet">
+        <link href="<?=base_url()?>assets/css/landingpage/bootstrap-responsive.css" rel="stylesheet">
+        <link href="<?=base_url()?>assets/css/landingpage/mystyle.css" rel="stylesheet">
+		<link href="<?=base_url()?>assets/css/jquery-ui.css" rel="stylesheet">
+		<title><?php echo $title;?></title>
+    </head>
+    <body>
+        <div class = "container header_bg">
+            <div class="row-fluid">
+                <div id="himg" class="text-center"> <img src="<?=base_url()?>assets/images/landingpage/img_logo.png"> </div>
+               
+                    <!-- <p class="text-center txt_success"><?php echo $content; ?></p>  -->
+                    <p class="text-center">
+                    	<img src="<?=base_url()?>assets/images/img_success_register.png" alt="">
+                    </p>
+                    <p class="text-center txt_success orange">
+                    	Like us on and share
+                    </p>
+                    <p class="text-center img_social_media">
+                    	<a href="" class="img_sc_1">
+                    		<img src="<?=base_url()?>assets/images/img_social_media_facebook.png" alt="facebook">
+                    	</a>
+                    	<a href="">
+                    		<img src="<?=base_url()?>assets/images/img_social_media_twitter.png" alt="Twitter">
+                    	</a>
+                    </p>                
+               
+            </div>
+        </div>
+       
+		
+		
+		
+       
+         <div class="footer2">
+            <div class="container">
+                <span class="help-block spnmsg"><br>Copryright &#169; 2014 easyshop.ph. All rights reserved<br></span>
+            </div>
+        </div>
+    </body>
+
+	
+	<script type='text/javascript' src="<?=base_url()?>assets/JavaScript/js/jquery-1.9.1.js" ></script>
+	<script type='text/javascript' src="<?=base_url()?>assets/JavaScript/js/jquery-ui.js"></script>
+	<script type='text/javascript' src='<?=base_url()?>assets/JavaScript/landingpage/bootstrap.min.js'></script>
+	
+	<!-- Form Plugins -->
+
+	
+	
+	
