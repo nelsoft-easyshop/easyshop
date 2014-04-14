e38c6f2810690dc6b4d7ed6380482a64cbfc9bd0
stephenjanz <stephenjanz@easyshop.ph>
2014-04-14 06:52:28 +0000

    Added landingpage email templates
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@984 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/views/templates/landingpage/lp_subscription_email.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		
+		<div class="contentWrap" style="background: #F1F1F1; width: 100%; padding-top: 30px;">
+			<div class="head" style="background-image: url('header_bg.jpg'); background-color: #019CDC; height: 100px; text-align: center; width: 600px; margin: 0 auto; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;">
+				<a href="https://easyshop.ph/" style="outline: none; border: 0;">
+					<img src="easyshoplogo-transp.png" style="width: 300px;	margin-top: 30px;" alt="EasyShop Logo">
+				</a>
+			</div>
+		
+			<div class="content" style="background-color: #ffffff; padding: 30px 20px; overflow: auto; display: block; width: 600px; margin: 0 auto; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;">
+
+				<h2 class="intro" style="margin-bottom: 30px; font-size: 20px; color: #003E91;">Greetings,</h2>
+				<p style="font-family: Helvetica, sans-serif; font-size: 16px;">Thank you for subscribing to <a href="#">easyshop.ph</a>!</p>
+				<p style="font-family: Helvetica, sans-serif; font-size: 16px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, repellen obcaecati officia inventore molestias delectus aspernatur incidunt laboriosam dolor quod explicabo provident culpa!</p>
+			</div>
+		
+			<div class="footer" style="background: #3C475C;	padding: 20px; border-bottom: 5px solid #F18200; width: 600px; margin: 0 auto; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;">
+				<div class="socialMedia" style="width: 160px; margin: 0 auto;">
+					<a href="#" style="outline: none; border: 0;"><img src="facebook.png" style="width: 50px;"></a>
+					<a href="#" style="outline: none; border: 0;"><img src="googleplus.png" style="width: 50px;"></a>
+					<a href="#" style="outline: none; border: 0;"><img src="twitter.png" style="width: 50px;"></a>
+				</div>
+				<p style="color: #ffffff; text-align: center;">Copyright &copy; 2014 Easyshop.ph</p>
+			</div>
+		</div>
+		
+	</body>
+</html>
+
