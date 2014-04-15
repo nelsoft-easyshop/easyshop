5a398b495af8588d2311f87a8c0dc58098567326
Sam Gavinio <samgavinio@easyshop.ph>
2014-04-15 03:26:50 +0000

    Applied google anlytics code
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1026 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/landingpage.php
+++ application/views/pages/landingpage.php
@@ -11,8 +11,31 @@
         <link href="<?=base_url()?>assets/css/landingpage/mystyle.css" rel="stylesheet">
 		<link href="<?=base_url()?>assets/css/jquery-ui.css" rel="stylesheet">
 		<title><?php echo $title;?></title>
+        
+         <!-- Google Analytics -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-48811886-1', 'easyshop.ph');
+          ga('send', 'pageview');
+        </script>
+        <!-- End of Google Analytics -->
     </head>
     <body>
+                 <!-- Google Tag Manager -->
+         <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KP5F8R"
+         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+         })(window,document,'script','dataLayer','GTM-KP5F8R');</script>
+         <!-- End Google Tag Manager -->
+    
+    
         <div class = "container header_bg">
             <div class="row-fluid">
                 <div id="himg" class="text-center"> <img src="<?=base_url()?>assets/images/landingpage/img_logo.png"> </div>
@@ -36,7 +59,7 @@
 									<fieldset>
 
 											<h4>Username</h4>
-											<input type="text" placeholder="" id="username" name="username" class="reqfield">
+											<input type="text" placeholder="" id="username" name="username" class="reqfield" autocomplete="off"/>
 											<input type="hidden" id="usernamecheck" value="" name="usernamecheck">
 											<span class="red ci_form_validation_error"><?php echo form_error('username'); ?></span>
 											<img id="username_loader" class="img_loader_small" src="<?=base_url()?>/assets/images/orange_loader_small.gif" style="display:none;">
