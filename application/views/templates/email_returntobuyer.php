2078a939ee2047ce4a8e8c0cbc6eeceddb537da6
paulmodino <paul.modino@easyshop.ph>
2014-04-16 03:49:16 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1047 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/email_returntobuyer.php
+++ application/views/templates/email_returntobuyer.php
@@ -1,37 +1,50 @@
 <html>
 	<body>
-		<table>
-			<tr>
-				<td>
-					{user} has just confirmed to return your payment for the following product:
-				</td>
-			</tr>
-			<tr>
-				<td>Transaction # : </td>
-				<td>{order_id}</td>
-			</tr>
-			<tr>
-				<td>Product Order # : </td>
-				<td>{order_product_id}</td>
-			</tr>
-			<tr>
-				<td>Product Name : </td>
-				<td><a href="{product_link}" target="_blank">{name}</a></td>
-			</tr>
-			<tr>
-				<td>Qty : </td>
-				<td>{order_quantity}</td>
-			</tr>
-			<tr>
-				<td>Price : Php</td>
-				<td>{price}</td>
-			</tr>
-			<tr>
-				<td>Product Attributes</td>
-				{attr}
-					<td>{field} : </td>
-					<td>{value}</td>
-				{/attr}
-			</tr>
+		<table cellspacing="0" cellpadding="10" style="width:100%; max-width:780px; margin:0px auto; border-top:5px solid #f18200; 
+		border-left:1px solid #f18200;border-right:1px solid #f18200;border-bottom:1px solid #f18200; font-family:arial; 
+		font-size:12px;">
+			<thead>
+		 		<tr>
+		 			<td colspan="3">
+		 				<a href="" style="padding:10px 0; display:inline-block">
+		 					<img src="img_logo.png" alt="Easyshop.ph">
+		 				</a>
+		 			</td>
+		 		</tr>
+		 	</thead>
+		 	<tbody>
+				<tr>
+					<td colspan="3">
+						{user} has just confirmed to return your payment for the following product:
+					</td>
+				</tr>
+				<tr>
+					<td width="100px">Transaction # : </td>
+					<td colspan="2">{order_id}</td>
+				</tr>
+				<tr>
+					<td>Product Order # : </td>
+					<td colspan="2">{order_product_id}</td>
+				</tr>
+				<tr>
+					<td>Product Name : </td>
+					<td colspan="2"><a href="{product_link}" target="_blank">{name}</a></td>
+				</tr>
+				<tr>
+					<td>Qty : </td>
+					<td colspan="2">{order_quantity}</td>
+				</tr>
+				<tr>
+					<td>Price : Php</td>
+					<td colspan="2">{price}</td>
+				</tr>
+				<tr>
+					<td>Product Attributes</td>
+					{attr}
+						<td>{field} : </td>
+						<td>{value}</td>
+					{/attr}
+				</tr>
+			</tbody>
 	</body>
 </html>
