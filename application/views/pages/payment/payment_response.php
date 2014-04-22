05cf87ec6bd7436a1d6e3b73b56b9a3cf0f7a766
paulmodino <paul.modino@easyshop.ph>
2014-04-22 07:16:02 +0000

    apply css styles
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1126 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/payment/payment_response.php
+++ application/views/pages/payment/payment_response.php
@@ -7,36 +7,35 @@
   <div class="wrapper">
     <h2 class="checkout_title">Payment Result</h2>
  
-    <div class="payment_wrapper">
-      <br><br>
+    <div class="payment_result_con">
+      <div class="payment_status_con">
       <?php echo (isset($message_status) ? $message_status : "" ); ?>
       <br>
       <?php echo $message; ?>
+      </div>
       <h2>Transaction Details</h2>
-      <div>Invoice Number: <?php echo $invoice_no; ?></div> 
-      <div>Total Amount: P <?php echo $total; ?></div> 
-      <div>Date Added: <?php echo date("D-m-Y h:m", strtotime($dateadded)); ?></div> 
-      <h2>Purchased Product</h2> 
+      <div class="payment_items_con">
+        <div><span>Invoice Number:</span>  <?php echo $invoice_no; ?></div>
+        <div><span>Total Amount:</span>  P <?php echo $total; ?></div>
+        <div><span>Date Added:</span>  <?php echo date("D-m-Y h:m", strtotime($dateadded)); ?></div>
+      </div>
+      <h2>Purchased Product</h2>
+      <div class="payment_items_con">
          <?php 
          foreach ($itemList as $key => $value) {
             ?>
-            Product Name: <?php echo $value['name']; ?>
-            <br>
-            Name of Seller: <?php echo $value['seller_username']; ?>
-            <br>
-            Product Price: <?php echo $value['price']; ?>
-            <br>
-            Product Quantity: <?php echo $value['qty']; ?>
-            <br>
-            Product Subtotal: <?php echo $value['subtotal']; ?>
-            <br>
-            Product Other Price: <?php echo $value['otherFee']; ?>
-            <br>
-            Product Total Fee: <?php echo $value['subtotal'] + $value['otherFee']; ?>
-            <hr>
+            <div><span>Product Name:</span> <?php echo $value['name']; ?></div>
+            <div><span>Name of Seller:</span> <?php echo $value['seller_username']; ?></div>
+            <div><span>Product Price:</span> <?php echo $value['price']; ?></div>
+            <div><span>Product Quantity:</span> <?php echo $value['qty']; ?></div>
+            <div><span>Product Subtotal:</span> <?php echo $value['subtotal']; ?></div>
+            <div><span>Product Other Price:</span> <?php echo $value['otherFee']; ?></div>
+            <div><span>Product Total Fee:</span> <?php echo $value['subtotal'] + $value['otherFee']; ?></div>
          <?php
          }
          ?>
+      </div> 
+      
     </div>
  
   </div>
