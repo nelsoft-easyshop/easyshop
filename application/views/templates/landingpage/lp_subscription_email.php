e7e68b554b49d1b5561563eb77e0723fa629ff0a
stephenjanz <stephenjanz@easyshop.ph>
2014-04-22 08:52:44 +0000

    Updated links and content
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1129 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/landingpage/lp_subscription_email.php
+++ application/views/templates/landingpage/lp_subscription_email.php
@@ -37,7 +37,7 @@
 		<table style="margin: 0 auto;" cellspacing="0" cellpadding="0" width="600px">
 			<tr>
 				<td style="text-align: center;">
-					<a href="#" style="outline: none; border: 0;">
+					<a href="https://easyshop.ph/" style="outline: none; border: 0;">
 						<img src="header-img.png" style="width: 600px; height: 129px;" alt="Easyshop.ph">
 					</a>
 				</td>
@@ -45,16 +45,16 @@
 			<tr>
 				<td style="background-color: #ffffff; padding: 30px 20px;">
 					<h2 class="intro" style="margin-bottom: 30px; font-size: 20px~; color: #003E91;">Greetings,</h2>
-					<p style="font-family: Helvetica, sans-serif; font-size: 16px;">Thank you for subscribing to <a href="#">Easyshop.ph</a>!</p>
-					<p style="font-family: Helvetica, sans-serif; font-size: 16px;">Easyshop.ph aims to provide a fast and easy online shopping experience. A place where you can enjoy the benefits of one-stop shopping - any time, anywhere.</p>
+					<p style="font-family: Helvetica, sans-serif; font-size: 16px;">Thank you for subscribing!</p>
+					<p style="font-family: Helvetica, sans-serif; font-size: 16px;"><a href="https://easyshop.ph/">EasyShop.ph</a> targets to cater a platform, where you can facilitate easy and safe online business transactions. We are offering you a free stage for your business, and ensuring the buyers scam-free and easy-shopping experience. <a href="https://easyshop.ph/">EasyShop.ph</a> is where shopping made easy!</p>
 				</td>
 			</tr>
 			<tr>
 				<td style="background: #3C475C;	padding: 20px; border-bottom: 5px solid #F18200;">
 					<div class="socialMedia" style="width: 160px; margin: 0 auto;">
-						<a href="#"><img src="facebook.png" style="width: 50px;" alt="Share us on Facebook"></a>
+						<a href="https://www.facebook.com/pages/EasyShopph/211771799032417?ref=hl"><img src="facebook.png" style="width: 50px;" alt="Share us on Facebook"></a>
 						<!--<a href="#"><img src="googleplus.png" style="width: 50px;" alt="Connect with Google+"></a>-->
-						<a href="#"><img src="twitter.png" style="width: 50px;" alt="Tweet us on Twitter"></a>
+						<a href="https://twitter.com/EasyShopPH"><img src="twitter.png" style="width: 50px;" alt="Tweet us on Twitter"></a>
 					</div>
 					<p style="color: #ffffff; text-align: center;">Copyright &copy; 2014 Easyshop.ph</p>
 				</td>
