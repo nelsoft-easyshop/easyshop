593b26117925cc6cfbf60c08b218ef5602b85705
Sam Gavinio <samgavinio@easyshop.ph>
2014-04-22 06:16:56 +0000

    Removed analytics code in footers
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1118 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -7,21 +7,12 @@
 	</footer>
 	<div class="notification_container">
 		<h2>Notification</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Suspendisse ultricies sem quam, id fringilla arcu lobortis a. 
-			Maecenas in ligula vitae justo molestie gravida. Proin sem magna, 
-			porta sit amet ligula id, volutpat fringilla arcu. Suspendisse tempus condimentum turpis ut tempor. </p>
+		<p>Our vision is to be the leading Online Shopping website in South East Asia. 
+           The mission that EasyShop has is to provide its customer with a Fast and Easy 
+           online shopping experience. Our goal is to be the first website you think about
+           when buying online.
+        </p>
 	</div>	
 	</body>
 </html>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-48811886-1', 'easyshop.ph');
-  ga('send', 'pageview');
-
-</script>
