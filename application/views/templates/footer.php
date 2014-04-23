1dcf27726275c71afa16935f9026d2596fe7aa97
Sam Gavinio <samgavinio@easyshop.ph>
2014-04-23 04:15:57 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1149 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -7,23 +7,10 @@
 	</footer>
 	<div class="notification_container">
 		<h2>Notification</h2>
-		<p>Our vision is to be the leading Online Shopping website in South East Asia. 
-           The mission that EasyShop has is to provide its customer with a Fast and Easy 
-           online shopping experience. Our goal is to be the first website you think about
-           when buying online.
+		<p> EasyShop.ph is a platform where you can facilitate easy and safe online business transactions. We are offering you a free stage for your business, and ensuring that buyers have a scam-free and easy-shopping experience. EasyShop.ph is where shopping is made easy!
         </p>
 	</div>	
 	</body>
 </html>
 
-<script>
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-48811886-1', 'easyshop.ph');
-  ga('send', 'pageview');
-
-</script>
