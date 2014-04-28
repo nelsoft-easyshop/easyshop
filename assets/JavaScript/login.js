9362dbb2baa0c0a4c100ff599092f76519f31da1
Rain_Jorque <Rain_Jorque@mycompany.com>
2014-04-28 09:02:38 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1298 2929c1f8-612a-3548-b275-2a983e7c7391

--- assets/JavaScript/login.js
+++ assets/JavaScript/login.js
@@ -61,15 +61,16 @@ $(document).ready(function(){
 						}else{
 							window.location = config.base_url+$.cookie('rn');
 						}
-                        
-                        var url = $('#redirect_url').val();
-                        var first_uri_segment = url.substring(0, url.indexOf('/'));
-                        if((url == 'sell/step1')||(first_uri_segment == 'item')){
-                            window.location = config.base_url+ url;
-                        }
-                        else{
-                            window.location = config.base_url+ 'home';
-                        }                        
+
+//						  si rain ang nag comment dito.                        
+//                        var url = $('#redirect_url').val();
+//                        var first_uri_segment = url.substring(0, url.indexOf('/'));
+//                        if((url == 'sell/step1')||(first_uri_segment == 'item')){
+//                            window.location = config.base_url+ url;
+//                        }
+//                        else{
+//                            window.location = config.base_url+ 'home';
+//                        }                        
                     }
                 }
             });
