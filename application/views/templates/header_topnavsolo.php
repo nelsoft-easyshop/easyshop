4c00a3e1b2a72f95c6718d1ff427bf9220fc3b47
Rain_Jorque <Rain_Jorque@mycompany.com>
2014-04-30 09:14:09 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1397 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/header_topnavsolo.php
+++ application/views/templates/header_topnavsolo.php
@@ -60,7 +60,7 @@
               <!-- <li><a href="<?=base_url()?>home/under_construction">Visiting a new Shop</a></li> -->
               <!-- <li><a href="<?=base_url()?>home/under_construction">Discounts</a></li> -->
               <li><a href="<?=base_url()?>category/all">Shopping Categories</a></li>
-              <li><a href="<?=base_url()?>product_search/advance">Advance Search</a></li>
+              <li><a href="<?=base_url()?>advsrch">Advance Search</a></li>
             </ul>
           </li>
           <li class="top_nav_main">Seller Center
