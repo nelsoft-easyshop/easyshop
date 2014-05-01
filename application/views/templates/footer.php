c0090b3acd4cadceef6833be34f26f76de680c02
Sam Gavinio <samgavinio@easyshop.ph>
2014-05-01 18:08:04 +0000

    Added disclaimer for browser compatibility
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1419 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,12 @@
 	<footer>
 		  <div class="wrapper">
+            
 			<div class="copyright">
-			  <p>Copyright &copy; <?php echo date("Y"); ?> Easyshop.ph</p>
+            <p style='font-weight:bold'>This page is best viewed with one of these browsers:
+                <a href='https://www.google.com/intl/en/chrome/browser/' target="_blank"><img src='<?=base_url()?>assets/images/icon_browser_chrome.png' style='margin-left:2px; margin-top:3px;'/> <span style='position:absolute;'>Chrome</span></a>
+                <a href='http://www.mozilla.org/en-US/firefox/new/' target="_blank"><img src='<?=base_url()?>assets/images/icon_browser_firefox.png' style='margin-left:50px; margin-top:3px;'/> <span style=' position:absolute; margin-left:2px;'>Firefox</span></a>
+            </p>
+			<p>Copyright &copy; <?php echo date("Y"); ?> Easyshop.ph</p>
 			</div>
 		  </div>
 	</footer>
