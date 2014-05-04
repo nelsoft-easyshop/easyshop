14bfc614cd0050e01cbd71318c6b9bfc54bb3d6d
Sam Gavinio <samgavinio@easyshop.ph>
2014-05-04 13:33:17 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1490 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/search/search_display_v2.php
+++ application/views/pages/search/search_display_v2.php
@@ -189,7 +189,7 @@ function in_array_r($needle, $haystack, $strict = false) {
       <?php
 			} // end of loop
 		}else{
-			echo "<br><br><h3 align='center'>No resuls found!</h3>";
+			echo "<br><br><h3 align='center'>No results found.</h3>";
 		} // end of isset
       ?>
     </div>
