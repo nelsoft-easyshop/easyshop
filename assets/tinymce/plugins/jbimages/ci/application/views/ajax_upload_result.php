e5c1b45b8fc4daa0697248acdbd333fc8ffa650c
stephenjanz <stephenjanz@easyshop.ph>
2014-05-05 11:28:44 +0000

    Fixed compatibility issue with IE 9
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1525 2929c1f8-612a-3548-b275-2a983e7c7391

--- assets/tinymce/plugins/jbimages/ci/application/views/ajax_upload_result.php
+++ assets/tinymce/plugins/jbimages/ci/application/views/ajax_upload_result.php
@@ -1,18 +1,35 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!--Modifed by Janz - to include "status" under uploadFinish function-->
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><!--<![endif]-->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>JustBoil's Result Page</title>
 <script language="javascript" type="text/javascript">
-	//window.parent.window.jbImagesDialog.uploadFinish({
-	window.jbImagesDialog.uploadFinish({
-		filename:'<?php echo $file_name; ?>',
-		result: '<?php echo $result; ?>',
-		resultCode: '<?php echo $resultcode; ?>',
-		<!--Added code-->
-		status: '<?php echo $status?>'
-	});
+	var htmlclass = document.documentElement.className;
+	if( htmlclass == 'ie6' || htmlclass == 'ie7' || htmlclass == 'ie8' || htmlclass == 'ie9' ) {
+		window.parent.window.jbImagesDialog.uploadFinish({
+			filename:'<?php echo $file_name; ?>',
+			result: '<?php echo $result; ?>',
+			resultCode: '<?php echo $resultcode; ?>',
+			<!--Added code-->
+			status: '<?php echo $status?>'
+		});
+	} else {
+		window.jbImagesDialog.uploadFinish({
+			filename:'<?php echo $file_name; ?>',
+			result: '<?php echo $result; ?>',
+			resultCode: '<?php echo $resultcode; ?>',
+			<!--Added code-->
+			status: '<?php echo $status?>'
+		});
+	}
+	
+	
 </script>
 </head>
 
--- assets/tinymce/plugins/jbimages/js/dialog-v4.js
+++ assets/tinymce/plugins/jbimages/js/dialog-v4.js
@@ -46,34 +46,43 @@ var jbImagesDialog = {
 		document.getElementById("upload_additional_info").innerHTML = '';
 		document.getElementById("upload_in_progress").style.display = 'block';
 
-		var origForm = document.getElementById('upl' + this.uploadCounter);
-		if(this.isInProgress){
-			this.myformdata.push(origForm);
-		} else {
-			this.isInProgress = true;
-			var options = {
-				target: '#upload_target',
-				uploadProgress: OnProgress('test')
-			};
-			$(origForm).ajaxSubmit(options);
-			document.getElementById('progressbox').style.display = 'block';
+		var oldIE;
+		if ($('html').is('.ie6, .ie7, .ie8, .ie9')) {
+			oldIE = true;
 		}
-		var newForm = origForm.cloneNode(true);
-		this.uploadCounter++;
-		origForm.style.display = 'none';
-		newForm.id = "upl" + this.uploadCounter;
-		origForm.parentNode.appendChild(newForm);
-		var filenames = origForm.children[0].children[0].files;
-		var uploadlist = document.getElementById("fileupload_list");
-		for (var i = 0; i<filenames.length; i++){
-			var fileExtension = filenames[i].name.split('.').pop()
-			var allowedFileType = ['jpg', 'gif', 'png'];
-			if ( allowedFileType.join('|').indexOf(fileExtension) != -1 ){
-				uploadlist.innerHTML += filenames[i].name + ' ';
+
+		if (oldIE) {
+			document.getElementById('upl1').submit();
+		} else {
+			var origForm = document.getElementById('upl' + this.uploadCounter);
+			if(this.isInProgress){
+				this.myformdata.push(origForm);
+			} else {
+				this.isInProgress = true;
+				var options = {
+					target: '#upload_target',
+					uploadProgress: OnProgress('test')
+				};
+				$(origForm).ajaxSubmit(options);
+				document.getElementById('progressbox').style.display = 'block';
 			}
-			if ( filenames.length-1 == i ) {
-				//uploadlist.innerHTML += '<br>' + $('.progressbox').html();
-				uploadlist.innerHTML += '<br>';
+			var newForm = origForm.cloneNode(true);
+			this.uploadCounter++;
+			origForm.style.display = 'none';
+			newForm.id = "upl" + this.uploadCounter;
+			origForm.parentNode.appendChild(newForm);
+			var filenames = origForm.children[0].children[0].files;
+			var uploadlist = document.getElementById("fileupload_list");
+			for (var i = 0; i<filenames.length; i++){
+				var fileExtension = filenames[i].name.split('.').pop()
+				var allowedFileType = ['jpg', 'gif', 'png'];
+				if ( allowedFileType.join('|').indexOf(fileExtension) != -1 ){
+					uploadlist.innerHTML += filenames[i].name + ' ';
+				}
+				if ( filenames.length-1 == i ) {
+					//uploadlist.innerHTML += '<br>' + $('.progressbox').html();
+					uploadlist.innerHTML += '<br>';
+				}
 			}
 		}
 		
