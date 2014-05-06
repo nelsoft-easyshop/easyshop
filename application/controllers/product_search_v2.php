c3db76d4f34bf730194f642a8bb04d811005b095
Rain_Jorque <Rain_Jorque@mycompany.com>
2014-05-06 08:25:47 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1541 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/controllers/product_search_v2.php
+++ application/controllers/product_search_v2.php
@@ -18,7 +18,7 @@ class product_search_v2 extends MY_Controller {
 			print_r($target);
 			echo "</pre>";
 		}else{
-			echo "<br>----------------<br> No array found or array is empty <br>----------------<br>";
+			echo "<br>No array found or array is empty<br>";
 		}
 		return;
 	}
@@ -65,275 +65,241 @@ class product_search_v2 extends MY_Controller {
 			'title' => 'Easyshop.com - Advanced Search'
 		);
 		$data = array_merge($data, $this->fill_header());
+
+		$condition = $this->input->get();		
+		$category = $this->input->get('_cat');
+		$gsc = $this->input->get('_subcat');
+		$gis = $this->input->get('_is');	
+		$gloc = $this->input->get('_loc');
+		$gcon = $this->input->get('_con');		
+		$gp1 = $this->input->get('_price1');
+		$gp2 = $this->input->get('_price2');		
+		$sort = $this->input->get('_sop');
+
+		$response['condition'] = $condition;
+		$response['getcat'] = $category;
+		$response['getsubcat'] = $gsc;
+		$response['getis'] = $gis;
+		$response['getloc'] = $gloc;
+		$response['getcon'] = $gcon;
+		$response['getprice1'] = $gp1;
+		$response['getprice2'] = $gp2;
+		$response['getsop'] = $sort;			
 		$response['firstlevel'] = $this->search_model->getFirstLevelNode();
 		$response['ctrl_subcat'] = $this->product_model->getDownLevelNode(1);
 		$response['items'] = "";
 		
-		$response['getsubcat'] = $this->input->get("_subcat");
-		$response['getis'] = $this->input->get('_is');
-		$response['getcat'] = $this->input->get('_cat');
-		$response['getloc'] = $this->input->get('_loc');
-		$response['getcon'] = $this->input->get('_con');
-		$response['getprice1'] = $this->input->get('_price1');
-		$response['getprice2'] = $this->input->get('_price2');
-		$response['getsop'] = $this->input->get('_sop');			
-		
-		$condition = $this->input->get();
-		$response['condition'] = $condition;
-		
 		if($condition){
-			$category = $this->input->get('_cat');
-			if($category){ // kapag may laman ang main category magsesearch
-				
-				$response['ctrl_subcat'] = $this->product_model->getDownLevelNode($category);
-				/////////////////////////////////////////////////////////////////////////////
-				
-					$category = $this->input->get('_cat');	
-					$start = 0; # start series
-					$per_page = $this->per_page; # no of display
-					$test = "";			
-								
-					$getParentId = $this->search_model->getParentId($category);
-					if(!empty($getParentId)){
-						foreach($getParentId as $row){
-							$ParentID[] = $row['id_cat'];		
-						}
-						$ParentIDList = implode(',', $ParentID);
-					}else{
-						$ParentIDList = $id;
-					}
-		
-					$child = $this->search_model->selectChild($category);				
-					if($child[0] == 0 && $child[1] == 0){
-						$catID = $category;
-					}else{
-						$catID = implode(',', $child);
-					} // end - array check
-								
-					$attr_values = "";
-					$attr_values_array = "";
-					$attr_brand = ""; # Brands
-					$ctr = 0;
-					$ctrA = 0;
-					foreach ($condition as $name => $val) { # get all values from querystring
-						
-						$chk = strpos($name, "_");
-						if($chk === false && $name != "BRAND") { # other item attributes			
-							if (is_array($val)){ # this is for checkboxes with multiple values.
-								foreach($val as $row => $values){
-									$ctrA = $ctrA + 1;
-									$attr_values_array = $attr_values_array . " OR (REPLACE(UPPER(ea.`name`),' ','') = '". strtoupper($name) ."' AND UPPER(epa.`attr_value`) = '" . strtoupper($values) ."') ";
-								}
-							}else{ # single value
-								if(!empty($val)){
-									$ctr = $ctr + 1;
-									$attr_values = " OR (REPLACE(UPPER(ea.`name`),' ','') = '". strtoupper($name) ."' AND UPPER(epa.`attr_value`) = '" . strtoupper($val) ."') ";
-								}
-							}// end
-						} # other item attributes - end
-						
-						if($name == "BRAND"){ # brand					
-							if(is_array($val)){
-								$arr_brand = "";
-								
-								foreach($val as $row => $brands){
-									if(!empty($brands)){
-										$arr_brand = $arr_brand . "'" . $brands . "',";
-									}
-								}
-								
-								if(!empty($arr_brand)){
-									$fin_arr_brand = substr($arr_brand, 0, strlen($arr_brand) - 1 );
-									
-									if(!empty($fin_arr_brand)){
-										$attr_brand = $attr_brand . " AND eb.`name` IN (". $fin_arr_brand . ") ";
-									}
-								}
-							}	
-						} # brand end
-					} # get all values from querystring - end
-					       
-					$raw_attr_values = $attr_values . $attr_values_array;
-					$fix_attr_values = substr($raw_attr_values, 3, strlen($raw_attr_values));
-					
-					$fin_attr_values = "";
-					$fin_count = 0;
-					if(isset($fix_attr_values)){
-						$fin_attr_values = " AND (" . $fix_attr_values . ")";
-						$fin_count = ($ctr + $ctrA);
-					}
-					
-					##### Parameters starts here ####################################################
-					
-					///// ATTRIBUTES ////////////////////////////
-					
-					$QAtt = "";
-					if (!empty($fin_attr_values) && !empty($fin_count)){	
-						$QAtt = " AND ep.`id_product` IN (SELECT epa.`product_id` FROM `es_attr` ea
-								LEFT JOIN `es_product_attr` epa ON ea.`id_attr` = epa.`attr_id`
-								WHERE epa.`product_id` IS NOT NULL 
-									AND ea.`cat_id` IN (". $catID .", ". $ParentIDList .")
-									". $fin_attr_values ."
-								GROUP BY epa.`product_id`
-								HAVING COUNT(*) = ". $fin_count ." 
-								)";
-					}
-					
-					///////////////////////////////////////////
-		
-					$sort = $this->input->get('_sop');
-					if($sort){
-						switch($sort){
-							case "hot": $colsort = "ep.is_hot"; break;
-							case "new": $colsort = "ep.is_new"; break;
-							case "popular": $colsort = "ep.clickcount"; break;
-							case "con": $colsort = "ep.condition"; break;
-							default: $colsort = "ep.clickcount";							
-						}
-						unset($sort);
-					}else{
-						$colsort = "ep.`id_product`";
-					}
-					
-					$sc = "";
-					$gsc = $this->input->get('_subcat');
-                                        
-					$child = $this->search_model->selectChild($gsc);	 
-					if($child[0] == 0 && $child[1] == 0){
-						$gsubcat = $gsc;
-					}else{
-						$gsubcat = implode(',', $child);
-					} // end - array check					
-					
-					if($gsc){
-						$sc = " AND ep.`cat_id` IN (" . $gsubcat . ") ";
-					}
-					
-					$loc = "";
-					$gloc = $this->input->get('_loc');
-					if($gloc){
-						$loc = " AND ep.`id_product` IN (SELECT `product_id` FROM `es_product_shipping_head` WHERE `location_id` = " . $gloc . ") ";
-					}					
-					
-					$is = "";
-					$gis = $this->input->get('_is');			
-					if(strlen($gis) > 0){
-						$is = " AND MATCH(ep.`name`,keywords) AGAINST(CONCAT('".$gis."','*') IN BOOLEAN MODE) ";
-					}
-					
-					$con = "";
-					$gcon = $this->input->get('_con');
-					if(strlen($gcon) > 0){
-						$con = " AND ep.`condition` = '". $gcon ."' ";
-					}
-					
-					$gp1 = $this->input->get('_price1');
-					$gp2 = $this->input->get('_price2');
-					
-					$gp = "";
-					if(strlen($gp1) > 0 && strlen($gp2) > 0){
-						$gp = " AND ep.`price` BETWEEN " . $gp1 . " AND " . $gp2 . " ";
-					}
-					
-					##### Parameters end here ####################################################
-										
-					# get all items here (right pane)
-					
-					$items = $this->search_model->SearchProduct($catID, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test);
 
-					// if(!empty($items)){ # check if it has items
-							
-						$response['items'] = $items; ### pass to view
-                    
-						$product_id = $this->search_model->getProductID($catID);
-						
-						$pid_values = "";
-						$bid_values = "";
-						foreach ($product_id as $row){
-							$pid_values[] = $row['product_id'];
-							$bid_values[] = $row['brand_id'];
-						}						
-                        
-						# get all attributes here (left pane)
-						
-						# brands
-						$get_brand_array = array();
-						$brand_names = $this->search_model->getBrandName($bid_values,'id');
-                        foreach ($brand_names as $bn) {
-							array_push($get_brand_array,$bn['name']);	
-						}
-						$fin_brand_array = array('name'=>'BRAND',$get_brand_array);
-						
-						# attribute group
-						$attribute = $this->search_model->getAttributesWithParam($catID,$pid_values);
-						# attribute values		
-						for ($i=0; $i < sizeof($attribute) ; $i++) { 
-							$attrib_values = $this->search_model->getAttributesWithParamAndName($catID,$pid_values,$attribute[$i]['name']);
-							array_push($attribute[$i], $attrib_values);	
-						}
-						# merging of attributes and brand names
-						array_unshift($attribute,$fin_brand_array);
-								
-						$response['arrayofparams'] = $attribute; ### pass to view
-						#$this->arrvwr($attribute);
-						
-						###########################################
+			$response['ctrl_subcat'] = $this->product_model->getDownLevelNode($category);	
+			$start = 0; # start series
+			$per_page = $this->per_page; # no of display
+			$catID = $category;
+			$cat = "";
+			$test = "";			
 
-				//	}
-				/////////////////////////////////////////////////////////////////////////////
-					
-				$this->load->view('templates/header_plain', $data); 
-				$this->load->view('pages/search/search_display_v2',$response);
-				$this->load->view('templates/footer_full');
-				
+			$child = $this->search_model->selectChild($category);
+			if($child[0] != 0 && $child[1] != 0){
+				$catID = implode(',', $child);
+				$cat = " AND ep.`cat_id` IN (". $catID . ")";
 			}else{
-				// no category found
-				$this->load->view('templates/header_plain', $data); 
-				$this->load->view('pages/search/search_display_v2',$response);
-				$this->load->view('templates/footer_full');
-			} // end check category
-		}else{
-			// no query string found
+				if($category != 1){
+					$cat = " AND ep.`cat_id` IN (". $catID . ")";
+				}
+			} // end - category check
+						
+			$attr_values = "";
+			$attr_values_array = "";
+			$attr_brand = ""; # Brands
+			$ctr = 0;
+			$ctrA = 0;
+			foreach ($condition as $name => $val) { # get all values from querystring
+				
+				$chk = strpos($name, "_");
+				if($chk === false && $name != "BRAND") { # other item attributes			
+					if (is_array($val)){ # this is for checkboxes with multiple values.
+						foreach($val as $row => $values){
+							$ctrA = $ctrA + 1;
+							$attr_values_array = $attr_values_array . " OR (REPLACE(UPPER(ea.`name`),' ','') = '". strtoupper($name) ."' AND UPPER(epa.`attr_value`) = '" . strtoupper($values) ."') ";
+						}
+					}else{ # single value
+						if(!empty($val)){
+							$ctr = $ctr + 1;
+							$attr_values = " OR (REPLACE(UPPER(ea.`name`),' ','') = '". strtoupper($name) ."' AND UPPER(epa.`attr_value`) = '" . strtoupper($val) ."') ";
+						}
+					}// end
+				} # other item attributes - end
+				
+				if($name == "BRAND"){ # brand					
+					if(is_array($val)){
+						$arr_brand = "";
+						
+						foreach($val as $row => $brands){
+							if(!empty($brands)){
+								$arr_brand = $arr_brand . "'" . $brands . "',";
+							}
+						}
+						
+						if(!empty($arr_brand)){
+							$fin_arr_brand = substr($arr_brand, 0, strlen($arr_brand) - 1 );
+							
+							if(!empty($fin_arr_brand)){
+								$attr_brand = $attr_brand . " AND eb.`name` IN (". $fin_arr_brand . ") ";
+							}
+						}
+					}	
+				} # brand end
+			} # get all values from querystring - end
+				   
+			$raw_attr_values = $attr_values . $attr_values_array;
+			$fix_attr_values = substr($raw_attr_values, 3, strlen($raw_attr_values));
+			
+			$fin_attr_values = "";
+			$fin_count = 0;
+			if(isset($fix_attr_values)){
+				$fin_attr_values = " AND (" . $fix_attr_values . ")";
+				$fin_count = ($ctr + $ctrA);
+			}
+			
+			##### Parameters starts here ####################################################
+			
+			///// ATTRIBUTES ////////////////////////////
+			
+			$QAtt = "";
+			if (!empty($fin_attr_values) && !empty($fin_count)){	
+				$QAtt = " AND ep.`id_product` IN (SELECT epa.`product_id` FROM `es_attr` ea
+						LEFT JOIN `es_product_attr` epa ON ea.`id_attr` = epa.`attr_id`
+						WHERE epa.`product_id` IS NOT NULL 
+							AND ea.`cat_id` IN (". $catID .")
+							". $fin_attr_values ."
+						GROUP BY epa.`product_id`
+						HAVING COUNT(*) = ". $fin_count ." 
+						)";
+			}
+			
+			///////////////////////////////////////////
+
+			if($sort){
+				switch($sort){
+					case "hot": $colsort = "ep.is_hot"; break;
+					case "new": $colsort = "ep.is_new"; break;
+					case "popular": $colsort = "ep.clickcount"; break;
+					case "con": $colsort = "ep.condition"; break;
+					default: $colsort = "ep.clickcount";							
+				}
+				unset($sort);
+			}else{
+				$colsort = "ep.`id_product`";
+			}
+			
+			$sc = "";
+			$child = $this->search_model->selectChild($gsc);	 
+			if($child[0] == 0 && $child[1] == 0){
+				$gsubcat = $gsc;
+			}else{
+				$gsubcat = implode(',', $child);
+			} // end - subcat check					
+			
+			if($gsc){
+				$sc = " AND ep.`cat_id` IN (" . $gsubcat . ") ";
+			}
+			
+			$loc = "";
+			if($gloc){
+				$loc = " AND ep.`id_product` IN (SELECT `product_id` FROM `es_product_shipping_head` WHERE `location_id` = " . $gloc . ") ";
+			}					
+			
+			$is = "";
+			if(strlen($gis) > 0){
+				$is = " AND MATCH(ep.`name`,keywords) AGAINST(CONCAT('".$gis."','*') IN BOOLEAN MODE) ";
+			}
+			
+			$con = "";
+			if(strlen($gcon) > 0){
+				$con = " AND ep.`condition` = '". $gcon ."' ";
+			}
+			
+			$gp = "";
+			if(strlen($gp1) > 0 && strlen($gp2) > 0){
+				$gp = " AND ep.`price` BETWEEN " . $gp1 . " AND " . $gp2 . " ";
+			}
+			
+			##### Parameters end here ####################################################
+								
+			# get all items here (right pane)
+			
+			$items = $this->search_model->SearchProduct($cat, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test);
+	
+			$response['items'] = $items; ### pass to view
+		
+			$product_id = $this->search_model->getProductID($catID);
+			
+			$pid_values = "";
+			$bid_values = "";
+			foreach ($product_id as $row){
+				$pid_values[] = $row['product_id'];
+				$bid_values[] = $row['brand_id'];
+			}						
+			
+			# get all attributes here (left pane)
+			
+			# brands
+			$get_brand_array = array();
+			$brand_names = $this->search_model->getBrandName($bid_values,'id');
+			foreach ($brand_names as $bn) {
+				array_push($get_brand_array,$bn['name']);	
+			}
+			$fin_brand_array = array('name'=>'BRAND',$get_brand_array);
+			
+			# attribute group
+			$attribute = $this->search_model->getAttributesWithParam($catID,$pid_values);
+			# attribute values		
+			for ($i=0; $i < sizeof($attribute) ; $i++) { 
+				$attrib_values = $this->search_model->getAttributesWithParamAndName($catID,$pid_values,$attribute[$i]['name']);
+				array_push($attribute[$i], $attrib_values);	
+			}
+			# merging of attributes and brand names
+			array_unshift($attribute,$fin_brand_array);
+					
+			$response['arrayofparams'] = $attribute; ### pass to view
+			#$this->arrvwr($attribute);
+			
+			###########################################
+
 			$this->load->view('templates/header_plain', $data); 
 			$this->load->view('pages/search/search_display_v2',$response);
 			$this->load->view('templates/footer_full');
-		} // end check get
-				
+		}else{
+			$this->load->view('templates/header_plain', $data); 
+			$this->load->view('pages/search/search_display_v2',$response);
+			$this->load->view('templates/footer_full');		
+		}// end check get
 	}
 	
 #####################################################################################
-####################################################################################
-###################################################################################
-
-	// pag nag scroll down iloload yung ibang items
 	
 	function load_product() # ROUTING
 	{
 
 		if($this->input->post()){
 				
-			$category = $this->input->post('id_cat');
-			$condition = $this->input->post('parameters');
 			$start = $this->input->post('page_number') * $this->per_page; # start series
 			$per_page = $this->per_page ; # no of display
-			$test = "";	
-			
-			$getParentId = $this->search_model->getParentId($category);
-			if(!empty($getParentId)){
-				foreach($getParentId as $row){
-					$ParentID[] = $row['id_cat'];		
-				}
-				$ParentIDList = implode(',', $ParentID);
-			}else{
-				$ParentIDList = $id;
-			}			
-			
-			$child = $this->search_model->selectChild($category);		
-			if($child[0] == 0 && $child[1] == 0){
-				$catID = $category;
-			}else{
+			$condition = $this->input->post('parameters');
+			$category = $this->input->post('id_cat');
+			$catID = $category;
+			$cat = "";
+			$test = "";			
+
+			$child = $this->search_model->selectChild($category);
+			if($child[0] != 0 && $child[1] != 0){
 				$catID = implode(',', $child);
-			} // end - array check
+				$cat = " AND ep.`cat_id` IN (". $catID . ")";
+			}else{
+				if($category != 1){
+					$cat = " AND ep.`cat_id` IN (". $catID . ")";
+				}
+			} // end - category check
 
 			$attr_values = "";
 			$attr_values_array = "";
@@ -410,7 +376,7 @@ class product_search_v2 extends MY_Controller {
 				$QAtt = " AND ep.`id_product` IN (SELECT epa.`product_id` FROM `es_attr` ea
 						LEFT JOIN `es_product_attr` epa ON ea.`id_attr` = epa.`attr_id`
 						WHERE epa.`product_id` IS NOT NULL 
-							AND ea.`cat_id` IN (". $catID .", ". $ParentIDList .")
+							AND ea.`cat_id` IN (". $catID .")
 							". $fin_attr_values ."
 						GROUP BY epa.`product_id`
 						HAVING COUNT(*) = ". $fin_count ."
@@ -441,7 +407,7 @@ class product_search_v2 extends MY_Controller {
 				$gsubcat = $gsc;
 			}else{
 				$gsubcat = implode(',', $child);
-			} // end - array check					
+			} // end - subcat check					
 			
 			if($gsc){
 				$sc = " AND ep.`cat_id` IN (" . $gsubcat . ") ";
@@ -472,7 +438,7 @@ class product_search_v2 extends MY_Controller {
 			##### Parameters end here ####################################################
 					
 			# get all items here (right pane)
-			$items = $this->search_model->SearchProduct($catID, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test);
+			$items = $this->search_model->SearchProduct($cat, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test);
 			if(isset($items) && !empty($items)){ # check if it has items		
 				$response['items'] = $items; ### pass to view	
 				$data = json_encode($this->load->view('pages/search/search_display_scroll',$response,TRUE));
@@ -485,4 +451,4 @@ class product_search_v2 extends MY_Controller {
 }
 
 /* End of file search.php */
-/* Location: ./application/controllers/product_search.php */
+/* Location: ./application/controllers/product_search_v2.php */
