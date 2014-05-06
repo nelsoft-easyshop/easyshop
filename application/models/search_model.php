036904bcb94d94f428e3c886cdada068af5fe361
Rain_Jorque <Rain_Jorque@mycompany.com>
2014-05-06 08:25:36 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1540 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/models/search_model.php
+++ application/models/search_model.php
@@ -201,7 +201,7 @@ class search_model extends CI_Model
 		return $row;
 	}	
 		
-	public function SearchProduct($category_id, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test){ 
+	public function SearchProduct($cat, $start, $per_page, $colsort, $is, $con, $gp, $attr_brand, $QAtt, $sc, $loc, $test){ 
 	
 	$start = (int)$start;
 	$per_page = (int)$per_page;		
@@ -214,7 +214,7 @@ class search_model extends CI_Model
 		FROM `es_product` ep
 		LEFT JOIN `es_product_image` epi ON ep.`id_product` = epi.`product_id` AND epi.`is_primary` = 1
 		LEFT JOIN `es_brand` eb ON ep.`brand_id` = eb.`id_brand`
-		WHERE ep.`cat_id` IN (". $category_id . ") AND ep.`is_draft` = 0 AND ep.`is_delete` = 0 
+		WHERE ep.`is_draft` = 0 AND ep.`is_delete` = 0 ". $cat ."   
 		". $QAtt . " ". $sc ." ". $loc . " " . $is ." ". $con ." ". $gp ." ". $attr_brand ."  
 		ORDER BY ". $colsort ." DESC 
 		LIMIT :start, :per_page ";
