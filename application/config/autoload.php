9349d47ce399b5150b3d2f1c0662d1c72ddd1a13
Czar Pino <czarpino@easyshop.ph>
2014-05-07 07:47:44 +0000

    Enabled Kernel library
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1587 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,10 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'form_validation', 'image_lib','upload');
-
-// Uncomment to enable Kernel
-//$autoload['libraries'] = array('database', 'session', 'form_validation', 'image_lib', 'upload', 'Kernel');
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'image_lib', 'upload', 'Kernel');
 
 /*
 | -------------------------------------------------------------------
