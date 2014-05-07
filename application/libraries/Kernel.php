a77de51e57d1306485983137b778811a7fef2836
Czar Pino <czarpino@easyshop.ph>
2014-05-07 07:50:29 +0000

    Added service container configuration
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1589 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/libraries/Kernel.php
+++ application/libraries/Kernel.php
@@ -10,11 +10,19 @@
 class Kernel
 {
     /**
+     * Service container
+     * 
+     * @var Pimple\Container
+     */
+    public $serviceContainer;
+    
+    /**
      * Constructor.
      */
     public function __construct()
     {
         $this->_bootstrap();
+        $this->_configure();
     }
     
     /**
@@ -29,5 +37,25 @@ class Kernel
         
         /* We register 3rd party autoloader */
         require_once 'vendor/autoload.php';
+    }
+    
+    /**
+     * Configure Kernel
+     */
+    private function _configure()
+    {
+        $container = new Pimple\Container();
+        
+        /* Register services BEGIN */
+        
+        // JavascriptIncluder
+        $container['javacsript_includer'] = function ($c) {
+            return new \EasyShop\View\Helpers\JavascriptIncluder(
+                    ENVIRONMENT === 'production', 'assets/js/src', 'assets/js/min');
+        };
+        
+        /* Register services END */
+        
+        $this->serviceContainer = $container;
     }
 }
