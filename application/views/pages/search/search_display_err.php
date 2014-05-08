5144abd5b76ecf767630b838b3540539400a6db4
Rain_Jorque <Rain_Jorque@mycompany.com>
2014-05-08 08:18:38 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1668 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/search/search_display_err.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="<?=base_url()?>assets/css/sell_item.css?ver=1.0" type="text/css" media="screen"/>
-
-<div class="clear"></div>
-<div class="wrapper search_results_wrapper">
-	<div class="results_container">
-		<h2>No Results Found</h2>
-		<p>Please try different search and try again.</p>
-	</div>
-</div>
