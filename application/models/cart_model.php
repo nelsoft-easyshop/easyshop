9784253ccd10c7399c003de9f7ced89edacd78a0
Sam Gavinio <samgavinio@easyshop.ph>
2014-05-14 07:40:57 +0000

    Removed deprecated and unused getproductquantity function
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@1818 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/models/cart_model.php
+++ application/models/cart_model.php
@@ -57,47 +57,6 @@ class cart_model extends CI_Model
         $sth->execute();
     }
     
-    
-    /*  This function is no longer used.
-     *  Marked for removal. 5/10/2014
-     */
-	public function getSpecificProductQuantity($product_id,$product_attr_id,$length){ 
-	    $query = "
-		SELECT a.id_product_item, a.quantity, COALESCE(b.product_attr_id,0) AS product_attr_id,cnt.count,
-		COALESCE(b.is_other,0) AS is_other  FROM es_product_item a
-		LEFT JOIN es_product_item_attr b ON b.product_id_item = a.id_product_item 
-		INNER JOIN (
-		SELECT a.id_product_item, a.quantity, COALESCE(b.product_attr_id,0) AS product_attr_id,COUNT(*) AS `count`,
-		COALESCE(b.is_other,0) AS is_other  FROM es_product_item a
-		LEFT JOIN es_product_item_attr b ON b.product_id_item = a.id_product_item 
-		WHERE a.product_id = $product_id AND b.product_attr_id IN($product_attr_id) GROUP BY a.`id_product_item`
-		) AS `cnt` ON a.`id_product_item` = cnt.id_product_item
-		WHERE a.product_id = $product_id AND b.product_attr_id IN($product_attr_id) HAVING `count` = $length
-		UNION
-			
-		SELECT b.id_product_item,b.quantity,'','',''
-		FROM es_product AS a
-		LEFT JOIN `es_product_item` AS b ON a.`id_product` = b.`product_id`
-		WHERE b.product_id = $product_id";
-	    $sth = $this->db->conn_id->prepare($query);
-	    $sth->execute();
-	    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
-    
-	    $data = array();
-	    $ctr = 0;
-	    foreach($rows as $row){
-            if(!array_key_exists($ctr,  $data)){
-                $data[$ctr] = array();
-                $data[$ctr]['quantity'] = $row['quantity'];
-                $data[$ctr]['id_product_item'] = $row['id_product_item'];
-                $data[$ctr]['product_attribute_ids'] = array();
-                $data[$ctr]['attr_lookuplist_item_id'] = array();
-            }
-            array_push($data[$ctr]['product_attribute_ids'], array('id'=>$row['product_attr_id'], 'is_other'=> $row['is_other']));
-	    }
-	    
-	    return $data;
-	}
 }
 
 /* End of file cart_model.php */
