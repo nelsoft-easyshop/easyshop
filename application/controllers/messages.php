f1523a7fa52751fdff9b32c5e4e17189b3b76343
Czar Pino <czarpino@easyshop.ph>
2014-05-28 07:03:54 +0000

    Reformatted
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2238 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/controllers/messages.php
+++ application/controllers/messages.php
@@ -1,75 +1,83 @@
 <?php
 
 if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
+    exit('No direct script access allowed');
 
-    class messages extends MY_Controller {
-    
-	public $user_ID = null;
-	
-    function __construct(){
-	    parent::__construct();
-		$this->load->helper('htmlpurifier');
-	    $this->load->library('session');
-	    $this->load->model('messages_model');
-		$this->user_ID = $this->session->userdata('member_id');
-	}
-    
-    public function index() {
-        if($this->session->userdata('usersession')){
-			$result = $this->messages_model->get_all_messages($this->user_ID,"kurt");
-			$title = (!isset($result['unread_msgs']) || $result['unread_msgs'] == 0  ? 'Message | Easyshop.ph' :'Message ('.$result['unread_msgs'].') | Easyshop.ph' );
-			$data['title'] = $title;
-			$data['result'] = $result;
-			$data = array_merge($data,$this->fill_header());
-			$this->load->view('templates/header_plain', $data);
-			$this->load->view('pages/messages/inbox_view');
-			$this->load->view('templates/footer_full');
-        }else{
-            redirect(base_url().'home', 'refresh');
-        }   
-    }    
+class messages extends MY_Controller
+{
+
+    public $user_ID = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('htmlpurifier');
+        $this->load->library('session');
+        $this->load->model('messages_model');
+        $this->user_ID = $this->session->userdata('member_id');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('usersession')) {
+            $result = $this->messages_model->get_all_messages($this->user_ID, "kurt");
+            $title = (!isset($result['unread_msgs']) || $result['unread_msgs'] == 0
+                    ? 'Message | Easyshop.ph'
+                    : 'Message (' . $result['unread_msgs'] . ') | Easyshop.ph' );
+            $data['title'] = $title;
+            $data['result'] = $result;
+            $data = array_merge($data, $this->fill_header());
+            $this->load->view('templates/header_plain', $data);
+            $this->load->view('pages/messages/inbox_view');
+            $this->load->view('templates/footer_full');
+        } else {
+            redirect(base_url() . 'home', 'refresh');
+        }
+    }
+
     public function send_msg() {
         $session_data = $this->session->all_userdata();
-		$result = trim($this->input->post("recipient"));
-		if(!is_numeric($result)){
-			$result = $this->messages_model->get_recepientID($result);
-		}
-        if($session_data['member_id'] == $result){
+        $result = trim($this->input->post("recipient"));
+        if (!is_numeric($result)) {
+            $result = $this->messages_model->get_recepientID($result);
+        }
+        if ($session_data['member_id'] == $result) {
             $result = "false";
-        }else if($result != "false"){
-			$msg = trim($this->input->post("msg"));
-			$result = $this->messages_model->send_message($session_data['member_id'],$result,$msg);
-			if($result = 1){
-				$result = $this->messages_model->get_all_messages($this->user_ID,"kurt");
-			}
-		}
-		echo json_encode($result);
-	
+        } else if ($result != "false") {
+            $msg = trim($this->input->post("msg"));
+            $result = $this->messages_model->send_message($session_data['member_id'], $result, $msg);
+            if ($result = 1) {
+                $result = $this->messages_model->get_all_messages($this->user_ID, "kurt");
+            }
+        }
+        echo json_encode($result);
     }
-    public function delete_msg(){
-		$id = $this->input->post("id_msg");
-		
-		$result = $this->messages_model->delete_msg($id,$this->user_ID);
-		if($result > 0 ){
-			$result = $this->messages_model->get_all_messages($this->user_ID,"kurt");	
-		} else {
-			$result = "";
-		}
-		echo json_encode($result);
+
+    public function delete_msg() {
+        $id = $this->input->post("id_msg");
+
+        $result = $this->messages_model->delete_msg($id, $this->user_ID);
+        if ($result > 0) {
+            $result = $this->messages_model->get_all_messages($this->user_ID, "kurt");
+        } else {
+            $result = "";
+        }
+        echo json_encode($result);
     }
-	public function retrieve_msgs(){
-		$todo = $this->input->post("todo");
-		$result = $this->messages_model->get_all_messages($this->user_ID,$todo);
-		echo json_encode($result);
-	}	
-	public function is_seened(){
-		$id = $this->user_ID;
-		$from_ids = $this->input->post('checked');
-		$result = $this->messages_model->is_seened($id,$from_ids);
-		
-		echo json_encode($result);
-	}
-    
+
+    public function retrieve_msgs() {
+        $todo = $this->input->post("todo");
+        $result = $this->messages_model->get_all_messages($this->user_ID, $todo);
+        echo json_encode($result);
+    }
+
+    public function is_seened() {
+        $id = $this->user_ID;
+        $from_ids = $this->input->post('checked');
+        $result = $this->messages_model->is_seened($id, $from_ids);
+
+        echo json_encode($result);
+    }
+
 }
-?>
+
