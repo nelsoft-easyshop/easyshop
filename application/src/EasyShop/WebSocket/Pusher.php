b596b5664cbf2503c100fdbd2dc8d9df141ee163
Czar Pino <czarpino@easyshop.ph>
2014-06-04 05:54:45 +0000

    Added pusher class
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2432 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/src/EasyShop/WebSocket/Pusher.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EasyShop\WebSocket;
+
+/**
+ * Description of Pusher
+ *
+ * @author czarpino
+ */
+class Pusher
+{
+    private $socket;
+    
+    private $data;
+    
+    public function __construct($socket)
+    {
+        $this->socket = $socket;
+    }
+    
+    public function addData($key, $data)
+    {
+        $this->data[$key] = $data;
+    }
+    
+    public function clearData()
+    {
+        $this->data = [];
+        return $this;
+    }
+    
+    public function push($sessionId)
+    {
+        $this->data['session_id'] = $sessionId;
+        $this->socket->send(json_encode($this->data));
+        
+        return $this;
+    }
+}
