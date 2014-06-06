37f14ffddd2430a5678c88b3a7051c5d10245d71
paulmodino <paul.modino@easyshop.ph>
2014-06-06 08:06:45 +0000

    add countdown sale view
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2541 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/product/product_search_by_category_final.php
+++ application/views/pages/product/product_search_by_category_final.php
@@ -45,7 +45,7 @@ $_SESSION['start'] = "0";
 </section>    
 
 <div class="wrapper" id="main_search_container">
-
+<!--
     <div class="left_attribute">
         <h3>Price</h3>
         <?php
@@ -120,6 +120,7 @@ $_SESSION['start'] = "0";
     </div>
 
     <div class="right_product">
+        
             <?php if(count($subcategories) !== 0):?>
             <div class="filters">           
               <h2>Categories:</h2>    
@@ -138,13 +139,13 @@ $_SESSION['start'] = "0";
                         <?PHP endforeach;?>
                     </div>  
                 </div>
-                <!-- Controls -->
+                /start comment **** Controls ****end here/
                 <a href="#" class="jcarousel-control-prev inactive category_carousel2_prev">&lsaquo;</a>
                 <a href="#" class="jcarousel-control-next inactive category_carousel2_next">&rsaquo;</a>            
             </div>
         <?php endif; ?>        
         <div class="clear"></div>
-        <p class="search_result"><!-- Showing 1 - 48 of 13,152 Results --></p>
+        <p class="search_result">/start comment **** Showing 1 - 48 of 13,152 Results ****end here/ </p>
 
         Sort by: 
         <select data-url="<?php echo site_url(uri_string() . '?' . $_SERVER['QUERY_STRING']).'&sop='; ?>" id="sort_order"> 
@@ -214,7 +215,238 @@ $_SESSION['start'] = "0";
         <div class="loading_products" style="display: inline-block;text-align: center;width: 100%;"></div>
     </div>
 
-</div>
+</div>-->
+    
+    <!-- start of countdown sales -->
+    <div class="countdown_category_banner">
+        <div class="cd_timer_container">
+            <div class="cd_timer_days">
+                <span id='countdown_days'>00</span>
+                <span>DAYS</span>
+            </div>
+            <div class="cd_timer_hours">
+                <span id='countdown_hours'>00</span>
+                <span>HOURS</span>
+            </div>
+            <div class="cd_timer_minutes">
+                <span id='countdown_minutes'>00</span>
+                <span>MINUTES</span>
+            </div>
+            <div class="cd_timer_seconds">
+                <span id="countdown_second">00</span>
+                <span>SECONDS</span> 
+            </div>
+        </div>
+    </div>
+    <div class="cd_product_container">
+        <div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div class="cd_soldout">
+
+                    <p>SOLDOUT</p>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/img_cd_prod1.jpg">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">iPhone 5s(16GB)</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/landingpage/img_canon60d.png">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">Canon DSLR 60D</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 23,900.00 </div>
+                    <div class="discount_price">Php 32,900.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/landingpage/img_samsung_ref.png">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">Samsung 7 cu. ft. REFRIGERATOR</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/landingpage/img_samsung_ledtv.png">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">Canon DSLR 60D</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/img_cd_prod1.jpg">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">iPhone 5s(16GB)</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/img_cd_prod1.jpg">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">iPhone 5s(16GB)</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/img_cd_prod1.jpg">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">iPhone 5s(16GB)</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+            <div class="cd_product">
+                <div class="product_buy_con">
+                        <span><a href="" class="orange_btn3">BUY NOW</a></span>
+                </div>
+                <div>
+                    <span class="cd_slide_discount">
+                        <span>77%<br>OFF</span>
+                    </span>
+                </div>
+                <a href="">
+                    <span class="cd_prod_img_con">
+                        <img src="<?= base_url() ?>assets/images/img_cd_prod1.jpg">
+                    </span>
+                </a>
+                <h3>
+                    <a href="">iPhone 5s(16GB)</a>
+                </h3>
+                <div class="price-cnt">
+                    <div class="price"> Php 1,000.00 </div>
+                    <div class="discount_price">Php 100.00</div>
+                </div>
+                <div class="cd_condition">
+                    Condition: NEW
+                </div>
+            </div>
+        </div>
+    </div>
 </div>   
 </div>
 
