54622ee795d2f6c8860af26cd05281e990e350fc
Czar Pino <czarpino@easyshop.ph>
2014-06-06 11:32:03 +0000

    Removed comment
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2554 2929c1f8-612a-3548-b275-2a983e7c7391

--- web/assets/includes/js.php
+++ web/assets/includes/js.php
@@ -43,37 +43,3 @@
         });
     }
 </script>
-        
-<!-- <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1395192884090886";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<script type="text/javascript">
-$(document).ready(function(){
-	$('#es_fb_share').on('click', function(e){
-		var name = $(this).data('name');
-		var link = $(this).data('link');
-		var pic = $(this).data('pic');
-		var caption = $(this).data('caption');
-		var desc = $(this).data('desc');
-		var message = $(this).data('message');
-		e.preventDefault();
-		FB.ui(
-		{
-			method: 'FEED',
-			display: 'popup',
-			name: name,
-			link: link,
-			picture: pic,
-			caption: caption,
-			description: desc,
-			message: message
-		});
-	});
-});
-</script> -->
