b3407a0878941fa5251d65d008c4e2394c307327
Sam Gavinio <samgavinio@easyshop.ph>
2014-06-07 14:52:49 +0000

    Added indeces to protected_category config
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2566 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/config/protected_category.php
+++ application/config/protected_category.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array(
-    0,     //non-existent category id
-    1,     //top parent category
-    1000,  //'promoted items'  
+    'error'  => 0,     //non-existent category id
+    'parent' => 1,     //top parent category
+    'promo'  => 1000,  //'promoted items'  
 );
