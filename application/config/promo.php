26182e7212ec582702bd591f68da18677e1a566b
kurt.pasamba <kurtwilkinson024@easyshop.ph>
2014-06-10 04:27:14 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2662 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/config/promo.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config = array(
+    'Promo' => array(
+        '1' => array(
+            'calculation_id' => '1',
+            'purchase_limit' => '1'
+        )
+    )
+);
+
+
+/* End of file promo.php */
+/* Location: ./application/config/promo.php */
+
