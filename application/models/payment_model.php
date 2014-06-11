77992cceb9ca8191f0364e3dd9c73816a86541d0
ryan.vasquez <ryanannatuvasquez@easyshop.ph>
2014-06-11 11:09:44 +0000

    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2728 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/models/payment_model.php
+++ application/models/payment_model.php
@@ -71,7 +71,7 @@ class payment_model extends CI_Model
 			  	AND order_id = :order_id
     		";
     	}
-    	;
+    	echo $query;
     	$sth = $this->db->conn_id->prepare($query);
     	$sth->bindParam(':item_id',$itemId,PDO::PARAM_INT);
     	$sth->bindParam(':quantity',$qty,PDO::PARAM_INT); 
@@ -131,7 +131,7 @@ class payment_model extends CI_Model
         $query = $this->xmlmap->getFilenameID('sql/payment','updatePaymentIfComplete');
     	$sth = $this->db->conn_id->prepare($query);
 
-    	$orderStatus = ($paymentType == 2) ? 99 : 0;
+    	$orderStatus = ($paymentType == 2) ? 0 : 0;
     	$sth->bindParam(':order_status',$orderStatus,PDO::PARAM_STR);
     	$sth->bindParam(':data',$data,PDO::PARAM_STR);
     	$sth->bindParam(':id_order',$id,PDO::PARAM_INT);
