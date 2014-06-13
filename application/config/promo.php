175fb06a9740e14a7f58eb3a828b0c83aca55e68
Sam Gavinio <samgavinio@easyshop.ph>
2014-06-13 00:28:28 +0000

    added payment_mdethod
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@2789 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/config/promo.php
+++ application/config/promo.php
@@ -1,14 +1,23 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+define("PAYPAL",  1);
+define("DRAGONPAY_OFFLINE",2);
+define("CASH_ON_DELIVERY",3);
+define("DRAGONPAY_ONLINE",4);
+define("DIRECT_BANK",5);
+
 $config = array(
     'Promo' => array(
         '0' =>array(
             'calculation_id' => '0',
-            'purchase_limit' => PHP_INT_MAX
+            'purchase_limit' => PHP_INT_MAX,
+            'payment_method' => array(PAYPAL,DRAGONPAY_OFFLINE,CASH_ON_DELIVERY,DRAGONPAY_ONLINE,DIRECT_BANK),
         ),
         '1' => array(
             'calculation_id' => '1',
-            'purchase_limit' => '1'
-        )
+            'purchase_limit' => '1',
+            'payment_method' => array(PAYPAL),
+        ),
     )
 );
 
