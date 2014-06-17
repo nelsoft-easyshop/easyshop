82cc5d1191e2d925af99c9c9c5377b0d2c91cfce
Sam Gavinio <samgavinio@easyshop.ph>
2014-06-17 16:45:20 +0000

    Removed subscribe function. Moved to register
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3096 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -84,30 +84,7 @@ class Home extends MY_Controller {
         $this->load->view('templates/footer_full');
     }
     
-    /*
-	 *	Subscription Handler
-	 */
-	public function subscribe()
-	{
-		if($this->input->post('subscribe_btn') && $this->form_validation->run('subscription_form')){
-            $this->load->model('register_model');
-			$data['email'] = $this->input->post('subscribe_email');
-            $result = $this->register_model->subscribe($data['email']);
-			// Send notification email to user 
-			if($result){		
-              $this->register_model->sendNotification($data, 'subscribe');
-              $data['title'] = 'Successful Subscription | Easyshop.ph';
-              $data['content'] = 'You have successfully Subscribed!';
-              $data['sub_content'] =  'Thank you for choosing to keep in touch with Easyshop.ph. Expect to hear many things from us soon.';
-              $this->load->view('pages/landingpage_success', $data);
-            }else{
-                redirect('home','refresh');
-            }
-		}else{
-            redirect('home','refresh');
-        }
 
-	}
 }
 
 /* End of file home.php */
