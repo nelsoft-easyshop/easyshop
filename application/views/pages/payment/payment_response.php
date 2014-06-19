59ac6d468bf3f413bf2b6d92f57336c1d4de46de
ryan.vasquez <ryanannatuvasquez@easyshop.ph>
2014-06-19 09:23:42 +0000

    Issue: 0000630 Issue Description: incomplete date format for Transaction Date for COD Transaction Summary. Current format is like this "Mon-06-2014 10:06". Exact date is not shown. Mostly there are 4 Mondays in a week.
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3183 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/payment/payment_response.php
+++ application/views/pages/payment/payment_response.php
@@ -20,7 +20,7 @@
       <div class="payment_items_con">
         <div><span>Invoice Number:</span>  <?php echo $invoice_no; ?></div>
         <div><span>Total Amount:</span>  P <?php echo $total; ?></div>
-        <div><span>Date Added:</span>  <?php echo date("D-m-Y h:m", strtotime($dateadded)); ?></div>
+        <div><span>Date Added:</span>  <?php echo date("d/m/Y H:i", strtotime($dateadded)); ?></div>
       </div>
       <?php }else{
         ?>
