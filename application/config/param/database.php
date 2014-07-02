521840d9679896f9f2be47692cb3f2188a9ac895
Sam Gavinio <samgavinio@easyshop.ph>
2014-07-02 19:10:54 +0000

    Readded database.php. Waiting for final word from network admin
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3664 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/config/param/database.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Database param values
+ */
+
+return [
+    
+    'host'      => '127.0.0.1',
+    'driver'    => 'pdo_mysql',
+    'user'      => 'root',
+    'password'  => '121586',
+    'dbname'    => 'easyshop',
+];
