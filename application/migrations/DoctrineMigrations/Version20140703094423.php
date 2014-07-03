7f6148b098827f3bf987166fd285a2014a52fb8f
stephenjanz <stephenjanz@easyshop.ph>
2014-07-03 07:55:31 +0000

    Migrations. added new column in member table for "Vendor Store Description"
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3679 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/migrations/DoctrineMigrations/Version20140703094423.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140703094423 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE `easyshop`.`es_member` 
+ADD COLUMN `store_desc` TEXT NULL DEFAULT NULL AFTER `is_admin`;');
+
+    }
+
+    public function down(Schema $schema)
+    {
+         $this->addSql('ALTER TABLE `easyshop`.`es_member` DROP `store_desc`');
+
+    }
+}
