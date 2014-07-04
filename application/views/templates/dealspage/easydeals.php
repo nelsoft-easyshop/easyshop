25931eac1a8811550ac563dd6996bb25d4d01c9b
paulmodino <paul.modino@easyshop.ph>
2014-07-04 10:33:29 +0000

    apply new layout of deal page
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3702 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/dealspage/easydeals.php
+++ application/views/templates/dealspage/easydeals.php
@@ -1,4 +1,33 @@
+   
+
    <!-- start of easydeals-->
-    <div class='easydeals_category_banner'>
+    <!-- <div class='easydeals_category_banner'>
      
+    </div> -->
+    <div>
+    	<ul class="idTabs deals_tab">
+    		<li>
+    			<a href="#promo1">
+    				<img src="<?=base_url()?>assets/images/img_promo1.jpg" alt="Peak Hour Promo" class="deals_tab_img">
+    				<div class="deals_tab_btn_con">
+    					<span class="deals_tab_btn">Peak Hour Promo</span>
+    				</div>
+    			</a>
+    		</li>
+    		<li>
+    			<a href="#promo2">
+    				<img src="<?=base_url()?>assets/images/img_promo2.jpg" alt="Easy Treat Promo" class="deals_tab_img">
+    				<div class="deals_tab_btn_con">
+    					<span class="deals_tab_btn">Easy Treat Promo</span>
+    				</div>
+    			</a>
+    		</li>
+    	</ul>
     </div>
+    <div class="clear"></div>
+
+
+
+
+    <script src="<?=base_url()?>assets/js/src/vendor/jquery.idTabs.min.js" type="text/javascript"></script>
+    
