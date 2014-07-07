d89d0c015288793023789b89a05e311b5a744263
stephenjanz <stephenjanz@easyshop.ph>
2014-07-07 06:39:20 +0000

    Corrected Migration for datatype store_desc in es_member
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3791 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/migrations/DoctrineMigrations/Version20140707071620.php
+++ application/migrations/DoctrineMigrations/Version20140707071620.php
@@ -15,12 +15,16 @@ class Version20140707071620 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-		ALTER TABLE easyshop.es_member MODIFY store_desc VARCHAR(1024) DEFAULT '';
+		$this->addSql("
+			ALTER TABLE easyshop.es_member MODIFY store_desc VARCHAR(1024) DEFAULT ''
+		");
     }
 
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-		ALTER TABLE easyshop.es_member MODIFY store_desc TEXT;
+		$this->addSql("
+			ALTER TABLE easyshop.es_member MODIFY store_desc TEXT
+		");
     }
 }
