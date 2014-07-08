5ee979a27a92547407e9608cbe28da4cdac730d0
Sam Gavinio <samgavinio@easyshop.ph>
2014-07-08 16:45:03 +0000

    Updated home file prod for new companion product and updated file version to 3.92
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/branch/rb-6.3@3909 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,7 +39,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 /*   
  *  EASYSHOP CUSTOM CONSTANTS
  */
-define('ES_FILE_VERSION','3.6.91');
+define('ES_FILE_VERSION','3.6.92');
 define('ES_PRODUCTION',TRUE);
 
 
