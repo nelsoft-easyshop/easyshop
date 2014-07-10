708e5be62846d62e3de196a3d1d0c7334bf74645
Sam Gavinio <samgavinio@easyshop.ph>
2014-07-10 03:27:18 +0000

    Added new database parameters
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@3986 2929c1f8-612a-3548-b275-2a983e7c7391

---
+++ application/config/param/database/staging.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Database param values
+ */
+
+return [
+    
+    'host'      => '127.0.0.1',
+    'driver'    => 'pdo_mysql',
+    'user'      => 'staging_easyshop',
+    'password'  => 'MYSQL345y5h0p',
+    'dbname'    => 'staging_easyshop',
+];
