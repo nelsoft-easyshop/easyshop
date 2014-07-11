cbf4119cda6fa57ae502b33204f510dee19be6e8
paulmodino <paul.modino@easyshop.ph>
2014-07-11 04:57:14 +0000

    add style.css link
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/trunk@4016 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/web/how-to-buy.php
+++ application/views/pages/web/how-to-buy.php
@@ -1,4 +1,6 @@
 
+<link type='text/css' href='<?=base_url()?>assets/css/style.css' rel='stylesheet' media='screen' />
+</style>
       <div class="wrapper info_wrapper">
         <img src="/assets/images/img_how-to-buy.png" alt="How to Buy">
       </div>
