e93a5498466a6a26d7270927ef6a8f3a3ae9076f
Sam Gavinio <samgavinio@easyshop.ph>
2014-07-14 13:55:38 +0000

    Updated constant file version
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/branch/rb-6.3@4092 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,7 +39,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 /*   
  *  EASYSHOP CUSTOM CONSTANTS
  */
-define('ES_FILE_VERSION','3.6.93');
+define('ES_FILE_VERSION','3.6.11');
 define('ES_PRODUCTION',TRUE);
 
 
