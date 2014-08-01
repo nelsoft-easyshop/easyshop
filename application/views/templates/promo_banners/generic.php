64a15e932d205d386e34c461cea7d61c17b64015
Sam Gavinio <samgavinio@easyshop.ph>
2014-08-01 04:53:16 +0000

    Modified link to FB post
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/branch/rb-6.3@4383 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/templates/promo_banners/generic.php
+++ application/views/templates/promo_banners/generic.php
@@ -1,6 +1,6 @@
 <?php if($start_promo): ?>
     <div class="prod_alert_box" style="float:right; width: 545px;">
         <span class="span_bg icon_info"></span> <strong>This product is for promo use only. For more information, visit our 
-        <a href="https://www.facebook.com/EasyShopPhilippines">Facebook page</a></strong>
+        <a href="https://www.facebook.com/EasyShopPhilippines/photos/a.214678272075103.1073741828.211771799032417/251329425076654/?type=1&theater">Facebook page</a></strong>
     </div>
 <?PHP endif; ?>
