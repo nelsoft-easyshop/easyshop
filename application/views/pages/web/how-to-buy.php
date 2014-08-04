adac0d7f2b09adad4dd8c0714f59e6f0cd76bacb
Sam Gavinio <samgavinio@easyshop.ph>
2014-08-04 10:57:27 +0000

    Added fix for unreferenced hyperlinks
    git-svn-id: https://192.168.1.50:9880/easyshop_svn/branch/rb-6.3@4404 2929c1f8-612a-3548-b275-2a983e7c7391

--- application/views/pages/web/how-to-buy.php
+++ application/views/pages/web/how-to-buy.php
@@ -3,5 +3,16 @@
 </style>
       <div class="wrapper info_wrapper">
         <img src="/assets/images/img_how-to-buy.png" alt="How to Buy">
+        <div>
+        	<a href="mailto:info@easyshop.ph" class="mrginrght-20">
+        		<img src="/assets/images/img_email_lnk.png" alt="eMail" >
+        	</a>
+        	<a href="https://facebook.com/easyshopphilippines" class="mrginrght-20">
+        		<img src="/assets/images/img_fb_lnk.png" alt="Facebook - easyshopphilippines">
+        	</a> 
+        	<a href="https://twitter.com/EasyShopPH">
+        		<img src="/assets/images/img_twitter_lnk.png" alt="Twitter - EasyShopPH">
+        	</a>
+        </div>
       </div>
 
