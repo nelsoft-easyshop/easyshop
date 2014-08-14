78aeedcba78ce518158f9d465070530c8ab488f9
Sam <samgavinio@easyshop.ph>
2014-08-14 19:45:11 +0800

    #8 Replaced tabs with spaces. Moved config.php reference one directory closer

--- web/index.php
+++ web/index.php
@@ -19,16 +19,16 @@
  *
  */
 
-    $server_config_path = dirname(__FILE__).'/../../config.php';
+    $server_config_path = dirname(__FILE__).'/../config.php';
     if(file_exists ($server_config_path)){
-	$serverConfig = require  $server_config_path;
-	if(isset($serverConfig['environment'])){
-	    define('ENVIRONMENT', $serverConfig['environment']);
-	}else{
-	    define('ENVIRONMENT', 'development');
-	}
+    $serverConfig = require  $server_config_path;
+        if(isset($serverConfig['environment'])){
+            define('ENVIRONMENT', $serverConfig['environment']);
+        }else{
+            define('ENVIRONMENT', 'development');
+        }
     }else{
-	define('ENVIRONMENT', 'development');
+        define('ENVIRONMENT', 'development');
     }
 
 
@@ -44,23 +44,23 @@
 
 if (defined('ENVIRONMENT'))
 {
-	switch (ENVIRONMENT)
-	{
-		case 'development':
-			//error_reporting(0);
-			ini_set('display_errors', 1);
-			error_reporting(E_ALL);
-			break;
-		case 'staging':
-			error_reporting(0);
-			break;
-		case 'production':
-			error_reporting(0);
-			break;
+    switch (ENVIRONMENT)
+    {
+        case 'development':
+            //error_reporting(0);
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+            break;
+        case 'staging':
+            error_reporting(0);
+            break;
+        case 'production':
+            error_reporting(0);
+            break;
 
-		default:
-			exit('The application environment is not set correctly.');
-	}
+        default:
+            exit('The application environment is not set correctly.');
+    }
 }
 
 /*
@@ -73,7 +73,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = '../system';
+    $system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -89,7 +89,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = '../application';
+    $application_folder = '../application';
 
 /*
  * --------------------------------------------------------------------
@@ -152,61 +152,61 @@ if (defined('ENVIRONMENT'))
  */
 
 	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
+    if (defined('STDIN'))
+    {
+        chdir(dirname(__FILE__));
+    }
 
-	if (realpath($system_path) !== FALSE)
-	{
-		$system_path = realpath($system_path).'/';
-	}
+    if (realpath($system_path) !== FALSE)
+    {
+        $system_path = realpath($system_path).'/';
+    }
 
-	// ensure there's a trailing slash
-	$system_path = rtrim($system_path, '/').'/';
+    // ensure there's a trailing slash
+    $system_path = rtrim($system_path, '/').'/';
 
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
+    // Is the system path correct?
+    if ( ! is_dir($system_path))
+    {
+        exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+    }
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+    // The name of THIS file
+    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// The PHP file extension
-	// this global constant is deprecated.
-	define('EXT', '.php');
+    // The PHP file extension
+    // this global constant is deprecated.
+    define('EXT', '.php');
 
-	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
+    // Path to the system folder
+    define('BASEPATH', str_replace("\\", "/", $system_path));
 
-	// Path to the front controller (this file)
-	define('FCPATH', str_replace(SELF, '', __FILE__));
+    // Path to the front controller (this file)
+    define('FCPATH', str_replace(SELF, '', __FILE__));
 
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+    // Name of the "system folder"
+    define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
 
-	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		define('APPPATH', $application_folder.'/');
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
+    // The path to the "application" folder
+    if (is_dir($application_folder))
+    {
+        define('APPPATH', $application_folder.'/');
+    }
+    else
+    {
+        if ( ! is_dir(BASEPATH.$application_folder.'/'))
+        {
+            exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+        }
 
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
+        define('APPPATH', BASEPATH.$application_folder.'/');
+    }
 
 /*
  * --------------------------------------------------------------------
