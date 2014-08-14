a82b80608fa3018aeca8421a838d959ae0a2eb8d
Sam <samgavinio@easyshop.ph>
2014-08-14 19:44:29 +0800

    #8 Moved references to config.php one directory closer

--- application/config/param/database.php
+++ application/config/param/database.php
@@ -1,29 +1,30 @@
 <?php
 
-/* 
+/**
  * Database param values
+ *
  */
 
-$server_config_path = dirname(__FILE__).'/../../../../config.php';
+$server_config_path = dirname(__FILE__).'/../../../config.php';
 
 if(file_exists ($server_config_path)){
-	$serverConfig = require  $server_config_path;
-	$dbConfig = [
-	    'host'      => $serverConfig['DB_host'],
-	    'driver'    => $serverConfig['DB_driver'],
-	    'user'      => $serverConfig['DB_user'],
-	    'password'  => $serverConfig['DB_password'],
-	    'dbname'    => $serverConfig['DB_dbname'],
-	];
-	
+    $serverConfig = require  $server_config_path;
+    $dbConfig = [
+        'host'      => $serverConfig['DB_host'],
+        'driver'    => $serverConfig['DB_driver'],
+        'user'      => $serverConfig['DB_user'],
+        'password'  => $serverConfig['DB_password'],
+        'dbname'    => $serverConfig['DB_dbname'],
+    ];
+
 }else{
-	$dbConfig = [
-	    'host'      => '127.0.0.1',
-	    'driver'    => 'pdo_mysql',
-	    'user'      => 'root',
-	    'password'  => '121586',
-	    'dbname'    => 'easyshop',
-	];
+    $dbConfig = [
+        'host'      => '127.0.0.1',
+        'driver'    => 'pdo_mysql',
+        'user'      => 'root',
+        'password'  => '121586',
+        'dbname'    => 'easyshop',
+    ];
 }
 
 return $dbConfig;
--- application/config/routes.php
+++ application/config/routes.php
@@ -41,7 +41,7 @@
 $enable_splash = false;
 $bypass_IP = '124.104.99.90';
 
-$server_config_path = dirname(__FILE__).'/../../../config.php';
+$server_config_path = dirname(__FILE__).'/../../config.php';
 
 if(file_exists ($server_config_path)){
   $serverConfig = require  $server_config_path;
