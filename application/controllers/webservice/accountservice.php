5de6a42eaeb4b0dfb6f1b90e61fb17314d759e7a
unknown <inon.baguio@yahoo.com>
2014-08-18 19:07:25 +0800

    #1 Added docblocks to the functions

--- application/controllers/webservice/accountservice.php
+++ application/controllers/webservice/accountservice.php
@@ -5,6 +5,13 @@ if (!defined('BASEPATH'))
 
 class AccountService extends MY_Controller
 {
+
+    /**
+     *  Constructor Declaration
+     *  1. All posted data will be brought first for authentication evaluation
+     *  2. Authentication method is located under MY_Controller.php
+     *  @return true/false
+     */
     public function __construct()
     {   
         parent::__construct();
@@ -14,12 +21,20 @@ class AccountService extends MY_Controller
 
     }
 
+    /**
+     *  Loading of test view for accountservice
+     *  @return View
+     */
     public function index() {
         $this->load->view("pages/accounts");
     }
 
-    public function getProductCount()
-    {   
+    /**
+     *  method to access the product_model with the getProdCount method to return the count of a certain product
+     *  @return View
+     */
+    public function getProductCount() { 
+        
         $this->load->model("product_model");
         $id = $this->input->post("id");
         $count = $this->product_model->getProdCount($id);
@@ -27,14 +42,18 @@ class AccountService extends MY_Controller
         return $count;
     }
 
-    public function getUserCount()
-    {
+    /**
+     *  method to access the user_model with the CountUsers method to return the count of users
+     *  @return View
+     */
+    public function getUserCount() {
+
         $this->load->model("user_model");
         $count = $this->user_model->CountUsers();
-
         return $count;
     }
-
-    
 }
- ?>
+
+?> 
+
+
