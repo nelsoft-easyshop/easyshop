29cb82f51d65c9087aedd93eca38a170a6db4da9
unknown <inon.baguio@yahoo.com>
2014-08-19 19:05:04 +0800

    #1 Fixed indentions

--- application/views/pages/accounts.php
+++ application/views/pages/accounts.php
@@ -5,36 +5,35 @@
 </head>
 <body>	
 
-		<hr>
+<hr>
 	<?php 
 
-	$data = array("id" => "formid");
-	echo "<b>getProdCount:</b><br/>";
-	echo form_open("webservice/accountservice/getproductcount",$data);
+		$data = array("id" => "formid");
+		echo "<b>getProdCount:</b><br/>";
+		echo form_open("webservice/accountservice/getproductcount",$data);
 	
-
-	
-	echo "ID:".form_input('id','0')."<br/>";
-	echo "Hash:".form_input('hash','100BF27B6F9B7BB4B789C0410856F0776E7661EE*')."<br/>";
-	
-	
-	echo form_submit('submit','Post',"id=submit");
-	echo form_close();
+		echo "ID:".form_input('id','0')."<br/>";
+		echo "Hash:".form_input('hash','100BF27B6F9B7BB4B789C0410856F0776E7661EE*')."<br/>";
+		
+		echo form_submit('submit','Post',"id=submit");
+		echo form_close();
 	?>
 
-	<hr>
-	<hr>
+<hr>
+<hr>
 	<?php 
 
-	$data = array("id" => "formid");
-	echo "<b>getAccounts:</b><br/>";
-	echo form_open("webservice/accountservice/getusercount",$data);
-	
-	echo form_submit('submit','Post',"id=submit");
-	echo form_close();
+		$data = array("id" => "formid");
+		echo "<b>getAccounts:</b><br/>";
+		echo form_open("webservice/accountservice/getusercount",$data);
+		
+		echo form_submit('submit','Post',"id=submit");
+		echo form_close();
 	?>
 
-	<hr>
+<hr>
 
 </body>
 </html>	
+
+
