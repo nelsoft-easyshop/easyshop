12e771173900a1a0cbb64f7421606958a77ea343
Sam <samgavinio@easyshop.ph>
2014-08-21 03:51:40 +0800

    #115 Added browser-disclaimer class

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,7 +4,7 @@
                 <div class="res_wrapper wrapper">
                 
                 <div class="copyright">
-                <p>This page is best viewed with one of these browsers:
+                <p class='browser-disclaimer'>This page is best viewed with one of these browsers:
                     <a href='https://www.google.com/intl/en/chrome/browser/' target="_blank"><img src='<?=base_url()?>assets/images/icon_browser_chrome.png' style='margin-left:2px; margin-top:3px;'/> <span style='position:absolute;'>Chrome</span></a>
                     <a href='http://www.mozilla.org/en-US/firefox/new/' target="_blank"><img src='<?=base_url()?>assets/images/icon_browser_firefox.png' style='margin-left:50px; margin-top:3px;'/> <span style=' position:absolute; margin-left:2px;'>Firefox</span></a>
                 </p>
