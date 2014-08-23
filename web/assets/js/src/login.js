4edc5c22bfc3278ca7023ff21f8d6013d5321492
Sam <samgavinio@easyshop.ph>
2014-08-23 16:41:24 +0800

    #159 Action variable is stored in cookie and used to trigger send message modal show

--- web/assets/js/src/login.js
+++ web/assets/js/src/login.js
@@ -1,82 +1,82 @@
+(function($){    
 
-$(document).ready(function(){
-    $(".search_box").css('display','none');
-});
+    $(document).ready(function(){
+        $(".search_box").css('display','none');
+    });
 
-$(document).ready(function(){
-   $("#login_form").validate({
-         rules: {
-            login_username: {
-                required: true
-                },
-            login_password: {
-                required: true
-                }
-         },
-         messages: {
-            login_username: {
-                required: 'Username is required.'
-                },
-            login_password: {
-                required: 'Password is required.'
-                }
-         },
-         errorElement: "span",
-         errorPlacement: function(error, element) {
-				if(element.attr('name') === 'login_username'){
-					error.appendTo($('#username_error'));
-				}
-				else{
-					error.appendTo($('#passw_error'));
-				}
-         },
-         submitHandler: function(form) {
-			$('#loading_img').show();
-			$('#login').hide();
-            
-            $.ajax({
-                type: "POST",
-                dataType: "JSON",
-                url: config.base_url + "login/authenticate",
-                data: $(form).serializeArray(),
-                success:function(data){
-                    if(data.o_success <= 0){
-			$("#login_error").empty();
-                        $("#login_error").html(data[3]);
-			$('#loading_img').hide();
-			$('#login').show();
+    $(document).ready(function(){
+        $("#login_form").validate({
+            rules: {
+                login_username: {
+                    required: true
+                    },
+                login_password: {
+                    required: true
                     }
-                    else{
-			var curl = $.cookie('rn');
-                        $('.error_cont').text('');
-			$('#login_error').text('');
-                        $('#loading_img').hide();
-                        $('#login').val('Redirecting...');
-                        $('#login')[0].disabled = true;
-                        $('#login').show();
-
-                        var url = $('#redirect_url').val();
-                        var first_uri_segment = url.substring(0, url.indexOf('/'));			
-                        if((url == 'sell/step1')||(first_uri_segment == 'item')|| (url == 'cart')){
-                          window.location = config.base_url+ url;
+            },
+            messages: {
+                login_username: {
+                    required: 'Username is required.'
+                    },
+                login_password: {
+                    required: 'Password is required.'
+                    }
+            },
+            errorElement: "span",
+            errorPlacement: function(error, element) {
+                if(element.attr('name') === 'login_username'){
+                    error.appendTo($('#username_error'));
+                }
+                else{
+                    error.appendTo($('#passw_error'));
+                }
+            },
+            submitHandler: function(form) {
+                $('#loading_img').show();
+                $('#login').hide();
+                
+                $.ajax({
+                    type: "POST",
+                    dataType: "JSON",
+                    url: config.base_url + "login/authenticate",
+                    data: $(form).serializeArray(),
+                    success:function(data){
+                        if(data.o_success <= 0){
+                            $("#login_error").empty();
+                            $("#login_error").html(data[3]);
+                            $('#loading_img').hide();
+                            $('#login').show();
                         }
-                        else if(first_uri_segment == 'cart'){
-			  window.location = config.base_url + first_uri_segment;
-			}
                         else{
-                            window.location = config.base_url;
-                        }                        
-                    }
-                }
-            });
-            return false;
-         }
-    });
-     
-    $('.login_box input').on('focus', function(){
-		$('#login_error').text('');
-    });
+                            $('.error_cont').text('');
+                            $('#login_error').text('');
+                            $('#loading_img').hide();
+                            $('#login').val('Redirecting...');
+                            $('#login')[0].disabled = true;
+                            $('#login').show();
 
-   $
+                            var url = $('#redirect_url').val();
+                            var first_uri_segment = url.substring(0, url.indexOf('/'));
+                            
+                            if((url == 'sell/step1')||(first_uri_segment == 'item')|| (url == 'cart')){
+                                window.location = config.base_url+ url;
+                            }
+                            else if(first_uri_segment == 'cart'){
+                                window.location = config.base_url + first_uri_segment;
+                            }
+                            else{
+                                window.location = config.base_url;
+                            }                        
+                        }
+                    }
+                });
+                return false;
+            }
+        });
+        
+        $('.login_box input').on('focus', function(){
+            $('#login_error').text('');
+        });
+    });
     
-});
+})(jQuery);  
