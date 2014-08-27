3d25e5c511521ff319f0a57a866721e03b95e259
Sam <samgavinio@easyshop.ph>
2014-08-27 21:45:43 +0800

    Updated to file version constant to 5.0.9

--- application/config/constants.php
+++ application/config/constants.php
@@ -39,7 +39,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 /*   
  *  EASYSHOP CUSTOM CONSTANTS
  */
-define('ES_FILE_VERSION','5.0.8');
+define('ES_FILE_VERSION','5.0.9');
 // define('ES_PRODUCTION',FALSE);
 
 
