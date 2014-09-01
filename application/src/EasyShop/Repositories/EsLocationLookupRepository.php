44055835d9621c22eaf74d261699f532fca75232
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-09-01 17:08:42 +0800

    #269 Add repository for es_location_lookup table

---
+++ application/src/EasyShop/Repositories/EsLocationLookupRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EasyShop\Repositories;
+
+use Doctrine\ORM\EntityRepository;
+use EasyShop\Entities\EsLocationLookup;
+use Doctrine\ORM\Query\ResultSetMapping;
+
+class EsLocationLookupRepository extends EntityRepository
+{
+    
+}
