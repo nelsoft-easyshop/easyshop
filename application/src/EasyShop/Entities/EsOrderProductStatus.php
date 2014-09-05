6e8e381306c4fb4b8184b5dde16cb47712c5bd95
Sam <samgavinio@easyshop.ph>
2014-09-05 19:47:00 +0800

    #353 Generated entities

---
+++ application/src/EasyShop/Entities/EsOrderProductStatus.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace EasyShop\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * EsOrderProductStatus
+ *
+ * @ORM\Table(name="es_order_product_status")
+ * @ORM\Entity
+ */
+class EsOrderProductStatus
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_order_product_status", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idOrderProductStatus;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=45, nullable=true)
+     */
+    private $name;
+
+
+
+    /**
+     * Get idOrderProductStatus
+     *
+     * @return integer 
+     */
+    public function getIdOrderProductStatus()
+    {
+        return $this->idOrderProductStatus;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return EsOrderProductStatus
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
