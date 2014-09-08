e5eebd9aca73c44678f7fb44a430a9c8e1337e55
LA Roberto <louise.roberto.1337@gmail.com>
2014-09-08 19:00:15 +0800

    added date_added @ es_problem_report

---
+++ application/migrations/DoctrineMigrations/Version20140908185359.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\Version;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140908185359 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_problem_report` 
+            ADD COLUMN `date_added` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `problem_description`;
+            ");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_problem_report` 
+            DROP COLUMN `date_added`;
+            ");
+    }
+}
