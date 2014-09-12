410b4bd9954c5d89107eaee9d658ff872fe33c10
es-paulmodino <paul.modino@easyshop.ph>
2014-09-12 19:15:27 +0800

    #420 covert new design of vendor to html

--- application/views/pages/user/vendor_view.php
+++ application/views/pages/user/vendor_view.php
@@ -2,38 +2,141 @@
 <link type="text/css" href='<?=base_url()?>assets/css/bootstrap.css' rel="stylesheet" media='screen'/>
 
 <section>
-    <div class="vendor-main-bg">
-        <img src="<?=base_url()?>assets/images/sample-vendor-img.jpg">
-    </div>
-    <div class="main-container vendor-main pos-ab">
-        <div class="vendor-profile-content">
-            <div class="pd-lr-20">
-                <div class="vendor-profile-img">
-                    <div class="vendor-profile-img-con">
-                        <img src="<?=base_url()?>assets/images/img_logo_air21.jpg">
+    <div class="pos-rel">
+        <div class="vendor-main-bg">
+            <img src="<?=base_url()?>assets/images/sample-vendor-img.jpg">
+        </div>
+        <div class="main-container vendor-main pos-ab">
+            <div class="vendor-profile-content">
+                <div class="pd-lr-20">
+                    <div class="vendor-profile-img">
+                        <div class="vendor-profile-img-con">
+                            <img src="<?=base_url()?>assets/images/img-default-vendor-profile-photo.jpg">
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h4>Air 21</h4>
+                    <p><strong>Contact No. :</strong>09171234567</p>
+                    <p>
+                        <span class="glyphicon glyphicon-map-marker"></span>
+                        <span class="cl-1"><strong>Location not set</strong></span>
+                    </p>
+                    <div class="vendor-profile-btn">
+                        <a href="" class="btn btn-default-2">
+                            <span class="glyphicon glyphicon-plus-sign"></span> Follow
+                        </a>
+                        <a href="" class="btn btn-default-1">
+                            <span class="glyphicon glyphicon-envelope"></span> Message
+                        </a>
                     </div>
                 </div>
             </div>
-            <div>
-                <h4>Air 21</h4>
-                <p><strong>Contact No. :</strong>09171234567</p>
-                <p>
-                    <a href="">
-                        <span class="glyphicon glyphicon-map-marker"></span>
-                        <span class="cl-1"><strong>Location not set</strong></span>
-                    </a>
-                </p>
-                <div class="vendor-profile-btn">
-                    <a href="" class="btn btn-default-2">
-                        <span class="glyphicon glyphicon-plus-sign"></span> Follow
-                    </a>
-                    <a href="" class="btn btn-default-1">
-                        <span class="glyphicon glyphicon-envelope"></span> Message
-                    </a>
+        </div>
+    </div>
+    <div class="pos-rel">
+        <div class="vendor-main-bg">
+            <div class="edit-cover-photo">
+                <a href="">
+                    <img src="<?=base_url()?>assets/images/img-default-cover-photo.png" alt="Change Cover Photo"><br />
+                    <h4><strong>Change Cover Photo</strong></h4>
+                </a>
+            </div>
+            <img src="<?=base_url()?>assets/images/sample-vendor-img.jpg" alt="sample cover photo">
+        </div>
+        <div class="main-container vendor-main pos-ab">
+            <div class="vendor-profile-content">
+                <div class="pd-lr-20">
+                    <div class="vendor-profile-img">
+                        <div class="vendor-profile-img-con">
+                            <div class="edit-profile-photo">
+                                <div>
+                                    <img src="<?=base_url()?>assets/images/img-default-cover-photo.png" alt="Edit Profile Photo">
+                                    <span>Change Profile Photo</span>
+                                </div>
+                            </div>
+                            <div class="edit-profile-photo-menu">
+                                <div>Upload Photo</div>
+                                <div>Remove Photo</div>
+                            </div>
+                            <img src="<?=base_url()?>assets/images/mg-default-cover-photo.jpg" alt="Profile Photo">
+                        </div>
+                    </div>
+                </div>
+                <div class="pd-lr-20">
+                    <input type="text" class="form-control mrgn-bttm-8 mrgn-top-10" placeholder="Seller Name">
+                    <input type="text" class="form-control mrgn-bttm-8" placeholder="Contact No.">
+                    <div class="mrgn-bttm-8 edit-vendor-location">
+                        <input type="text" class="ui-form-control">
+                        <input type="text" class="ui-form-control">
+                    </div>
+                    <div class="vendor-profile-btn edit-profile-btn">
+                        <a href="" class="btn btn-default-1">Cancel</a>
+                        <a href="" class="btn btn-default-3">Save Changes</a>
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
+</section>
 
-            <div class="vendor_products_wrapper user-tab">
+<section>
+    <div class="vendor-sticky-nav">
+        <div class="main-container">
+            <ul class="vendor-nav">
+                <li>
+                    <a href="" class="vendor-nav-active">
+                        <img src="<?=base_url()?>assets/images/img-sticky-nav-home-active.jpg" alt="Store">
+                    </a>
+                </li>
+                <li>
+                    <a href="">Promo Page</a>
+                </li>
+                <li>
+                    <a href="">Seller Information</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+            <ul class="sticky-nav">
+                <li>
+                    <div class="vendor-profile-img-con">
+                        <img src="<?=base_url()?>assets/images/img-default-vendor-profile-photo.jpg" alt="Profile Photo">
+                    </div>
+                    <h4>Air 21</h4>
+                </li>
+                <li>
+                    <a href=""><img src="<?=base_url()?>assets/images/img-vendor-icon-promo.jpg"></a>
+                    <a href=""><img src="<?=base_url()?>assets/images/img-vendor-icon-info.jpg"></a>
+                    <a href=""><img src="<?=base_url()?>assets/images/img-vendor-icon-contact.jpg"></a>
+                </li>
+                <li>
+                    <select>
+                        <option>On Seller's Page</option>
+                        <option>Main Page</option>
+                        <option>Other Page</option>
+                    </select>
+                    <input type="text">
+                    <input type="submit" value="" class="span_bg">
+                </li>
+                <li>
+                    <div>
+                        <span class="cart-items">
+                            2 item(s) in your cart
+                        </span>
+                        <span class="cart-icon-con glyphicon glyphicon-shopping-cart"></span>
+                    </div>
+                </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+    </div>
+</section>
+
+<section class="vendor-content-wrapper">
+    <div class="main-container">
+        <div class="vendor_products_wrapper user-tab">
                 <?php if($product_count > 0):?>
                     <?php foreach($products as $catID=>$p):?>
                     <div class="vendor_txt_prod_header" class="<?php echo $p['slug']?>">
@@ -73,12 +176,40 @@
                     <?php endforeach;?>
                 <?php endif;?>
                 </div>
-
-
-
-        </div>
     </div>
     
 </section>
 
 
+<script type="text/javascript">
+(function ($) {
+
+    //create a stick nav
+    var menuOffset = $('.vendor-sticky-nav')[0].offsetTop; // replace #menu with the id or class of the target navigation
+    $(document).bind('ready scroll', function() {
+        var docScroll = $(document).scrollTop();
+        if (docScroll >= 455) 
+            {
+                if (!$('.vendor-sticky-nav').hasClass('sticky-nav-fixed')) {
+                    $('.vendor-sticky-nav').addClass('sticky-nav-fixed').css({
+                        top: '-155px'
+                    }).stop().animate({
+                        top: 0
+                    }, 500);
+                    
+                }
+
+                $('.vendor-content-wrapper').addClass('fixed-vendor-content');
+
+            } 
+        else 
+            {
+                $('.vendor-sticky-nav').removeClass('sticky-nav-fixed').removeAttr('style');
+                $('.vendor-content-wrapper').removeClass('fixed-vendor-content');
+            }
+
+    });
+
+    
+})(jQuery);
+</script>
