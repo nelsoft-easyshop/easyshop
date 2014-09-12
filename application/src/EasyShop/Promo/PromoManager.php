b4f9b14a518f7de5d3170bc6a82e06b1d13656b2
Sam <samgavinio@easyshop.ph>
2014-09-12 11:36:10 +0800

    #243 Fixed syntax problem in accessing promoConfig

--- application/src/EasyShop/Promo/PromoManager.php
+++ application/src/EasyShop/Promo/PromoManager.php
@@ -24,7 +24,7 @@ class PromoManager
      */
     public function __construct(ConfigLoader $configLoader)
     {
-        $this->promoConfig = $configLoader->getItem('promo', 'Promo');
+        $this->promoConfig = $configLoader->getItem('promo')['Promo'];
     }
     
     /**
