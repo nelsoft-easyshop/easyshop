3c1c3e6ff65489dec1f762cc9d0afb5e9ca14930
Sam <samgavinio@easyshop.ph>
2014-09-15 16:32:18 +0800

    #457 added csrf config

---
+++ application/config/csrf.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
+            'bypassURI' => array(
+                            '/payment/dragonPayPostBack',
+                            '/payment/ipn2',
+                            '/payment/pesoPayDataFeed',
+            ),
+            'bypassFirstSegment' => array(
+                            'webservice',
+                            'mobile',
+            ),
+        );
+        
+/* End of file csrf.php */
+/* Location: ./application/config/csrf.php */
