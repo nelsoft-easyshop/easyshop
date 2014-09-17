b5200a884a31c70d0d6603cdb09fec9af4f12ec8
Sam <samgavinio@easyshop.ph>
2014-09-17 16:03:06 +0800

    Updated constant to 5.1.16

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '5.1.15';
+    $fileVersion = '5.1.16';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
