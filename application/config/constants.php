7647c0723b3eeb07533df3f364dbcd96dcb2c639
Sam <samgavinio@easyshop.ph>
2014-09-17 14:19:39 +0800

    Updated file version to 5.1.15

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '5.1.13';
+    $fileVersion = '5.1.15';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
