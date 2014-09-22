2461761f760d95d08b3345c19eef8b7dbea14f6c
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-09-22 19:28:58 +0800

    #402 add function getUserImage and register in kernel

--- application/libraries/Kernel.php
+++ application/libraries/Kernel.php
@@ -98,7 +98,8 @@ class Kernel
         
         //User Manager
         $container['user_manager'] = function ($c) use ($container) {
-            return new \EasyShop\User\UserManager($container['entity_manager']);
+            return new \EasyShop\User\UserManager($container['entity_manager']
+                                                ,$container['config_loader']);
         };
         
         //Account Manager
