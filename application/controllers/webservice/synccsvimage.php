c466c038929ab2968a8b29702285997d81baa5f2
unknown <inon.baguio@yahoo.com>
2014-09-24 15:45:35 +0800

    #538 controller for syncCSvimage

---
+++ application/controllers/webservice/synccsvimage.php
@@ -0,0 +1,108 @@
+<?php 
+
+use EasyShop\Entities\EsProductImage;
+use EasyShop\Entities\EsProduct;
+use EasyShop\Entities\EsMember;
+
+class SyncCsvImage extends MY_Controller 
+{
+    
+    /**
+     * The entity manager
+     *
+     */
+    private $em;   
+
+    public function __construct()  
+    { 
+        parent::__construct(); 
+        // Loading entity manager 
+        $this->load->library('image_lib');        
+        $this->em = $this->serviceContainer['entity_manager'];
+        $this->img_dimension['usersize'] = array(1024,768);
+        $this->img_dimension['small'] = array(400,535);
+        $this->img_dimension['categoryview'] = array(220,200);
+        $this->img_dimension['thumbnail'] = array(60,80);        
+    }
+
+    /**
+     * Passess the posted data to upload_files method
+     *
+     */ 
+    public function index()
+    {
+        $this->json = file_get_contents(APPPATH . "resources/json/jsonp.json");
+
+        $this->syncImages($this->input->get());
+    }
+
+    public function syncImages($imagesId)
+    {
+
+        foreach($imagesId["product"] as $ids)
+        {
+            $EsProductImagesRepository = $this->em->getRepository('EasyShop\Entities\EsProductImage');
+            $EsProductRepository = $this->em->getRepository('EasyShop\Entities\EsProduct');
+            $values = $EsProductImagesRepository->getDefaultImage($ids);            
+            
+            $images =  strtolower(str_replace("assets/product/", "", $values->getProductImagePath()));
+
+            $path = "./assets/admin/$images";
+
+            if(file_exists($path)) {
+
+                $date = date("Ymd");
+                $imageId =  $values->getIdProductImage();
+                $productId = $values->getProduct()->getIdProduct();
+                $memberId =  $values->getProduct()->getMember()->getIdMember();
+
+                $filename = $productId.'_'.$memberId.'_'.$date;
+                $newfilename = $productId.'_'.$memberId.'_'.$date.".".$values->getProductImageType();
+                $imageDirectory = "./assets/product/$filename/".$newfilename;
+                $tempDirectory = "./assets/product/".$filename."/";                
+
+                mkdir($tempDirectory.'categoryview/', 0777, true);
+                mkdir($tempDirectory.'small/', 0777, true);
+                mkdir($tempDirectory.'thumbnail/', 0777, true);
+                mkdir($tempDirectory.'other/', 0777, true);      
+                         
+                if(copy($path, $imageDirectory)){
+
+                    $this->imageresize($imageDirectory, $tempDirectory."small",$this->img_dimension["small"]);
+                    $this->imageresize($imageDirectory, $tempDirectory."categoryview",$this->img_dimension["categoryview"]);
+                    $this->imageresize($imageDirectory, $tempDirectory."thumbnail",$this->img_dimension["thumbnail"]);
+                    $this->imageresize($imageDirectory, $tempDirectory,$this->img_dimension["usersize"]);
+                    continue;
+                }
+
+            }
+            else {
+                echo "$images does not exists";
+            }
+        }
+                    return $this->output
+                    ->set_content_type('application/json')
+                    ->set_output("jsonCallback({'sites':[{'success': '"."success"."',},]});");         
+
+
+    }
+
+    private function imageresize($imageDirectory, $newDirectory, $dimension)
+    {
+        $config['image_library'] = 'GD2';
+        $config['source_image'] = $imageDirectory;
+        $config['maintain_ratio'] = true;
+        $config['quality'] = '85%';
+        $config['new_image'] = $newDirectory;
+        $config['width'] = $dimension[0];
+        $config['height'] = $dimension[1]; 
+
+        $this->image_lib->initialize($config); 
+        $this->image_lib->resize();
+        $this->image_lib->clear();        
+    }
+
+}
+
+
+
