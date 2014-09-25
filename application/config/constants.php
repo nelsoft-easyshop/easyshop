db3e274ce44f78ee85517042973d40694c923ba9
Sam <samgavinio@easyshop.ph>
2014-09-25 23:03:15 +0800

    Updated file version

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '5.1.16';
+    $fileVersion = '10.0';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
