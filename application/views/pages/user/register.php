a8307ee8724d56a8fc2cd93ef26080b855ead3a6
es-paulmodino <paul.modino@easyshop.ph>
2014-09-29 14:20:46 +0800

    #654 fixed loading gif out of bound

--- application/views/pages/user/register.php
+++ application/views/pages/user/register.php
@@ -338,8 +338,8 @@
 						<div class="reg2_btn_submit">
 							<!--<button type="button" class="btn btn-warning btn-large">SEND</button>-->
 							<input type="submit" class="btn btn_send" value="SEND" name="register_form1" id="register_form1_btn" >
-							<div style='display:inline-block; position:absolute;'>
-							<img style='display:none;margin-top:5px;margin-left:5px;' src="<?=base_url()?>assets/images/orange_loader_small.gif" class="img_loader_small2" id="register_form1_loadingimg"/>
+							<div style='display:inline-block; position:absolute; width:50px; overflow:hidden;'>
+							 <img style='display:none;margin-top:5px;margin-left:5px;' src="<?=base_url()?>assets/images/orange_loader_small.gif" class="img_loader_small2" id="register_form1_loadingimg"/>
 							</div>
 						</div>				
 					</fieldset>
