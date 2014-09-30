fa820f4e2f44cf6d4405dd80498dcc27ac6dc83d
unknown <inon.baguio@yahoo.com>
2014-09-30 18:19:17 +0800

    #538 added docblocks and added validation if the images already exists in the database

--- application/src/EasyShop/Repositories/EsAdminImagesRepository.php
+++ application/src/EasyShop/Repositories/EsAdminImagesRepository.php
@@ -1,20 +1,38 @@
 <?php
 
 namespace EasyShop\Repositories;
-
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\ResultSetMapping;
 use EasyShop\Entities\EsAdminImages; 
 
 class EsAdminImagesRepository extends EntityRepository
 {
+
+    /**
+     * Inserts image name under es_admin_images 
+     * @param string $filename
+     */    
     public function insertImages($filename)
     {
 
+
+        $qb = $this->_em->createQueryBuilder()
+                            ->select('pi')
+                            ->from('EasyShop\Entities\EsAdminImages','pi')
+                            ->where('pi.imageName = :name')
+                            ->setParameter('name', $filename)
+                            ->getQuery();
+
+        $result = $qb->getResult();
+
         $adminImage = new EsAdminImages;
-        $adminImage->setImageName($filename);
-        $this->_em->persist($adminImage);
-        $this->_em->flush();        
+        if(!$result) {
+            $adminImage->setImageName($filename);
+            $this->_em->persist($adminImage);
+            $this->_em->flush();        
+
+        }
+
 
     }
 }
