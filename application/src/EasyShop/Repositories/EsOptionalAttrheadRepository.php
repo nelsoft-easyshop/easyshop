c1e4a8eaaa0cdca8dba3ab005e26c127d01c680c
unknown <inon.baguio@yahoo.com>
2014-10-01 20:42:10 +0800

    #538 added method deleteAttrHeadById($id)

--- application/src/EasyShop/Repositories/EsOptionalAttrheadRepository.php
+++ application/src/EasyShop/Repositories/EsOptionalAttrheadRepository.php
@@ -3,8 +3,22 @@
 namespace EasyShop\Repositories;
 
 use Doctrine\ORM\EntityRepository; 
+use EasyShop\Entities\EsOptionalAttrdetail;
+use EasyShop\Entities\EsOptionalAttrhead;
 
 class EsOptionalAttrheadRepository extends EntityRepository
 {
+    /**
+     * Method used for reverting rubbish data from admin product csv uploads
+     * @param int $id
+     */ 
+    public function deleteAttrHeadById($id)
+    {
+
+        $query = $this->_em->createQuery("DELETE FROM EasyShop\Entities\EsOptionalAttrhead e 
+            WHERE e.product = ?3");
+        $query->setParameter(3, $id);
+        $query->execute();      
+    }
 
 }
