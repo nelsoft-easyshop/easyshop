595e70fa9ba432fd1de60011277173ded75a4db5
unknown <inon.baguio@yahoo.com>
2014-10-01 20:42:21 +0800

    #538 added method deleteAttrDetailByProductId($id)

--- application/src/EasyShop/Repositories/EsOptionalAttrdetailRepository.php
+++ application/src/EasyShop/Repositories/EsOptionalAttrdetailRepository.php
@@ -3,8 +3,34 @@
 namespace EasyShop\Repositories;
 
 use Doctrine\ORM\EntityRepository; 
+use EasyShop\Entities\EsOptionalAttrdetail;
+use EasyShop\Entities\EsOptionalAttrhead;
 
 class EsOptionalAttrdetailRepository extends EntityRepository
 {
 
+    /**
+     * Method used for reverting rubbish data from admin product csv uploads
+     * @param int $id
+     */    
+    public function deleteAttrDetailByProductId($id)
+    {
+        $this->em =  $this->_em;
+        $qb = $this->em->createQueryBuilder()
+                            ->select('pi')
+                            ->from('EasyShop\Entities\EsOptionalAttrhead','pi')
+                            ->where('pi.product = :productId')
+                            ->setParameter('productId', $id)
+                            ->getQuery();
+
+        $queryResult = $qb->getResult();
+        foreach ($queryResult as $result ) {
+
+            $query = $this->em->createQuery("DELETE FROM EasyShop\Entities\EsOptionalAttrdetail e 
+                WHERE e.head = ?2");
+            $query->setParameter(2, $result->getIdOptionalAttrhead());
+            $query->execute();
+        }       
+    }
 }
+
