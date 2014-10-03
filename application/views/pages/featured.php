e44442e0db220c8444552f5b0442a2af6ba24934
carlduazo <justine.duazo@easyshop.ph>
2014-10-03 16:47:06 +0800

    #544 Created include file for featured seller

---
+++ application/views/pages/featured.php
@@ -0,0 +1,227 @@
+<div class="row mo" style="padding-left: 30px; padding-right: 30px; margin-bottom: 20px;">
+    <div class="row" style="background: url('/assets/images/clothes-closet.jpg') no-repeat center; background-size: cover; padding: 10px 0px;">
+        <div class="col-lg-9 col-md-12 col-xs-12 col-featured-items">
+            <a class="prev"><i class="fa fa-angle-left fa-prev-slide"></i></a>
+            <a class="next"><i class="fa fa-angle-right fa-next-slide"></i></a>
+            <div id="featured-seller" class="owl-carousel owl-theme">
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/item1.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/item1-hover.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                        <span class="discount-circle">-25%</span>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Sleeves w/ Short</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100.00</span><span class="item-price">P70.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/uniqlo-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/big-dress1.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/big-dress2.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                        <span class="discount-circle">-25%</span>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">White Dress</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100.00</span><span class="item-price">P70.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/uniqlo-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/item2.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/item2-hover.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                        <span class="discount-circle">-25%</span>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Sleeves w/ Short</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100.00</span><span class="item-price">P70.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/uniqlo-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/phone3.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/phone3-hover.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                        <span class="discount-circle">-25%</span>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">White Sleeve w/ Short</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100.00</span><span class="item-price">P70.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/uniqlo-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/item3.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/item3-hover.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                        <span class="discount-circle">-25%</span>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">American Crop Top</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100.00</span><span class="item-price">P70.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/uniqlo-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="col-store-logo">
+            <center>
+            <div class="store-big-logo-container ">
+                <div class="span-store-big-logo">
+                   <img src="/assets/images/products/uniqlo-logo-big.png"/>
+                </div>
+            </div>
+            </center>
+        </div>
+        </div>
+    </div>
+</div>
