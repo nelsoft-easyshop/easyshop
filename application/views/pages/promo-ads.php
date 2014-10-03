cdf3da30ce4939e5521dc2f7ea8e0d2889b685ba
carlduazo <justine.duazo@easyshop.ph>
2014-10-03 16:47:53 +0800

    #544 Created promo party boxes

---
+++ application/views/pages/promo-ads.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <a href="#"><img src="/assets/images/big-banner-1.png" alt="Home Big Banner 1" class="img-responsive img-promo-banner"></a>
+            </div>
+            
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <a href="#"><img src="/assets/images/big-banner-2.png" alt="Home Big Banner 2" class="img-responsive img-promo-banner"></a>
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <a href="#"><img src="/assets/images/big-banner-3.png" alt="Home Big Banner 1" class="img-responsive img-promo-banner"></a>
+            </div>
+        </div>
+    </div>
+</div>
