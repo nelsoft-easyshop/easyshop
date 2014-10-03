37d5975565249aa041788dfddbef71f1a3d07cb4
stephenjanz <stephenjanz@easyshop.ph>
2014-10-03 16:43:08 +0800

    #885 config files for new notification service

---
+++ application/config/sms.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config = Array(
+            'api' => "dgsMQ8q77hewW766aqxK",
+            'from' => 'Easyshop.ph',
+            'outbound_endpoint' => "http://api.semaphore.co/api/sms"
+        );
+
+return $config;
+/* End of file sms.php */
+/* Location: ./application/config/sms.php */
