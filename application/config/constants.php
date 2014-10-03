e6f81987ef9ffe4bffbf5cebe26137943a361f26
Sam <samgavinio@easyshop.ph>
2014-10-03 13:49:50 +0800

    Updated file constant

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '5.2.2';
+    $fileVersion = '5.2.3';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
