d5af56e284c413b9b827b631fcf84da924a33759
carlduazo <justine.duazo@easyshop.ph>
2014-10-07 16:41:27 +0800

    #544 Changed id name for slider container

--- application/views/pages/featured-category.php
+++ application/views/pages/featured-category.php
@@ -30,7 +30,7 @@
                     Men
                 </button>
             </div>
-            <div id="momo" class="purchased-items-slider-cat-1 owl-carousel"> 
+            <div id="cat-items" class="purchased-items-slider-cat-1 owl-carousel"> 
               <div class="item">
                     <div class="item-image-container">
                         <figure>
@@ -288,8 +288,11 @@
                     ACCESSORIES
                 </button>
             </div>
-            <div id="momo" class="purchased-items-slider-cat-2 owl-carousel"> 
-              <div class="item">
+            <div id="cat-items" class="purchased-items-slider-cat-2 owl-carousel"> 
+            <?php
+                for($i=1; $i<=2; $i++){
+            ?>
+                <div class="item">
                     <div class="item-image-container">
                         <figure style="">
                             <a href="#">
@@ -489,7 +492,304 @@
                         </div>
                     </div>
                 </div>
+            <?php
+                }
+            ?>  
+            </div><!--purchased-items-slider -->
+        </div><!-- End .purchased-items-container -->
+    </div><!-- End .col-md-12 -->
+</div><!-- End .row -->
+
+<div class="row row-category">        
+    <div class="col-md-12">
+       <div class="purchased-items-container carousel-wrapper">
+            <header class="content-title">
+                <div class="title-bg">
+                    <span class="category-1-icon"></span><h3>JEWELLERY & WATCHES</h3>
+                </div><!-- End .title-bg -->
+            </header>
+        
+            <div class="carousel-controls div-slider-control">
                 
+               <a href="#"><span class="span-see-all">see all</span></a>
+                <span>
+                    <a id="purchased-items-slider-prev-cat-3"><i class="fa fa-angle-left fa-category-item-prev"></i></a>
+                    <a id="purchased-items-slider-next-cat-3"><i class="fa fa-angle-right fa-category-item-next"></i></a>
+                </span>
+            </div><!-- End .carousel-controllers -->
+            
+            <div class="tag-categories">
+                <button class="btn-tag btn-tag-active">
+                    New Arrival
+                </button>
+                <button class="btn-tag">
+                    Popular Items
+                </button>
+                <button class="btn-tag">
+                    Necklace
+                </button>
+                <button class="btn-tag">
+                    Watches
+                </button>
+                <button class="btn-tag">
+                    Bracelets
+                </button>
+                <button class="btn-tag">
+                    Earrings
+                </button>
+                <button class="btn-tag">
+                    Rings
+                </button>
+            </div>
+            <div id="cat-items" class="purchased-items-slider-cat-3 owl-carousel"> 
+            <?php
+                for($i=1; $i<=2; $i++){
+            ?>
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure style="">
+                            <a href="#">
+                                <img src="/assets/images/products/fossil-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/fossil-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        <span class="new-circle">NEW</span>    
+                        <span class="discount-circle">50%</span>    
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Fossil Watch</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P30,000.00</span><span class="item-price">15,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/fossil-logo.jpg"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/swar-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/swar-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Swarovski Necklace</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="item-price">P70,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/swar-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/chopard-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/chopard-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Chopard Earrings</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="item-price">P 80,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/chopard-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/bvlgari-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/bvlgari-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Bvlgari Necklace</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="item-price">P27,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/bvlgari-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/graff-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/graff-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Graff Diamond</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="item-price">P100,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/graff-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="item">
+                    <div class="item-image-container">
+                        <figure>
+                            <a href="#">
+                                <img src="/assets/images/products/tiffany-p.jpg" alt="Phone 1" class="item-image img-responsive">
+                                <img src="/assets/images/products/tiffany-p.jpg" alt="Phone 1 Hover" class="item-image-hover">
+                            </a>
+                            <span class="discount-circle">10%</span>
+                        </figure>
+                    </div><!-- End .item-image -->
+                                                    
+                    <div class="item-meta-container" align="left">
+                        <h3 class="item-name"><a href="#">Tiffany 18K Diamond</a></h3>
+                        <div class="item-meta-inner-container clearfix">
+                            <div class="item-price-container">
+                                <span class="old-price">P100,000.00</span><span class="item-price">P90,000.00</span>
+                            </div>
+                        </div>
+                        <div class="item-action">
+                            <table width="100%">
+                            <tr>
+                                <td>
+                                    <a class="btn btn-default-1 btn-add-cart" target="_blank" href="/item/boom">
+                                        <span class="fa fa-shopping-cart"></span> ADD TO CART
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="pull-right">
+                                        <div class="store-logo-container ">
+                                            <div class="span-store-logo">
+                                               <img src="/assets/images/products/tiffany-logo.png"/>
+                                            </div>
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            <?php
+                }
+            ?>  
             </div><!--purchased-items-slider -->
         </div><!-- End .purchased-items-container -->
     </div><!-- End .col-md-12 -->
