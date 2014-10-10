793e3b1e735bb4def0bbcdd51c1d581c6fc1cf3e
carlduazo <justine.duazo@easyshop.ph>
2014-10-10 10:53:31 +0800

    #544 Applied responsiveness for promo-ads

--- application/views/pages/promo-ads.php
+++ application/views/pages/promo-ads.php
@@ -1,15 +1,15 @@
-<div class="row-fluid">
+<div class="row row-promo-ads">
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="col-md-4 col-sm-4 col-xs-6 promo-1" style="padding: 4px;">
                 <a href="#"><img src="/assets/images/big-banner-1.png" alt="Home Big Banner 1" class="img-responsive img-promo-banner"></a>
             </div>
             
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="col-md-4 col-sm-4 col-xs-6 promo-2" style="padding: 4px;">
                 <a href="#"><img src="/assets/images/big-banner-2.png" alt="Home Big Banner 2" class="img-responsive img-promo-banner"></a>
             </div>
 
-            <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="col-md-4 col-sm-4 col-xs-6 promo-3" style="padding: 4px;">
                 <a href="#"><img src="/assets/images/big-banner-3.png" alt="Home Big Banner 1" class="img-responsive img-promo-banner"></a>
             </div>
         </div>
