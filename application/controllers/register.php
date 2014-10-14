eeed5aff8699decbd82748c00c11808927484660
Sam <samgavinio@easyshop.ph>
2014-10-14 15:46:36 +0800

    #544 Replaced tabs with spaces

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,20 +1,20 @@
 <?php
 
 if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
-	
+    exit('No direct script access allowed');
+
 class Register extends MY_Controller
 {
 
-	function __construct() 
-	{
-		parent::__construct();
-		$this->load->model("register_model");
-		$this->load->library('encrypt');
+    function __construct() 
+    {
+        parent::__construct();
+        $this->load->model("register_model");
+        $this->load->library('encrypt');
         $this->load->library('session');
-		$this->form_validation->set_error_delimiters('', '');
-	}
-	
+        $this->form_validation->set_error_delimiters('', '');
+    }
+
     public function index()
     {
         $url = 'landingpage';
@@ -110,78 +110,78 @@ class Register extends MY_Controller
 
         echo json_encode($serverResponse);
     }
-	
-	
-	public function username_check()
-	{
-		if($this->input->post('username')){
-			$username = $this->input->post('username');
-			if($this->register_model->validate_username($username))
-				echo 1;
-			else
-				echo 0;
-		}
-	}
-	
-	public function email_check()
-	{
-		if($this->input->post('email')){
-			$email = $this->input->post('email');
-			if($this->register_model->checkEmailIfExists($email)){
-				echo 0;
-			}
-			else {
-				echo 1;
-			}
-		}
-	}
-	
-	public function mobile_check()
-	{
-		if($this->input->post('mobile')){
-			$mobile = $this->input->post('mobile');
-			if($this->register_model->checkMobileIfExists($mobile)){
-				echo 0;
-			}
-			else {
-				echo 1;
-			}
-		}
-	}
 
-	public function external_callbacks( $postdata, $param )
-	{
-		 $param_values = explode( ',', $param ); 
-		 $model = $param_values[0];
-		 $this->load->model( $model );
-		 $method = $param_values[1];
-		 
-		 if( count( $param_values ) > 2 )
-		 {
-			  array_shift( $param_values );
-			  array_shift( $param_values );
-			  $argument = $param_values;
-		 }
-	
-		 if( isset( $argument ) )
-		 {
-			if($method == 'checkifrequired'){
-				$thisdata = trim(html_escape($postdata));
-				$checkwith = trim(html_escape($this->input->post($argument[0])));
-				$callback_result = $this->$model->$method($thisdata, $checkwith);
-			}
-			else
-				$callback_result = $this->$model->$method( $postdata, $argument );
-		 }
-		 else
-		 {
-			if($method == 'validate_captcha')
-				$callback_result = $this->$model->$method( $postdata, $this->session->userdata('captcha_word'));
-			else	 
-				$callback_result = $this->$model->$method( $postdata );
-		 }
-		 return $callback_result;
-	}
+
+    public function username_check()
+    {
+        if($this->input->post('username')){
+            $username = $this->input->post('username');
+            if($this->register_model->validate_username($username))
+                echo 1;
+            else
+                echo 0;
+        }
+    }
+
+    public function email_check()
+    {
+        if($this->input->post('email')){
+            $email = $this->input->post('email');
+            if($this->register_model->checkEmailIfExists($email)){
+                echo 0;
+            }
+            else {
+                echo 1;
+            }
+        }
+    }
+
+    public function mobile_check()
+    {
+        if($this->input->post('mobile')){
+            $mobile = $this->input->post('mobile');
+            if($this->register_model->checkMobileIfExists($mobile)){
+                echo 0;
+            }
+            else {
+                echo 1;
+            }
+        }
+    }
+
+    public function external_callbacks( $postdata, $param )
+    {
+            $param_values = explode( ',', $param ); 
+            $model = $param_values[0];
+            $this->load->model( $model );
+            $method = $param_values[1];
+            
+            if( count( $param_values ) > 2 )
+            {
+                array_shift( $param_values );
+                array_shift( $param_values );
+                $argument = $param_values;
+            }
+
+            if( isset( $argument ) )
+            {
+            if($method == 'checkifrequired'){
+                $thisdata = trim(html_escape($postdata));
+                $checkwith = trim(html_escape($this->input->post($argument[0])));
+                $callback_result = $this->$model->$method($thisdata, $checkwith);
+            }
+            else
+                $callback_result = $this->$model->$method( $postdata, $argument );
+            }
+            else
+            {
+            if($method == 'validate_captcha')
+                $callback_result = $this->$model->$method( $postdata, $this->session->userdata('captcha_word'));
+            else	 
+                $callback_result = $this->$model->$method( $postdata );
+            }
+            return $callback_result;
+    }
 
     public function success($action = '')
     {
@@ -207,30 +207,31 @@ class Register extends MY_Controller
         }
     } 
     
-    /*
-     *	Subscription Handler
+    /**
+     * Subscribe to the newsletter
+     * 
      */
-	public function subscribe()
-	{
-		if($this->input->post('subscribe_btn') && $this->form_validation->run('subscription_form')){
+    public function subscribe()
+    {
+        if($this->input->post('subscribe_btn') && $this->form_validation->run('subscription_form')){
             $this->load->model('register_model');
-			$data['email'] = $this->input->post('subscribe_email');
+            $data['email'] = $this->input->post('subscribe_email');
             $result = $this->register_model->subscribe($data['email']);
-			// Send notification email to user 
-			if($result){		
-              $this->register_model->sendNotification($data, 'subscribe');
-              $data['title'] = 'Successful Subscription | Easyshop.ph';
-              $data['content'] = 'You have successfully Subscribed!';
-              $data['sub_content'] =  'Thank you for choosing to keep in touch with Easyshop.ph. Expect to hear many things from us soon.';
-              $this->load->view('pages/user/register_subscribe_success', $data);
+            // Send notification email to user 
+            if($result){
+                $this->register_model->sendNotification($data, 'subscribe');
+                $data['title'] = 'Successful Subscription | Easyshop.ph';
+                $data['content'] = 'You have successfully Subscribed!';
+                $data['sub_content'] =  'Thank you for choosing to keep in touch with Easyshop.ph. Expect to hear many things from us soon.';
+                $this->load->view('pages/user/register_subscribe_success', $data);
             }else{
                 redirect('home','refresh');
             }
-		}else{
+        }else{
             redirect('home','refresh');
         }
 
-	}
+    }
     
     /**
      * Renders change password view
@@ -278,75 +279,74 @@ class Register extends MY_Controller
         $temp['result'] = $result;
         $this->load->view('pages/user/changepassword', $temp);
         $this->load->view('templates/footer');		
-		
-	}
+    }
     
     function email_verification(){
-	
-		$this->load->library('encrypt');
 
-		//Decrypt and re-assign data
-		$enc = html_escape($this->input->get('h'));
-		$enc = str_replace(" ", "+", $enc);
-		$decrypted = $this->encrypt->decode($enc);
-		$getdata = explode("|", $decrypted);
-		
-		$email = $getdata[0];
-		$username = $getdata[1];
-		$hash = $getdata[2];
+        $this->load->library('encrypt');
 
-		$data = array(
-			'title' => 'Easyshop.ph - Email Verification',
-			'member_username' => $username,
-			'email' => $email,
+        //Decrypt and re-assign data
+        $enc = html_escape($this->input->get('h'));
+        $enc = str_replace(" ", "+", $enc);
+        $decrypted = $this->encrypt->decode($enc);
+        $getdata = explode("|", $decrypted);
+        
+        $email = $getdata[0];
+        $username = $getdata[1];
+        $hash = $getdata[2];
+
+        $data = array(
+            'title' => 'Easyshop.ph - Email Verification',
+            'member_username' => $username,
+            'email' => $email,
             'render_logo' => false,
             'render_searchbar' => false,
-		);
-		$data = array_merge($data, $this->fill_header());
+        );
+        $data = array_merge($data, $this->fill_header());
 
-		$member_id = $this->register_model->get_memberid($username)['id_member'];
+        $member_id = $this->register_model->get_memberid($username)['id_member'];
 
-		if($member_id === 0){
-			$this->load->view('templates/header', $data);
-			$this->load->view('pages/user/err_email_verif', $data);
-			$this->load->view('templates/footer_full');
-			return;
-		}
+        if($member_id === 0){
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/user/err_email_verif', $data);
+            $this->load->view('templates/footer_full');
+            return;
+        }
 
-		$data_val = $this->register_model->get_verifcode($member_id);
+        $data_val = $this->register_model->get_verifcode($member_id);
 
 
-		if($email === $data_val['email'] && $hash === $data_val['emailcode'] && $username === $data_val['username'])
-		{
-		
-			if($data_val['is_email_verify'] == 1){
-				$data['verification_msg'] = $this->lang->line('expired_email_verification');
-				$this->load->view('templates/header', $data);
-				$this->load->view('pages/user/register_form3_view', $data);
-				$this->load->view('templates/footer_full');
-				return;
-			}
+        if($email === $data_val['email'] && $hash === $data_val['emailcode'] && $username === $data_val['username'])
+        {
+        
+            if($data_val['is_email_verify'] == 1){
+                $data['verification_msg'] = $this->lang->line('expired_email_verification');
+                $this->load->view('templates/header', $data);
+                $this->load->view('pages/user/register_form3_view', $data);
+                $this->load->view('templates/footer_full');
+                return;
+            }
 
-			$temp = array(
-				'is_email_verify' => 1,
-				'member_id' => $member_id
-			);
+            $temp = array(
+                'is_email_verify' => 1,
+                'member_id' => $member_id
+            );
 
-			$this->register_model->update_verification_status($temp);
-			
-			$data['verification_msg'] = $this->lang->line('success_email_verification');
+            $this->register_model->update_verification_status($temp);
+            
+            $data['verification_msg'] = $this->lang->line('success_email_verification');
             $data['render_searchbar'] = false;
             $data['render_logo'] = false;
-			$this->load->view('templates/header', $data);
-			$this->load->view('pages/user/register_form3_view', $data);
-			$this->load->view('templates/footer_full');
-		}
-		else{
-			$this->load->view('templates/header', $data);
-			$this->load->view('pages/user/err_email_verif', $data);
-			$this->load->view('templates/footer_full');
-		}
-	}
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/user/register_form3_view', $data);
+            $this->load->view('templates/footer_full');
+        }
+        else{
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/user/err_email_verif', $data);
+            $this->load->view('templates/footer_full');
+        }
+    }
  
  
     
