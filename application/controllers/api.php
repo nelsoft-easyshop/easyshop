01561ee20255a0e33c8e5cc5ecd20e055d67dce0
LA Roberto <louise.roberto.1337@gmail.com>
2014-10-15 19:09:12 +0800

    Created token generator

---
+++ application/controllers/api.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Api extends MY_Controller
+{
+
+    /**
+     * The oauth2 server
+     *
+     */
+    private $oauthServer;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->oauthServer = $this->serviceContainer['oauth2_server'];
+        header('Content-type: application/json');
+    }
+
+    /**
+     * Create the token
+     *
+     */
+    public function doCreateToken()
+    {
+        $this->oauthServer->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
+    }
+
+}
