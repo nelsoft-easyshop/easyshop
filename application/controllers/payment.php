035dcddee7d8703129ab8a873b322cadf6221897
Sam <samgavinio@easyshop.ph>
2014-10-15 13:10:04 +0800

    #1083 Removed call to deprecated removeFromCart method

--- application/controllers/payment.php
+++ application/controllers/payment.php
@@ -702,7 +702,6 @@ class Payment extends MY_Controller{
             $complete = $this->payment_model->updatePaymentIfComplete($orderId,json_encode($itemList),$txnId,$paymentType,$orderStatus,0);
 
             if($postBackCount == "0"){
-                $remove_to_cart = $this->payment_model->removeToCart($member_id,$itemList); 
                 // send email to buyer
                 $this->sendNotification(array('member_id'=>$member_id, 'order_id'=>$orderId, 'invoice_no'=>$invoice),TRUE,FALSE);  
             }
@@ -866,8 +865,6 @@ class Payment extends MY_Controller{
             }
 
             $complete = $this->payment_model->updatePaymentIfComplete($orderId,json_encode($itemList),$txnId,$paymentType,0,0);
-
-            $remove_to_cart = $this->payment_model->removeToCart($member_id,$itemList);
             $this->sendNotification(array('member_id'=>$member_id, 'order_id'=>$orderId, 'invoice_no'=>$invoice));  
 
         }else{
