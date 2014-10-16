c5f30eb851a281d17a29f129418462d4aec40cc5
unknown <inon.baguio@yahoo.com>
2014-10-16 16:58:38 +0800

    #990 added getOrderProductAttributes

--- application/src/EasyShop/Repositories/EsOrderProductAttrRepository.php
+++ application/src/EasyShop/Repositories/EsOrderProductAttrRepository.php
@@ -7,4 +7,25 @@ use Doctrine\ORM\EntityRepository;
 class EsOrderProductAttrRepository extends EntityRepository
 {
 
+    /**
+     * Returns product attributes in each order product
+     * @param int $orderid
+     * @return object
+     */    
+    public function getOrderProductAttributes($orderid)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $queryBuilder = $qb->select("orderAttr.attrName as attrName,                                                         
+                                     orderAttr.attrValue as attrValue,                                                         
+                                     orderAttr.attrPrice as attrPrice ")
+                            ->from('EasyShop\Entities\EsOrderProductAttr','orderAttr')
+                            ->leftJoin('EasyShop\Entities\EsOrderProduct', 'o', 'with', "o.idOrderProduct = orderAttr.orderProduct")    
+                            ->where("o.idOrderProduct = :orderid")
+                            ->setParameter('orderid', $orderid)
+                            ->getQuery(); 
+
+        return $queryBuilder->getResult();                                                 
+       
+    }
+
 }
