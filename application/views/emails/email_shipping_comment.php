aa243396c3b3a92ffd4212c6051389d74f5eed29
carlduazo <justine.duazo@easyshop.ph>
2014-10-17 11:29:40 +0800

    #1117 Modified email template

--- application/views/emails/email_shipping_comment.php
+++ application/views/emails/email_shipping_comment.php
@@ -1,54 +1,125 @@
-<html>
-    <body style="color:#494949;">
-        <table cellspacing="0" cellpadding="10" style="width:100%; max-width:780px; margin:0 auto; font-family: Arial, sans-serif;">
-            <thead>
-                <tr>
-                    <td colspan="3">
-                        <div style="border-bottom:3px solid #f18200; padding-bottom:10px;">
-                            <a href="" style="padding:10px 0; display:inline-block">
-                                <img src="img_logo.png" alt="Easyshop.ph">
-                            </a>
-                        </div>  
-                    </td>
-                </tr>
-            </thead>
-            <tbody style="font-size:12px;">
-                <tr>
-                    <td colspan="3">
-                        <span style="color:#f18200;font-weight:bold;">{seller}</span> has just provided new shipping details for your product
-                        <span style="margin-left:1em;">
-                            <a href="{store_link}" style="color:#f18200;">View Store</a> || <a href="{msg_link}" style="color:#f18200;">Send Message</a>
-                        </span>
-                    </td>
-                </tr>
-                <tr>
-                    <td width="120px"><strong>Transaction # :</strong> </td>
-                    <td colspan="2">{invoice}</td>
-                </tr>
-                <tr>
-                    <td width="120px"><strong>Product Name :</strong> </td>
-                    <td colspan="2">{product_name}</td>
-                </tr>
-                <tr>
-                    <td width="120px"><strong>Courier :</strong> </td>
-                    <td colspan="2">{courier}</td>
-                </tr>
-                <tr>
-                    <td><strong>Tracking # :</strong> </td>
-                    <td colspan="2">{tracking_num}</td>
-                </tr>
-                <tr>
-                    <td><strong>Delivery Date :</strong> </td>
-                    <td colspan="2">{delivery_date}</td>
-                </tr>
-                <tr>
-                    <td><strong>Expected Date of Arrival :</strong> </td>
-                    <td colspan="2">{expected_date}</td>
-                </tr>
-                <tr>
-                    <td><strong>Comments :</strong> </td>
-                    <td colspan="2">{comment}</td>
-                </tr>
-            </tbody>
+<html style="font-family: Century Gothic, Calibri">
+    <body>
+        <div style="padding-right: 15px; padding-left:15px; margin-right: auto; margin-left:auto;">
+            <div style="background-color: #013f94;">
+                <center>
+                    <img src="header-img.png" style="display: block; max-width: 100%; height: auto;">
+                </center>
+            </div>
+            <br/>
+            <div style="margin-top: 20px; font-size: 13px;">
+                <p>
+                    Dear {buyer_name},
+                </p>
+                <br/>
+                <p>
+                    <span style="color:#f18200;font-weight:bold;">{seller}</span> has just provided new shipping details for your product
+                </p>
+            </div>
+            <div style="margin-bottom: 20px;background-color: #fff;border: 1px solid transparent; border-radius: 4px; -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05); box-shadow: 0 1px 1px rgba(0, 0, 0, .05); border-color: #bce8f1; border-radius: 0px !important;">
+                <div style="padding: 10px 15px; border-bottom: 1px solid transparent; color: #31708f;background-color: #d9edf7; border-color: #bce8f1; font-weight: bold;">Summary of Shipment</div>
+                <div style="padding: 15px;" align="center">
+                   <div style="min-height: 300px; display: static">
+                        <center>
+                            <div style=" width: 83.33333333333334%;">
+                                <table border='0' width="100%">
+                                    <tr>
+                                        <td style="padding: 10px; vertical-align: top; width:50%">
+                                        <div style=' min-height:260px; max-height: 290px; width: 100%; padding-right: 7px; '>
+                                                <table width="100%" style="border-collapse: collapse; font-size: 12px;">
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Transaction Number:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px;  padding-top:10px; padding-right:7px; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;" align="right ">
+                                                            {order_product_id}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Product Name:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px;  padding-top:10px; padding-right:7px; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;" align="right ">
+                                                            {name}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Courier:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px;  padding-top:10px; padding-right:7px; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;" align="right ">
+                                                            {courier}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Tracking Number:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px;  padding-top:10px; padding-right:7px; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;" align="right ">
+                                                            {tracking_num}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Delivery Date:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px; padding-right:7px;" align="right">
+                                                            {delivery_date}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Expected Date of Arrival:
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px; padding-right:7px;" align="right">
+                                                            {expected_date}
+                                                        </td>
+                                                    </tr>
+                                                    <tr style="border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted;">
+                                                        <td style="font-weight:bold; border-color: #ADAD85; border-width: 0px 0px 1px 0px; border-style:dotted; padding-top:10px;">
+                                                            Comments
+                                                        </td>
+                                                        <td style="padding-left: 20px !important; text-align:right !important; padding-bottom:5px; padding-right:7px;" align="right">
+                                                            {comment}
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div style="background: #3c475c; width: 100%; min-height: 100px; border-width:0px 0px 5px 0px; border-style:solid; border-color:#f18200; padding-top: 15px; padding-bottom: 15px;">
+                <div style="min-height: 50px;">
+                    <center>
+                        <table width="100%">
+                            <tr>
+                                <td>
+                                    <a href="https://www.facebook.com/EasyShopPhilippines">
+                                        <img src="facebook.png" style="float: right; display: block; max-width: 100%; height: auto;" />
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="https://twitter.com/EasyShopPH">
+                                        <img src="twitter.png" style="display: block; max-width: 100%; height: auto;" />
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
+                    </center>
+                </div>
+                <div>
+                    <center>
+                    <p style="color: #ffffff; text-align:center; margin-top:5px; font-size: 12px;">
+                    Copyright &copy; 2014 Easyshop.ph
+                    </p>
+                    </center>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
