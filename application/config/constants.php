ff374ea57a106d5eccedcec57adffb1f94a707f5
Sam <samgavinio@easyshop.ph>
2014-10-21 20:48:17 +0800

    Updated file version to 6.3.2

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '6.3.1';
+    $fileVersion = '6.3.2';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
