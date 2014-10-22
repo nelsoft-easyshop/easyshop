d7192a60203d2d1c5bdbcb1b72b4b5d10d4a2fb3
carlduazo <justine.duazo@easyshop.ph>
2014-10-22 13:28:47 +0800

    #1209 Changed product page view load

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -449,15 +449,15 @@ class product extends MY_Controller
             $data['jsonReviewSchemaData'] = $this->assembleJsonReviewSchemaData($data);
             $data['title'] = es_string_limit(html_escape($product_row['product_name']), 60, '...', ' | Easyshop.ph');
             $data['metadescription'] = es_string_limit(html_escape($product_row['brief']), 155);
-            $this->load->view('templates/header', $data); 
-            $this->load->view('pages/product/productpage_view_responsive', $data);
+            $this->load->view('templates/header_new', $data); 
+            $this->load->view('pages/product/productpage_view_primary', $data);
         }
         else{
             $data['title'] =  'Easyshop.ph | Page Not Found';
             $this->load->view('templates/header', $data); 
             $this->load->view('pages/general_error', $data); 
         }
-        $this->load->view('templates/footer_full');
+        $this->load->view('templates/footer_primary');
     } 
 
     /**
