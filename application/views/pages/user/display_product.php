7748e3c6bb2bfbc239e4d2e3f4691530c40bf6b4
carlduazo <justine.duazo@easyshop.ph>
2014-10-22 14:57:47 +0800

    #1271 Added if-else condition for font-size manipulation of the price and discount value

--- application/views/pages/user/display_product.php
+++ application/views/pages/user/display_product.php
@@ -51,7 +51,10 @@
                         </p>
                         <div class="div-amount">
                             <p class="p-price">
-                                <span><s>  <?php if($percentage && $percentage > 0):?> P <?=$originalPrice?>   <?php endif;?> </s></span> P <?=$productPrice;?>
+                                  <?php if($percentage && $percentage > 0):?><span><s class="discount-value"> P <?=$originalPrice?>  </s> </span> <span style="font-size: 12px;"> P <?=$productPrice;?></span>
+                                  <?php else:?>
+                                  <span> P <?=$productPrice;?></span>
+                                  <?php endif;?>
                             </p>
                             <center>
                                 <a class="btn btn-default-cart" target="_blank" href="/item/<?=$productSlug; ?>">
