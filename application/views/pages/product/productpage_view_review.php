fbffbee42af566cfa3e2a99fa2b11f79bd7cf7ad
carlduazo <justine.duazo@easyshop.ph>
2014-10-22 17:03:48 +0800

    #1209 Added JS link

--- application/views/pages/product/productpage_view_review.php
+++ application/views/pages/product/productpage_view_review.php
@@ -28,7 +28,9 @@
                                     <div class="div-review-item-container">
                                         <div class="row">
                                             <div class="col-xs-2 col-user-image">
-                                                
+                                                <div class="div-user-image">
+                                                    
+                                                </div>
                                             </div>
                                             <div class="col-xs-10">
                                                 <div class="div-review-content-container">
@@ -45,11 +47,12 @@
                                                     <p class="p-review-content">
                                                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
                                                     </p>
-                                                    <a href="#" class="p-reply-text" >
-                                                        <p align="right">
-                                                            Reply
+                                                    <a class="p-reply-text" >
+                                                        <p class="pull-right">
+                                                            <span class="text-cancel">Cancel </span>Reply
                                                         </p>
                                                     </a>
+                                                    <div class="clear"></div>
                                                 </div>
                                                 <div class="div-reply-container">
                                                     <p class="p-reply-title">Write Reply</p>
@@ -117,3 +120,5 @@
         </div>
      </div>
 </div>
+
+<script type='text/javascript' src='/assets/js/src/product-page.js?ver=<?=ES_FILE_VERSION?>'></script>
