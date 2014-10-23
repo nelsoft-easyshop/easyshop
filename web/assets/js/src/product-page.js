f1403ff8a6dc94ec11b7f6aff7d54695319dcfb7
carlduazo <justine.duazo@easyshop.ph>
2014-10-23 15:16:39 +0800

    #1209 added javacript for slider

--- web/assets/js/src/product-page.js
+++ web/assets/js/src/product-page.js
@@ -3,4 +3,32 @@
         $( ".div-reply-container" ).toggle("slow");
         $( ".text-cancel" ).toggle("fade");
     });
+    
+    $( ".p-reply-text-replied-item" ).click(function() {
+        $( ".div-reply-container-replied-item" ).toggle("slow");
+        $( ".text-cancel-replied-item" ).toggle("fade");
+    });
+    
+    $(document).ready(function() {
+        var recommended = $("#recommended");
+        recommended.owlCarousel({
+            items : 4,
+            itemsCustom : false,
+            itemsDesktop : [1199,4],
+            itemsDesktopSmall : [980,3],
+            itemsTablet: [768,2],
+            itemsTabletSmall: false,
+            itemsMobile : [479,2],
+            pagination: false,
+            singleItem : false,
+            itemsScaleUp : false
+        });
+        $(".next").click(function(){
+            recommended.trigger('owl.next');
+        })
+        $(".prev").click(function(){
+            recommended.trigger('owl.prev');
+        })
+    });
+    
 })(jQuery);
