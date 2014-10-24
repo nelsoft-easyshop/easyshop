7f1d9ce58cbf9b4e747ce1984ba57c27ad9ae1ee
carlduazo <justine.duazo@easyshop.ph>
2014-10-24 15:07:41 +0800

    #1317 Added double quote on data-slug value

--- application/views/pages/home/featured.php
+++ application/views/pages/home/featured.php
@@ -47,7 +47,7 @@
                                 <table width="100%">
                                 <tr>
                                     <td>
-                                        <a class="btn btn-default-1 btn-add-cart" href="javascript:void(0);" data-slug="<?php echo $product['product']->getSlug(); ?>" data-productid="<?php echo $product['product']->getIdProduct(); ?>>
+                                        <a class="btn btn-default-1 btn-add-cart" href="javascript:void(0);" data-slug="<?php echo $product['product']->getSlug(); ?>" data-productid="<?php echo $product['product']->getIdProduct(); ?>">
                                             <span class="icon-cart"></span> ADD TO CART
                                         </a>     
                                     </td>
