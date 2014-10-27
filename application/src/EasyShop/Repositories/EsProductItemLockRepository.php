33778052740548c1e0168a4162d254a6038238ee
LA Roberto <louise.roberto.1337@gmail.com>
2014-10-27 12:03:44 +0800

    #1323 Updated php docs, refactored code base

--- application/src/EasyShop/Repositories/EsProductItemLockRepository.php
+++ application/src/EasyShop/Repositories/EsProductItemLockRepository.php
@@ -4,7 +4,7 @@ namespace EasyShop\Repositories;
 
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\ResultSetMapping;
-use EasyShop\Entities\EsProductItemLock as EsProductItemLock;
+use EasyShop\Entities\EsProductItemLock;
 use EasyShop\Entities\EsOrder;
 use EasyShop\Entities\EsProductItem;
 
@@ -14,72 +14,68 @@ class EsProductItemLockRepository extends EntityRepository
     /**
      * Returns the items locks for a given product_id
      *
-     * @param integer $productId
+     * @param int $productId
      * @return EsProductItemLock
-     *
      */
     public function getProductItemLockByProductId($productId)
     {
         $query =  $this->_em->createQueryBuilder()
-                        ->select(['lck.idItemLock, pi.idProductItem, lck.qty as lock_qty, pi.quantity', 'lck.timestamp'])
-                        ->from('EasyShop\Entities\EsProductItemLock','lck')
-                        ->innerJoin('EasyShop\Entities\EsProductItem', 'pi','WITH','lck.productItem = pi.idProductItem')
-                        ->where('pi.product = :product_id')
-                        ->setParameter('product_id', $productId)
-                        ->getQuery(); 
+                            ->select(['lck.idItemLock, pi.idProductItem, lck.qty as lock_qty, pi.quantity', 'lck.timestamp'])
+                            ->from('EasyShop\Entities\EsProductItemLock','lck')
+                            ->innerJoin('EasyShop\Entities\EsProductItem', 'pi','WITH','lck.productItem = pi.idProductItem')
+                            ->where('pi.product = :product_id')
+                            ->setParameter('product_id', $productId)
+                            ->getQuery(); 
         return $result = $query->getResult();
     }
 
     /**
      * Returns the amount of lock given an Order id
      *
-     * @param integer $orderId
+     * @param int $orderId
      * @return int
-     *
      */
     public function getLockCount($orderId)
     {
         $query = $this->_em->createQueryBuilder()
-                        ->select('count(lck.idItemLock) AS cnt')
-                        ->from('EasyShop\Entities\EsProductItemLock','lck')
-                        ->where('lck.order = :orderId')
-                        ->setParameter('orderId', $orderId) 
-                        ->getQuery()
-                        ->getSingleScalarResult();
+                            ->select('count(lck.idItemLock) AS cnt')
+                            ->from('EasyShop\Entities\EsProductItemLock','lck')
+                            ->where('lck.order = :orderId')
+                            ->setParameter('orderId', $orderId) 
+                            ->getQuery()
+                            ->getSingleScalarResult();
         return intval($query);
     }
 
     /**
      * Releases all locks held by member
      *
-     * @param integer $memberId
-     * @return void
-     *
+     * @param int $memberId
      */
     public function releaseAllLock($memberId)
     {
-        $query = $this->_em->createQueryBuilder()
-                        ->select("lck.idItemLock")
-                        ->from('EasyShop\Entities\EsProductItemLock','lck')
-                        ->innerJoin('EasyShop\Entities\EsOrder', 'ord', 'with', "ord.idOrder = lck.order")
-                        ->where("ord.orderStatus = 99")
-                        ->andWhere("ord.buyer = :memberId")
-                        ->setParameter('memberId', $memberId)
-                        ->getQuery()
-                        ->getScalarResult();
+        $itemLockIds = $this->_em->createQueryBuilder()
+                            ->select("lck.idItemLock")
+                            ->from('EasyShop\Entities\EsProductItemLock','lck')
+                            ->innerJoin('EasyShop\Entities\EsOrder', 'ord', 'with', "ord.idOrder = lck.order")
+                            ->where("ord.orderStatus = 99")
+                            ->andWhere("ord.buyer = :memberId")
+                            ->setParameter('memberId', $memberId)
+                            ->getQuery()
+                            ->getScalarResult();
         
-        $productIds = [];
-        foreach ($query as $arr => $value) {
-            $productIds[] = intval($value['idItemLock']);
-        }
-        
-        if(count($productIds) > 0){
+        if(count($itemLockIds) > 0){
+            $productIds = [];
+            foreach ($itemLockIds as $value) {
+                $productIds[] = intval($value['idItemLock']);
+            }
+            
             $qb = $this->_em->createQueryBuilder();
             $query = $this->_em->createQueryBuilder()
-                            ->delete('EasyShop\Entities\EsProductItemLock','lck')
-                            ->where($qb->expr()->in("lck.idItemLock", $productIds))
-                            ->getQuery()
-                            ->execute();
+                                ->delete('EasyShop\Entities\EsProductItemLock','lck')
+                                ->where($qb->expr()->in("lck.idItemLock", $productIds))
+                                ->getQuery()
+                                ->execute();
         }
     }
 
@@ -88,20 +84,19 @@ class EsProductItemLockRepository extends EntityRepository
      *
      * @param mixed $ids
      * @param int $orderId
-     * @return void
-     *
      */
-    public function deleteLockItem($ids = array(), $orderId)
+    public function deleteLockItem($orderId, $ids = array())
     {
-        foreach ($ids as $itemId => $qty) {
-            $item = $this->_em->getRepository('EasyShop\Entities\EsProductItemLock')
-                        ->findOneBy([
-                            'productItem' => $itemId,
-                            'qty' => $qty,
-                            'order' => $orderId
-                            ]);
-            $this->_em->remove($item);
-            $this->_em->flush();
+        if(count($ids) > 0){
+            $itemIds = array_keys($ids);
+            $qb = $this->_em->createQueryBuilder();
+            $this->_em->createQueryBuilder()
+                        ->delete('EasyShop\Entities\EsProductItemLock', 'lck')
+                        ->where($qb->expr()->in("lck.productItem"), $itemIds)
+                        ->andWhere('lck.order = :orderId')
+                        ->setParameter('orderId', $orderId)
+                        ->getQuery()
+                        ->execute();
         }
     }
 
@@ -110,28 +105,24 @@ class EsProductItemLockRepository extends EntityRepository
      *
      * @param mixed $ids
      * @param int $orderId
-     * @return void
-     *
      */
-    public function insertLockItem($ids = array(), $orderId)
+    public function insertLockItem($orderId, $ids = array())
     {
-        $esOrderObj = $this->_em->getRepository('EasyShop\Entities\EsOrder')
-                            ->find(intval($orderId));
+        if(count($ids) > 0){
+            $esOrderObj = $this->_em->getRepository('EasyShop\Entities\EsOrder')->find(intval($orderId));
+            $esProductItemRepo = $this->_em->getRepository('EasyShop\Entities\EsProductItem');
 
-        foreach ($ids as $itemId => $qty) {
-            $esProductItemObj = $this->_em->getRepository('EasyShop\Entities\EsProductItem')
-                                ->find(intval($itemId));
-
-            $lockItem = new EsProductItemLock();
-            $lockItem->setQty($qty);
-            $lockItem->setOrder($esOrderObj);
-            $lockItem->setProductItem($esProductItemObj);
-            $lockItem->setTimestamp(date_create(date("Y-m-d H:i:s")));
-
-            $this->_em->persist($lockItem);
+            foreach ($ids as $itemId => $qty) {
+                $esProductItemObj = $esProductItemRepo->find(intval($itemId));
+                $lockItem = new EsProductItemLock();
+                $lockItem->setQty($qty);
+                $lockItem->setOrder($esOrderObj);
+                $lockItem->setProductItem($esProductItemObj);
+                $lockItem->setTimestamp(date_create(date("Y-m-d H:i:s")));
+                $this->_em->persist($lockItem);
+            }        
             $this->_em->flush();
-        }        
+        }
     }
 }
-
 
