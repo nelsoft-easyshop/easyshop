b4adf5779b1a7b6a2335a4ddb552d521ad420f6d
Sam <samgavinio@easyshop.ph>
2014-10-28 18:46:09 +0800

    Updated file version constant to 7.0.4

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '7.0.0';
+    $fileVersion = '7.0.4';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
