acc93a53b61c68d4d9a5916745e3f6b06fae9e91
carlduazo <justine.duazo@easyshop.ph>
2014-10-29 17:53:41 +0800

    #1382 Modified page layout

--- application/views/pages/user/send-merge-email.php
+++ application/views/pages/user/send-merge-email.php
@@ -1,52 +1,59 @@
+<link type="text/css" href='/assets/css/main-style.css?ver=<?php echo ES_FILE_VERSION ?>' rel="stylesheet" media='screen'/>
 <link type="text/css" href='/assets/css/new-login.css?ver=<?php echo ES_FILE_VERSION ?>' rel="stylesheet" media='screen'/>
-<br/>
-<br/>
-<br/>
 <br/>
 <section class="section-login">
     <div class="container">
-        <div class="div-title-page-container">
-            Social Media Login
-        </div>
-        <div class="div-merge-container" align="center">
-            <p>
-                Your username <a href="#">kurt</a> has the same registered email address of <a href="#">kurt-wilkinson</a>
-                <br/>
-                Would you like to merge both accounts
-            </p>
-            <div class="div-btn-container">
-                <button class="btn btn-lg btn-orange-lg proceed">
-                    PROCEED <i class="glyphicon glyphicon-play"></i>
-                </button>
-                <div class="div-link-login">
-                    <a>Go back to login</a>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Update your account
+            </div>
+            <div class="panel-body div-merge-container" align="center">
+                <p>
+                    Your username <a href="#">kurt</a> has the same registered email address of <a href="#">kurt-wilkinson</a>
+                    <br/>
+                    Would you like to merge both accounts?
+                </p>
+                <div class="div-btn-container">
+                    <button class="btn  btn-orange-lg proceed">
+                        PROCEED <i class="glyphicon glyphicon-play"></i>
+                    </button>
+                    <b>
+                        <div class="div-link-login">
+                            <a href="#">
+                                No, I would like to register with a different email account
+                            </a>
+                        </div>
+                    </b>
                 </div>
             </div>
         </div>
     </div>
     <!-- modal content -->
-		<div id="basic-modal-content">
-			sdfsfsd
-		</div>
-<div style='display:none'>
-			<img src='img/basic/x.png' alt='' />
-		</div>
+    <div id="basic-modal-content-proceed">
+        <div class="modal-text-content">
+            We've just sent a verification message to kurtwilkinson024@yahoo.com account's inbox.<br/> Please login to your email account and follow the instructions provided to complete this process.
+        </div>
+         <center>
+            <span class="modalCloseImg simplemodal-close btn btn-default-1">Close</span>
+            <span class="modalCloseImg simplemodal-close btn btn-default-2">Go to homepage</span>
+        </center>
+    </div>
+    <div style='display:none;'>
+        <a class="modalCloseImg simplemodal-close" title="Close">
+            <i class="glyphicon glyphicon-remove"></i>
+        </a>
+    </div>
 </section>
-<br/>
-<br/>
 <br/>
 <br/>
 <script>
     jQuery(function ($) {
-	// Load dialog on page load
-	//$('#basic-modal-content').modal();
+        $('.proceed').click(function (e) {
+            $('#basic-modal-content-proceed').modal();
+            return false;
+        });
 
-	// Load dialog on click
-	$('.proceed').click(function (e) {
-		$('#basic-modal-content').modal();
-           
-		return false;
-	});
-});
+    });
 </script>
+
 <script src='/assets/js/src/vendor/jquery.simplemodal.js' type='text/javascript'></script>
