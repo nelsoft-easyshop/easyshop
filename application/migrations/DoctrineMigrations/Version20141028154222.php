8ad0247486b50de8b6d93f4ab924d1d52a4cb440
root <kurtwilkinson024@easyshop.ph>
2014-10-29 16:15:33 +0800

    fix issue on foreign keys

--- application/migrations/DoctrineMigrations/Version20141028154222.php
+++ application/migrations/DoctrineMigrations/Version20141028154222.php
@@ -12,25 +12,15 @@ class Version20141028154222 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("CREATE TABLE `es_member_merge` (
-                      `id_member_merge` INT NOT NULL AUTO_INCREMENT,
-                      `member_id` INT UNSIGNED NOT NULL,
-                      `social_media_provider_id` INT NOT NULL,
-                      `social_media_id` VARCHAR(255) NOT NULL,
-                      `created_at` DATETIME NULL,
-                      PRIMARY KEY (`id_member_merge`),
-                      INDEX `fk_es_member_merge_2_idx` (`social_media_provider_id` ASC),
-                      INDEX `idx_id_member` (`member_id` ASC),
-                      CONSTRAINT `fk_es_member_es_social_media_provider`
-                        FOREIGN KEY (`social_media_provider_id`)
-                        REFERENCES `es_social_media_provider` (`id_socialmedia_provider`)
-                        ON DELETE NO ACTION
-                        ON UPDATE NO ACTION)
-                        ");
+        $this->addSql("CREATE TABLE `es_social_media_provider` (
+                      `id_social_media_provider` INT NOT NULL AUTO_INCREMENT,
+                      `name` VARCHAR(45) NOT NULL,
+                      PRIMARY KEY (`id_social_media_provider`))
+                      ");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("DROP TABLE `es_member_merge`");
+        $this->addSql("DROP TABLE `es_socialmedia_provider`");
     }
 }
--- application/migrations/DoctrineMigrations/Version20141028154829.php
+++ application/migrations/DoctrineMigrations/Version20141028154829.php
@@ -12,15 +12,25 @@ class Version20141028154829 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("CREATE TABLE `es_socialmedia_provider` (
-                      `id_socialmedia_provider` INT NOT NULL AUTO_INCREMENT,
-                      `name` VARCHAR(45) NOT NULL,
-                      PRIMARY KEY (`id_socialmedia_provider`))
-                      ");
+        $this->addSql("CREATE TABLE `es_member_merge` (
+                      `id_member_merge` INT NOT NULL AUTO_INCREMENT,
+                      `member_id` INT UNSIGNED NOT NULL,
+                      `social_media_provider_id` INT NOT NULL,
+                      `social_media_id` VARCHAR(255) NOT NULL,
+                      `created_at` DATETIME NULL,
+                      PRIMARY KEY (`id_member_merge`),
+                      INDEX `fk_es_member_merge_2_idx` (`social_media_provider_id` ASC),
+                      INDEX `idx_id_member` (`member_id` ASC),
+                      CONSTRAINT `fk_es_member_es_social_media_provider`
+                        FOREIGN KEY (`social_media_provider_id`)
+                        REFERENCES `es_social_media_provider` (`id_social_media_provider`)
+                        ON DELETE NO ACTION
+                        ON UPDATE NO ACTION)
+                        ");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("DROP TABLE `es_socialmedia_provider`");
+        $this->addSql("DROP TABLE `es_member_merge`");
     }
 }
