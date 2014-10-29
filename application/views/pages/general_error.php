53a8f8b492925a5462cbd6650d82b3ea871afd0e
es-paulmodino <paul.modino@easyshop.ph>
2014-10-29 11:53:34 +0800

    #1388 apply new design of 404 page

--- application/views/pages/general_error.php
+++ application/views/pages/general_error.php
@@ -1,26 +1,34 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url()?>/assets/css/new-homepage.css" media='screen'>
 
+<section class="content-404-bg">
 <div class="container content-404">
-    <div class="img-404">
-        <img src="/assets/images/404page.png" alt="404 error">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <div class="img-404">
+                <img src="/assets/images/404page.png" alt="404 error">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6">
+            <h1>oopppsss!</h1>
+            <h2>
+                Looks like the page you were looking for doesn't exist...
+            </h2>
+            <div class="row form-container-404">
+                <div class="col-xs-12 col-sm-9 col-md-9 form-404-input">
+                    <div class="row">
+                        <input type="text" class="ui-form-control input-404">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-md-3">
+                    <input type="submit" class="btn btn-default-4 btn-no-br submit-404" value="search">
+                </div>
+            </div>
+            <div class="link-404">
+                <a href="/home">
+                    <img src="/assets/images/img-arrow-left.png" alt="return to homepage">Return to <strong>HOMEPAGE</strong>
+                </a>
+            </div>
+        </div>
     </div>
-    <div>
-        <h2>Sorry, the page you are trying to access cannot be found. You may try any of the following:</h2></div>
-        <ul>
-            <li>
-                Ensure that the web address you are accessing is correct.
-            </li>
-            <li>
-                Try locating the actual page again if you are accessing it through a bookmark.
-                It is possible that the page you are looking for may have been moved.
-            </li>
-            <li>
-                You can always make use of the search box available in most pages to find items you
-                want to check out.
-            </li>
-            <li>
-                Visit our sitemap to find easy navigation links throughout the site.
-            </li>
-
-        </ul>
-</div>	
+</div>
+</section>
