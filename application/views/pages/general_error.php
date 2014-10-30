2ed32dd9fc1c9cea266f2732d3d156d9f63911ce
unknown <inon.baguio@yahoo.com>
2014-10-30 17:48:50 +0800

    #1423 inherited search form from the header_primary

--- application/views/pages/general_error.php
+++ application/views/pages/general_error.php
@@ -14,14 +14,16 @@
                 The page that you were looking for has been abducted,... but have no fear, the search bar is here!
             </h2>
             <div class="row form-container-404">
-                <div class="col-xs-12 col-sm-9 col-md-9 form-404-input">
-                    <div class="row">
-                        <input type="text" class="ui-form-control input-404">
+                <form class="nav-searchbar-inner" accept-charset="utf-8" role="search" name="site-search" method="get" action="/search/search.html" id="nav-searchbar">
+                    <div class="col-xs-12 col-sm-9 col-md-9 form-404-input">
+                        <div class="row">
+                            <input type="text" name="q_str" placeholder="" class="ui-form-control input-404">
+                        </div>
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-3 col-md-3">
-                    <input type="submit" class="btn btn-default-4 btn-no-br submit-404" value="search">
-                </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3">
+                        <input type="submit" value="search" class="btn btn-default-4 btn-no-br submit-404" >
+                    </div>
+                </form>
             </div>
             <div class="link-404">
                 <a href="/home">
