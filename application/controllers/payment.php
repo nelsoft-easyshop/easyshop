e6256bedd01811642254b112dcb5fdcaee4dc1ff
Sam <samgavinio@easyshop.ph>
2014-10-30 17:09:23 +0800

    Temporarily removed SMS and email notification for staging.

--- application/controllers/payment.php
+++ application/controllers/payment.php
@@ -1349,6 +1349,7 @@ class Payment extends MY_Controller{
      */
     function sendNotification($data, $buyerFlag = TRUE, $sellerFlag = TRUE) 
     {
+        returnn true;
         $xmlResourceService = $this->serviceContainer['xml_resource'];
         $xmlfile =  $xmlResourceService->getContentXMLfile();
 
