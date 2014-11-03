65e8741015f4637fef653f89613739f29c3d5f89
carlduazo <justine.duazo@easyshop.ph>
2014-11-03 19:21:09 +0800

    #1451 Created new dashboard view

---
+++ application/views/pages/user/dashboard/dashboard-home.php
@@ -0,0 +1,18 @@
+<link type="text/css" href='/assets/css/easy-icons/easy-icons.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+<link type="text/css" href='/assets/css/main-style.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+<link type="text/css" href='/assets/css/contact.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+<link type="text/css" href='/assets/css/bootstrap.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+<link type="text/css" href='/assets/css/new-homepage.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+<link type="text/css" href='/assets/css/new-dashboard.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <ul class="sidebar-dashboard">
+                <li>Dashboard</li>
+                <li>My Store <i class="icon-control-down"></i></li>
+                <li>My Account</li>
+            </ul>
+        </div>
+    </div>
+</div>
