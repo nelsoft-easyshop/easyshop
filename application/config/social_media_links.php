8164064a6183757ac91e070df268ffd02c381af1
unknown <inon.baguio@yahoo.com>
2014-11-04 14:31:35 +0800

    #1421 removed forward slash at the end of the url

--- application/config/social_media_links.php
+++ application/config/social_media_links.php
@@ -1,8 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config = array(
-                'facebook' => "https://www.facebook.com/",
-                'twitter' => 'https://twitter.com/',
+                'facebook' => "https://www.facebook.com",
+                'twitter' => 'https://twitter.com',
             );
         
 /* End of file social_media_link.php */
