1bfc053d8ec9a1627cbfe350f21eb707b1ce17fe
carlduazo <justine.duazo@easyshop.ph>
2014-11-04 13:21:35 +0800

    #1451 Created dashboard js file

---
+++ web/assets/js/src/dashboard.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $( "#my-store-menu-trigger" ).click(function() {
+        $( "#my-store-menu" ).slideToggle( "slow", function() {
+            var attr = $("i.m").attr("class");
+        if(attr == "m icon-control-down toggle-down pull-right"){
+            $('i.m').removeClass("m icon-control-down toggle-down pull-right").addClass("m icon-control-up toggle-down pull-right");
+        }
+        else if(attr == "m icon-control-up toggle-down pull-right"){
+            $('i.m').removeClass("m icon-control-up toggle-down pull-right").addClass("m icon-control-down toggle-down pull-right");
+        }
+        });
+    });
+}(jQuery));
