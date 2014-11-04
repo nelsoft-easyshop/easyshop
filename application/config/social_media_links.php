047f8965084f981390bb21e91def4aabee8de555
unknown <inon.baguio@yahoo.com>
2014-11-04 12:37:43 +0800

    #1421 socia_media_links config file

---
+++ application/config/social_media_links.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
+                'facebook' => "https://www.facebook.com/",
+                'twitter' => 'https://twitter.com/',
+            );
+        
+/* End of file social_media_link.php */
+/* Location: ./application/config/social_media_link.php */
+
