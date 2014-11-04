1e64fc8c515d45a494cba90e1f8e2232c476f748
Sam <samgavinio@easyshop.ph>
2014-11-04 10:50:43 +0800

    Reenabled sendNotification

--- application/controllers/payment.php
+++ application/controllers/payment.php
@@ -1349,7 +1349,6 @@ class Payment extends MY_Controller{
      */
     function sendNotification($data, $buyerFlag = TRUE, $sellerFlag = TRUE) 
     {
-        return true;
         $xmlResourceService = $this->serviceContainer['xml_resource'];
         $xmlfile =  $xmlResourceService->getContentXMLfile();
 
