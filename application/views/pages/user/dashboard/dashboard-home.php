bd8287d3abe0dd35118103ccfb093f54b35f2d58
carlduazo <justine.duazo@easyshop.ph>
2014-11-05 10:33:12 +0800

    #1451 Partial commit

--- application/views/pages/user/dashboard/dashboard-home.php
+++ application/views/pages/user/dashboard/dashboard-home.php
@@ -5,10 +5,123 @@
             <img src="/assets/images/img_profile_pic.jpg" class="vendor-follower-img"/>
             <div class="cover-overlay"></div>
         </div>
-        <div class="div-top-name">
-            <div class="row">
-                <div class="col-md-6">
-                    <p><span class="icon-profile"> Mang Pedring</p>
+        <div class="clear"></div>
+        <div class="div-stats">
+            <div class="div-top-name">
+                <div class="row">
+                    <div class="col-xs-5">
+                        <p class="p-username"><span class="icon-profile"> </span>Mang Pedring</p>
+                    </div>
+                    <div class="col-xs-3 col-progress-text">
+                        <div class="div-progress-container-text" align="right">
+                            Account Progress: 70%
+                        </div>
+                    </div>
+                    <div class="col-xs-4 col-progress-bar">
+                        <div class="div-progress-bar-container" align="right">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+                                    <span class="sr-only">70% Complete</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="div-stats-numbers">
+                <div class="row">
+                    <div class="col-xs-7">
+                        <p class="p-stat-title">
+                            Shop Link: 
+                        </p>
+                        <div class="form-shop-link">
+                            <input type="text" class="input-shop-link" value="https://easyshop.ph/mangpedring"/>
+                            <span class="icon-web"></span>
+                        </div>
+                        <div class="div-stat-summary">
+                            <div class="row">
+                                <div class="col-xs-3" align="center">
+                                    <p class="p-label-stat">
+                                        Followers
+                                    </p>
+                                    <p class="p-stat-total">
+                                        15
+                                    </p>
+                                </div>
+                                <div class="col-xs-3" align="center">
+                                    <p class="p-label-stat">
+                                        Following
+                                    </p>
+                                    <p class="p-stat-total">
+                                        25
+                                    </p>
+                                </div>
+                                <div class="col-xs-3" align="center">
+                                    <p class="p-label-stat">
+                                        Item(s)
+                                    </p>
+                                    <p class="p-stat-total">
+                                        10
+                                    </p>
+                                </div>
+                                <div class="col-xs-3" align="center">
+                                    <p class="p-label-stat">
+                                        Sold
+                                    </p>
+                                    <p class="p-stat-total">
+                                        5
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-5">
+                        <div class="div-feedback-stat">
+                            <p class="p-stat-title">
+                                Feedback:
+                            </p>
+                            <table width="100%" class="table-stat-ratings">
+                                <tbody>
+                                    <tr>
+                                        <td class="td-criteria">Item Quality: </td>
+                                        <td class="td-star" align="right">
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-criteria">Communication: </td>
+                                        <td class="td-star" align="right">
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-criteria">Shipment Time: </td>
+                                        <td class="td-star" align="right">
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat star-active"></i>
+                                            <i class="icon-star star-stat"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="td-criteria">Total Reviews: </td>
+                                        <td class="td-total-review" align="center">
+                                            20
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
