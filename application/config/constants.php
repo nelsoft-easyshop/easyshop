6e44baffcf2ac654da7dd9968b27fe78f641b4ff
Sam <samgavinio@easyshop.ph>
2014-11-05 20:14:21 +0800

    Updated file constant to 7.1.7

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '7.1.5';
+    $fileVersion = '7.1.7';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
