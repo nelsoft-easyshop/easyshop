cb95a893c36893d4245770412cb7061b07734a77
Sam <samgavinio@easyshop.ph>
2014-11-07 16:50:17 +0800

    #1501 Added secondary image for template A view

--- application/views/partials/homesliders/templateA.php
+++ application/views/partials/homesliders/templateA.php
@@ -1,9 +1,10 @@
 <li class="slider-template-1 slider-alt-template2">
     <div class="slider-item">
         <?php $sliderImage = reset($image); ?>
+        <?php $smallerImage = end($image); ?>
         <a href="<?php echo $sliderImage['target']['url']?>" target=<?php echo $sliderImage['target']['targetString'];?>>
             <img src="<?php echo $sliderImage['path'] ?>" class="slider-temp1-img1">
-            <img src="<?php echo $sliderImage['path'] ?>" class="slider-temp1-img2">
+            <img src="<?php echo $smallerImage['path'] ?>" class="slider-temp1-img2">
         </a>
     </div>
 </li>
