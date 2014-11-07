13afd1df01cf277c8f054053f50fabe9dd6dbd6d
es-paulmodino <paul.modino@easyshop.ph>
2014-11-07 11:41:00 +0800

    #1501 fixed slider template A

--- application/views/partials/homesliders/templateA.php
+++ application/views/partials/homesliders/templateA.php
@@ -1,8 +1,9 @@
-<li class="slider-template-1">
+<li class="slider-template-1 slider-alt-template2">
     <div class="slider-item">
         <?php $sliderImage = reset($image); ?>
         <a href="<?php echo $sliderImage['target']['url']?>" target=<?php echo $sliderImage['target']['targetString'];?>>
-            <img src="<?php echo $sliderImage['path'] ?>">
+            <img src="<?php echo $sliderImage['path'] ?>" class="slider-temp1-img1">
+            <img src="<?php echo $sliderImage['path'] ?>" class="slider-temp1-img2">
         </a>
     </div>
 </li>
