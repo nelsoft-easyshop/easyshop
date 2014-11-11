008c87c84b7a14a39e9b511122638bbfea8408a0
Sam <samgavinio@easyshop.ph>
2014-11-11 20:29:56 +0800

    #1600 Do not diplay brand section if there are no brands to display

--- application/views/pages/home/featured-brands.php
+++ application/views/pages/home/featured-brands.php
@@ -1,35 +1,39 @@
-<div class="row-fluid row-category row-brand">   
-    <div class="container">
-        <div class="row row-category row-category-brand">        
-            <div class="col-md-12">
-               <div class="popular-brands carousel-wrapper">
-                    <header class="content-title">
-                        <div class="title-bg title-brand">
-                            <span class="category-1-icon"></span><h3><b>POPULAR BRANDS</b></h3>
+<?php if(count($homeContent['popularBrands']) > 0): ?>
+
+    <div class="row-fluid row-category row-brand">   
+        <div class="container">
+            <div class="row row-category row-category-brand">        
+                <div class="col-md-12">
+                <div class="popular-brands carousel-wrapper">
+                        <header class="content-title">
+                            <div class="title-bg title-brand">
+                                <span class="category-1-icon"></span><h3><b>POPULAR BRANDS</b></h3>
+                            </div>
+                        </header>
+                    
+                        <div class="carousel-controls div-slider-control slider-brand">
+                            <span class="pull-right brand-nav">
+                                <a id="popular-brand-prev"><i class="fa fa-angle-left fa-brand-prev"></i></a>
+                                <a id="popular-brand-next"><i class="fa fa-angle-right fa-brand-next"></i></a>
+                            </span>
                         </div>
-                    </header>
-                
-                    <div class="carousel-controls div-slider-control slider-brand">
-                        <span class="pull-right brand-nav">
-                            <a id="popular-brand-prev"><i class="fa fa-angle-left fa-brand-prev"></i></a>
-                            <a id="popular-brand-next"><i class="fa fa-angle-right fa-brand-next"></i></a>
-                        </span>
                     </div>
-                </div>
-                <div id="brand-items" class="popular-brands owl-carousel"> 
-                    <?php foreach($homeContent['popularCategory']['popularBrand'] as $brand) : ?>
-                    <div class="item">
-                        <div class="brand-container">
-                            <div class="span-brand">
-                                <a href="/advsrch?q_str=<?=$brand['brand']->getName()?>&category=1&seller=&location=&condition=&startprice=&endprice=">
-                                    <img class="brand-img" src="<?=$brand['image']['directory'] . $brand['image']['file']?>" />
-                                </a>
+                    <div id="brand-items" class="popular-brands owl-carousel"> 
+                        <?php foreach($homeContent['popularBrands'] as $brand) : ?>
+                        <div class="item">
+                            <div class="brand-container">
+                                <div class="span-brand">
+                                    <a href="/advsrch?q_str=<?=$brand['brand']->getName()?>&category=1&seller=&location=&condition=&startprice=&endprice=">
+                                        <img class="brand-img" src="<?=$brand['image']['directory'] . $brand['image']['file']?>" />
+                                    </a>
+                                </div>
                             </div>
                         </div>
+                        <?php endforeach; ?>
                     </div>
-                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+<?php endif; ?>
