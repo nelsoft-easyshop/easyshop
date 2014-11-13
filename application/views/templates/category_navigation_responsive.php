6490e3b8819bdd1dae4c942034415c29e0c375e8
Sam <samgavinio@easyshop.ph>
2014-11-13 13:43:44 +0800

    #1313 Used getAssetsDomain

--- application/views/templates/category_navigation_responsive.php
+++ application/views/templates/category_navigation_responsive.php
@@ -1,11 +1,11 @@
 <?php if(strtolower($environment) === "desktop"): ?>
     <div class="prod_categories">
-        <div class="nav_title">Categories <img src="/assets/images/img_arrow_down.png" class="drop-arrow"></div>
+        <div class="nav_title">Categories <img src="<?php echo getAssetsDomain(); ?>assets/images/img_arrow_down.png" class="drop-arrow"></div>
         <nav>
             <ul>
             <?php foreach ($parentCategory as $key => $value): ?>
                 <li  class="category_item">
-                    <p style="background:url('/assets/<?=$value->getImage(); ?>') no-repeat scroll left center #fff">
+                    <p style="background:url('<?php echo getAssetsDomain(); ?>assets/<?=$value->getImage(); ?>') no-repeat scroll left center #fff">
                         <a href="/category/<?=$value->getSlug(); ?>">
                             <?=$value->getName();?>
                         </a>
@@ -22,7 +22,7 @@
                 <h4 class="panel-title panel-title-category">
                     Categories
                     <a data-toggle="collapse" data-parent="#categories" href="#categories-body">
-                        <img class="pull-right" src="/assets/images/img_arrow_down.png">
+                        <img class="pull-right" src="<?php echo getAssetsDomain(); ?>assets/images/img_arrow_down.png">
                     </a>
                 </h4>
             </div>
