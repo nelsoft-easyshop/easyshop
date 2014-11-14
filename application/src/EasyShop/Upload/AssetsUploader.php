7e869092e89419b6c0df991256f980f34fda5d68
Sam <samgavinio@easyshop.ph>
2014-11-14 19:22:05 +0800

    #1313 Added method for uploading banner

--- application/src/EasyShop/Upload/AssetsUploader.php
+++ application/src/EasyShop/Upload/AssetsUploader.php
@@ -2,6 +2,8 @@
 
 namespace Easyshop\Upload;
 
+use \DateTime;
+
 /**
  * Easyshop Upload Class
  *
@@ -9,6 +11,30 @@ namespace Easyshop\Upload;
  */
 class AssetsUploader
 {
+
+    const MAX_IMAGE_HEIGHT = 1024;
+    
+    const MAX_IMAGE_WIDTH = 768;
+    
+    const MAX_ALLOWABLE_DIMENSION_PX = 5000;
+    
+    const MAX_ALLOWABLE_SIZE_KB = 5000; 
+    
+    const USER_BANNER_HEIGHT = 366;
+    
+    const USER_BANNER_WIDTH = 1475;
+    
+    const ALLOWABLE_IMAGE_MIME_TYPES = "gif|jpg|png|jpeg";
+
+    /**
+     * User image dimensions
+     * Defined as a class variable as PHP does not allow immutable arrays.
+     * Acess via the appropriate getter method.
+     *
+     * @var integer[]
+     */
+    private static $userImageDimensions = [['width' => 157, 'height' => 150], ['width' => 60, 'height' => 60 ]];
+
 
     /**
      * The AWS Uploader object
@@ -29,13 +55,6 @@ class AssetsUploader
      * @var Doctrine\ORM\EntityManager
      */
     private $entityManager;
-    
-    /**
-     * Image Utility
-     *
-     * @var EasyShop\Image\ImageUtility
-     */
-    private $imageUtility;
     
     /**
      * Config Loader
@@ -62,17 +81,25 @@ class AssetsUploader
      * Inject dependencies
      * @param \EasyShop\Upload\AwsUpload $awsUploader
      */
-    public function __construct($em, $awsUploader, $imageUtility, $configLoader, $uploadLibrary, $imageLibrary, $environment = 'DEVELOPMENT')
+    public function __construct($em, $awsUploader, $configLoader, $uploadLibrary, $imageLibrary, $environment = 'DEVELOPMENT')
     {
         $this->awsUploader = $awsUploader;
         $this->environment = $environment;
         $this->entityManager = $em;
-        $this->imageUtility = $imageUtility;
         $this->configLoader = $configLoader;
         $this->uploadLibrary = $uploadLibrary;
         $this->imageLibrary = $imageLibrary;
     }
     
+    /**
+     * Returns the user image dimensions
+     *
+     * @return integer[]
+     */
+    public function getUserImageDimensions()
+    {
+        return self::$userImageDimensions;
+    }
      
     /**
      * Uploads the temporary image directory structure into the appropriate location
@@ -123,100 +150,198 @@ class AssetsUploader
     
     
     /**
-     * 
+     * Uploads the user avatar. This method uploads the file in the $_FILES super global method
      *
+     * @param integer $memberId
+     * @param string $fieldName
+     * @param string[] $cropData
+     * @return mixed
      */
-    public function uploadUserAvatar($memberId, $cropData = array())
+    public function uploadUserAvatar($memberId, $fieldName = "userfile", $cropData = array())
     {
         $fileSuperGlobal = $_FILES;
         $member = $this->entityManager->getRepository('EasyShop\Entities\EsMember')->findOneBy(['idMember' => $memberId]);
-        $result = ['error' => array(), 'imagePath' => null];
+        
+        $result = ['error' => array(), 'member' => null];
         $filenames = ['usersize', '150x150', '60x60'];
+
         if($member){
             $imagePath = $member->getImgurl();
             $username = $member->getUsername();
             if(trim($imagePath) === "" || $imagePath === null){
                 $imagePath = $this->configLoader->getItem('image_path', 'config_loader').$memberId.'_'.$username;
             }
-            
-            if(strtolower($this->environment) === 'development'){
-                if(!is_dir($imagePath)){
-                    mkdir($imagePath,0755,true); 
-                }
-           
-                $pathInfo = pathinfo($fileSuperGlobal["userfile"]["name"]); // "userfile" is the form input field name
-                $fileExtension = $pathInfo['extension']; 
-                $filename = $filenames[0].'.'.$fileExtension;
 
-                $config['overwrite'] = true;
-                $config['file_name'] = $filename;
-                $config['upload_path'] = $imagePath; 
-                $config['allowed_types'] = 'gif|jpg|png|jpeg';
-                $config['max_size'] = '5000';
-                $config['max_width']  = '5000';
-                $config['max_height']  = '5000';
-                $this->uploadLibrary->initialize($config); 
-                if ( ! $this->uploadLibrary->do_upload()){
-                    $result['error'] = $this->uploadLibrary->display_errors();
-                }
-                else{
-                    $config = array();
-                    $config['image_library'] = 'gd2';
-                    $config['source_image'] = $imagePath.'/'.$filename;
-                    $config['new_image'] = $imagePath.'/'.$filename;
-                    $config['maintain_ratio'] = true;
-                    $imageData = $this->uploadLibrary->data(); 
-                    $this->imageLibrary->initialize($config);
-                    $this->imageLibrary->convert('png', true);
-                    
-                    // If cropped
-                    if($cropData['w'] > 0 && $cropData['h'] > 0){
-                        $config['width'] = $cropData['w'];
-                        $config['height'] = $cropData['h'];
-                        $config['x_axis'] = $cropData['x'];
-                        $config['y_axis'] = $cropData['y'];
-                        $this->imageLibrary->initialize($config);  
-                        $this->imageLibrary->image_process_gd('crop');
-                        $this->imageLibrary->clear();
-                        $config['x_axis'] = $config['y_axis'] = '';
-                        
-                        $imageData['image_width'] = $cropData['w'] - $cropData['x'];
-                        $imageData['image_height'] = $cropData['h'] - $cropData['y'];
-                    }
-                    
-                    if( $imageData['image_width'] > 1024 || $imageData['image_height'] > 768 ){     
-                        $config['width'] = 1024;
-                        $config['height'] = 768;
-                        $this->imageLibrary->initialize($config);  
-                        $this->imageLibrary->resize(); 
-                          $this->imageLibrary->clear();
-                    }
-                    
-                    $config['new_image'] =  $imagePath.'/'.$filenames[1].'.png';
-                    $config['source_image'] = $imagePath.'/'.$filenames[0].'.png';
-                    $config['width'] = 157;
-                    $config['height'] = 150;
-                    $this->imageLibrary->initialize($config);  
-                    $this->imageLibrary->resize();   
-                    $this->imageLibrary->clear();
-                    
-                    $config['new_image'] =  $imagePath.'/'.$filenames[2].'.png';
-                    $config['source_image'] = $imagePath.'/'.$filenames[0].'.png';
-                    $config['width'] = 60;
-                    $config['height'] = 60;
-                    $this->imageLibrary->initialize($config);  
-                    $this->imageLibrary->resize();
-                    $this->imageLibrary->clear();
-                    
-                }
+            if(!is_dir($imagePath)){
+                mkdir($imagePath,0755,true); 
+            }
+        
+            $pathInfo = pathinfo($fileSuperGlobal[$fieldName]["name"]); // "userfile" is the form input field name
+            $fileExtension = $pathInfo['extension']; 
+            $filename = $filenames[0].'.'.$fileExtension;
+
+            $config['overwrite'] = true;
+            $config['file_name'] = $filename;
+            $config['upload_path'] = $imagePath; 
+            $config['allowed_types'] = self::ALLOWABLE_IMAGE_MIME_TYPES;
+            $config['max_size'] = self::MAX_ALLOWABLE_SIZE_KB;
+            $config['max_width']  = self::MAX_ALLOWABLE_DIMENSION_PX;
+            $config['max_height']  = self::MAX_ALLOWABLE_DIMENSION_PX;
+            $this->uploadLibrary->initialize($config); 
+            
+            if ( ! $this->uploadLibrary->do_upload($fieldName)){
+                $result['error'] = $this->uploadLibrary->display_errors();
             }
             else{
-                $this->awsUploader->uploadFile($fileSuperGlobal["userfile"]["tmp_name"], $imagePath."/".$filename);
+                $config = array();
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = $imagePath.'/'.$filename;
+                $config['new_image'] = $imagePath.'/'.$filename;
+                $config['maintain_ratio'] = true;
+                $imageData = $this->uploadLibrary->data(); 
+                $this->imageLibrary->initialize($config);
+                $this->imageLibrary->convert('png', true);
+                
+                if($cropData['w'] > 0 && $cropData['h'] > 0){
+                    $config['width'] = $cropData['w'];
+                    $config['height'] = $cropData['h'];
+                    $config['x_axis'] = $cropData['x'];
+                    $config['y_axis'] = $cropData['y'];
+                    $this->imageLibrary->initialize($config);  
+                    $this->imageLibrary->image_process_gd('crop');
+                    $this->imageLibrary->clear();
+                    $config['x_axis'] = $config['y_axis'] = '';
+                    
+                    $imageData['image_width'] = $cropData['w'] - $cropData['x'];
+                    $imageData['image_height'] = $cropData['h'] - $cropData['y'];
+                }
+                
+                if( $imageData['image_width'] > self::MAX_IMAGE_HEIGHT || $imageData['image_height'] > self::MAX_IMAGE_WIDTH ){     
+                    $config['width'] = self::MAX_IMAGE_HEIGHT;
+                    $config['height'] = self::MAX_IMAGE_WIDTH;
+                    $this->imageLibrary->initialize($config);  
+                    $this->imageLibrary->resize(); 
+                    $this->imageLibrary->clear();
+                }
+                $imageDimensions = self::$userImageDimensions;
+                $config['new_image'] =  $imagePath.'/'.$filenames[1].'.png';
+                $config['source_image'] = $imagePath.'/'.$filenames[0].'.png';
+                $config['width'] = $imageDimensions[0]['width'];
+                $config['height'] = $imageDimensions[0]['height'];
+                $this->imageLibrary->initialize($config);  
+                $this->imageLibrary->resize();   
+                $this->imageLibrary->clear();
+                
+                $config['new_image'] =  $imagePath.'/'.$filenames[2].'.png';
+                $config['source_image'] = $imagePath.'/'.$filenames[0].'.png';
+                $config['width'] = $imageDimensions[1]['width'];
+                $config['height'] = $imageDimensions[1]['height'];
+                $this->imageLibrary->initialize($config);  
+                $this->imageLibrary->resize();
+                $this->imageLibrary->clear();
             }
-            print_r($result);
+      
+            if(strtolower($this->environment) !== 'development' || true){
+                try{
+                    $this->awsUploader->uploadFile($imagePath.'/'.$filenames[0].'.png', $imagePath."/".$filenames[0].".png");
+                    $this->awsUploader->uploadFile($imagePath.'/'.$filenames[1].'.png', $imagePath."/".$filenames[1].".png");
+                    $this->awsUploader->uploadFile($imagePath.'/'.$filenames[2].'.png', $imagePath."/".$filenames[2].".png");
+                } catch(\Exception $e){
+                    $result['error'][] = $e->getMessage();
+                }
+            }
             
-            
+            if(empty($result['error'])){
+                $member->setImgurl($imagePath);
+                $member->setIsHideAvatar(false);
+                $member->setLastmodifieddate(new DateTime('now'));
+                $this->entityManager->flush();
+            }   
         }
+        $result['member'] = $member;
+        return $result;
+    }
+    
+    
+    public function uploadUserBanner($memberId, $fieldName = "userfile", $cropData = array())
+    {
+        $fileSuperGlobal = $_FILES;
+        $member = $this->entityManager->getRepository('EasyShop\Entities\EsMember')->findOneBy(['idMember' => $memberId]);
+        
+        $result = ['error' => array(), 'member' => null];
+        if($member){
+            $imagePath = $member->getImgurl();
+            $username = $member->getUsername();
+            if(trim($imagePath) === "" || $imagePath === null){
+                $imagePath = $this->configLoader->getItem('image_path', 'config_loader').$memberId.'_'.$username;
+            }
+            if(!is_dir($imagePath)){
+                mkdir($imagePath,0755,true); 
+            }
+            
+            $pathInfo = pathinfo($fileSuperGlobal[$fieldName]["name"]); // "userfile" is the form input field name
+            $fileExtension = $pathInfo['extension']; 
+            $filename = 'banner.'.$fileExtension;
+            
+            $config['overwrite'] = true;
+            $config['file_name'] = $filename;
+            $config['upload_path'] = $imagePath; 
+            $config['allowed_types'] = self::ALLOWABLE_IMAGE_MIME_TYPES;
+            $config['max_size'] = self::MAX_ALLOWABLE_SIZE_KB;
+            $config['max_width']  = self::MAX_ALLOWABLE_DIMENSION_PX;
+            $config['max_height']  = self::MAX_ALLOWABLE_DIMENSION_PX;
+            $this->uploadLibrary->initialize($config); 
+            if ( ! $this->uploadLibrary->do_upload($fieldName)){
+                $result['error'] = $this->uploadLibrary->display_errors();
+            }
+            else{
+                $config = array();
+                $config['image_library'] = 'gd2';
+                $config['source_image'] = $imagePath.'/'.$filename;
+                $config['new_image'] = $imagePath.'/'.$filename;
+                $config['maintain_ratio'] = true;
+                $imageData = $this->uploadLibrary->data(); 
+                $this->imageLibrary->initialize($config);
+                $this->imageLibrary->convert('png', true);
+                
+                if($cropData['w'] > 0 && $cropData['h'] > 0){
+                    $config['width'] = $cropData['w'];
+                    $config['height'] = $cropData['h'];
+                    $config['x_axis'] = $cropData['x'];
+                    $config['y_axis'] = $cropData['y'];
+                    $this->imageLibrary->initialize($config);  
+                    $this->imageLibrary->image_process_gd('crop');
+                    $this->imageLibrary->clear();
+                    $config['x_axis'] = $config['y_axis'] = '';
+                }
+
+                $config['new_image'] = $imagePath.'/'.$filename;
+                $config['width'] = self::USER_BANNER_WIDTH;
+                $config['height'] = self::USER_BANNER_HEIGHT;
+                $this->imageLibrary->initialize($config);
+                $this->imageLibrary->resize(); 
+                $this->imageLibrary->clear();
+                
+                
+                if(strtolower($this->environment) !== 'development' || true){
+                    try{
+                        $this->awsUploader->uploadFile($imagePath.'/banner.png', $imagePath.'/banner.png');
+                    } catch(\Exception $e){
+                        $result['error'][] = $e->getMessage();
+                    }
+                }
+
+                if(empty($result['error'])){
+                    $member->setImgurl($imagePath);
+                    $member->setIsHideAvatar(false);
+                    $member->setLastmodifieddate(new DateTime('now'));
+                    $this->entityManager->flush();
+                }   
+            }
+        }
+        $result['member'] = $member;
+        return $result;
     }
     
 }
+
