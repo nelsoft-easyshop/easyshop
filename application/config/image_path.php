17d2615918be9b82f20bb3637c4cba47c88326ce
unknown <inon.baguio@yahoo.com>
2014-11-18 19:14:04 +0800

    #1723 added config value for product_img_directory and homeslider_img_directory

--- application/config/image_path.php
+++ application/config/image_path.php
@@ -1,5 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config = array(
 			'user_img_directory' => 'assets/user/',
-			'product_img_directory' => 'assets/product/',
+            'product_img_directory' => 'assets/product/',
+			'homeslider_img_directory' => 'assets/images/homeslider/',
 			);
