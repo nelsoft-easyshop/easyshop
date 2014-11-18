af5e2705f7cde86d08d09e48e5126a1b79b0265d
Sam <samgavinio@easyshop.ph>
2014-11-18 20:10:53 +0800

    Fixed problem with undefine  variable

--- application/controllers/memberpage.php
+++ application/controllers/memberpage.php
@@ -737,8 +737,10 @@ class Memberpage extends MY_Controller
             $memberId = $this->session->userdata('member_id');
             
             if($this->input->post('username') && $this->input->post('password')){
-                $authenticationResult = $this->serviceContainer['account_manager']->authenticateMember($this->input->post('username'), 
-                                                                                                       $this->input->post('password'));
+                $authenticateData = ['username' => $this->input->post('username'),
+                                     'password' => $this->input->post('password')];
+                $authenticationResult = $this->serviceContainer['account_manager']->authenticateMember($authenticateData['username'], 
+                                                                                                       $authenticateData['password']);
                 if( $authenticationResult['member'] === null || 
                     !empty($authenticationResult['errors']) ||  
                     (int)$authenticationResult['member']->getIdMember() !==  (int)$memberId
