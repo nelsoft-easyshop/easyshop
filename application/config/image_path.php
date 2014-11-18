91aa8aa418942e3cd9bb577ba600f553dfa83397
inon <inon.baguio@yahoo.com>
2014-11-19 06:58:50 +0800

    #1723 added config image directory for 'ads_img_directory'

--- application/config/image_path.php
+++ application/config/image_path.php
@@ -3,4 +3,5 @@ $config = array(
 			'user_img_directory' => 'assets/user/',
             'product_img_directory' => 'assets/product/',
 			'homeslider_img_directory' => 'assets/images/homeslider/',
+			'ads_img_directory' => 'assets/images/ads/',
 			);
