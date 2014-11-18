676f60700863c676ba5382375c252de9e30a2676
carlduazo <justine.duazo@easyshop.ph>
2014-11-18 11:54:58 +0800

    #1451 Created store setup include file

---
+++ application/views/pages/user/dashboard/dashboard-store-setup.php
@@ -0,0 +1,217 @@
+<div class="div-tab">
+    <div class="div-store-setup">
+        <p class="panel-setup-title">Store Name</p>
+        <div class="div-setup-content">
+            <div class="current-store-name">
+                <span class="setting-current-email">Mang Pedring Store</span> 
+                <button class="btn btn-setting-edit-btn" id="btn-edit-store-name">
+                   <i class="icon-edit"></i> Edit
+                </button>
+            </div>
+            <div class="edit-store-name">
+                <div class="row">
+                    <div class="col-md-5 col-inline-textbtn">
+                        <input type="text" class="text-info text-required" value="Mang Pedring Store"/>
+                        <!-- DISPLAY WHEN ERROR 
+                        <span class="val-error-icon-setup"><i class="fa fa-times"></i></span>
+                        <span class="val-error">Please enter at least 6 characters.</span>
+                        -->
+                        <!--DISPLAY WHEN OK-->
+                        <span class="val-success"><i class="fa fa-check"></i></span>
+                    </div>
+                    <div class="col-md-5">
+                        <button class="btn btn-setting-save-btn">
+                            Save changes
+                        </button>
+                        <button class="btn btn-setting-cancel-btn" id="cancel-edit-store-name">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+        </div>
+    </div>
+    <div class="div-store-setup">
+        <p class="panel-setup-title">Store Link</p>
+        <div class="div-setup-content">
+            <div class="current-store-url">
+                <span class="setting-current-email"><a href="#">https://easyshop.ph/mangpedring</a></span> 
+                <button class="btn btn-setting-edit-btn" id="btn-edit-store-url">
+                   <i class="icon-edit"></i> Edit
+                </button>
+            </div>
+            <div class="edit-store-url">
+                <div class="row">
+                    <div class="col-md-6 col-inline-textbtn">
+                         <span class="setting-edit-url">https://easyshop.ph/</span> 
+                         <input type="text" class="text-info-url text-required" value="Mang Pedring Store"/>
+                        <!-- DISPLAY WHEN ERROR 
+                        <span class="val-error-icon-pass"><i class="fa fa-times"></i></span>
+                        <span class="val-error">Please enter at least 6 characters.</span>
+                        -->
+                        <!--DISPLAY WHEN OK-->
+                        <span class="val-success-url"><i class="fa fa-check"></i></span>
+                    </div>
+                    <div class="col-md-5 col-inline-btn-url">
+                        <button class="btn btn-setting-save-btn">
+                            Save changes
+                        </button>
+                        <button class="btn btn-setting-cancel-btn" id="cancel-edit-store-url">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+        </div>
+    </div>
+    <div class="div-store-setup">
+        <p class="panel-setup-title">STORE QUICK RESPONSE CODE</p>
+        <div class="div-setup-content">
+            <button class="btn btn-default-1 btn-deactivate">
+               GENERATE MY STORE QR CODE
+            </button>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+        </div>
+    </div>
+    <div class="div-store-setup">
+        <p class="panel-setup-title">My Theme</p>
+        <div class="div-setup-content">
+            <div class="current-store-theme">
+                <form class="form-horizontal">
+                   <div class="form-group">
+                        <label class="col-sm-2 control-label">Color : </label>
+                        <div class="col-sm-5 col-xs-12 col-with-radio">
+                            <div class="current-color-choice" style="background: #ff893a;">
+                                Easyshop
+                            </div>
+                        </div>
+                        <span class="btn btn-setting-edit-btn" id="btn-edit-store-theme">
+                           <i class="icon-edit"></i> Edit
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div class="edit-store-theme form-horizontal">
+               <div class="form-group">
+                    <label class="col-sm-3 control-label">Pick Your Color : </label>
+                    <div class="col-sm-5 col-xs-12 col-with-radio">
+                        <div class="color-theming">
+                            <div class="current-color-drop" style="background: #ff893a;">
+                                <span class="color-name-drop">Easyshop</span>
+                                <i class="cd icon-dropdown pull-right"></i>
+                            </div>
+                            <div class="color-dropdown">
+                                <ul class="color-list">
+                                    <li style="background: #FF893A" id="color-item-1">EASYSHOP <i class="fa fa-check pull-right"></i></li>
+                                    <li style="background: #D24D57" id="color-item-2">Chestnut Rose</li>
+                                    <li style="background: #F22613" id="color-item-3">POMEGRANATE</li>
+                                    <li style="background: #FF0000" id="color-item-4">RED</li>
+                                    <li style="background: #D91E18" id="color-item-5">THUNDERBIRD</li>
+                                    <li style="background: #EF4836" id="color-item-6">FLAMINGO</li>
+                                    <li style="background: #C0392B" id="color-item-7">TALL POPPY</li>
+                                    <li style="background: #CF000F" id="color-item-8">MONZA</li>
+                                    <li style="background: #E74C3C" id="color-item-9">CINNABAR</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-5">
+                        <button class="btn btn-setting-edit-btn">
+                            Preview
+                        </button>
+                        <button class="btn btn-setting-save-btn">
+                            Save changes
+                        </button>
+                        <span class="btn btn-setting-cancel-btn" id="cancel-edit-store-theme">
+                            Cancel
+                        </span>
+                    </div>
+                </div>
+            </div>
+                
+            <div class="clear"></div>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+        </div>
+    </div>
+    <div class="div-store-setup">
+        <p class="panel-setup-title">CATEGORIES</p>
+        <div class="div-store-content">
+            <div class="current-store-cat">
+                <div class="form-horizontal">
+                   <div class="form-group">
+                        <label class="col-sm-3 control-label">Current Order : </label>
+                        <div class="col-sm-9 col-with-radio">
+                            <div class="div-cat">Clothing &amp; Accessories</div>
+                            <div class="div-cat">Electronics &amp; Gadgets</div>
+                            <div class="div-cat">Toys, Hobbies &amp; Collection</div>
+                            <br/>
+                            <button class="btn btn-setting-edit-btn" id="btn-edit-store-cat-new">
+                               <i class="fa fa-plus"></i> Add Category
+                            </button>
+                            <button class="btn btn-setting-edit-btn" id="btn-edit-store-cat">
+                               <i class="icon-edit"></i> Edit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="edit-store-cat form-horizontal">
+               <div class="form-group">
+                    <label class="col-sm-3 control-label">Reorder List : </label>
+                    <div class="col-sm-5 col-with-radio">
+                        <ul class="category_sort list">
+                            <li><i class="fa fa-sort"></i> Clothing &amp; Accessories</li>
+                            <li><i class="fa fa-sort"></i> Electronics &amp; Gadgets</li>
+                            <li><i class="fa fa-sort"></i> Toys, Hobbies &amp; Collection</li>
+                        </ul> 
+                        <p class="note-category">Drag the category items to reorder</p>
+                        <button class="btn btn-setting-save-btn">
+                           Save Changes
+                        </button>
+                        <button class="btn btn-setting-cancel-btn" id="cancel-edit-store-cat">
+                           Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="edit-store-cat-new">
+                <div class="row">
+                    <div class="col-md-5 col-inline-textbtn">
+                        <input type="text" class="text-info text-required" value="Professional Services"/>
+                         <!--DISPLAY WHEN ERROR -->
+                        <span class="val-error-icon-setup"><i class="fa fa-times"></i></span>
+                        <span class="val-error">Please enter at least 6 characters.</span>
+
+                        <!--DISPLAY WHEN OK
+                        <span class="val-success-url"><i class="fa fa-check"></i></span>-->
+                    </div>
+                    <div class="col-md-5">
+                        <button class="btn btn-setting-save-btn">
+                            Submit
+                        </button>
+                        <button class="btn btn-setting-cancel-btn" id="cancel-store-cat-new">
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+        </div>
+    </div>
+    
+</div>
