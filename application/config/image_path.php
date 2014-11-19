723fc8e29efcd1492689d735561adfbadf3f8344
unknown <inon.baguio@yahoo.com>
2014-11-19 14:37:31 +0800

    #1723 indented properly the config array values

--- application/config/image_path.php
+++ application/config/image_path.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config = array(
 			'user_img_directory' => 'assets/user/',
-            'product_img_directory' => 'assets/product/',
+                       'product_img_directory' => 'assets/product/',
 			'homeslider_img_directory' => 'assets/images/homeslider/',
 			'ads_img_directory' => 'assets/images/ads/',
 			);
