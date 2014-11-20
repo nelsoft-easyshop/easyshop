79364243a8f7e369d762816f9c1c11ff577380f3
Sam <samgavinio@easyshop.ph>
2014-11-20 19:35:50 +0800

    Updated config constant to 8.1.4

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '8.1.2';
+    $fileVersion = '8.1.4';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
