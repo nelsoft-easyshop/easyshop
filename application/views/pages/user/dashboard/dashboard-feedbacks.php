cd35ae2cce5152012dec2e0272fff7fedf4606da
carlduazo <justine.duazo@easyshop.ph>
2014-11-20 17:26:53 +0800

    #1451 Modified markup for feedbacks

--- application/views/pages/user/dashboard/dashboard-feedbacks.php
+++ application/views/pages/user/dashboard/dashboard-feedbacks.php
@@ -1,94 +1,30 @@
-<div class="feedback-from-seller">
-    <span class="trans-title">Feedback from Seller</span> 
-    <span class="count">1</span> 
-</div>
-<br/>
-<div class="feedback-from-seller-container">
-    <div class="row">
-        <div class="col-xs-2">
-            <center>
-                <div class="div-user-image">
-                    <a href="/justineduazo">
-                        <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
-                    <a/>
-                </div>
-                <p class="p-user-name">
-                    <a href="/justineduazo">
-                        justineduazo
-                    </a>
-                </p>
-                <p class="p-date-feedback">
-                    2nd September, 2014
-                </p>
-            </center>
-        </div>
-        <div class="col-xs-10 col-feedback-container" style="padding-left: 0px;">
-            <div class="panel panel-default panel-feedback-item">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <table>
-                            <tr>
-                                <td class="td-feedback-criteria">Item quality</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Communication</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Shipment time</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 4; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 4; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-xs-6 col-item-message">
-                        Test tets
-                    </div>
-                </div>
-            </div>
+ <div class="row">
+    
+    <div class="col-xs-12">
+        <div class="form-filter">
+            <label class="label-sort">Sort by:</label>
+            <select class="select-filter-item filter-feedbacks">
+                <option>All</option>
+                <option>Feedback as seller</option>
+                <option>Feedback as buyer</option>
+                <option>Feedback for seller</option>
+                <option>Feedback for buyer</option>
+            </select>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-xs-2">
-            <center>
-                <div class="div-user-image">
-                    <a href="/justineduazo">
-                        <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
-                    <a/>
+</div>
+<br/>
+ <div class="row">
+        <div class="col-md-2 col-feedback-user">
+            <div class="user-feeder">
+                <div class="user-image-container">
+                    <center class="center-image">
+                        <div class="div-user-image">
+                            <a href="/justineduazo">
+                                <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
+                            <a/>
+                        </div>
+                    </center>
                 </div>
                 <p class="p-user-name">
                     <a href="/justineduazo">
@@ -98,12 +34,36 @@
                 <p class="p-date-feedback">
                     2nd September, 2014
                 </p>
-            </center>
+            </div>
+            <table class="table-feed-mobile">
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="div-user-image">
+                                <a href="/justineduazo">
+                                    <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
+                                <a/>
+                            </div>
+                        </td>
+                        <td class="td-info-mobile">
+                            <p class="p-user-name">
+                                <a href="/justineduazo">
+                                    justineduazo
+                                </a>
+                            </p>
+                            <p class="p-date-feedback">
+                                2nd September, 2014
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <div class="col-xs-10 col-feedback-container" style="padding-left: 0px;">
+        <div class="col-md-10 col-feedback-container" style="padding-left: 0px;">
             <div class="panel panel-default panel-feedback-item">
                 <div class="row">
-                    <div class="col-xs-6">
+                    <div class="col-md-6">
+                        <p class="feedback-cat-title">Feedback as seller</p>
                         <table>
                             <tr>
                                 <td class="td-feedback-criteria">Item quality</td>
@@ -131,7 +91,6 @@
                                         <?php for($i = 0; $i < 5 - 5; $i++): ?>
                                             <i class="fa fa-star star-feed"></i>
                                         <?php endfor; ?>
-
                                     </span>
                                 </td>
                             </tr>
@@ -152,7 +111,7 @@
                             </tr>
                         </table>
                     </div>
-                    <div class="col-xs-6 col-item-message">
+                    <div class="col-md-6 col-item-message">
                         Test tets
                     </div>
                 </div>
@@ -180,213 +139,3 @@
         </ul>
     </center>
     <br/>
-</div>
-
-
-<div class="feedback-from-buyer">
-    <span class="trans-title">Feedback from Buyer</span> 
-    <span class="count">1</span> 
-</div>
-<br/>
-<div class="feedback-from-buyer-container">
-    <div class="row">
-        <div class="col-xs-2">
-            <center>
-                <div class="div-user-image">
-                    <a href="/justineduazo">
-                        <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
-                    <a/>
-                </div>
-                <p class="p-user-name">
-                    <a href="/justineduazo">
-                        justineduazo
-                    </a>
-                </p>
-                <p class="p-date-feedback">
-                    2nd September, 2014
-                </p>
-            </center>
-        </div>
-        <div class="col-xs-10 col-feedback-container" style="padding-left: 0px;">
-            <div class="panel panel-default panel-feedback-item">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <table>
-                            <tr>
-                                <td class="td-feedback-criteria">Item quality</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Communication</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Shipment time</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 4; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 4; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-xs-6 col-item-message">
-                        Test tets
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-2">
-            <center>
-                <div class="div-user-image">
-                    <a href="/justineduazo">
-                        <img src="/assets/images/products/nikon-logo.png" class="img-user-image"/>
-                    <a/>
-                </div>
-                <p class="p-user-name">
-                    <a href="/justineduazo">
-                        justineduazo
-                    </a>
-                </p>
-                <p class="p-date-feedback">
-                    2nd September, 2014
-                </p>
-            </center>
-        </div>
-        <div class="col-xs-10 col-feedback-container" style="padding-left: 0px;">
-            <div class="panel panel-default panel-feedback-item">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <table>
-                            <tr>
-                                <td class="td-feedback-criteria">Item quality</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Communication</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 5; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 5; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="td-feedback-criteria">Shipment time</td>
-                                <td>
-                                    <span>
-                                        <?php for($i = 0; $i < 4; $i++): ?>
-                                            <i class="fa fa-star star-feed star-active"></i>
-                                        <?php endfor; ?>
-                                        
-                                        <?php for($i = 0; $i < 5 - 4; $i++): ?>
-                                            <i class="fa fa-star star-feed"></i>
-                                        <?php endfor; ?>
-
-                                    </span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-xs-6 col-item-message">
-                        Test tets
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <center>
-        <ul class="pagination pagination-items" data-lastpage="1">
-            <li data-page="1" class="extremes previous">
-                <a href="javascript:void(0)">
-                    <span> « </span>
-                </a>
-            </li>
-            <li class="active individual" data-page="1">
-                <a href="javascript:void(0)">
-                    <span>1</span>
-                </a>
-            </li>
-            <li data-page="1" class="extremes next">
-                <a href="javascript:void(0)">
-                    <span> » </span>
-                </a>
-            </li>
-        </ul>
-    </center>
-    <br/>
-</div>
-
-<div class="feedback-for-seller">
-    <span class="trans-title">Feedback for Seller</span> 
-    <span class="count">0</span> 
-</div>
-<br/>
-<div class="feedback-for-seller-container">
-    <div class="jumbotron no-items">
-        <i class="icon-category"></i>No items for this category
-    </div>
-    <br/>
-</div>
-
-<div class="feedback-for-buyer">
-    <span class="trans-title">Feedback for Buyer</span> 
-    <span class="count">0</span> 
-</div>
-<br/>
-<div class="feedback-for-buyer-container">
-    <div class="jumbotron no-items">
-        <i class="icon-category"></i>No items for this category
-    </div>
-    <br/>
-</div>
