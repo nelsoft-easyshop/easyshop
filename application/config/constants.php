f6f293f159204a39727093fcd7d69087138e84e7
Sam <samgavinio@easyshop.ph>
2014-11-21 20:51:41 +0800

    Updated file constant to 8.1.12

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '8.1.11';
+    $fileVersion = '8.1.12';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
