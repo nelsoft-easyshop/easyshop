ea69f4046ab936557c796dc473e6024ed21c87aa
Sam <samgavinio@easyshop.ph>
2014-11-21 15:23:00 +0800

    Updated file version to 8.1.9

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '8.1.8';
+    $fileVersion = '8.1.9';
 }
 define('ES_FILE_VERSION',$fileVersion);
 
