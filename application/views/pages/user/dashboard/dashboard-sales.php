4860b710ce6c611abc8a1435cef4b145249a5eb4
carlduazo <justine.duazo@easyshop.ph>
2014-11-25 13:40:25 +0800

    #1451 Added hide columns ids

--- application/views/pages/user/dashboard/dashboard-sales.php
+++ application/views/pages/user/dashboard/dashboard-sales.php
@@ -32,70 +32,53 @@
         </div>
         <div class="col-sm-3 col-xs-12" >
             <div style="position: relative; width: 100%;">
-                <div class="hide-selection-box">
+                <div class="hide-selection-box" id="sc-selection-trigger">
                     Hide a column <i class="icon-dropdown pull-right"></i>
                 </div>
                 <div class="hide-selection-cont">
                     <div>
-                        <input type="checkbox" id="hide-date"/><label class="control-label">Hide date</label>
+                        <input type="checkbox" id="hide-date"/><label class="control-label" for="hide-date">Hide date</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="hide-trans"/><label class="control-label">Hide transaction ID</label>
+                        <input type="checkbox" id="hide-trans"/><label class="control-label" for="hide-trans">Hide transaction no.</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="hide-trans"/><label class="control-label">Base Price</label>
+                        <input type="checkbox" id="hide-base-price"/><label class="control-label" for="hide-base-price">Hide base price</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-quantity"/><label class="control-label" for="hide-quantity">Hide quantity</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-handling"/><label class="control-label" for="hide-handling">Hide handling fee</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-handling"/><label class="control-label" for="hide-handling">Hide total amount</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-es-charge"/><label class="control-label" for="hide-es-charge">Hide EasyShop charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-payment"/><label class="control-label" for="hide-payment">Hide payment charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-payment"/><label class="control-label" for="hide-payment">Hide payment charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-net"/><label class="control-label" for="hide-net">Hide net amount</label>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div style="overflow-y:no-scroll;overflow-x:scroll; height:auto; height: auto;">
-        <!--<table class="table table-total-sales table-striped"style="overflow-y:no-scroll;overflow-x:scroll; height:auto; height: auto;"> 
-            <thead>
-                <tr class="tr-orange">
-                    <th width="250px" class="me1"style="position: absolute; background: #355ba9; ">Product Name</th>
-                    <th width="250px" align="center" style="padding-left: 100px;">Date Bought</th>
-                    <th width="250px" align="center">Transaction ID</th>
-                    <th width="250px" align="center">Base Price</th>
-                    <th width="250px" align="center">Quantity</th>
-                    <th width="250px" align="center">Handling Fee</th>
-                    <th width="250px" align="center">Total</th>
-                    <th width="250px" align="center">Easyshop Charge</th>
-                    <th width="250px" align="center">Payment Method Charge</th>
-                    <th width="250px" align="right" style="text-align: right;">Net Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td width="250px" class="me1" style="position: absolute; background: #4370cd; height: auto; color: #fff;">Samsung Galaxy S5 dasdsadsadas asdasdasd </td>
-                    <td width="250px" align="center" style="padding-left: 100px; ">11/24/2014</td>
-                    <td width="250px" align="center">111111-1111-111111</td>
-                    <td width="250px" align="center">&#8369; 24, 000.00</td>
-                    <td width="250px" align="center">1</td>
-                    <td width="250px" align="center">&#8369; 100.00</td>
-                    <td width="250px" align="center">&#8369; 25 000.00</td>
-                    <td width="250px" align="center">&#8369; 20.00</td>
-                    <td width="250px" align="center">&#8369; 20.00</td>
-                    <td width="250px" align="right">&#8369; 24 440.00</td>
-                </tr>
-                
-                
-                <!-- DISPLAY THIS WHEN NO DATA RESULT
-                <tr>
-                    <td colspan="8" align="center">No data available</td>
-                </tr>
-            </tbody>
-        </table>
-        -->
-        
         <table class="table table-total-sales table-striped"> 
             <thead>
                 <tr class="tr-orange">
                     <th width="250px">Product Name</th>
-                    <th width="250px" align="center" id="th-date">Date Bought</th>
+                    <th width="100px" align="center" id="th-date">Date Bought</th>
                     <th width="250px" align="center" id="th-trans">Transaction ID</th>
                     <th width="250px" align="center" id="th-base-price">Base Price</th>
-                    <th width="250px" align="center" id="th-quantity">Quantity</th>
+                    <th width="100px" align="center" id="th-quantity">Quantity</th>
                     <th width="250px" align="center" id="th-handling">Handling Fee</th>
                     <th width="250px" align="center" id="th-total">Total</th>
                     <th width="250px" align="center" id="th-es-charge">Easyshop Charge</th>
@@ -105,11 +88,11 @@
             </thead>
             <tbody>
                 <tr>
-                    <td width="250px">Samsung Galaxy S5 dasdsadsadas asdasdasd </td>
-                    <td width="250px" align="center" class="td-date">11/24/2014</td>
+                    <td width="250px">Samsung Galaxy S5 </td>
+                    <td width="150px" align="center" class="td-date">11/24/2014</td>
                     <td width="250px" align="center" class="td-trans">111111-1111-111111</td>
                     <td width="250px" align="center" class="td-base-price">&#8369; 24, 000.00</td>
-                    <td width="250px" align="center" class="td-quantity">1</td>
+                    <td width="100px" align="center" class="td-quantity">1</td>
                     <td width="250px" align="center" class="td-handling">&#8369; 100.00</td>
                     <td width="250px" align="center" class="td-total">&#8369; 25 000.00</td>
                     <td width="250px" align="center" class="td-es-charge">&#8369; 20.00</td>
@@ -166,89 +149,73 @@
         </div>
         <div class="col-sm-3 col-xs-12" >
             <div style="position: relative; width: 100%;">
-                <div class="hide-selection-box">
+                <div class="hide-selection-box" id="sc-p-selection-trigger">
                     Hide a column <i class="icon-dropdown pull-right"></i>
                 </div>
-                <div class="hide-selection-cont">
+                <div class="hide-p-selection-cont">
                     <div>
-                        <input type="checkbox" id="hide-date"/><label class="control-label">Hide date</label>
+                        <input type="checkbox" id="hide-p-date"/><label class="control-label" for="hide-p-date">Hide date</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="hide-trans"/><label class="control-label">Hide transaction ID</label>
+                        <input type="checkbox" id="hide-p-trans"/><label class="control-label" for="hide-p-rans">Hide transaction no.</label>
                     </div>
                     <div>
-                        <input type="checkbox" id="hide-trans"/><label class="control-label">Base Price</label>
+                        <input type="checkbox" id="hide-p-base-price"/><label class="control-label" for="hide-p-base-price">Hide base price</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-quantity"/><label class="control-label" for="hide-p-quantity">Hide quantity</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-handling"/><label class="control-label" for="hide-p-handling">Hide handling fee</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-handling"/><label class="control-label" for="hide-p-handling">Hide total amount</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-es-charge"/><label class="control-label" for="hide-p-es-charge">Hide EasyShop charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-payment"/><label class="control-label" for="hide-p-payment">Hide payment charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-payment"/><label class="control-label" for="hide-p-payment">Hide payment charge</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hide-p-net"/><label class="control-label" for="hide-p-net">Hide net amount</label>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div style="overflow-y:no-scroll;overflow-x:scroll; height:auto; height: auto;">
-        <!--<table class="table table-total-sales table-striped"style="overflow-y:no-scroll;overflow-x:scroll; height:auto; height: auto;"> 
-            <thead>
-                <tr class="tr-orange">
-                    <th width="250px" class="me1"style="position: absolute; background: #355ba9; ">Product Name</th>
-                    <th width="250px" align="center" style="padding-left: 100px;">Date Bought</th>
-                    <th width="250px" align="center">Transaction ID</th>
-                    <th width="250px" align="center">Base Price</th>
-                    <th width="250px" align="center">Quantity</th>
-                    <th width="250px" align="center">Handling Fee</th>
-                    <th width="250px" align="center">Total</th>
-                    <th width="250px" align="center">Easyshop Charge</th>
-                    <th width="250px" align="center">Payment Method Charge</th>
-                    <th width="250px" align="right" style="text-align: right;">Net Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td width="250px" class="me1" style="position: absolute; background: #4370cd; height: auto; color: #fff;">Samsung Galaxy S5 dasdsadsadas asdasdasd </td>
-                    <td width="250px" align="center" style="padding-left: 100px; ">11/24/2014</td>
-                    <td width="250px" align="center">111111-1111-111111</td>
-                    <td width="250px" align="center">&#8369; 24, 000.00</td>
-                    <td width="250px" align="center">1</td>
-                    <td width="250px" align="center">&#8369; 100.00</td>
-                    <td width="250px" align="center">&#8369; 25 000.00</td>
-                    <td width="250px" align="center">&#8369; 20.00</td>
-                    <td width="250px" align="center">&#8369; 20.00</td>
-                    <td width="250px" align="right">&#8369; 24 440.00</td>
-                </tr>
-                
-                
-                <!-- DISPLAY THIS WHEN NO DATA RESULT
-                <tr>
-                    <td colspan="8" align="center">No data available</td>
-                </tr>
-            </tbody>
-        </table>
-        -->
         
         <table class="table table-total-sales table-striped"> 
             <thead>
                 <tr class="tr-orange">
                     <th width="250px">Product Name</th>
-                    <th width="250px" align="center" id="th-date">Date Bought</th>
-                    <th width="250px" align="center" id="th-trans">Transaction ID</th>
-                    <th width="250px" align="center" id="th-base-price">Base Price</th>
-                    <th width="250px" align="center" id="th-quantity">Quantity</th>
-                    <th width="250px" align="center" id="th-handling">Handling Fee</th>
-                    <th width="250px" align="center" id="th-total">Total</th>
-                    <th width="250px" align="center" id="th-es-charge">Easyshop Charge</th>
-                    <th width="27px" align="center" id="th-payment">Payment Method Charge</th>
-                    <th width="250px" align="right" style="text-align: right;" id="th-net">Net Amount</th>
+                    <th width="250px" align="center" id="th-p-date">Date Bought</th>
+                    <th width="250px" align="center" id="th-p-trans">Transaction ID</th>
+                    <th width="250px" align="center" id="th-p-base-price">Base Price</th>
+                    <th width="250px" align="center" id="th-p-quantity">Quantity</th>
+                    <th width="250px" align="center" id="th-p-handling">Handling Fee</th>
+                    <th width="250px" align="center" id="th-p-total">Total</th>
+                    <th width="250px" align="center" id="th-p-es-charge">Easyshop Charge</th>
+                    <th width="27px" align="center" id="th-p-payment">Payment Method Charge</th>
+                    <th width="250px" align="right" style="text-align: right;" id="th-p-net">Net Amount</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td width="250px">Samsung Galaxy S5 dasdsadsadas asdasdasd </td>
-                    <td width="250px" align="center" class="td-date">11/24/2014</td>
-                    <td width="250px" align="center" class="td-trans">111111-1111-111111</td>
-                    <td width="250px" align="center" class="td-base-price">&#8369; 24, 000.00</td>
-                    <td width="250px" align="center" class="td-quantity">1</td>
-                    <td width="250px" align="center" class="td-handling">&#8369; 100.00</td>
-                    <td width="250px" align="center" class="td-total">&#8369; 25 000.00</td>
-                    <td width="250px" align="center" class="td-es-charge">&#8369; 20.00</td>
-                    <td width="270px" align="center" class="td-payment">&#8369; 20.00</td>
-                    <td width="250px" align="right" class="td-net">&#8369; 24 440.00</td>
+                    <td width="250px" align="center" class="td-p-date">11/24/2014</td>
+                    <td width="250px" align="center" class="td-p-trans">111111-1111-111111</td>
+                    <td width="250px" align="center" class="td-p-base-price">&#8369; 24, 000.00</td>
+                    <td width="250px" align="center" class="td-p-quantity">1</td>
+                    <td width="250px" align="center" class="td-p-handling">&#8369; 100.00</td>
+                    <td width="250px" align="center" class="td-p-total">&#8369; 25 000.00</td>
+                    <td width="250px" align="center" class="td-p-es-charge">&#8369; 20.00</td>
+                    <td width="270px" align="center" class="td-p-payment">&#8369; 20.00</td>
+                    <td width="250px" align="right" class="td-p-net">&#8369; 24 440.00</td>
                 </tr>
                 
                 
