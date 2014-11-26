d8e7e5645e53180298b29cbe4f4b1467adeafeb7
carlduazo <justine.duazo@easyshop.ph>
2014-11-26 19:39:30 +0800

    #1772 Modified markup for featured seller

--- application/views/pages/home/featured.php
+++ application/views/pages/home/featured.php
@@ -15,7 +15,7 @@
                     <?php $defaultImage = $product['product']->getDefaultImage(); ?>
                     <?php $secondaryImage = $product['secondaryProductImage']; ?>
 
-                    <div class="item">
+                    <div class="item" style="position: relative;">
                         <center>
                             <a href="/item/<?php echo $productSlug ?>">
                             
@@ -45,9 +45,29 @@
                         <!-- End .item-image -->
                           
                         <div class="item-meta-container" align="left">
-                            <h3 class="item-name">
-                                <a href="/item/<?=$productSlug?>">
-                                    <?php echo html_escape((strlen($product['product']->getName())>20) ? substr_replace($product['product']->getName(), "...", 20): $product['product']->getName()) ;?>
+                           
+                            <h3 class="item-name" style="position: relative;">
+                                <?php
+                                    if(strlen($product['product']->getName())>20){
+                                ?>
+                                 <div class="tooltip-home">
+                                   <?php echo html_escape($product['product']->getName())?>
+                                 </div>
+                                <?php
+                                    }
+                                ?>
+                                 <a href="/item/<?=$productSlug?>">
+                                     <?php
+                                        if(strlen($product['product']->getName())>20){
+                                    ?>
+                                    <span style="width: 100%; display: inline-block; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+                                    <?php }
+                                        else{?>
+                                            <span>
+                                           <?php } ?>
+                                            
+                                        <?php echo html_escape((strlen($product['product']->getName())>20) ? substr_replace($product['product']->getName(), "...", 20): $product['product']->getName()) ;?>
+                                    </span>
                                 </a>
                             </h3>
                             <div class="item-meta-inner-container clearfix">
