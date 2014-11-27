b7f9a5b4ec80b5e2540dfe76fd6c2a93c42088bc
es-paulmodino <paul.modino@easyshop.ph>
2014-11-27 19:54:44 +0800

    #1931 add payment account

---
+++ application/views/pages/user/dashboard/dashboard-payment-account.php
@@ -0,0 +1,65 @@
+<div class="div-tab">
+    
+    <div class="div-tab-inner">
+        <div class="div-account-setting">
+            <p class="panel-setting-title">Payment Account</p>
+            <p class="p-note-setting">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            </p>
+            <button class="btn btn-default-3 add-bank-account"><i class="fa fa-plus"></i>Add a Bank Account</button>
+            <div class="select-bank">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12"><strong>Bank Name:</strong></div>
+                    <div class="col-xs-12 col-sm-12">
+                        <select class="text-info mrgn-top-10">
+                            <option>Please Select...</option>
+                            <option>Banco De Oro</option>
+                            <option>Bank of the Philippine Island</option>
+                            <optgroup>Metrobank</optgroup>
+                        </select>
+                    </div>
+                    <div class="clear pd-top-10">
+                        <div class="col-xs-12 col-sm-6">
+                            <strong>Account Name:</strong>
+                            <input type="text" class="text-info mrgn-top-10">
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <strong>Account Number:</strong>
+                            <input type="text" class="text-info mrgn-top-10">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 text-center bank-btns-con pd-top-30">
+                        <span class="btn btn-default-1 cancel-add-bank">Cancel</span>
+                        <input type="submit" class="btn btn-default-3" value="Save">
+                    </div> 
+                </div>
+            </div>
+        </div>
+       <div class="clear"></div>
+        <div class="bank-account-item">
+            <div class="col-xs-8">
+                <div class="pd-top-10">
+                    <span class="col-xs-12 col-sm-4 bank-label">Bank Name:</span>
+                    <span class="col-xs-12 col-sm-8">BDO - Banco de Oro</span>
+                </div>
+                <div class="clear pd-top-10">
+                    <span class="col-xs-12 col-sm-4 bank-label">Account Name:</span>
+                    <span class="col-xs-12 col-sm-8">Juan Dela Cruz</span>
+                </div>
+                <div class="clear pd-top-10">
+                    <span class="col-xs-12 col-sm-4 bank-label">Account Number:</span>
+                    <span class="col-xs-12 col-sm-8">1234567890</span>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="col-xs-4 bank-account-item-btns">
+                <span class="btn btn-default-3">Set as Default Account</span>
+                <span class="btn btn-default-1 mrgn-2 mrgn-top-10"><span class="icon-edit"></span> Edit</span>
+                <span class="btn btn-default-1 mrgn-top-10"><span class="icon-delete"></span> Delete</span>
+            </div>
+            <div class="clear"></div>
+        </div>
+        
+    </div>
+</div>
