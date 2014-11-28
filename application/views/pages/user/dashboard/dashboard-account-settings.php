1b8c804f02421edbee3c5efe1ff2d567d03d3cda
unknown <inon.baguio@yahoo.com>
2014-11-28 11:29:08 +0800

    #1850 added modal

--- application/views/pages/user/dashboard/dashboard-account-settings.php
+++ application/views/pages/user/dashboard/dashboard-account-settings.php
@@ -194,3 +194,20 @@
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                You have successfully deactivated your account.<br/><br/> If you have been unsatisfied with EasyShop as an e-Commerce platform, please send us an email at info@easyshop.ph to let us know how we can improve our website further.<br/><br/> Have a nice day!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
