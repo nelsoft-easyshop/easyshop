77925f6a7adab384711071e18410493c2f8ccec8
Sam <samgavinio@easyshop.ph>
2014-11-28 19:05:43 +0800

    #1954 added ckk

--- application/views/pages/web/christmas-promo.php
+++ application/views/pages/web/christmas-promo.php
@@ -58,7 +58,7 @@
         <section>
             <div class="container load-animate">
 
-                <h3 class="align-center padding-top-30">SELLERS</h3>
+                <h3 class="align-center padding-top-30">Hurry! Check out these selected items from our exclusive sellers!</h3>
 
                 <div class="row-fluid padding-top-30"> 
                     <div class="span4 box seller-list">
@@ -112,6 +112,13 @@
                         </a>
                     </div>
 
+                </div>
+                
+                <div class="span4 box seller-list">
+                    <a href="https://www.easyshop.ph/ckkmobile" target="_blank">
+                        <img src="/assets/images/promo-images/item8.jpg">
+                        <p>ckkmobile</p>
+                    </a>
                 </div>
 
             </div>
