7586a87899e1ed3280a73fe739cee91cb067a24a
carlduazo <justine.duazo@easyshop.ph>
2014-11-28 11:27:27 +0800

    #1850 Modified markup for activate account

--- application/views/pages/user/MemberPageAccountActivate.php
+++ application/views/pages/user/MemberPageAccountActivate.php
@@ -1,12 +1,48 @@
-Hello there!<br>
+<link type="text/css" href='/assets/css/new-dashboard.css' rel="stylesheet" media='screen'/>
+<div class="activateAccount">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-default panel-activate">
+                    <div class="panel-heading">Activate your account</div>
+                    <div class="panel-body">
+                        <p class="p-panel-body">
+                        Hello there!<br>
 
-We're really excited that you've decided to reactivate your account with us.<br>
-Just follow the instructions below and and you'll be done in a jiffy.<br>
-<br>
-Please enter your new password:
-<input type="password" id="new-password-txt"><br>
-Please enter your new password again:
-<input type="password" id="retype-password-txt"><br>
+                        We're really excited that you've decided to reactivate your account with us.<br>
+                        Just follow the instructions below and and you'll be done in a jiffy.<br>
+                        </p>
+                        <br>
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-sm-5 control-label">New password : </label>
+                                <div class="col-sm-7">
+                                    <input type = "password"  id="new-password-txt" class="text-info text-required" placeholder="Type your new password here">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-5 control-label">Confirm new password : </label>
+                                <div class="col-sm-7">
+                                    <input type = "password"  id="retype-password-txt" class="text-info text-required" placeholder="Confirm your new password here">
+                                </div>
+                            </div>
+                        </form>
+                        <div class="activateActions">
+                            <center>
+                                <input type="submit" class="btn btn-setting-save-btn" id="changePassBtn" name="changePassBtn"  value="Save Changes">
+                                <span class="btn btn-setting-cancel-btn" id="cancel-edit-password">
+                                    Cancel
+                                </span>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script>
     (function ($) {
@@ -27,4 +63,5 @@ Please enter your new password again:
             }
         });
     })(jQuery)
+
 </script>
