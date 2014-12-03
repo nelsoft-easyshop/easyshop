b1103e3ffe4edc48ddce625839c2736a9b66913a
Sam <samgavinio@easyshop.ph>
2014-12-03 15:44:11 +0800

    #2053 Added get all bank list

--- application/src/EasyShop/Repositories/EsBankInfoRepository.php
+++ application/src/EasyShop/Repositories/EsBankInfoRepository.php
@@ -4,8 +4,26 @@ namespace EasyShop\Repositories;
 
 use Doctrine\ORM\EntityRepository;
 use EasyShop\Entities\EsBankInfo;
+use Doctrine\ORM\Query;
 
 class EsBankInfoRepository extends EntityRepository
 {
-
+    
+    /**
+     * Returns all the banks
+     *
+     * @param boolean $getAsArray
+     * @return mixed
+     */
+    public function getAllBanks($getAsArray = false)
+    {
+        $em = $this->_em;       
+        $query = $em->createQueryBuilder()
+                    ->select('b')
+                    ->from('EasyShop\Entities\EsBankInfo','b')
+                    ->getQuery();
+        $banks =  $getAsArray ? $query->getResult(Query::HYDRATE_ARRAY) : $query->getResult();                 
+        return $banks;
+    }                          
+                              
 }
