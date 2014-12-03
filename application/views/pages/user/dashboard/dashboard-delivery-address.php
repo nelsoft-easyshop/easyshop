2e31e3f07e7fa1598676207643788b4dc067547f
unknown <inon.baguio@yahoo.com>
2014-12-03 14:34:32 +0800

    #2049 used $address object to display values insteaed of declared variables from the controller

--- application/views/pages/user/dashboard/dashboard-delivery-address.php
+++ application/views/pages/user/dashboard/dashboard-delivery-address.php
@@ -14,7 +14,7 @@
                 <div class="form-group">
                     <label class="col-sm-3 control-label">Consignee Name : </label>
                     <div class="col-sm-8">
-                        <input type="text" id="consigneeName" name="consignee" value="<?php echo html_escape($address->getConsignee())?>"  class="text-info" placeholder="Consignee name">
+                        <input type="text" id="consigneeName" name="consignee" value="<?php echo html_escape(($address) ? $address->getConsignee() : '')?>"  class="text-info" placeholder="Consignee name">
                         <div id="errorsDivConsignee" style="display:none;">
                             <span class="val-error-icon"><i class="fa fa-times"></i></span>
                             <span class="val-error" id="errorTextConsignee"></span>
@@ -25,7 +25,7 @@
                 <div class="form-group">
                     <label class="col-sm-3 control-label">Mobile Number : </label>
                      <div class="col-sm-8">
-                        <input type="text" class="text-info text-required" value="<?php echo html_escape($address->getMobile())?>" id="consigneeMobile" name="c_mobile" placeholder="Enter your 11 digit mobile number here">
+                        <input type="text" class="text-info text-required" value="<?php echo html_escape(($address) ? $address->getMobile() : '')?>" id="consigneeMobile" name="c_mobile" placeholder="Enter your 11 digit mobile number here">
                         <div id="errorsDivMobile" style="display:none;">
                             <span class="val-error-icon"><i class="fa fa-times"></i></span>
                             <span class="val-error" id="errorTextMobile"></span>
@@ -36,7 +36,7 @@
                 <div class="form-group">
                     <label class="col-sm-3 control-label">Telephone Number : </label>
                      <div class="col-sm-8">
-                        <input type="text" class="text-info text-required" value="<?php echo html_escape($address->getTelephone())?>" id="consigneeLandLine" name="c_telephone" placeholder="Enter your telephone number here">
+                        <input type="text" class="text-info text-required" value="<?php echo html_escape(($address) ? $address->getTelephone() : '')?>" id="consigneeLandLine" name="c_telephone" placeholder="Enter your telephone number here">
                         <!--DISPLAY WHEN OK-->
                         <!-- <span class="val-success"><i class="fa fa-check"></i></span> -->
                     </div>
@@ -59,7 +59,7 @@
                             <option value="0">--- Select City ---</option>
                             <option class="optionclone" value="" style="display:none;" disabled></option>
                             <?php if($consigneeCityId != '' && $consigneeStateRegionId != ''):?>
-                                <?php foreach($cityLookup[$consigneeStateRegionId] as $lockey=>$city):?>
+                                <?php foreach($cities[$consigneeStateRegionId] as $lockey=>$city):?>
                                     <option class="echo" value="<?php echo $lockey?>" <?php echo $consigneeCityId == $lockey ? "selected":"" ?> ><?php echo $city?></option>
                                 <?php endforeach;?>
                             <?php endif;?>
@@ -77,7 +77,7 @@
                     <div class="col-sm-8">
                         <input type="text" class="text-info text-address" id="deliveryAddress" value="Brgy. San Roque, Evergreen Village" name="c_address" placeholder="Enter your street address here">
                         <span class="span-label-address">Street Address</span>
-                        <input type="hidden" name="caddress_orig" value="<?php echo html_escape($consigneeAddress)?>">
+                        <input type="hidden" name="caddress_orig" value="<?php echo html_escape(($address) ? $address->getAddress : '')?>">
                         <div id="errorsDivStreetAddress" style="display:none;">
                             <span class="val-error-icon"><i class="fa fa-times"></i></span>
                             <span class="val-error" id="errorTextStreetAddress"></span>
@@ -99,10 +99,10 @@
                     <label class="col-sm-3 control-label">Map Location : </label>
                     <div class="col-sm-8 col-map">
                         <span>Location not marked</span> <span class="map-trigger">Mark on map <i class="fa fa-caret-down"></i></span>
-                        <input type="hidden" name="map_lat" id="map_clat" value="<?php echo $latitude;?>">
-                        <input type="hidden" name="map_lng" id="map_clng" value="<?php echo $longitude;?>">
-                        <input type="hidden" name="temp_lat" id="temp_clat" value="<?php echo $latitude;?>">
-                        <input type="hidden" name="temp_lng" id="temp_clng" value="<?php echo $longitude;?>">
+                        <input type="hidden" name="map_lat" id="map_clat" value="<?php ($address) ? $address->getLat() : '' ?>">
+                        <input type="hidden" name="map_lng" id="map_clng" value="<?php ($address) ? $address->getLng() : ''?>">
+                        <input type="hidden" name="temp_lat" id="temp_clat" value="<?php ($address) ? $address->getLat() : '' ?>">
+                        <input type="hidden" name="temp_lng" id="temp_clng" value="<?php ($address) ? $address->getLng() : ''?>">
                         <input type="hidden" name="current_lat" id="current_lat" value="">
                         <input type="hidden" name="current_lang" id="current_lang" value="">
                         <div class="map-container">
