414e4e08035be569cc47cb6881dea977c89d86c5
Sam <samgavinio@easyshop.ph>
2014-12-05 18:20:28 +0800

    #2119 Added stable sort in SortUtility

---
+++ application/src/EasyShop/Utility/SortUtility.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace EasyShop\Utility;
+
+class SortUtility
+{
+
+    /**
+     * Stable UASORT. Regular uasort returns inconsistent
+     * results for elements of equal value 
+     * 
+     * @param mixed $array
+     * @param function $cmp_function
+     * @source http://php.net/manual/en/function.uasort.php#114535
+     */
+    public function stableUasort(&$array, $cmp_function) 
+    {
+        if(count($array) < 2) {
+            return;
+        }
+        $halfway = count($array) / 2;
+        $array1 = array_slice($array, 0, $halfway, TRUE);
+        $array2 = array_slice($array, $halfway, NULL, TRUE);
+
+        $this->stableUasort($array1, $cmp_function);
+        $this->stableUasort($array2, $cmp_function);
+        if(call_user_func($cmp_function, end($array1), reset($array2)) < 1) {
+            $array = $array1 + $array2;
+            return;
+        }
+        $array = array();
+        reset($array1);
+        reset($array2);
+        while(current($array1) && current($array2)) {
+            if(call_user_func($cmp_function, current($array1), current($array2)) < 1) {
+                $array[key($array1)] = current($array1);
+                next($array1);
+            } else {
+                $array[key($array2)] = current($array2);
+                next($array2);
+            }
+        }
+        while(current($array1)) {
+            $array[key($array1)] = current($array1);
+            next($array1);
+        }
+        while(current($array2)) {
+            $array[key($array2)] = current($array2);
+            next($array2);
+        }
+        return;
+    }
+
+}
+
