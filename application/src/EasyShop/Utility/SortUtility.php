3ce73ee0411848e5b434785bdb9c74dbd24ac303
Sam <samgavinio@easyshop.ph>
2014-12-07 22:40:36 +0800

    #2119 updated PHP's stableUasort to follow easyshop coding standards.

--- application/src/EasyShop/Utility/SortUtility.php
+++ application/src/EasyShop/Utility/SortUtility.php
@@ -19,8 +19,8 @@ class SortUtility
             return;
         }
         $halfway = count($array) / 2;
-        $array1 = array_slice($array, 0, $halfway, TRUE);
-        $array2 = array_slice($array, $halfway, NULL, TRUE);
+        $array1 = array_slice($array, 0, $halfway, true);
+        $array2 = array_slice($array, $halfway, null, true);
 
         $this->stableUasort($array1, $cmp_function);
         $this->stableUasort($array2, $cmp_function);
@@ -28,14 +28,15 @@ class SortUtility
             $array = $array1 + $array2;
             return;
         }
-        $array = array();
+        $array = [];
         reset($array1);
         reset($array2);
         while(current($array1) && current($array2)) {
             if(call_user_func($cmp_function, current($array1), current($array2)) < 1) {
                 $array[key($array1)] = current($array1);
                 next($array1);
-            } else {
+            } 
+            else {
                 $array[key($array2)] = current($array2);
                 next($array2);
             }
