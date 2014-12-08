13a105b16c24337761d3ce38337ca59734f0d89b
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-12-08 15:49:33 +0800

    #2199 convert tabs to spaces

--- application/libraries/dragonpay.php
+++ application/libraries/dragonpay.php
@@ -1,8 +1,8 @@
 <?php
 class DragonPay {
-    	
-	public $merchantId  = 'EASYSHOP'; 
-	public $merchantPwd = 'UT78W5VQ';  
+        
+    public $merchantId  = 'EASYSHOP'; 
+    public $merchantPwd = 'UT78W5VQ';  
     public $url;
     public $ps;
 
@@ -20,23 +20,23 @@ class DragonPay {
         } 
     }
 
-	
-	public $errorCodes = array(
-			'000' => 'SUCCESS',
-			'101' => 'Ivalid payment gateway id',
-			'102' => 'Incorrect secret key',
-			'103' => 'Invalid reference number',
-			'104' => 'Unauthorized access',
-			'105' => 'Invalid token',
-			'106' => 'Currency not supported',
-			'107' => 'Transaction cancelled',
-			'108' => 'Insufficient funds',
-			'109' => 'Transaction limit exceeded',
-			'110' => 'Error in operation',
-			'111' => 'Invalid parameters',
-			'201' => 'Invalid Merchant Id',
-			'202' => 'Invalid Merchant Password'
-		);
+    
+    public $errorCodes = array(
+            '000' => 'SUCCESS',
+            '101' => 'Ivalid payment gateway id',
+            '102' => 'Incorrect secret key',
+            '103' => 'Invalid reference number',
+            '104' => 'Unauthorized access',
+            '105' => 'Invalid token',
+            '106' => 'Currency not supported',
+            '107' => 'Transaction cancelled',
+            '108' => 'Insufficient funds',
+            '109' => 'Transaction limit exceeded',
+            '110' => 'Error in operation',
+            '111' => 'Invalid parameters',
+            '201' => 'Invalid Merchant Id',
+            '202' => 'Invalid Merchant Password'
+        );
 
     public function __construct() {
         require_once('NuSOAP/lib/nusoap.php'); 
@@ -45,60 +45,60 @@ class DragonPay {
 
     function getProcessors()
     { 
-    	$client = new nusoap_client($this->url, 'wsdl');
-    	$result = $client->call('GetProcessors');
-    	return $result['GetProcessorsResult']['ProcessorInfo'];
+        $client = new nusoap_client($this->url, 'wsdl');
+        $result = $client->call('GetProcessors');
+        return $result['GetProcessorsResult']['ProcessorInfo'];
     }
 
     function getTxnToken($amount,$description,$email,$txnId)
     {
-    	$errorCodes = $this->errorCodes;
-    	 
-    	$ccy = 'PHP';
-    	$param = array(
-    		'merchantId' => $this->merchantId,
-    		'password' => $this->merchantPwd,
-    		'merchantTxnId' => $txnId,
-    		'amount' => $amount,
-    		'ccy' => $ccy,
-    		'description' => $description,
-    		'email' => $email,
-    		'mode'=>'1'
-    		);
-    	$client = new nusoap_client($this->url, 'wsdl');
-    	$result = $client->call('GetTxnToken',$param);
-    	$token = $result['GetTxnTokenResult'];
+        $errorCodes = $this->errorCodes;
+         
+        $ccy = 'PHP';
+        $param = array(
+            'merchantId' => $this->merchantId,
+            'password' => $this->merchantPwd,
+            'merchantTxnId' => $txnId,
+            'amount' => $amount,
+            'ccy' => $ccy,
+            'description' => $description,
+            'email' => $email,
+            'mode'=>'1'
+            );
+        $client = new nusoap_client($this->url, 'wsdl');
+        $result = $client->call('GetTxnToken',$param);
+        $token = $result['GetTxnTokenResult'];
 
-    	if(strlen($token) <= 3){
-    		return '{"e":"0","m":"'.$errorCodes[$token].'","c":"'.$token.'"}';
-    	}else{
-    		return '{"e":"1","m":"SUCCESS","c":"'.$token.'","tid":"'.$txnId.'","u":"'.$this->ps.'?tokenid='.$token.'&mode=7"}';
-    	}
+        if(strlen($token) <= 3){
+            return '{"e":"0","m":"'.$errorCodes[$token].'","c":"'.$token.'"}';
+        }else{
+            return '{"e":"1","m":"SUCCESS","c":"'.$token.'","tid":"'.$txnId.'","u":"'.$this->ps.'?tokenid='.$token.'&mode=7"}';
+        }
     }
 
-	function getStatus($txnId)
-	{ 
-		$param = array(
-        		'merchantId' => $this->merchantId,
-        		'password' => $this->merchantPwd,
-        		'txnId' => $txnId
-        	);
+    function getStatus($txnId)
+    { 
+        $param = array(
+                'merchantId' => $this->merchantId,
+                'password' => $this->merchantPwd,
+                'txnId' => $txnId
+            );
         $client = new nusoap_client($this->url, 'wsdl');
         $result = $client->call('GetTxnStatus',$param);
         return $result['GetTxnStatusResult'];
-	}
+    }
 
-	function voidTransaction($txnId)	
-	{
-		$param = array(
-        		'merchantId' => $this->merchantId,
-        		'password' => $this->merchantPwd,
-        		'merchantTxnId' => $txnId
-        	);
+    function voidTransaction($txnId)    
+    {
+        $param = array(
+                'merchantId' => $this->merchantId,
+                'password' => $this->merchantPwd,
+                'merchantTxnId' => $txnId
+            );
         $client = new nusoap_client($this->url, 'wsdl');
         $result = $client->call('CancelTransaction',$param);
         return $result['CancelTransactionResult'];
-	}
+    }
 }
 
 /* End of file dragonpay.php */
