e9f058f243c19a0466eb43a422edda280aa2e292
Sam <samgavinio@easyshop.ph>
2014-12-09 13:22:25 +0800

    #2229 added reserved keyword configs

---
+++ application/config/reserved.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = [
+    'easyshop',
+    'easydeal',
+    'easyshopping',
+];
+        
+/* End of file reserved.php */
+/* Location: ./application/config/reserved.php */
+
