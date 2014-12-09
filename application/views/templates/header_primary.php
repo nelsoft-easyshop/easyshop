cb7cc384283f137e6cb1d5ea90ee6344515cb08e
Sam <samgavinio@easyshop.ph>
2014-12-09 11:42:10 +0800

    #2201 Used tab get param instead of hash parameter

--- application/views/templates/header_primary.php
+++ application/views/templates/header_primary.php
@@ -96,13 +96,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                                                         <a href="/me">Dashboard</a>
                                                     </li>
                                                     <li>
-                                                        <a href="/me#pending">On-going Transactions</a>
+                                                        <a href="/me?tab=ongoing">On-going Transactions</a>
                                                     </li>
                                                     <li>
                                                         <a href="/?view=basic">Easyshop.ph</a>
                                                     </li>
                                                     <li class="nav-dropdown-border">
-                                                        <a href="/me#settings">Settings</a>
+                                                        <a href="/me?tab=settings">Settings</a>
                                                     </li>
                                                     <li class="nav-dropdown-border pos-rel">
                                                         <a href="/messages">Messages</a>
@@ -384,13 +384,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                                                                 <a href="/me">Dashboard</a>
                                                             </li>
                                                             <li>
-                                                                <a href="/me#pending">On-going Transactions</a>
+                                                                <a href="/me?tab=ongoing">On-going Transactions</a>
                                                             </li>
                                                             <li>
                                                                 <a href="/?view=basic">Easyshop.ph</a>
                                                             </li>
                                                             <li class="nav-dropdown-border">
-                                                                <a href="/me#settings">Settings</a>
+                                                                <a href="/me?tab=settings">Settings</a>
                                                             </li>
                                                             <li class="nav-dropdown-border pos-rel">
                                                                 <a href="/messages">Messages</a>
