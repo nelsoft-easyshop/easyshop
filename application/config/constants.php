30b1d894a3c3722495a3a056cac201454e8416a3
Sam <samgavinio@easyshop.ph>
2014-12-10 21:17:13 +0800

    Updated file cosntant to 9.0.4

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '9.0.3';
+    $fileVersion = '9.0.4';
 }
 define('ES_FILE_VERSION',$fileVersion);
 define('ES_ENABLE_CHRISTMAS_MODS', true);
