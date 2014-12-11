99df743959cb8b8bb6d1ffb880fee7cfd5b83d2f
Sam <samgavinio@easyshop.ph>
2014-12-11 19:39:18 +0800

    Updated file version to 9.0.5

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '9.0.4';
+    $fileVersion = '9.0.5';
 }
 define('ES_FILE_VERSION',$fileVersion);
 define('ES_ENABLE_CHRISTMAS_MODS', true);
