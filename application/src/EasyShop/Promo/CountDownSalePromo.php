02b225ffdcd9e038ba67369d4ece5fe14ec4f8f6
root <kurtwilkinson024@easyshop.ph>
2014-12-12 19:41:54 +0800

    #2425 sync merge

--- application/src/EasyShop/Promo/CountDownSalePromo.php
+++ application/src/EasyShop/Promo/CountDownSalePromo.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace EasyShop\Promo;
-
-use Symfony\Component\Validator\Constraints\DateTime;
+use Doctrine\ORM\Mapping\Entity;
+use EasyShop\Entities\EsProduct;
+use DateTime;
 
 class CountDownSalePromo extends AbstractPromo
 {
@@ -19,8 +20,8 @@ class CountDownSalePromo extends AbstractPromo
      * @var float
      *
      */
-    private static $percentagePerHour = 2.00;
-    
+    private static $percentagePerHour = 5.00;
+
     /**
      * Applies the count down sale calculations
      *
@@ -35,19 +36,20 @@ class CountDownSalePromo extends AbstractPromo
             $this->product->getPrice(),
             $this->startDateTime,
             $this->endDateTime,
-            $this->product->getDiscount()
+            $this->product->getDiscount(),
+            $this->option
         );
-
         $this->promoPrice = $promoData['promoPrice'];
         $this->isStartPromo = $promoData['isStartPromo'];
         $this->isEndPromo = $promoData['isEndPromo'];
+        $this->deleteExpired($this->product, $promoData['augmentedDiscount'], $this->isStartPromo);
         $this->persist();
-
         return $this->product;
     }
 
     /**
      * Calculates Promo Price and Checks if promo has started and if promo promo has ended.
+     * NOTE : Commented lines are formula use for the normal countdown sale
      * @param $price
      * @param $startDate
      * @param $endDate
@@ -61,11 +63,26 @@ class CountDownSalePromo extends AbstractPromo
         $dateToday = $date->getTimestamp();
         $startDateTime = $startDate->getTimestamp();
         $endDateTime = $endDate->getTimestamp();
-        $promoDetails = array(
+        $promoDetails = [
             'isStartPromo' => false,
             'isEndPromo' => false,
             'promoPrice' => $price
-        );
+        ];
+        $timeNow = strtotime(date('H:i:s', $dateToday));
+        $discountPerHour = 0;
+
+        foreach ($option as $promoPeriod) {
+            if ( strtotime($promoPeriod['start']) <= $timeNow ) {
+                $start_date = new DateTime($promoPeriod['start']);
+                if ($timeNow <= strtotime($promoPeriod['end'])) {
+                    $since_start = $start_date->diff(new DateTime(date('H:i:s', $dateToday)));
+                }
+                else {
+                    $since_start = $start_date->diff(new DateTime($promoPeriod['end']));
+                }
+                $discountPerHour = $discountPerHour + (($since_start->h + 1) * $promoPeriod['discount']);
+            }
+        }
 
         if (($dateToday < $startDateTime) || ($endDateTime < $dateToday)) {
             $diffHours = 0;
@@ -79,12 +96,26 @@ class CountDownSalePromo extends AbstractPromo
             $promoDetails['isStartPromo'] = true;
         }
 
-        $promoPrice = $price - (($diffHours * self::$percentagePerHour / 100.0) * $price);
+        // $promoPrice = $price - (($diffHours * self::$percentagePerHour / 100.0) * $price);
+        $promoPrice = $price - (($discountPerHour / 100.0) * $price);
         $promoPrice = ($promoPrice <= 0) ? 0.01 : $promoPrice;
         $promoDetails['promoPrice'] = $promoPrice;
-        $promoDetails['isEndPromo'] = ($dateToday > $endDateTime) ? true : false;
-
+        $promoDetails['isEndPromo'] = ($dateToday > $endDateTime);
+        // $promoDetails['augmentedDiscount'] = $diffHours * self::$percentagePerHour;
+        $promoDetails['augmentedDiscount'] = $discountPerHour;
         return $promoDetails;
     }
 
+    /**
+     * Soft delete product if reached the max allowable discount
+     * @param $product
+     * @param $augmentedDiscount
+     * @param $isStartPromo
+     */
+    private function deleteExpired($product, $augmentedDiscount, $isStartPromo)
+    {
+        if ( (int) $augmentedDiscount >= (int) $product->getDiscount() && (int) $product->getDiscount() !== 0 && $isStartPromo ) {
+            $product->setIsDelete(EsProduct::DELETE);
+        }
+    }
 }
