6dfd984e7be0ad685a6ef18011fdbc47ad29ddbc
root <kurtwilkinson024@easyshop.ph>
2014-12-12 10:26:06 +0800

    #2396 promo feature

---
+++ application/controllers/promo/TwelveDaysOfChristmas.php
@@ -0,0 +1,29 @@
+<?php
+
+class TwelveDaysOfChristmas extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->em = $this->serviceContainer['entity_manager'];
+        $this->promoManager = $this->serviceContainer['promo_manager'];
+    }
+
+    /**
+     * Retrieve page for twelve Days Of Christmas Promo
+     */
+    public function twelveDaysOfChristmasPromo()
+    {
+        $product = $this->promoManager->getActiveDataForTwelveDaysOfChristmasByDate(new DateTime('now'));
+        if ($product) {
+            $this->promoManager->hydratePromoData($product);
+            $promoData = [
+                'product' => $product,
+                'image' => $this->em->getRepository('EasyShop\Entities\EsProductImage')->getDefaultImage($product->getIdProduct())
+            ];
+        }
+
+        $this->load->view('/pages/web/christmas-promo', $promoData);
+    }
+}
--- application/src/EasyShop/Promo/PromoManager.php
+++ application/src/EasyShop/Promo/PromoManager.php
@@ -3,6 +3,7 @@
 namespace EasyShop\Promo;
 
 use EasyShop\ConfigLoader\ConfigLoader as ConfigLoader;
+use EasyShop\Entities\EsProduct;
 use EasyShop\Entities\EsPromo;
 use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\DateTime;
@@ -11,7 +12,7 @@ use Symfony\Component\Validator\Constraints\DateTime;
  * PromoManager Class
  *
  */
-class PromoManager 
+class PromoManager
 {
     /**
      * Codeigniter Config Loader
@@ -26,7 +27,7 @@ class PromoManager
      * @var mixed
      */
     private $promoConfig = array();
-    
+
     /**
      * Entity Manager instance
      *
@@ -45,11 +46,11 @@ class PromoManager
         $this->promoConfig = $configLoader->getItem('promo', 'Promo');
         $this->em = $em;
     }
-    
+
     /**
      * Returns the promo configuration array
      *
-     * @param integer $type 
+     * @param integer $type
      * @return mixed
      */
     public function getPromoConfig($type = null)
@@ -67,7 +68,7 @@ class PromoManager
      *
      * @param Product $product
      *
-     */   
+     */
     public function hydratePromoData(&$product)
     {
         $product->setOriginalPrice($product->getPrice());
@@ -76,7 +77,7 @@ class PromoManager
             $promoType = $product->getPromoType();
             if(isset($this->promoConfig[$promoType])){
                 if(isset($this->promoConfig[$promoType]['implementation']) &&
-                   trim($this->promoConfig[$promoType]['implementation']) !== ''
+                    trim($this->promoConfig[$promoType]['implementation']) !== ''
                 ){
                     $promoImplementation = $this->promoConfig[$promoType]['implementation'];
                     $promoOptions = $this->promoConfig[$promoType]['option'];
@@ -84,7 +85,7 @@ class PromoManager
                     $promoObject->setOptions($promoOptions);
                     $product = $promoObject->apply();
                 }
-              
+
             }
         }
         else{
@@ -98,7 +99,7 @@ class PromoManager
         if($product->getOriginalPrice() > 0){
             $percentage = 100.00 * ($product->getOriginalPrice() - $product->getFinalPrice())/$product->getOriginalPrice();
         }
-        $product->setDiscountPercentage($percentage); 
+        $product->setDiscountPercentage($percentage);
     }
 
     /**
@@ -113,7 +114,7 @@ class PromoManager
         if(!$productDetails){
             return NULL;
         }
-        
+
         $price = $productDetails['price'];
         $discount = $productDetails['discount'];
         $isPromote = $productDetails['is_promote'];
@@ -122,7 +123,7 @@ class PromoManager
         $promoType = $productDetails['promo_type'];
 
         $startDate = date_create($startDate);
-        $endDate = date_create($endDate); 
+        $endDate = date_create($endDate);
 
         $promoPrice = $price;
         if (intval($isPromote) === 1) {
@@ -149,44 +150,44 @@ class PromoManager
     /**
      * Returns the product checkout limit based on a promo.
      * This method does not take into consideration which user will buy the
-     * the product. Checking if an item can be bought by a particular user is 
+     * the product. Checking if an item can be bought by a particular user is
      * the responsibility of a separate service. Also note that the option
      * quantity limit will take precedence over any other quantity limits.
      *
      * @param Product $product
      * @return integer
-     * 
+     *
      */
     public function getPromoQuantityLimit($product)
     {
         $promoQuantityLimit = PHP_INT_MAX;
         $isPromoActive = $product->getStartPromo();
-        
-        if($product->getStartPromo() && $product->getIsPromote()){  
-                $promoConfig = $this->promoConfig[$product->getPromoType()];
-                $promoOptions = $promoConfig['option'];
-                $timeNow = strtotime(date('H:i:s'));
-                $startDatetime = $product->getStartdate()->getTimestamp();
-                $endDatetime = $product->getEnddate()->getTimestamp();
-                
-                foreach($promoOptions as $option ){
-                    if((strtotime($option['start']) <= $timeNow) && (strtotime($option['end']) > $timeNow)){
-                        $promoQuantityLimit = $option['purchase_limit'];
-                        $startDatetime = date('Y-m-d',strtotime($startDatetime)).' '.$option['start'];
-                        $endDatetime = date('Y-m-d',strtotime($endDatetime)).' '.$option['end'];
-                        break;
-                    }
-                }
 
-                if(isset($opt['puchase_limit'])){
-                    $soldCount = $this->em->getRepository('EasyShop\Entities\EsOrderProduct')
-                                        ->getSoldCount($product->product_id, $startDatetime, $endDatetime);
-                    $promoQuantityLimit = $option['purchase_limit'] - $soldCount;
-                    $promoQuantityLimit = ($promoQuantityLimit >= 0) ? $promoQuantityLimit : 0;
+        if($product->getStartPromo() && $product->getIsPromote()){
+            $promoConfig = $this->promoConfig[$product->getPromoType()];
+            $promoOptions = $promoConfig['option'];
+            $timeNow = strtotime(date('H:i:s'));
+            $startDatetime = $product->getStartdate()->getTimestamp();
+            $endDatetime = $product->getEnddate()->getTimestamp();
+
+            foreach($promoOptions as $option ){
+                if((strtotime($option['start']) <= $timeNow) && (strtotime($option['end']) > $timeNow)){
+                    $promoQuantityLimit = $option['purchase_limit'];
+                    $startDatetime = date('Y-m-d',strtotime($startDatetime)).' '.$option['start'];
+                    $endDatetime = date('Y-m-d',strtotime($endDatetime)).' '.$option['end'];
+                    break;
                 }
-                else{
-                    $promoQuantityLimit = $promoConfig['purchase_limit'];
-                }
+            }
+
+            if(isset($opt['puchase_limit'])){
+                $soldCount = $this->em->getRepository('EasyShop\Entities\EsOrderProduct')
+                    ->getSoldCount($product->product_id, $startDatetime, $endDatetime);
+                $promoQuantityLimit = $option['purchase_limit'] - $soldCount;
+                $promoQuantityLimit = ($promoQuantityLimit >= 0) ? $promoQuantityLimit : 0;
+            }
+            else{
+                $promoQuantityLimit = $promoConfig['purchase_limit'];
+            }
 
         }
 
@@ -271,6 +272,28 @@ class PromoManager
         $this->em->flush();
 
         return (int) $memberId === (int) $promo->getMemberId();
+    }
+
+    /**
+     * Get active product for twelve days of Christmas promo
+     * @param $date
+     * @return EsProduct
+     */
+    public function getActiveDataForTwelveDaysOfChristmasByDate($date)
+    {
+        $qb = $this->em->createQueryBuilder();
+        $query = $qb->select('tbl_product')
+            ->from('EasyShop\Entities\EsProduct', 'tbl_product')
+            ->where(':dateTime >= tbl_product.startdate')
+            ->andWhere(':dateTime <= tbl_product.enddate')
+            ->andWhere('tbl_product.isPromote = :isPromote')
+            ->andWhere('tbl_product.promoType = 1')
+            ->setParameter('dateTime', $date)
+            ->setParameter('isPromote', EsProduct::PRODUCT_IS_PROMOTE_ON)
+            ->getQuery();
+        $product = $query->getOneOrNullResult();
+
+        return $product;
     }
 }
 
