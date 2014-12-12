732d9739631669c2fdf371ee80084c714b4f52cf
unknown <inon.baguio@yahoo.com>
2014-12-12 15:49:17 +0800

    #2361 removed unused echo

--- application/views/pages/user/dashboard/dashboard-delivery-address.php
+++ application/views/pages/user/dashboard/dashboard-delivery-address.php
@@ -42,7 +42,7 @@
                     <div class="form-group">
                         <label class="col-sm-3 control-label">Telephone Number : </label>
                          <div class="col-sm-8">
-                            <input type="text" class="text-info text-required" value="<?php echo html_escape(($address) ? $address->getTelephone() : '')?>" id="consigneeLandLine" name="c_telephone" placeholder="Enter your telephone number here">
+                            <input type="text" class="text-info text-required" value="" id="consigneeLandLine" name="c_telephone" placeholder="Enter your telephone number here">
                             <div id="errorsDivTelephone" style="display:none;">
                                 <span class="val-error-icon"><i class="fa fa-times"></i></span>
                                 <span class="val-error" id="errorTextTelephone"></span>
@@ -53,31 +53,21 @@
                      <div class="form-group">
                         <label class="col-sm-3 control-label">Address : </label>
                         <div class="col-sm-4">
-                            <select class="text-info text-address address_dropdown stateregionselect" id="deliver_stateregion" name="c_stateregion" data-status="<?php echo $consigneeStateRegionId?>">
+                            <select class="text-info text-address address_dropdown stateregionselect" id="deliver_stateregion" name="c_stateregion">
                                 <option value="0">--Select State/Region--</option>
-                                <?php foreach($stateRegionLists as $srkey=>$stateregion):?>
-                                    <option class="echo" value="<?php echo $srkey?>" <?php echo $consigneeStateRegionId == $srkey ? "selected":"" ?>><?php echo $stateregion?></option>
-                                <?php endforeach;?>
+   
                             </select>
-                            <input type="hidden" name="cstateregion_orig" value="<?php echo $consigneeStateRegionId?>">
                             <span class="span-label-address">State/Region</span>
                         </div>
                         <div class="col-sm-4 col-city">
-                            <select class="text-info text-address address_dropdown cityselect stateregionselect" id="delivery_city" name="c_city" data-status="<?php echo $consigneeCityId?>">
+                            <select class="text-info text-address address_dropdown cityselect" id="delivery_city" name="c_city">
                                 <option value="0">--- Select City ---</option>
                                 <option class="optionclone" value="" style="display:none;" disabled></option>
-                                <?php if($consigneeCityId != '' && $consigneeStateRegionId != ''):?>
-                                    <?php foreach($cityLookup[$consigneeStateRegionId] as $lockey=>$city):?>
-                                        <option class="echo" value="<?php echo $lockey?>" <?php echo $consigneeCityId == $lockey ? "selected":"" ?> ><?php echo $city?></option>
-                                    <?php endforeach;?>
-                                <?php endif;?>
+        
                             </select>
-                            <input type="hidden" name="ccity_orig" value="<?php echo $consigneeCityId?>">                        
                             <span class="span-label-address">City</span>
                         </div>
-                        <div class="col-sm-4 col-city">
-                            <input type="hidden" name="c_country" value="<?php echo $countryId?>">                        
-                        </div>                    
+                
                     </div>
                     
                     <div class="form-group">
@@ -85,7 +75,6 @@
                         <div class="col-sm-8">
                             <input type="text" class="text-info text-address" id="deliveryAddress" value="Brgy. San Roque, Evergreen Village" name="c_address" placeholder="Enter your street address here">
                             <span class="span-label-address">Street Address</span>
-                            <input type="hidden" name="caddress_orig" value="<?php echo html_escape(($address) ? $address->getAddress() : '')?>">
                             <div id="errorsDivStreetAddress" style="display:none;">
                                 <span class="val-error-icon"><i class="fa fa-times"></i></span>
                                 <span class="val-error" id="errorTextStreetAddress"></span>
@@ -96,22 +85,12 @@
                     <div class="form-group">
                         <label class="col-sm-3 control-label">Map Location : </label>
                         <div class="col-sm-8 col-map">
-                            <span id="locationMarkedText">
-                                <?php if($address):?>
-                                    <?php if((int)$address->getLat() === 0 && (int)$address->getLng() === 0): ?>
-                                        <?php echo "Location not marked" ?>
-                                    <?php else: ?>
-                                        <?php echo "Location Marked" ?>
-                                    <?php endif; ?>
-                                <?php else: ?>
-                                    <?php echo "Location not marked" ?>
-                                <?php endif; ?>
-                            </span>
+                            <span id="locationMarkedText"></span>
                             <span class="map-trigger">Mark on map <i class="fa fa-caret-down"></i></span>
-                            <input type="hidden" name="map_lat" id="map_clat" value="<?php echo ($address) ? $address->getLat() : '' ?>">
-                            <input type="hidden" name="map_lng" id="map_clng" value="<?php echo ($address) ? $address->getLng() : ''?>">
-                            <input type="hidden" name="temp_lat" id="temp_clat" value="<?php echo ($address) ? $address->getLat() : '' ?>">
-                            <input type="hidden" name="temp_lng" id="temp_clng" value="<?php echo ($address) ? $address->getLng() : ''?>">
+                            <input type="hidden" name="map_lat" id="map_clat" value="">
+                            <input type="hidden" name="map_lng" id="map_clng" value="">
+                            <input type="hidden" name="temp_lat" id="temp_clat" value="">
+                            <input type="hidden" name="temp_lng" id="temp_clng" value="">
                             <input type="hidden" name="current_lat" id="current_lat" value="">
                             <input type="hidden" name="current_lang" id="current_lang" value="">
                             <div class="map-container">
@@ -145,3 +124,5 @@
         </div>
     </div>
 </div>
+
+
