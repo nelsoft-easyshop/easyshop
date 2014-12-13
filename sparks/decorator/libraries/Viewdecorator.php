b2b725219ed5ee72067ef76a2772a0fccf181e14
Sam <samgavinio@easyshop.ph>
2014-12-13 17:33:53 +0800

    #2415 added author and source docs

--- sparks/decorator/libraries/Viewdecorator.php
+++ sparks/decorator/libraries/Viewdecorator.php
@@ -6,6 +6,9 @@
  * Renamed to Viewdecorator due to issues with the CI prefix as the
  * spark was written for an older version of CI.
  *
+ * @source https://github.com/ccschmitz/codeigniter-decorator
+ * @author Chris Schmitz <ccschmitz@gmail.com>
+ *
  */
 class Viewdecorator extends MY_Controller 
 {
