1da2356942b92c9444e21dc9b2ca584387c27914
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-12-17 21:38:04 +0800

    #2530 modify language file

--- application/language/english/resources_lang.php
+++ application/language/english/resources_lang.php
@@ -166,14 +166,14 @@ $lang['update_product'] = [
     ];
 
 $lang['update_transaction'] = [
-        'buy' => 'you have purchased an item with :phrase',
+        'buy' => 'You have purchased an item with :phrase',
         'add_ship_detail' => 'You have added shipment details on order :phrase ',
         'edit_ship_detail' => 'You have modified shipment details on order :phrase',
         'item_received' => '',
-        'item_cancel' => '',
         'item_reject' => '',
         'item_unreject' => '',
-        'completed' => '',
+        'item_cancel' => 'You cancelled',
+        'completed' => 'You marked as completed',
     ];
 ?>
 
---
+++ application/src/EasyShop/Doctrine/Listeners/EsOrderProductListener.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace EasyShop\Doctrine\Listeners;
+
+use Doctrine\ORM\Events;
+use Doctrine\Common\EventSubscriber as EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use EasyShop\Entities\EsOrderProduct as EsOrderProduct;
+use EasyShop\Entities\EsOrderProductStatus as EsOrderProductStatus;
+use EasyShop\Entities\EsActivityType as EsActivityType;
+
+class EsOrderListener implements EventSubscriber
+{
+    protected $changeSet = [];
+
+    /**
+     * Activity Manager Instance
+     *
+     * @var Easyshop\Activity\ActivityManager
+     */
+    private $activityManager;
+
+    /**
+     * Language Loader Instance
+     *
+     * @var Easyshop\LanguageLoader\LanguageLoader
+     */
+    private $languageLoader;
+
+    /**
+     * Constructor.
+     * 
+     */
+    public function __construct($activityManager, $languageLoader)
+    {
+        $this->activityManager = $activityManager;
+        $this->languageLoader = $languageLoader;
+    }
+
+    /**
+     * The preUpdate event occurs before the database update operations to entity data.
+     * 
+     * @param  LifecycleEventArgs $event
+     */
+    public function preUpdate(LifecycleEventArgs $event)
+    {
+        $em = $event->getEntityManager();
+        $entity = $event->getEntity();
+        if ( !$entity instanceOf EsOrderProduct) {
+            return;
+        }
+
+        if ($event->hasChangedField('status')) {
+            $this->changeSet['status'] = $entity->getStatus();
+        }
+    }
+
+    /**
+     * The postUpdate event occurs after the database update operations to entity data.
+     * 
+     * @param  LifecycleEventArgs $event
+     */
+    public function postUpdate(LifecycleEventArgs $event)
+    {
+        $this->saveActivity($event);
+    }
+
+    /**
+     * Trigger save activity
+     * @param  LifecycleEventArgs $event
+     */
+    private function saveActivity(LifecycleEventArgs $event)
+    {
+        $em = $event->getEntityManager();
+        $entity = $event->getEntity();
+        $phrase = "";
+        if ( $entity instanceOf EsOrderProduct) {
+            if(count($this->changeSet) > 0){
+                $member = null;
+                $status = $entity->getStatus()->getIdOrderProductStatus();
+                $activityType = $em->getRepository('EasyShop\Entities\EsActivityType')
+                                   ->find(EsActivityType::TRANSACTION_UPDATE);
+                $phraseArray = $this->languageLoader
+                                    ->getLine($activityType->getActivityPhrase());
+
+                if($status === EsOrderProductStatus::CASH_ON_DELIVERY){
+                    $member = $entity->getSeller();
+                    $unparsedPhrase = $phraseArray['completed'];
+                }
+                elseif($status === EsOrderProductStatus::RETURNED_BUYER){
+                    $member = $entity->getSeller();
+                    $unparsedPhrase = $phraseArray['item_cancel'];
+                }
+
+                $phrase = $this->activityManager
+                               ->constructActivityPhrase([],
+                                                         $unparsedPhrase,
+                                                         'EsOrder');
+                if($phrase !== ""){
+                    $em->getRepository('EasyShop\Entities\EsActivityHistory')
+                       ->createAcitivityLog($activityType, $phrase, $member);
+                }
+           }
+        }
+    }
+
+    /**
+     * Return all subscribed events for this class
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::preUpdate,
+            Events::postUpdate,
+        ];
+    }
+}
+
