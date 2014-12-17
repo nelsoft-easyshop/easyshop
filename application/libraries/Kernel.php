1e8d31f622c29cdb8989e3fb301ad780ab33003f
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-12-17 14:41:59 +0800

    #2519 register listener on kernel

--- application/libraries/Kernel.php
+++ application/libraries/Kernel.php
@@ -85,6 +85,12 @@ class Kernel
                     $container['language_loader']
                 )
             );
+            $em->getEventManager()->addEventSubscriber(
+                new \EasyShop\Doctrine\Listeners\EsProductReviewListener(
+                    $container['activity_manager'],
+                    $container['language_loader']
+                )
+            );
             return $em;
         };
 
