5e10488b165e36962c586161e4b78aaa786a1b14
root <kurtwilkinson024@easyshop.ph>
2014-12-18 14:59:56 +0800

    #2457 partial add of migrations:

---
+++ application/migrations/DoctrineMigrations/Version20141218144518.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141218144518 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->addSql("");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("");
+    }
+}
