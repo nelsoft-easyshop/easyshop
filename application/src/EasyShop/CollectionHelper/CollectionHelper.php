f378c0bb812357fa9b5743261ef9b1da0446a655
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-12-18 20:00:23 +0800

    #2573 add function for generating combination

--- application/src/EasyShop/CollectionHelper/CollectionHelper.php
+++ application/src/EasyShop/CollectionHelper/CollectionHelper.php
@@ -95,4 +95,34 @@ class CollectionHelper
 
         return $newFormedArray;
     }
+
+    /**
+     * Generate all the possible combinations among a set of nested arrays.
+     *
+     * @param array $data  The entrypoint array container.
+     * @param array $all   The final container (used internally).
+     * @param array $group The sub container (used internally).
+     * @param mixed $val   The value to append (used internally).
+     * @param int   $i     The key index (used internally).
+     */
+    public function generateCombinations(array $data, array &$all = array(), array $group = array(), $value = null, $i = 0)
+    {
+        $keys = array_keys($data);
+        if (isset($value) === true) {
+            $group[] = $value;
+        }
+
+        if ($i >= count($data)) {
+            $all[] = $group;
+        } 
+        else {
+            $currentKey     = $keys[$i];
+            $currentElement = $data[$currentKey];
+            foreach ($currentElement as $val) {
+                $this->generateCombinations($data, $all, $group, $val, $i + 1);
+            }
+        }
+
+        return $all;
+    }
 }
