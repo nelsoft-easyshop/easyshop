082838446f2ddd76e12a53e865f3bab5ec451580
root <kurtwilkinson024@easyshop.ph>
2014-12-18 16:22:23 +0800

    #2457 add migration for es_promo_type table

--- application/migrations/DoctrineMigrations/Version20141218144518.php
+++ application/migrations/DoctrineMigrations/Version20141218144518.php
@@ -12,11 +12,14 @@ class Version20141218144518 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("");
+        $this->addSql("CREATE TABLE `es_promo_type` (
+                      `id_promo_type` INT NOT NULL,
+                      `promo_name` VARCHAR(45) NULL DEFAULT '',
+                      PRIMARY KEY (`id_promo_type`));");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("");
+        $this->addSql("DROP TABLE `es_promo_type`");
     }
 }
