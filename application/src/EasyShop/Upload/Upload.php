64145cf42fcd1df9e54ef2f6ee6fec6f9cfc095f
unknown <inon.baguio@yahoo.com>
2014-12-18 17:07:03 +0800

    #2513 refined codes

--- application/src/EasyShop/Upload/Upload.php
+++ application/src/EasyShop/Upload/Upload.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 namespace EasyShop\Upload;
 
 /**
@@ -29,7 +28,28 @@ class Upload
      *
      *  @var Doctrine\ORM\EntityManager
      */
-    private $em;          
+    private $em;    
+
+    /**
+     *  Upload Errors
+     *
+     *  @var array
+     */
+    private $errors;    
+
+    /**
+     *  Upload Data
+     *
+     *  @var array
+     */
+    private $uploadData;     
+
+    /**
+     *  Allowed Image Types
+     *
+     *  @var string
+     */
+    private $allowedImageTypes = 'gif|jpg|png|jpeg';             
 
     /**
      * Constructor. Retrieves Upload Library instance
@@ -42,9 +62,32 @@ class Upload
         $this->em = $em;               
     }
 
-    public function uploadImage($uid, $data)
+    public function uploadImage($path, 
+                                $filename, 
+                                $isOverWriteImage = true, 
+                                $maxSize = 5000, 
+                                $maxWidth = 5000, 
+                                $maxHeight = 5000)
     {   
-        
+        $config['overwrite'] = $isOverWriteImage;
+        $config['file_name'] = $filename;
+        $config['upload_path'] = $path; 
+        $config['allowed_types'] = $this->allowedImageTypes;
+        $config['max_size'] = $maxSize;
+        $config['max_width']  = $maxWidth;
+        $config['max_height']  = $maxHeight;
+        $this->uploadLibrary->initialize($config);  
+
+        if (!$this->uploadLibrary->do_upload()){
+            $this->errors = $this->uploadLibrary->display_errors();
+        }
+        else {
+            $this->uploadData = $this->uploadLibrary->data();
+        }
+        return [
+                'errors' => $this->errors,
+                'uploadData' => $this->uploadData
+            ];        
     }
 }
 
