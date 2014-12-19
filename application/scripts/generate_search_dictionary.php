805d27d3889f95cd9b3d3655f5039b43f928319a
Sam <samgavinio@easyshop.ph>
2014-12-19 11:00:33 +0800

    #2517 added code to add brands to dictionary

--- application/scripts/generate_search_dictionary.php
+++ application/scripts/generate_search_dictionary.php
@@ -53,15 +53,51 @@
         }
     }
 
+    $productsWithBrandsSql = "SELECT 
+                            es_brand.name as brandName,
+                            COUNT(es_brand.name) as brandOccurences
+                        FROM es_product 
+                        INNER JOIN es_brand 
+                            ON es_product.brand_id = es_brand.id_brand
+                        INNER JOIN es_member
+                            ON es_member.is_active = :memberActive
+                            AND es_member.id_member = es_product.member_id
+                        WHERE
+                            es_product.is_delete = :deleteStatus AND 
+                            es_product.is_draft = :draftStatus AND
+                            es_product.brand_id <> :customBrand
+                        GROUP BY es_product.brand_id
+                        HAVING brandOccurences >= :brandOccurenceLimit
+    ";
 
- 
+    
+    $preparedStatement = $dbConnection->prepare($productsWithBrandsSql);
+    $memberStatus = EasyShop\Entities\EsMember::DEFAULT_ACTIVE;
+    $deleteStatus = EasyShop\Entities\EsProduct::ACTIVE;
+    $draftStatus = EasyShop\Entities\EsProduct::ACTIVE;
+    $customBrand = EasyShop\Entities\EsBrand::CUSTOM_CATEGORY_ID;
+    $brandOccurenceLimit = $dictionary->getOccurencesLimit();
+
+    $preparedStatement->bindParam("memberActive", $memberStatus);
+    $preparedStatement->bindParam("deleteStatus", $deleteStatus);
+    $preparedStatement->bindParam("draftStatus", $draftStatus);
+    $preparedStatement->bindParam("customBrand", $customBrand);
+    $preparedStatement->bindParam("brandOccurenceLimit", $brandOccurenceLimit);
+    $preparedStatement->execute();
+    $brands = $preparedStatement->fetchAll(PDO::FETCH_ASSOC);
+    
+    var_dump($brands);
+    
+    foreach($brands as $brand){
+        $dictionary->insertWordIntoDictionary($brand['brandName'],$brand['brandOccurences'] );
+    }
     
     print_r($dictionary->getDictionary());
     
 class Dictionary
 {
 
-    const WORD_MIN_LENGTH = 3;
+    const WORD_MIN_LENGTH = 2;
     
     const DEV_MIN_OCCURENCES = 3;
     
@@ -81,8 +117,13 @@ class Dictionary
             $this->occurencesMinimum = self::DEV_MIN_OCCURENCES;
         }
     }
+    
+    public function getOccurencesLimit()
+    {
+        return $this->occurencesMinimum;
+    }
 
-    public function insertWordIntoDictionary($word)
+    public function insertWordIntoDictionary($word, $sqlOccurences = null)
     {
         if(strlen(trim($word)) >= self::WORD_MIN_LENGTH){
             $word = htmlspecialchars($word);
@@ -90,15 +131,23 @@ class Dictionary
             $word = preg_replace('/[^A-Za-z0-9\ ]/', '', $word);
             if(isset($this->dictionary[$word])){
                 $occurences = $this->dictionary[$word];
-                $occurences++;
+                if($sqlOccurences === null){
+                    $occurences++;
+                }
+                else{   
+                    $occurences = $occurences + $sqlOccurences;
+                }
                 $this->dictionary[$word] = $occurences;
             }
             else{
-                $this->dictionary[$word] = 1;
+                $occurences = $sqlOccurences === null ? 1 : $sqlOccurences;
+                $this->dictionary[$word] = $occurences;
             }
+    
         }
     }
     
+
     public function getDictionary($isRaw = false)
     {
         foreach($this->dictionary as $index=>$word){
