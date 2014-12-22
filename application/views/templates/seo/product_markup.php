00d2047c84ec92abf56112157a8c04e1017514ea
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2014-12-22 17:53:15 +0800

    #2596 add template markup

---
+++ application/views/templates/seo/product_markup.php
@@ -0,0 +1,23 @@
+
+<div itemscope itemtype="http://schema.org/Product">
+    <?php foreach ($breadCrumbs as $crumbs): ?> 
+    <div itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+        <a href="<?=base_url().'/category/'.html_escape($crumbs['slug']); ?>" itemprop="url"> 
+            <meta itemprop="title" content="<?=html_escape($crumbs['name']);?>" />
+        </a>
+    </div> 
+    <?php endforeach; ?>
+    
+    <meta itemprop="name" content="<?=html_escape($product->getName());?>">
+    <div itemprop="offers" itemscope itemtype="http://schema.org/Offer"> 
+        <meta itemprop="price" content="<?=number_format($product->getFinalPrice(), 2); ?>" />
+        <meta itemprop="priceCurrency" content="PHP" />
+        <meta itemprop="seller" content="<?=html_escape($product->getMember()->getStoreName());?>" />
+    </div>
+    <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"> 
+        <meta itemprop="ratingValue" content="<?=$averageRating; ?>" />
+        <meta itemprop="ratingCount" content="<?=$reviewCount; ?>" /> 
+        <meta itemprop="bestRating" content="5" />
+    </div>
+</div>
+
