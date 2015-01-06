7f05d28e592afc128abef3a115a76f663a9edefa
unknown <inon.baguio@yahoo.com>
2015-01-06 14:30:09 +0800

    #2606 service class that handles authentication for webservice

---
+++ application/src/EasyShop/Webservice/AuthenticateRequest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace EasyShop\Webservice;
+
+/**
+ * Image Utility Class
+ *
+ * @author Inon Baguio
+ */
+class AuthenticateRequest
+{
+
+    /**
+     *  Entity Manager Instance
+     *
+     *  @var Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     *  The hash string
+     *
+     *  @var string
+     */
+    private $hash = "";    
+
+    /**
+     *  Exceptional values that must not be included in the hash string
+     *
+     *  @var string
+     */
+    private $exceptionalData = ["hash", "_token", "csrfname", "callback", "password", "_", "checkuser"];  
+
+    function __construct($em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Handles the request authentication method
+     * @param  array  $postedData
+     * @param  string  $postedHash    
+     * @param  boolean $isAdminRequest
+     * @return bool
+     */
+    public function authenticate($postedData, $postedHash, $isAdminRequest = false)
+    {
+
+        foreach ($postedData as $data => $value) {
+            if(in_array($data, $this->exceptionalData)) {
+                 continue;               
+            }
+            else{
+                $this->hash .= $value;
+            }
+        }
+
+        if($isAdminRequest) {
+            $adminUser = $this->em->getRepository("EasyShop\Entities\EsAdminMember")
+                                            ->find($postedData["userid"]);
+
+            $this->hash .= $adminUser->getPassword();            
+        }
+
+        return $isAuthenticated = (sha1($this->hash) != $postedHash) ? false : true;
+    }
+
+    
+
+}
+
+
