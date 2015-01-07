6af082439f865f0751472d22c9dc3bd0095d5a6a
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-01-07 15:16:25 +0800

    #2621 moved script into js file

--- application/views/templates/header_alt.php
+++ application/views/templates/header_alt.php
@@ -97,7 +97,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             <option value="1">On Seller's Page</option>
                             <option value="2">Main Page</option> 
                         </select>
-                        <input type="text" class="search-bar-input ui-form-control" name="q_str" value="<?=($this->input->get('q_str'))?trim($this->input->get('q_str')):""?>" class="ui-form-control">
+                        <input type="text" id="main_search_alt" autocomplete="off" class="search-bar-input ui-form-control" name="q_str" value="<?=($this->input->get('q_str'))?trim($this->input->get('q_str')):""?>" class="ui-form-control">
                         <input type="submit"  value="" class="submitSearch span_bg">
                     </form>
                 </div>
@@ -299,67 +299,5 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 <script type="text/javascript" src="/assets/js/src/vendor/bootstrap.js?ver=<?=ES_FILE_VERSION?>" ></script>
 <script src="/assets/js/src/vendor/bootstrap-typeahead.min.js" type="text/javascript"></script>
-<script>
-    (function ($) { 
+<script src="/assets/js/src/header_alt.js?ver=<?php echo ES_FILE_VERSION ?>" type="text/javascript"></script>
 
-        $('input#main_search_alt')
-            .typeahead({
-                ajax: { 
-                    url: '/search/suggest',
-                    triggerLength: 3, // This is the minimum length of text to take action on
-                    timeout: 450, //  Specify the amount of time to wait for keyboard input to stop until you send the query to the server.
-                    preProcess: function (data) { 
-                        if ($.isEmptyObject(data)) { 
-                            $('.suggested-result-container-alt').empty();
-                        } 
-                        return data;
-                    }
-                },
-                items: 10, // The maximum number of items to show in the results. 
-                menu: '<ul class="typeahead suggested-result-container-alt"></ul>' ,
-                item: '<li><a href="#"></a></li>'
-            }) 
-            .focus(function() { 
-                if($(this).val().length >= 3){ 
-                    if ($('.suggested-result-container-alt').is(':empty') === false){ 
-                        $('.suggested-result-container-alt').show();
-                    }
-                }
-            })
-            .focusout(function() { 
-                $('.suggested-result-container-alt').hide();
-                $('.suggested-result-container-alt2').html($('.suggested-result-container-alt').html());
-            });
-
-        $('input#main_search_alt2')
-            .typeahead({
-                ajax: { 
-                    url: '/search/suggest',
-                    triggerLength: 3, // This is the minimum length of text to take action on
-                    timeout: 450, //  Specify the amount of time to wait for keyboard input to stop until you send the query to the server.
-                    preProcess: function (data) { 
-                        if ($.isEmptyObject(data)) { 
-                            $('.suggested-result-container-alt2').empty();
-                        } 
-                        return data;
-                    }
-                },
-                items: 10, // The maximum number of items to show in the results. 
-                menu: '<ul class="typeahead suggested-result-container-alt2"></ul>' ,
-                item: '<li><a href="#"></a></li>'
-            }) 
-            .focus(function() { 
-                if($(this).val().length >= 3){ 
-                    if ($('.suggested-result-container-alt2').is(':empty') === false){ 
-                        $('.suggested-result-container-alt2').show();
-                    }
-                }
-            })
-            .focusout(function() { 
-                $('.suggested-result-container-alt2').hide();
-                $('.suggested-result-container-alt').html($('.suggested-result-container-alt2').html());
-            });
-
-
-    })(jQuery);
-</script>
