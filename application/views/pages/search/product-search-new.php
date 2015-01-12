4d5925f4469cf85700aa31305a6bbcc309453d8e
carlduazo <justine.duazo@easyshop.ph>
2015-01-12 18:09:56 +0800

    #2733 Created view for new search results

---
+++ application/views/pages/search/product-search-new.php
@@ -0,0 +1,75 @@
+<link rel="stylesheet" type="text/css" href="/assets/css/easy-icons/easy-icons.css" media='screen'>
+<link rel="stylesheet" type="text/css" href="/assets/css/product-search-new.css" media='screen'>
+
+<section class="bg-search-section color-default">
+    <br>
+    <div class="container-non-responsive">
+        <div class="row">
+            <div class="col-md-3">
+            asdas
+            </div>
+            
+            <div class="col-md-9">
+                <div class="search-header">
+                    <h5>
+                    <strong>300</strong>
+                    results for
+                    <strong>gadgets</strong>
+                    </h5>
+                </div>
+                <div class="div-product-view-option">
+                    <table class="p-view color-default pull-left">
+                        <tbody>
+                            <tr>
+                                <td class="td-view p-view2 color-default">VIEW STYLE:</td>
+                                <td class="td-view" style="padding-top: 3px;">
+                                    <span class="gv fa fa-icon-view-grid fa-2x icon-view icon-grid active-view"></span>
+                                    <span class="lv fa fa-icon-view-list fa-2x icon-view icon-list"></span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="vendor-select-con">
+                    <select class="sort_select form-select-default color-default pull-right">
+                        <option value="2">Default Sorting</option>
+                        <option value="1">Popularity</option>
+                        <option value="3">Hot</option>
+                    </select>
+                    <div class="clear"></div>
+                </div>
+                
+                <div class="search-results-container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="search-item-container">
+                                <div class="search-item-img-container">
+                                    
+                                </div>
+                                <a class="search-item-name">
+                                    breitling-chronometer
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="search-item-container">
+                                ad
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="search-item-container">
+                                ad
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="search-item-container">
+                                ad
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
