cf35f8a7b262fddb5a38bd462e8a80fd5f35037c
Sam <samgavinio@easyshop.ph>
2015-01-14 14:30:48 +0800

    #2757 renamed and handled view for expired verification link

--- application/views/pages/user/email-verification-success.php
+++ application/views/pages/user/email-verification-success.php
@@ -12,8 +12,14 @@
         <div class="success_content">
             <center>
             <div>
-                <p style="color:#2e5014; font-weight:bold;">Hi <?php echo html_escape($member_username); ?>, congratulations! <p>
-                <p style="color:#2e5014; font-size: 16px; ">Your account is now fully verified.</p>
+                <?php if(isset($isAlreadyVerified) && $isAlreadyVerified): ?>
+                    <p style="color:#2e5014; font-weight:bold;">Hi <?php echo html_escape($username); ?>,<p>
+                <?php else: ?>
+                    <p style="color:#2e5014; font-weight:bold;">Hi <?php echo html_escape($username); ?>, congratulations! <p>
+                <?php endif; ?>
+                
+                <p style="color:#2e5014; font-size: 16px; "><?php echo html_escape($verificationMessage); ?></p>
+                
                 <br/>
                 <p style='font-size: 14px; color:black;'>
                     You may now  <a href="/login">log-in</a> to begin your online shopping experience.
