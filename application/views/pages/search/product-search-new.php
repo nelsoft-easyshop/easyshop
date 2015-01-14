b137da130bde0d337fae4a7da1296341c7d8dccd
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-01-14 16:35:18 +0800

    #2762 can display product

--- application/views/pages/search/product-search-new.php
+++ application/views/pages/search/product-search-new.php
@@ -199,8 +199,21 @@
                 
                 <div class="search-results-container">
                     <div class="row">
+                        <?php foreach ($products as $value): ?>
                         <?php
-                            for($i=0; $i<=20; $i++){
+                            $productEntity = $value;
+                            $productName = html_escape($productEntity->getName());
+                            $productSlug = $productEntity->getSlug();
+                            $productPrice = number_format($productEntity->getFinalPrice(), 2,'.',',');
+                            $productCondition = html_escape($productEntity->getCondition());
+                            $originalPrice = number_format($productEntity->getOriginalPrice(),2,'.',',');
+                            $percentage = $productEntity->getDiscountPercentage(); 
+                            $isFreeShipping = $productEntity->getIsFreeShipping(); 
+                            $productImagePath = $productEntity->directory .'categoryview/'. $productEntity->imageFileName;
+
+                            $memberEntity = $value->getMember();
+                            $sellerStoreName = html_escape($memberEntity->getStoreName());
+                            $sellerSlug = html_escape($memberEntity->getSlug());
                         ?>
                         <div class="col-search-item col-xs-3">
                             <div class="search-item-container">
@@ -214,15 +227,17 @@
                                     </div>
                                 </a>
                                 <div class="search-item-meta">
-                                    <a href="#" class="search-item-name">
-                                        breitling-chronometer
+                                    <a href="/item/<?=$productSlug;?>" class="search-item-name">
+                                        <?=$productName; ?>
                                     </a>
                                     <div class="search-item-price">
+                                        <?php if($percentage > 0):?>
                                         <span class="original-price">
-                                            <s>P8,000</s>
+                                            <s>P<?=$originalPrice; ?></s>
                                         </span>
+                                        <?php endif; ?>
                                         <span class="new-price">
-                                            P7,500
+                                            P<?=$productPrice; ?>
                                         </span>
                                     </div>
                                  </div>
@@ -250,8 +265,8 @@
                                                 </a>
                                             </td>
                                             <td class="search-item-td-meta">
-                                                <a href="#" class="search-item-name">
-                                                    breitling-chronometer
+                                                <a href="/item/<?=$productSlug;?>" class="search-item-name">
+                                                    <?=$productName; ?>
                                                 </a>
                                                 <span class="search-item-description">
                                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac rutrum augue, at pellentesque est. Proin ullamcorper laoreet dolor. Vestibulum quis placerat enim.
@@ -264,8 +279,8 @@
                                                                 <img src="/assets/images/img_how-to-buy.png" class="search-item-seller-img" />
                                                             </div>
                                                         </div>
-                                                        <a href="" class="search-item-seller-name">
-                                                            Seller2DaMax
+                                                        <a href="/<?=$sellerSlug;?>" class="search-item-seller-name">
+                                                            <?=$sellerStoreName; ?>
                                                         </a>
                                                     </div>
                                                     <div class="col-xs-6">
@@ -278,11 +293,13 @@
                                             </td>
                                             <td class="search-item-td-price">
                                                 <div class="search-item-price">
+                                                    <?php if($percentage > 0):?>
                                                     <span class="original-price">
-                                                        <s>P8,000</s>
+                                                        <s>P<?=$originalPrice; ?></s>
                                                     </span>
+                                                    <?php endif; ?> 
                                                     <span class="new-price">
-                                                        P7,500
+                                                        P<?=$productPrice; ?>
                                                     </span>
                                                 </div>
                                                 <button class="btn btn-search-add-cart">
@@ -295,101 +312,7 @@
                                 </table>
                             </div>
                         </div>
-                        
-                        
-                        <div class="col-search-item col-xs-3">
-                            <div class="search-item-container">
-                                <a href="#" class="search-item-link-image">
-                                    <div class="search-item-img-container" style="background: url(/assets/images/products/samsung-p.jpg) center no-repeat; background-size: cover;">
-                                        <div class="search-item-img-container-hover" style="background: url(/assets/images/products/samsung-p-h.jpg) center no-repeat; background-size: cover;">
-                                            
-                                        </div>
-                                        <span class="discount-circle-2">76%</span>
-                                        <span class="new-circle-2">NEW</span>
-                                    </div>
-                                </a>
-                                <div class="search-item-meta">
-                                    <a href="#" class="search-item-name">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac rutrum augue, at pellentesque est. Proin ullamcorper laoreet dolor
-                                    </a>
-                                    <div class="search-item-price">
-                                        <span class="new-price">
-                                            P7,500
-                                        </span>
-                                    </div>
-                                 </div>
-                                <div class="search-item-actions">
-                                    <button class="btn btn-search-add-cart">
-                                        <span class="fa icon-cart fa-lg"></span>
-                                        Add to cart
-                                    </button>
-                                    <div class="search-item-seller-cont pull-right">
-                                        <img src="/assets/images/img_how-to-buy.png" class="search-item-seller-img" />
-                                    </div>
-                                </div>
-                                <table class="search-item-list-table">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <a href="#">
-                                                    <div class="search-item-img-container" style="background: url(/assets/images/products/samsung-p.jpg) center no-repeat; background-size: cover;">
-                                                        <div class="search-item-img-container-hover" style="background: url(/assets/images/products/samsung-p-h.jpg) center no-repeat; background-size: cover;">
-                                                            
-                                                        </div>
-                                                        <span class="discount-circle-2">76%</span>
-                                                        <span class="new-circle-2">NEW</span>
-                                                    </div>
-                                                </a>
-                                            </td>
-                                            <td class="search-item-td-meta">
-                                                <a href="#" class="search-item-name">
-                                                    Lorem ipsum dolor sit amet, consectetur
-                                                </a>
-                                                <span class="search-item-description">
-                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac rutrum augue, at pellentesque est. Proin ullamcorper laoreet dolor. Vestibulum quis placerat enim. Vestibulum at aliquet nibh, fringilla porta nulla. Cras at sem convallis, convallis magna vitae, porta leo. Phasellus suscipit pulvinar tortor, ac gravida felis sodales et.
-                                                </span>
-                                                <div class="divider-gray"></div>
-                                                <div class="row">
-                                                    <div class="col-xs-6">
-                                                        <div class="search-item-seller-img-list">
-                                                            <div class="search-item-seller-cont">
-                                                                <img src="/assets/images/img_how-to-buy.png" class="search-item-seller-img" />
-                                                            </div>
-                                                        </div>
-                                                        <a href="" class="search-item-seller-name">
-                                                            Seller2DaMax
-                                                        </a>
-                                                    </div>
-                                                    <div class="col-xs-6">
-                                                        <span class="search-item-shipping-text pull-right">
-                                                            <span class="search-item-shipping-label">Shipping : </span>
-                                                            <span class="search-item-shipping-data">Free</span>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td class="search-item-td-price">
-                                                <div class="search-item-price">
-                                                    <span class="original-price">
-                                                        <s> </s>
-                                                    </span>
-                                                    <span class="new-price">
-                                                        P7,500
-                                                    </span>
-                                                </div>
-                                                <button class="btn btn-search-add-cart">
-                                                    <span class="fa icon-cart fa-lg"></span>
-                                                    Add to cart
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <?php
-                            }
-                        ?>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
