4248ee3a8d280a95d51bc6f813f02c720b126c19
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-01-21 08:22:55 +0800

    #2775 remove or condition

--- application/src/EasyShop/Product/ProductManager.php
+++ application/src/EasyShop/Product/ProductManager.php
@@ -717,8 +717,8 @@ class ProductManager
             $product->soldCount = $esProductRepo->getSoldProductCount($product->getIdProduct());
             $productAttributes = $esProductRepo->getAttributesByProductIds([$product->getIdProduct()]);
             $product->attributes = $this->collectionHelper->organizeArray($productAttributes);
-            if ( strlen(trim($product->getCatOtherName())) <= 0 
-                 || !trim($product->getCatOtherName()) ){
+
+            if ( strlen(trim($product->getCatOtherName())) <= 0 ){
                 $product->category = trim($product->getCat()->getName());
             }
             else{
