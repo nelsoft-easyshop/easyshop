a943d7fb616ec9a1136a320a0a944e00b1c18935
Sam <samgavinio@easyshop.ph>
2015-01-28 19:10:49 +0800

    #2882 used minified css

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,8 +132,9 @@ module.exports = function (grunt) {
                          'web/assets/css/product_advance_search.css', 
                          'web/assets/css/style_new.css', 
                          'web/assets/css/bootstrap.css', 
-                         'web/assets/css/bootstrap-mods.css']
-                     
+                         'web/assets/css/bootstrap-mods.css'],
+                    'web/assets/css/min-easyshop.product-search-new.css':
+                        ['web/assets/css/product-search-new.css']             
                 }
             },
         }
