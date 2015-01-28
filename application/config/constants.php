f4fdc53e1d9284def3c60259a0a96509b826990f
Sam <samgavinio@easyshop.ph>
2015-01-28 12:07:28 +0800

    File version updated to 10.1.3.1

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '10.1.2';
+    $fileVersion = '10.1.3.1';
 }
 define('ES_FILE_VERSION',$fileVersion);
 define('ES_ENABLE_CHRISTMAS_MODS', false);
