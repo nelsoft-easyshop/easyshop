0448cd029e063faa82a5d0749b165c7719aa75bb
Sam <samgavinio@easyshop.ph>
2015-01-28 17:07:57 +0800

    #2882 used minified css

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,11 @@ module.exports = function (grunt) {
                          'web/assets/css/owl.carousel.css',
                          'web/assets/css/jquery.bxslider.css'],
                     'web/assets/css/min-easyshop.product-image-gallery.css':
-                        ['web/assets/css/jquery.jqzoom.css']
+                        ['web/assets/css/jquery.jqzoom.css'],
+                    'web/assets/css/min-easyshop.productpage-primary.css':
+                        ['web/assets/css/product-page-css.css',
+                         'web/assets/css/jquery.jqzoom.css',
+                         'web/assets/css/owl.carousel.css'] 
                 }
             },
         }
