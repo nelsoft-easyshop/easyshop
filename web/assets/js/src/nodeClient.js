c6cba6683ac48353bc49fc9bcc09365a0e60f671
Sam <samgavinio@easyshop.ph>
2015-01-29 19:35:09 +0800

    #3012 handled problem of duplicate message count in title

--- web/assets/js/src/nodeClient.js
+++ web/assets/js/src/nodeClient.js
@@ -43,7 +43,21 @@
             success : function(count)
             {   
                 var numberOfUnreadMessages = $.parseJSON(count);
-                $(document).prop('title', '(' + numberOfUnreadMessages + ') ' + $(document).prop('title'));
+                var title = '';
+                
+                if($('#original-title').length === 0){
+                    var originalTitleTag = document.createElement('meta');
+                    originalTitleTag.id = "original-title";
+                    originalTitleTag.name = "original-title";
+                    title = $(document).prop('title');
+                    originalTitleTag.content = title;
+                    document.getElementsByTagName('head')[0].appendChild(originalTitleTag);
+                }
+                else{
+                    title = $('#original-title').attr('content')
+                }
+           
+                $(document).prop('title', '(' + numberOfUnreadMessages + ') ' + title);
                 $('#unread-messages-count').html(numberOfUnreadMessages);
                 if(parseInt(numberOfUnreadMessages) > 0){
                     $('#unread-messages-count').css('display','inline-block');
