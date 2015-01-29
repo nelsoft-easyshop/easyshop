254b99a683143b28b4dd4f73d3885f6b36c5d560
Sam <samgavinio@easyshop.ph>
2015-01-29 17:45:16 +0800

    #3012 Added code for updating header icons (partial)

--- web/assets/js/src/nodeClient.js
+++ web/assets/js/src/nodeClient.js
@@ -1,23 +1,47 @@
 (function ($) {
+    
     $(document).ready(function () {
+        
         var $chatClient = $('#chatClientInfo');
         var $isLoggedIn = $('[name="is-logged-in"]');
-        if ($isLoggedIn.val() === "true") {
+
+        if ($.parseJSON($isLoggedIn.val())) {
+
             var socket = io.connect( 'https://' + $chatClient.data('host') + ':' + $chatClient.data('port'));
+     
             var setAccountOnline = function(storeName) {
                 socket.emit('set account online', storeName);
             };
-            setAccountOnline($chatClient.data('store-name'));
 
+            
             var setAccountOffline = function(storeName) {
                 socket.emit('set account offline', storeName, function(data) {});
             }
+       
+            /* Register events */
+            socket.on('send message', function( data ) {
+                $.ajax({
+                    type:"get",
+                    dataType : "json",
+                    url : "/MessageController/getNumberOfUnreadMessages",
+                    success : function(count)
+                    {   
+                       var numberOfUnreadMessages = $.parseJSON(count);
+                       $(document).prop('title', '(' + numberOfUnreadMessages + ') ' + $(document).prop('title'));
+                       $('#unread-messages-count').html(numberOfUnreadMessages);
+                       $('#unread-messages-count').css('display','inline');
+                    }
+                }); 
+            });
+
+            setAccountOnline($chatClient.data('store-name'));
+
+            $('.logoutClient').on('click', function(e) {
+                e.preventDefault();
+                setAccountOffline($chatClient.data('store-name'));
+                window.location.replace('/login/logout');
+            });
         }
 
-        $('.logoutClient').on('click', function(e) {
-            e.preventDefault();
-            setAccountOffline($chatClient.data('store-name'));
-            window.location.replace('/login/logout');
-        });
     });
 })(jQuery);
