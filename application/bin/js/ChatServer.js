1efbb64cf81efeb546ade9d79048a850a7bdf6b0
Sam <samgavinio@easyshop.ph>
2015-02-02 13:05:54 +0800

    #3012 used room implementation

--- application/bin/js/ChatServer.js
+++ application/bin/js/ChatServer.js
@@ -17,48 +17,25 @@ io = require('socket.io').listen(server);
 
 var container = {};
 io.sockets.on( 'connection', function(client) {
+    
+    client.on('set account online', function(storename) {
+        client.join(storename);
+    });
+
+    client.on('set account offline', function(storename) {
+        client.leave(storename);
+    });
 
     client.on('send message', function(data) {
-        if (data.recipient in container) {
-            var clients = container[data.recipient];
-            for(var i = 0; i < clients.length;i++){
-                   clients[i].emit('send message', {recipient: data.recipient, message: data.message });
-            }
-        }
-    });
-
-    client.on('set account online', function(storeName) {
-        //TODO : add feature to see if user is online
-        
-        if (!(storeName in container)) {
-            console.log(storeName + " is now Online" );
-            container[storeName] = [];
-        }
-        client.storeName = storeName;
-        container[storeName].push(client);
-    });
-
-    client.on('set account offline', function(storeName, callback) {
-        if (storeName in container) {
-            callback(true);
-            delete container[storeName];
-            console.log(storeName + " is now Offline" );
-        }
-        else {
-            callback(false);
-        }
+        io.to(data.recipient).emit('send message', {
+            recipient: data.recipient,
+            message: data.message
+        });
     });
     
     client.on('message opened', function(storename) {
-        if (storename in container) {
-            var currentClientId = client.id;
-            var clients = container[storename];
-            for(var i = 0; i < clients.length;i++){
-                if(currentClientId !== clients[i].id){
-                    clients[i].emit('message opened')
-                }
-            }
-        }
+        io.to(storename).emit('message opened');
     });
+    
 
 });
