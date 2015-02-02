fa0d46d5bb306f8c867d9221c32edee386c7f532
KEI.PI <kurtwilkinson024@easyshop.ph>
2015-02-02 13:54:37 +0800

    #2961 add route

---
+++ application/controllers/promo/Estudyantrepreneur.php
@@ -0,0 +1,15 @@
+<?php
+
+class Estudyantrepreneur extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function EstudyantrepreneurPromo()
+    {
+        echo 'hello students!';
+    }
+}
