b20a31a98da6576481f8bcc764aaf5126fa9357e
Sam <samgavinio@easyshop.ph>
2015-02-05 15:50:55 +0800

    #3055 removed debug codes

--- application/bin/js/ChatServer.js
+++ application/bin/js/ChatServer.js
@@ -9,6 +9,7 @@ require('./config').configureExpress(app);
 var PORT = app.get('PORT');
 var HOST = app.get('HOST');
 var JWT_SECRET = app.get('JWT_SECRET');
+var CHAT_CHANNEL_NAME = app.get('CHAT_CHANNEL_NAME');
 var https_options = {
     key: app.get('KEY'),
     cert: app.get('CERT')
@@ -18,7 +19,7 @@ var server = https.createServer(https_options, app).listen(PORT, HOST);
 
 //You can specify port and host : redis.createClient(PORT, HOST, options)
 var clientSubscribe = redis.createClient();
-clientSubscribe.subscribe("chat-channel");
+clientSubscribe.subscribe(CHAT_CHANNEL_NAME);
 
 console.log('HTTPS Server listening on %s:%s', HOST, PORT);
 io = require('socket.io').listen(server);
@@ -28,21 +29,14 @@ io.set('authorization',socketioJwt.authorize({
     handshake: true
 }));
 
-
-
 io.sockets.on( 'connection', function(socket) {
  
     socket.on('set account online', function() {
         var storename = socket.client.request.decoded_token.storename; 
-        console.log(storename + " is online.");
-        socket.join(storename);
     });
     
     clientSubscribe.on("message", function(channel, jsonString){
-
         var data = JSON.parse(jsonString);
-        console.log(data.recipient);
-        console.log(data.message);
         io.to(data.recipient).emit('send message', {
             recipient: data.recipient,
             message: data.message
