e0a4a37ca0edfd8e24a2d613734c94f6b09a8c9d
KEI.PI <kurtwilkinson024@easyshop.ph>
2015-02-10 19:51:13 +0800

    #3151 uncomment node functions

--- web/assets/js/src/nodeClient.js
+++ web/assets/js/src/nodeClient.js
@@ -7,31 +7,31 @@
 
         if ($.parseJSON($isLoggedIn.val())) {
 
-//            var socket = io.connect( 'https://' + $chatClient.data('host') + ':' + $chatClient.data('port'));
+            var socket = io.connect( 'https://' + $chatClient.data('host') + ':' + $chatClient.data('port'));
      
-//            var setAccountOnline = function(storeName) {
-//                socket.emit('set account online', storeName);
-//            };
-//
-//            var setAccountOffline = function(storeName) {
-//                socket.emit('set account offline', storeName, function(data) {});
-//            };
-//
-//            /* Register events */
-//            socket.on('send message', function( data ) {
-//                updateMessageCountIcons();
-//            });
-//
-//            /* Register events */
-//            socket.on('message opened', function( data ) {
-//                updateMessageCountIcons();
-//            });
-//
-//            setAccountOnline($chatClient.data('store-name'));
+            var setAccountOnline = function(storeName) {
+                socket.emit('set account online', storeName);
+            };
+
+            var setAccountOffline = function(storeName) {
+                socket.emit('set account offline', storeName, function(data) {});
+            };
+
+            /* Register events */
+            socket.on('send message', function( data ) {
+                updateMessageCountIcons();
+            });
+
+            /* Register events */
+            socket.on('message opened', function( data ) {
+                updateMessageCountIcons();
+            });
+
+            setAccountOnline($chatClient.data('store-name'));
 
             $('.logoutClient').on('click', function(e) {
                 e.preventDefault();
-//                setAccountOffline($chatClient.data('store-name'));
+                setAccountOffline($chatClient.data('store-name'));
                 window.location.replace('/login/logout');
             });
         }
