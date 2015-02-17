c40bd82611503d5c966b5ac7b33b542d4d8e1b82
carlduazo <justine.duazo@easyshop.ph>
2015-02-17 17:03:48 +0800

    #3224 Added label content for product management

--- application/views/pages/user/dashboard/dashboard-product-management.php
+++ application/views/pages/user/dashboard/dashboard-product-management.php
@@ -22,14 +22,14 @@
     </div>
     <div class="div-tab-inner">
         <div class="div-account-setting">
-            <p class="panel-setting-title">ACTIVATE PRODUCTS</p>
+            <p class="panel-setting-title">Restore Inactive Items</p>
             <div class="div-setting-content">
                 <p class="p-deact-note-setting">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat
+                    Restores all inactive products to an Active status, making them available to all users again.
                 </p>
                 <div class="current-activate-prod">
                     <button class="btn btn-default-3 btn-prod-manage" id="activate-products">
-                       Activate all deactivated products
+                       Restore all inactive items
                     </button>
                 </div>
                 <div class="edit-activate-prod">
@@ -63,14 +63,14 @@
     </div>
     <div class="div-tab-inner">
         <div class="div-account-setting">
-            <p class="panel-setting-title">DEACTIVATE PRODUCTS</p>
+            <p class="panel-setting-title">Disable Active Items</p>
             <div class="div-setting-content">
                 <p class="p-deact-note-setting">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat
+                    Sets all your active products to an Inactive status. This will remove your product from our search results and will no longer be visible to our users.
                 </p>
                 <div class="current-deactivate-prod">
                     <button class="btn btn-default-3 btn-prod-manage" id="deactivate-products">
-                       Deactivate all active products
+                       Disable all active items
                     </button>
                 </div>
                 <div class="edit-deactivate-prod">
@@ -104,14 +104,14 @@
     </div>
     <div class="div-tab-inner">
         <div class="div-account-setting">
-            <p class="panel-setting-title">DELETE PRODUCTS</p>
+            <p class="panel-setting-title">Delete Inactive Items</p>
             <div class="div-setting-content">
                 <p class="p-deact-note-setting">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat
+                    Permanently deletes all your inactive products. This is irreversible, please use this button with caution.
                 </p>
                 <div class="current-delete-prod">
                     <button class="btn btn-default-1 btn-prod-manage"  id="delete-products">
-                       Delete all active products
+                       Delete all inactive items
                     </button>
                 </div>
                 <div class="edit-delete-prod">
