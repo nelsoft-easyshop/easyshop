abf467ba213f2159d97ee4951c98a1ca6aa88b31
KEI.PI <kurtwilkinson024@easyshop.ph>
2015-02-18 19:05:23 +0800

    #3267 fix scratch and win view

--- application/controllers/promo/ScratchCard.php
+++ application/controllers/promo/ScratchCard.php
@@ -132,22 +132,22 @@ class ScratchCard extends MY_Controller
         else if (intval($viewData['product']['c_id_code']) !== 0) {
             $viewData['product'] = 'purchase-limit-error';
         }
-        $slugs = array(
+        $slugs = [
             'lg-optimus-g-pro-lite-black',
             'lg-nexus-5',
             'apple-iphone-5c-16gb-1',
             'lenovo-s820-red',
             'lenovo-a316i-android-42-4gb-black',
             'lg-optimus-l5-ii-e450-black'
-        );
-        $product = array();
+        ];
+        $product = [];
         foreach($slugs as $slug){
             $dbProduct = $this->product_model->getProductBySlug($slug, false);
             $product[] = $dbProduct;
         }
         $viewData['gadgets_galore'] = $product;
 
-        $this->load->spark('decorator');    
+        $this->load->spark('decorator');
         $this->load->view('templates/header',  $this->decorator->decorate('header', 'view', $headerData));
         $this->load->view('pages/promo/scratch_to_win', $viewData);
         $this->load->view('templates/footer');
