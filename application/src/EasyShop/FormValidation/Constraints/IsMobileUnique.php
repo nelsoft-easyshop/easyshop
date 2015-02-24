20ca6ed5d64bfca6b9eb39fd392cee77c446b899
unknown <inon.baguio@yahoo.com>
2015-02-24 16:55:07 +0800

    #3380 passed memberId

--- application/src/EasyShop/FormValidation/Constraints/IsMobileUnique.php
+++ application/src/EasyShop/FormValidation/Constraints/IsMobileUnique.php
@@ -10,6 +10,37 @@ use Symfony\Component\Validator\Constraint;
 class IsMobileUnique extends Constraint
 {
     public $message = 'The mobile "0%string%" is already in use';
-        
+
+    /**
+     * Member ID
+     *
+     * @var integer
+     */
+    private $memberId;
+
+    /**
+     * Constraint constructor
+     *
+     * @param integer $memberId
+     */
+    public function __construct($options)
+    {      
+        if(isset($options["memberId"]) && is_int($options['memberId'])){
+            $this->memberId = $options['memberId'];
+        }
+        else{
+            throw new \Exception("The member ID must be an integer");
+        }
+    }
+
+    /**
+     * Returns the memberId
+     *
+     * @return integer
+     */
+    public function getMemberId()
+    {
+        return $this->memberId;
+    }    
 }
 
