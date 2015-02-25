95653d5cb92ec4fc6a128c040d409c3c62b5490b
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-02-25 15:54:04 +0800

    #3286 remove unused function

--- application/src/EasyShop/Repositories/EsAttrRepository.php
+++ application/src/EasyShop/Repositories/EsAttrRepository.php
@@ -4,39 +4,9 @@ namespace EasyShop\Repositories;
 
 use Doctrine\ORM\EntityRepository;
 use EasyShop\Entities\EsAttr;
-use Doctrine\ORM\Query\ResultSetMapping;
 
 class EsAttrRepository extends EntityRepository
 {
-    /**
-     * Get attribute available in category
-     * @param  array $categories 
-     * @return mixed
-     */
-    public function getAttributeListByCategory($categories)
-    {
-        $this->em =  $this->_em;
-        $rsm = new ResultSetMapping(); 
-        $rsm->addScalarResult('attrName', 'attrName'); 
-        $rsm->addScalarResult('lookuplistId', 'lookuplistId'); 
-        $query = $this->em->createNativeQuery("
-        SELECT DISTINCT
-            a.name AS attrName 
-            , a.attr_lookuplist_id as lookuplistId
-        FROM
-            es_attr a 
-            , es_attr_lookuplist c
-        WHERE a.datatype_id = b.id_datatype
-            AND a.attr_lookuplist_id = c.id_attr_lookuplist
-            AND a.cat_id IN (:categories)
-            GROUP BY attrName, a.attr_lookuplist_id
-            ORDER BY attrName ASC
-        ", $rsm);
 
-        $query->setParameter('categories', $categories); 
-        $results = $query->getResult();
-
-        return $results;
-    }
 }
 
