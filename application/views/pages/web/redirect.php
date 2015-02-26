18dd55545ae96dd43e3daf302ad6175b09b5a1e9
KEI.PI <kurtwilkinson024@easyshop.ph>
2015-02-26 13:57:21 +0800

    #3375 fix design

--- application/views/pages/web/redirect.php
+++ application/views/pages/web/redirect.php
@@ -1,18 +1,16 @@
-<br>
+<link rel="stylesheet" type="text/css" href="/assets/css/new-login.css" media='screen'/>
 <div class="clear"></div>
 <section>
     <div class="col-md-12 container">
         <center>
-            <div class="input-group col-md-4 ">
+            <div class="input-group col-md-4 identify-email-top-group ">
                 <p>
                     You are now leaving EasyShop.ph and are going to a website that is not operated by EasyShop Online Inc.
                     We have no responsibility over the content of the site you are navigating to. Thank you.
                 </p>
             </div>
-            <div class="input-group col-md-4 response-dialog">
-                <div class="alert alert-success" role="alert">
-                    <a href="<?=html_escape($url)?>">Click here to continue</a>
-                </div>
+            <div class="input-group col-md-2 identify-email-bottom-group ">
+                <a class="form-control submit-email-form" href="<?=html_escape($url)?>"> Click here to continue</a>
             </div>
         </center>
     </div>
