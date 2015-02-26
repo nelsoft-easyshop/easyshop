8810e48c34f2081d6a2a70d9fb681ce59c171139
KEI.PI <kurtwilkinson024@easyshop.ph>
2015-02-26 14:46:53 +0800

    #3375 add design

--- application/views/pages/web/redirect.php
+++ application/views/pages/web/redirect.php
@@ -1,16 +1,20 @@
+
+<style>
+
+</style>
 <link rel="stylesheet" type="text/css" href="/assets/css/new-login.css" media='screen'/>
 <div class="clear"></div>
 <section>
     <div class="col-md-12 container">
         <center>
             <div class="input-group col-md-4 identify-email-top-group ">
-                <p>
+                <p class="disclaimer-message">
                     You are now leaving EasyShop.ph and are going to a website that is not operated by EasyShop Online Inc.
                     We have no responsibility over the content of the site you are navigating to. Thank you.
                 </p>
             </div>
-            <div class="input-group col-md-2 identify-email-bottom-group ">
-                <a class="form-control submit-email-form" href="<?=html_escape($url)?>"> Click here to continue</a>
+            <div class="input-group identify-email-bottom-group ">
+                <a class="form-control submit-email-form redirect-link" href="<?=html_escape($url)?>"> Click here to continue</a>
             </div>
         </center>
     </div>
