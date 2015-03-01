c8124aa40291b15535e2dbef077f96b8f20bfceb
Sam <samgavinio@easyshop.ph>
2015-03-02 07:09:30 +0800

    Updated file version for express edit feature

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '13.0.5';
+    $fileVersion = '14.0.0';
 }
 define('ES_FILE_VERSION',$fileVersion);
 define('ES_ENABLE_CHRISTMAS_MODS', false);
