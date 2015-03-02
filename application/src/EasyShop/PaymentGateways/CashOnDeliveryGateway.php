15c8681d06e571d40979b49ae819eb032f6c6991
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-02 17:13:22 +0800

    #3491 moved persistPayment to superClass

--- application/src/EasyShop/PaymentGateways/CashOnDeliveryGateway.php
+++ application/src/EasyShop/PaymentGateways/CashOnDeliveryGateway.php
@@ -36,13 +36,13 @@ class CashOnDeliveryGateway extends AbstractGateway
      * Pay method for Cash On Delivery Gateway Class
      * 
      */
-    public function pay($validatedCart, $memberId, $paymentService)
+    public function pay($validatedCart, $memberId)
     {
         // Set status response
         $response['status'] = PaymentService::STATUS_FAIL;
         
         // Point Gateway
-        $pointGateway = $paymentService->getPointGateway();
+        $pointGateway = $this->paymentService->getPointGateway();
         $response['paymentType'] = EsPaymentMethod::PAYMENT_CASHONDELIVERY;
         $response['textType'] = 'cashondelivery';
         $response['message'] = 'Your payment has been completed through Cash on Delivery.';
@@ -55,7 +55,7 @@ class CashOnDeliveryGateway extends AbstractGateway
                             ->getShippingAddress(intval($memberId));
 
         // Compute shipping fee
-        $prepareData = $paymentService->computeFeeAndParseData($validatedCart['itemArray'], intval($address));
+        $prepareData = $this->paymentService->computeFeeAndParseData($validatedCart['itemArray'], intval($address));
         $grandTotal = $prepareData['totalPrice'];
         $this->setParameter('amount', $grandTotal);
         $productString = $prepareData['productstring'];
@@ -64,7 +64,7 @@ class CashOnDeliveryGateway extends AbstractGateway
         $response['txnid'] = $txnid;
 
         if($validatedCart['itemCount'] === $productCount){
-            $return = $paymentService->persistPayment(
+            $return = $this->persistPayment(
                 $grandTotal, 
                 $memberId, 
                 $productString, 
