42382a37bcb6451ddd920689f530e2da2660e75e
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-03 14:52:32 +0800

    #3508 fix error in delete lockitem

--- application/src/EasyShop/Repositories/EsProductItemLockRepository.php
+++ application/src/EasyShop/Repositories/EsProductItemLockRepository.php
@@ -91,7 +91,7 @@ class EsProductItemLockRepository extends EntityRepository
      * @param mixed $ids
      * @param int $orderId
      */
-    public function deleteLockItem($orderId, $ids = array())
+    public function deleteLockItem($ids, $orderId)
     {
         if(count($ids) > 0){
             $itemIds = array_keys($ids);
