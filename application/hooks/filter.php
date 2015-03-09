f3d85ee097b44fca96acd0d9eceec121517fee2d
Sam <samgavinio@easyshop.ph>
2015-03-09 16:40:10 +0800

    #3574 add hook to disallow access from staging.

---
+++ application/hooks/filter.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Filter Class
+ */
+class IP_Filter
+{
+    /**
+     * Holds CI instance
+     *
+     * @var CI instance
+     */
+    private $CI;
+    
+    /**
+     * Enables splash
+     *
+     * @var boolean
+     */
+    private $enableSplash = false;
+    
+    /**
+     * Bypass IP
+     *
+     * @var string
+     */
+    private $bypassIp;
+    
+
+    public function __construct()
+    {
+        $this->CI =& get_instance();
+    }
+    
+    
+    /**
+     * Checks if the IP is allowed to access the site
+     *
+     */
+    public function isIpAllowed()
+    {
+        $clientIP = $this->CI->kernel
+                   ->serviceContainer['http_request']
+                   ->getClientIp(); 
+
+        $configService = $this->CI->kernel
+                              ->serviceContainer['local_configuration'];
+        if($configService->isConfigFileExists()){
+            $serverConfig = $configService->getConfigValue();
+            if(isset($serverConfig['maintenance_on'])){
+                $this->enableSplash = $serverConfig['maintenance_on'];
+            }
+            if(isset($serverConfig['maintenance_bypass_ip'])){
+                $this->bypassIp = $serverConfig['maintenance_bypass_ip'];
+            }
+        }
+
+        $splashPageUrl = 'home/splash';
+        $currentUrl = $this->CI->uri->uri_string();
+        if($splashPageUrl !== $currentUrl){
+            if ($this->enableSplash && ($this->bypassIp !== $clientIP)){
+                redirect('/'.$splashPageUrl);  
+            }
+        }
+        
+        if(strtolower(ENVIRONMENT) === 'staging'){
+            show_error('You are not allowed to access this page', 400);
+        }
+    
+    }
+
+}
+
