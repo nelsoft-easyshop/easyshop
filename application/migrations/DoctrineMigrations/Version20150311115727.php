7b2b19bfb32f5bd364c62b6f8c4fd6adc934156a
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-03-12 10:33:00 +0800

    #3589 removed added table

--- application/migrations/DoctrineMigrations/Version20150311115727.php
+++ application/migrations/DoctrineMigrations/Version20150311115727.php
@@ -18,20 +18,6 @@ class Version20150311115727 extends AbstractMigration
             ALTER TABLE `es_point` 
             DROP COLUMN `credit_point`;
         ");
-
-        $this->addSql("
-            CREATE TABLE `es_order_points` (
-                `id_order_points` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                `order_id` INT(10) UNSIGNED NOT NULL,
-                `credit_points` INT(10) NOT NULL,
-            PRIMARY KEY (`id_order_points`),
-            INDEX `fk_es_order_points_1_idx` (`order_id` ASC),
-            CONSTRAINT `fk_es_order_points_1`
-                FOREIGN KEY (`order_id`)
-                REFERENCES `es_order` (`id_order`)
-                ON DELETE NO ACTION
-                ON UPDATE NO ACTION);
-        ");
     }
 
     public function down(Schema $schema)
@@ -40,10 +26,6 @@ class Version20150311115727 extends AbstractMigration
         $this->addSql("
             ALTER TABLE `es_point` 
             ADD COLUMN `credit_point` INT(10) UNSIGNED NOT NULL AFTER `point`;
-        ");
-
-        $this->addSql("
-            DROP TABLE `es_order_points`;
         ");
     }
 }
