4dadd2337dd42ad263762e0ec2e0e6973fb28701
carlduazo <justine.duazo@easyshop.ph>
2015-03-13 17:04:37 +0800

    #3700 Created mark up file for new cart

---
+++ application/views/pages/cart/cart-primary.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" type="text/css" href="/assets/css/new-cart.css?ver=<?=ES_FILE_VERSION?>" media='screen'>
+<div class="container">
+    <div class="cart-container">
+        <!--Start of title breadcrumb-->
+        <div class="cart-breadcrumb">
+            <div class="row">
+                <div class="col-md-12">
+                    <span class="cart-breadcrumb-title active">Shopping Cart</span>
+                    <i class="fa fa-arrow-right fa-lg"></i>
+                    <span class="cart-breadcrumb-title">Checkout Details</span>
+                    <i class="fa fa-arrow-right fa-lg"></i>
+                    <span class="cart-breadcrumb-title">Order Complete</span>
+                </div>
+            </div>
+        </div>
+        <!--End of title breadcrumb-->
+
+
+        <!--Start of cart items-->
+
+        <!--End of cart items-->
+
+
+        <!--Start of middle buttons-->
+
+        <!--End of middle buttons-->
+
+        <!--Start of trio cart options-->
+            
+            <!--Start of shipping calculator-->
+
+            <!--End of shipping calculator-->
+
+            <!--Start of points-->
+
+            <!--End of points-->
+
+            <!--Start of summary-->
+
+            <!--End of summary-->
+
+        <!--End of trio cart options-->
+    </div>
+</div>
+<script src="/assets/js/src/cart.js?ver=<?php echo ES_FILE_VERSION ?>" type="text/javascript"></script>
+
