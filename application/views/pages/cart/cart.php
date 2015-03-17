82f96649f875ad52a686deecdcb4bb8638bc3859
carlduazo <justine.duazo@easyshop.ph>
2015-03-17 13:28:02 +0800

    #3750 Create new cart markup

---
+++ application/views/pages/cart/cart.php
@@ -0,0 +1,53 @@
+<link type="text/css" href='/assets/css/new-cart.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+
+<div class="transaction-container">
+    <div class="container">
+        <!--Start of transaction breadcrumb-->
+
+        <!--End of transaction breadcrumb-->
+        <!--Start of transaction cart items-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="transaction-container bg-white">
+                    <table class="cart-table" width="100%">
+                        <thead>
+                            <tr>
+                                <th colspan="3">
+                                    Item List
+                                </th>
+                                <th>
+                                    Price
+                                </th>
+                                <th>
+                                    Quantity
+                                </th>
+                                <th>
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td valign="middle">
+                                    <i class="fa fa-times fa-lg"></i>
+                                </td>
+                                <td>
+                                    <div class="cart-item-thumbnail" style="background: url(assets/images/products/apple-p.jpg) center no-repeat; background-size: cover;"></div>
+                                </th>
+                                <td>
+                                    Quantity
+                                </td>
+                                <td>
+                                    Total
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!--End of cart items-->
+    </div>
+</div>
+
+<script src="/assets/js/src/cart.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
