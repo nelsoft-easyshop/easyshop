1b55e134761c19419bc378886d6cb3fd9211987a
carlduazo <justine.duazo@easyshop.ph>
2015-03-19 18:49:31 +0800

    #3750 Added js for payment selection

--- web/assets/js/src/cart.js
+++ web/assets/js/src/cart.js
@@ -7,5 +7,14 @@
 			$(".remove-item-modal").parents(".simplemodal-container").addClass("my-modal").removeAttr("id").removeClass("feedback-modal-container");
 			$(".my-modal").css("height", heightOfRemoveItemModal+"px");
 		});
+
+		$(".payment-label").click(function(){
+			var subCatContainer = $(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();
+			var paymentName = $(this).parents(".payment-method-container").find("label").text();
+			$(".payment-method-desc").not(subCatContainer).slideUp();
+			$(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();		
+			$(".btn-payment-button").text("Pay Via "+paymentName);
+		});
+
 	});
 })(jQuery);
