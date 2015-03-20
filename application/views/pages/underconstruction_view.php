ee455e40b74ffa7b613044c17a5eded0c2b30c69
es-paulmodino <paul.modino@easyshop.ph>
2015-03-20 14:32:09 +0800

    #3722 fixed underconstruction view page

--- application/views/pages/underconstruction_view.php
+++ application/views/pages/underconstruction_view.php
@@ -1,19 +1,19 @@
-  <section>
-    <div class="wrapper page_underconstruction_container">
-      <div class="bg_ribbon">
+  <section class="bg-cl-fff">
+    <div class="container page_underconstruction_container">
         <h2 >
-          Page Underconstruction
+            Page Underconstruction
         </h2>
-        <p>
-        We are currently working on an awesome new site.
-      </p>
-      </div>
+        <h4>
+            We are currently working on an awesome new site.
+        </h4>
      
-      <h2>Stay in touch to get the latest updates</h2>
-		      <?php echo form_open('subscribe');?>
-			<input type="text" name="subscribe_email"><input type="submit" value="Submit" name="subscribe_btn">
-            <?php echo form_close();?>
-	  
+        <div class="underconstruction-form">
+        <h4>Stay in touch to get the latest updates</h4>
+		    <?php echo form_open('subscribe');?>
+    			    <input type="text" name="subscribe_email" class="ui-form-control">
+              <input type="submit" value="Submit" name="subscribe_btn" class="btn btn-default-4">
+        <?php echo form_close();?>
+	      </div>
     </div>
 
   </section>
