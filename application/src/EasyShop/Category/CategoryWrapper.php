b803081ee0a1acde342cf2d21948f73cd2d2533d
Sam <samgavinio@easyshop.ph>
2015-03-22 18:43:41 +0800

    #3731 add methods to handle noncustomized categories

--- application/src/EasyShop/Category/CategoryWrapper.php
+++ application/src/EasyShop/Category/CategoryWrapper.php
@@ -15,21 +15,36 @@ class CategoryWrapper
      *
      * @var string
      */
-    private $categoryName;
+    private $categoryName = '';
 
     /**
      * Sort Order
      *
      * @var integer
      */
-    private $sortOrder;
+    private $sortOrder = 0;
 
     /** 
      * Member Category Id
      *
      * @var integer
      */
-    private $memberCategoryId;
+    private $memberCategoryId = 0;
+    
+    /** 
+     * EsCat Category Id
+     *
+     * @var integer
+     */
+    private $nonMemberCategoryId = 0;
+    
+    /** 
+     * Boolean value if the category is custom or not
+     *
+     * @var boolean
+     */
+    private $isCustom = true;
+
 
     /**
      * Children categories
@@ -117,6 +132,86 @@ class CategoryWrapper
     {
         return $this->children;
     }
+
+    /**
+     * Set the isCustom flag
+     *
+     * @param booleam $isCustom
+     */
+    public function setIsCustom($isCustom)
+    {
+        $this->isCustom = $isCustom;
+    }
+    
+    /**
+     * Get the isCustom flag
+     *
+     * @return boolean
+     */
+    public function getIsCustom()
+    {
+        return $this->isCustom;
+    }
+    
+    
+    /**
+     * Set the EsCat Category Id
+     *
+     * @param integer $isCustom
+     */
+    public function setNonMemberCategoryId($categoryId)
+    {
+        $this->nonMemberCategoryId = $categoryId;
+    }
+    
+    /**
+     * Get the EsCat Category Id
+     *
+     * @return integer
+     */
+    public function getNonMemberCategoryId()
+    {
+        return $this->nonMemberCategoryId;
+    }
+    
+    /**
+     * Checks if a certain ID has already been appended to the children array
+     *
+     * @param integer $childId
+     * @return boolean
+     */
+    public function isChildAvailable($childId)
+    {
+        $childIds = $this->getChildrenAsArray();
+        
+        return in_array($childId, $childIds);
+    }
+    
+    /**
+     * Get childs IDs as array
+     *
+     * @return integer[]
+     */
+    public function getChildrenAsArray()
+    {
+        $childIds = [];
+        foreach($this->children as $child){
+            $childIds[] = $child->getId();
+        }
+        
+        return $childIds;
+    }
+    
+    
+    /**
+     * Get the category Id depending on if the category is custom or nit
+     *
+     * @return integer[]
+     */
+    public function getId()
+    {
+        return $this->isCustom ? $this->memberCategoryId : $this->nonMemberCategoryId;
+    }
     
     /**
      * Convert the entire object into an array
@@ -129,6 +224,8 @@ class CategoryWrapper
             'categoryName' => $this->categoryName,
             'sortOrder' => $this->sortOrder,
             'memberCategoryId' => $this->memberCategoryId,
+            'nonMemberCategoryId' => $this->nonMemberCategoryId,
+            'isCustom' => $this->isCustom,
             'children' => [],
         ];
         foreach($this->children as $child){
