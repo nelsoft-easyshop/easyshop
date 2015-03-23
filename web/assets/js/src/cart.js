b00cbacf529cafe738c21a009d7291b71f182882
carlduazo <justine.duazo@easyshop.ph>
2015-03-23 16:56:35 +0800

    #3750 Added js for shipping calculator

--- web/assets/js/src/cart.js
+++ web/assets/js/src/cart.js
@@ -8,6 +8,11 @@
 			$(".my-modal").css("height", heightOfRemoveItemModal+"px");
 		});
 
+		$(".calculate-shipping-label").click(function(){
+			$(".shipping-calculator-container").slideToggle("fast");
+			$(this).toggleClass("toggleShippingIcon");
+		});
+
 		$(".payment-label").click(function(){
 			var subCatContainer = $(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();
 			var paymentName = $(this).parents(".payment-method-container").find("label").text();
