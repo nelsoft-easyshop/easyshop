8732ae599607f5c026b4a4a360031d8b71ea484d
carlduazo <justine.duazo@easyshop.ph>
2015-03-23 19:44:09 +0800

    #3750 Added changed shipping jquery

--- web/assets/js/src/cart.js
+++ web/assets/js/src/cart.js
@@ -4,8 +4,6 @@
 		var browserWidth = $(window).width();
 		var heightOfModal = $(".simplemodal-wrap").outerHeight();
 		
-		$('.circle-breadcrumb').animate({ background:'#000000' }, 3000);
-
 		$(".cart-item-remove").click(function(){
 			$(".remove-item-modal").modal();
 			$(".remove-item-modal").parents(".simplemodal-container").addClass("my-modal").removeAttr("id").removeClass("feedback-modal-container");
@@ -20,15 +18,29 @@
 			});
 			$(".shipping-calculator-modal").parents(".simplemodal-container").addClass("my-modal").removeAttr("id").removeClass("feedback-modal-container");
 		});
+	});
 
-		$(".payment-label").click(function(){
-			var subCatContainer = $(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();
-			var paymentName = $(this).parents(".payment-method-container").find("label").text();
-			$(".payment-method-desc").not(subCatContainer).slideUp();
-			$(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();		
-			$(".btn-payment-button").text("Pay Via "+paymentName);
-		});
+	$(".payment-label").click(function(){
+		var subCatContainer = $(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();
+		var paymentName = $(this).parents(".payment-method-container").find("label").text();
+		$(".payment-method-desc").not(subCatContainer).slideUp();
+		$(this).parents(".payment-method-container").find(".payment-method-desc").slideDown();		
+		$(".btn-payment-button").text("Pay Via "+paymentName);
+	});
 
-		
+	$(".btn-change-shipping").click(function(){
+		$(".div-change-shipping-btn").slideToggle("fast");
+		$(".div-save-shipping-btn").slideToggle("fast");
+		$("#fname, #lname, #contact, #fullAddress").removeAttr("readonly");
+		$("#city, #state").removeAttr("disabled");
+	});
+
+	$(".btn-change-shipping-cancel").click(function(){
+		$(".div-change-shipping-btn").slideToggle("fast");
+		$(".div-save-shipping-btn").slideToggle("fast");
+		$("#fname, #lname, #contact, #fullAddress").attr("readonly", "true");
+		$("#city, #state").attr("disabled", "true");
+		$("#fname, #lname, #contact, #fullAddress").prop("readonly", "true");
+		$("#city, #state").prop("disabled", "true");
 	});
 })(jQuery);
