dfcb0429480d5b8622c411ba424cc4c0b4d9db6d
carlduazo <justine.duazo@easyshop.ph>
2015-03-23 19:56:02 +0800

    #3750 Changed view load for payment response

--- application/controllers/payment.php
+++ application/controllers/payment.php
@@ -1143,9 +1143,9 @@ class Payment extends MY_Controller{
         $this->session->unset_userdata('payment_status');
 
         $this->load->spark('decorator');  
-        $this->load->view('templates/header', $this->decorator->decorate('header', 'view', $headerData));
-        $this->load->view('pages/payment/payment_response_responsive' ,$response);
-        $this->load->view('templates/footer_full', $this->decorator->decorate('footer', 'view'));
+        $this->load->view('templates/header_primary', $this->decorator->decorate('header', 'view', $headerData));
+        $this->load->view('pages/payment/payment-response');
+        $this->load->view('templates/footer_primary', $this->decorator->decorate('footer', 'view'));
    }
 
 
