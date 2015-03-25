e5a95b8b25539f60b158f2a0280f246a409976db
carlduazo <justine.duazo@easyshop.ph>
2015-03-25 14:04:43 +0800

    #3750 Added reset button

--- application/views/pages/cart/cart.php
+++ application/views/pages/cart/cart.php
@@ -244,6 +244,7 @@
                     </div>
                     <div class="form-group">
                         <button class="btn btn-es-green btn-sm">Use Points</button>
+                        <button class="btn btn-es-white btn-sm">Reset</button>
                     </div>
                 </div>
             </div>
