96e78446f7e2f1d20cf0b465a959c0849624f642
carlduazo <justine.duazo@easyshop.ph>
2015-03-26 18:03:37 +0800

    #3908 Convert tabs to spaces

--- web/assets/js/src/message-box.js
+++ web/assets/js/src/message-box.js
@@ -1,31 +1,30 @@
 (function ($) {
-	$(window).on("load", function(){
-		$(".message-box").fadeIn().modal({
-	        onOpen: function(dialog) {
-		            dialog.overlay.fadeIn('fast', function () {
-		                dialog.container.fadeIn('fast', function () {
-		                    dialog.data.fadeIn('fast');
-		                });
-		            });
-		        },
-		    onClose: function (dialog) {
-			    dialog.data.fadeOut('fast', function () {
-			        dialog.container.fadeOut('fast', function () {
-			            dialog.overlay.fadeOut('fast', function () {
-			                $.modal.close();
-			            });
-			        });
-			    });
-			}
-	    });
-		$(".message-box").parents(".simplemodal-container").removeAttr("id").addClass("my-modal");
-		$(".my-modal").css("height", "403px");
-		var containerHeight = $(".my-modal").outerHeight();
-		$(".message-container").css("height", containerHeight);
+    $(window).on("load", function(){
+        $(".message-box").fadeIn().modal({
+            onOpen: function(dialog) {
+                    dialog.overlay.fadeIn('fast', function () {
+                        dialog.container.fadeIn('fast', function () {
+                            dialog.data.fadeIn('fast');
+                        });
+                    });
+                },
+            onClose: function (dialog) {
+                dialog.data.fadeOut('fast', function () {
+                    dialog.container.fadeOut('fast', function () {
+                        dialog.overlay.fadeOut('fast', function () {
+                            $.modal.close();
+                        });
+                    });
+                });
+            }
+        });
+        $(".message-box").parents(".simplemodal-container").removeAttr("id").addClass("my-modal");
+        $(".my-modal").css("height", "403px");
+        var containerHeight = $(".my-modal").outerHeight();
+        $(".message-container").css("height", containerHeight);
 
 
-	});
-		
+    });
 }(jQuery));
 
 
