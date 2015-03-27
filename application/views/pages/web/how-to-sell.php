011e12ec099845ca750ffd5c91898f551939ec94
Sam <samgavinio@easyshop.ph>
2015-03-28 00:57:39 +0800

    #3956 update titles

--- application/views/pages/web/how-to-sell.php
+++ application/views/pages/web/how-to-sell.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Seller Transaction</title>
+        <title>Easyshop.ph | How to Sell</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
