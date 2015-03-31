ed0a54f657956af910cba8540670fd2a1a2df111
Sam <samgavinio@easyshop.ph>
2015-03-31 20:33:23 +0800

    Update file version

--- application/config/constants.php
+++ application/config/constants.php
@@ -44,7 +44,7 @@ if(ENVIRONMENT == 'development'){
     $fileVersion = date('Ymdhis');
 }
 else{
-    $fileVersion = '16.0.11';
+    $fileVersion = '17.0.0';
 }
 define('ES_FILE_VERSION',$fileVersion);
 define('ES_ENABLE_CHRISTMAS_MODS', false);
