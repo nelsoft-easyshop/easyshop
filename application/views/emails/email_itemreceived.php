ce93c3bed679a64e8708e43c8a6b3d8a0301949f
carlduazo <justine.duazo@easyshop.ph>
2015-04-06 16:49:37 +0800

    #4101 Replaced dummy image link

--- application/views/emails/email_itemreceived.php
+++ application/views/emails/email_itemreceived.php
@@ -651,7 +651,7 @@
                                                         <table width="230" border="0" cellspacing="0" cellpadding="0" align="left">
                                                             <tr>
                                                                 <td width="65">
-                                                                    <img src="http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/13/2014/03/hs50exr_600.jpg" width="60" height="60" style="width: 60px; height: 60px; border: solid #e2e2e2 1px; text-align: center;">
+                                                                    <img src="{primaryImage}" width="60" height="60" style="width: 60px; height: 60px; border: solid #e2e2e2 1px; text-align: center;">
                                                                 </td>
                                                                 <td valign="bottom" style="padding-left: 5px;">
                                                                     <a href="{itemLink}" style="font-size: 15px; line-height: 22px; font-family:Roboto,Open Sans, Arial,Tahoma, Helvetica, sans-serif; color:#555555; font-weight:400; text-align:left; ">
