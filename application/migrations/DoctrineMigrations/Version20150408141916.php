7b617830bfe1e9cbdc6ed72c292182720b617623
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-08 14:33:47 +0800

    #4197 add migration file

---
+++ application/migrations/DoctrineMigrations/Version20150408141916.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\Version;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150408141916 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_point` 
+            CHANGE COLUMN `point` `point` DECIMAL(15,4) UNSIGNED NOT NULL DEFAULT '0' ;
+        ");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_point` 
+            CHANGE COLUMN `point` `point` INT(10) UNSIGNED NOT NULL DEFAULT '0' ;
+        ");
+    }
+}
