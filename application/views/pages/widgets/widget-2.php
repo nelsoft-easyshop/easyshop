9db27077d29aa8c292c66dc40a1e45bb0a60e04a
carlduazo <justine.duazo@easyshop.ph>
2015-04-14 18:59:47 +0800

    #4329 Widget markup

---
+++ application/views/pages/widgets/widget-2.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <link type="text/css" href='/assets/css/bootstrap.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+        <link type="text/css" href='/assets/css/easy-icons/easy-icons.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+        <link type="text/css" href='/assets/css/widget.css?ver=<?=ES_FILE_VERSION?>' rel="stylesheet" media='screen'/>
+    </head>
+    <body>
+        <div class="es-widget-2">
+            <div class="title-bar">
+                <a href="#">
+                    <img src="/assets/images/categories/icon-widget/easyshop-logo.png">
+                </a>
+            </div>
+            <div class="search-form">
+                <input type="text" class="search-textfield" placeholder="Search item here"/>
+                <i class="icon-search"></i>
+            </div>
+            <div class="container-title">
+                Popular items
+            </div>
+            <div class="items-wrapper">
+                <center>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/apple-p.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/big-dress1.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/big-dress5.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/lg-p.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/tiffany-p.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#">
+                                        <div class="item-img-container">
+                                            <img src="/assets/images/products/graff-p.jpg">
+                                        </div>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </center>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript" src="/assets/js/src/vendor/jquery-1.9.1.js"></script>
+    <script src="/assets/js/src/widget.js?ver=<?php echo ES_FILE_VERSION ?>" type='text/javascript' ></script>
+</html>
