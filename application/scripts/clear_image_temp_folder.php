bd1d39957bc326cd82482f04370a62310bd5efd5
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-16 14:37:31 +0800

    #4380 add class for script file

--- application/scripts/clear_image_temp_folder.php
+++ application/scripts/clear_image_temp_folder.php
@@ -1,46 +1,83 @@
-<?php 
+<?php
 
-$path = dirname(__FILE__).'/../../web/assets/temp_product/*';
-$files = glob($path);
+include_once  __DIR__.'/bootstrap.php';
 
-foreach($files as $file){ 
- 
+$CI =& get_instance();
+$emailService = $CI->kernel->serviceContainer['email_notification'];
+$configLoader = $CI->kernel->serviceContainer['config_loader'];
+$viewParser = new \CI_Parser();
 
-    $datecreated = date ("Y-m-d H:i:s.", filectime($file));
-    $now = date("Y-m-d H:i:s");
+use EasyShop\Script\ScriptBaseClass as ScriptBaseClass;
 
-    if(intval(round((strtotime($now)) - strtotime($datecreated))/3600) >= 3){
-        if(is_dir($file)){
-            deleteDir($file);
-        }
-        else{
-             unlink($file);
-        }
+class ClearImageTempFolder extends ScriptBaseClass
+{
+    private $emailService;
+    private $configLoader;
+    private $viewParser;
+
+    /**
+     * Constructor
+     * @param EasyShop\Notifications\EmailNotification $emailService
+     * @param EasyShop\ConfigLoader\ConfigLoader       $configLoader
+     * @param \CI_Parser                               $viewParser
+     */
+    public function __construct(
+        $emailService,
+        $configLoader,
+        $viewParser
+    )
+    {
+        parent::__construct($emailService, $configLoader, $viewParser);
     }
 
+    public function execute()
+    {
+        $path = dirname(__FILE__).'/../../web/assets/temp_product/*';
+        $files = glob($path);
 
+        foreach ($files as $file) {
+            $datecreated = date("Y-m-d H:i:s.", filectime($file));
+            $now = date("Y-m-d H:i:s");
+            if ((int)round((strtotime($now)) - strtotime($datecreated)) / 3600 >= 3) {
+                if (is_dir($file)) {
+                    $this->deleteDir($file);
+                }
+                else {
+                    unlink($file);
+                }
+            }
+        }
+        echo "\nDone. The directory '.$path.' has been emptied. \n\n";
+    }
+
+    /**
+     * Deletes a directory and all its content. Recursive implementation.
+     * @param  string $dirPath
+     */
+    private function deleteDir($dirPath)
+    {
+        if (! is_dir($dirPath)) {
+            throw new InvalidArgumentException("$dirPath must be a directory");
+        }
+        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+            $dirPath .= '/';
+        }
+        $files = glob($dirPath . '*', GLOB_MARK);
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                $this->deleteDir($file);
+            } else {
+                unlink($file);
+            }
+        }
+        rmdir($dirPath);
+    }
 }
-echo 'Done. The directory '.$path.' has been emptied.';
 
+$clearTempFolder  = new ClearImageTempFolder(
+    $emailService,
+    $configLoader,
+    $viewParser
+);
 
-/*
- *   Deletes a directory and all its content. Recursive implementation.
- */
-
-function deleteDir($dirPath) {
-    if (! is_dir($dirPath)) {
-        throw new InvalidArgumentException("$dirPath must be a directory");
-    }
-    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-        $dirPath .= '/';
-    }
-    $files = glob($dirPath . '*', GLOB_MARK);
-    foreach ($files as $file) {
-        if (is_dir($file)) {
-            deleteDir($file);
-        } else {
-            unlink($file);
-        }
-    }
-    rmdir($dirPath);
-}
+$clearTempFolder->execute();
