dc1613f3284307a7b9ea77c29ecd4902ec7360be
Sam <samgavinio@easyshop.ph>
2015-04-17 00:30:37 +0800

    #4393 improve class naming. ss is a terrible name.

--- application/views/pages/promo/estudyantrepreneur_success.php
+++ application/views/pages/promo/estudyantrepreneur_success.php
@@ -15,24 +15,22 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript">
 
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-33801742-8']);
-          _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-33801742-8']);
+            _gaq.push(['_trackPageview']);
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+            (function ($) {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                
+                $(document).ready(function() {
+                    $('html, body').animate({
+                        scrollTop: $(".resultContainer").offset().top
+                    }, 300);
+                });
+            }(jQuery));
 
-
-          (function ($) {
-              $(document).ready(function() {
-                  $('html, body').animate({
-                      scrollTop: $(".ss").offset().top
-                  }, 300);
-              });
-          })(jQuery);
         </script>
     </head>
     <body class="animated fadeIn">
@@ -64,7 +62,7 @@
 
         <section class="ty-comment-section">
             <div class="container load-animate">
-                <div class="box ss">
+                <div class="box resultContainer">
                     <?PHP if ($result['isSuccessful'] === true) : ?>
                     <div id="success">
                         <div class="padding-top-70 padding-bottom-70 padding-left-30 padding-right-30">
