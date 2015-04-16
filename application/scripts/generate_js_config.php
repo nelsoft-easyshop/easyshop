3935f2048b81160108eb42772cbb93ed12aaefeb
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-16 19:06:45 +0800

    #4404 add class

--- application/scripts/generate_js_config.php
+++ application/scripts/generate_js_config.php
@@ -2,21 +2,65 @@
 include_once  __DIR__.'/bootstrap.php';
 $CI =& get_instance();
 $messageManager = $CI->kernel->serviceContainer['message_manager'];
-$file =  __DIR__.'/../bin/js/config.js';
-$configString = "    var fs = require('fs');
+$emailService = $CI->kernel->serviceContainer['email_notification'];
+$configLoader = $CI->kernel->serviceContainer['config_loader'];
+$viewParser = new \CI_Parser();
 
-    exports.configureExpress = function(app) {
-        app.set('NODE_PORT', " . $messageManager->getChatPort() . ");
-        app.set('NODE_HOST', '" . $messageManager->getChatHost() . "');
-        app.set('PROTOCOL', 'https');
-        app.set('KEY', fs.readFileSync('" . __DIR__ . "/../bin/js/key/easyshop.key'));
-        app.set('CERT', fs.readFileSync('" . __DIR__ . "/../bin/js/key/easyshop.crt'));
-        app.set('JWT_SECRET', '".$messageManager->getWebTokenSecret()."');
-        app.set('REDIS_PORT', ".$messageManager->getRedisPort().");
-        app.set('REDIS_HOST', '".$messageManager->getRedisHost()."');
-        app.set('REDIS_CHANNEL_NAME', '".$messageManager->getRedisChannelName()."');
-    };
-    ";
+use EasyShop\Script\ScriptBaseClass as ScriptBaseClass;
 
-file_put_contents($file, $configString);
-echo "Config file is created in application/bin/js";
+class GenerateJsConfig extends ScriptBaseClass
+{
+    private $emailService;
+    private $configLoader;
+    private $viewParser;
+    private $messageManager;
+
+    /**
+     * Constructor
+     * @param EasyShop\Notifications\EmailNotification $emailService
+     * @param EasyShop\ConfigLoader\ConfigLoader       $configLoader
+     * @param \CI_Parser                               $viewParser
+     * @param EasyShop\Message\MessageManager          $messageManager
+     */
+    public function __construct(
+        $emailService,
+        $configLoader,
+        $viewParser,
+        $messageManager
+    )
+    {
+        parent::__construct($emailService, $configLoader, $viewParser);
+        $this->messageManager = $messageManager;
+    }
+
+    public function execute()
+    {
+        $file =  __DIR__.'/../bin/js/config.js';
+        $configString = "
+            var fs = require('fs');
+            exports.configureExpress = function(app) {
+                app.set('NODE_PORT', " . $this->messageManager->getChatPort() . ");
+                app.set('NODE_HOST', '" . $this->messageManager->getChatHost() . "');
+                app.set('PROTOCOL', 'https');
+                app.set('KEY', fs.readFileSync('" . __DIR__ . "/../bin/js/key/easyshop.key'));
+                app.set('CERT', fs.readFileSync('" . __DIR__ . "/../bin/js/key/easyshop.crt'));
+                app.set('JWT_SECRET', '".$this->messageManager->getWebTokenSecret()."');
+                app.set('REDIS_PORT', ".$this->messageManager->getRedisPort().");
+                app.set('REDIS_HOST', '".$this->messageManager->getRedisHost()."');
+                app.set('REDIS_CHANNEL_NAME', '".$this->messageManager->getRedisChannelName()."');
+            };
+        ";
+
+        file_put_contents($file, $configString);
+        echo "Config file is created in application/bin/js";
+    }
+}
+
+$generateJsConfig  = new GenerateJsConfig(
+    $emailService,
+    $configLoader,
+    $viewParser,
+    $messageManager
+);
+
+$generateJsConfig->execute();
