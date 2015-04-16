0640e5323eb9e5b0088f94900563d577b0b16ec9
es-paulmodino <paul.modino@easyshop.ph>
2015-04-16 17:02:23 +0800

    #4299 fixed forgot password design issue

--- application/views/pages/user/forgotpass.php
+++ application/views/pages/user/forgotpass.php
@@ -1,19 +1,15 @@
 <link rel="stylesheet" type="text/css" href="/assets/css/new-login.css" media='screen'/>
 
-<div class="clear"></div>
-<br/>
-<br/>
-<section class="section-login">
+<section class="section-login bg-cl-fff">
     <div class="container pd-bttm-30">
-        <div class="panel panel-default">
-            <div class="panel-body div-merge-container" >
+        <div class="mrgn-top-40">
+            <div class="" >
                     <div class="identify-email-container">
-                        <p class="panel-title-merge">Forgot Password</p>
+                        <h1 class="panel-title-merge border-bottom pd-bttm-10 text-left">Forgot Password</h1>
                         Please type in your email address. We will send you an email to reset your password.
                         
                         <div class="row"><div style="display: block; height: 40px;"></div>
-                            <div class="col-sm-2 col-md-3"></div>
-                            <div class="col-xs-12 col-sm-8 col-md-6">
+                            <div class="col-xs-12 col-sm-7 col-md-6">
                                 <div class="identify-email-bottom-group response-dialog">
                                     <?php if(isset($isPost) && $isPost): ?>
                                         <?php if(isset($isSuccessful) && $isSuccessful): ?>
@@ -33,27 +29,24 @@
                         </div>
                         <?php echo form_open(); ?>
                         <div class="row">
-                            <div class="col-sm-2 col-md-3"></div>
-                            <div class="col-xs-12 col-sm-8 col-md-6  ">
-                                <div class="form-horizontal">
-                                    <label class="col-md-2"><span>Email : </span></label>
-                                    <div class="col-md-10">
+                            <div class="col-xs-12 col-sm-7 col-md-6">
+                                <div class="row form-horizontal">
+                                    <label class="col-xs-12 col-sm-2"><span>Email : </span></label>
+                                    <div class="col-xs-12 col-sm-10">
                                         <input type="text" class="form-control" name="email" placeholder="Please enter your registered email address..." id="email" aria-describedby="basic-addon1">   
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-2 col-md-3"></div>
                         </div>
                         <div class="row">
-                            <div class="col-sm-2 col-md-3"></div>
-                            <div class="col-xs-12 col-sm-8 col-md-6">
-                                <div class="col-md-2"></div>
-                                <div class="col-xs-12 col-sm-9 col-md-10 identify-email-bottom-group ">
-                                    <input type="submit" class="btn submit-email-form" name="submit" value="Submit"/>  
-                                    <a href="/" class="btn btn-default-1">CANCEL</a>
+                            <div class="col-xs-12 col-sm-7 col-md-6">
+                                <div class="row form-horizontal">
+                                    <div class="col-xs-12 col-sm-10 col-sm-offset-2 identify-email-bottom-group ">
+                                        <input type="submit" class="btn submit-email-form" name="submit" value="Submit"/>  
+                                        <a href="/" class="btn btn-default-1">CANCEL</a>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-sm-2 col-md-3"></div>
                         </div>
                         <?php echo form_close(); ?>
                     </div>
