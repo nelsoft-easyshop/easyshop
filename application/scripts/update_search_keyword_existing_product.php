909e7d3dbf042865e14e599cd559eeb8816d02ce
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-17 19:10:48 +0800

    #4448 add class

--- application/scripts/update_search_keyword_existing_product.php
+++ application/scripts/update_search_keyword_existing_product.php
@@ -1,30 +1,88 @@
 <?php
 
 include_once  __DIR__.'/bootstrap.php';
-
 $CI =& get_instance();
-
 $productManager = $CI->kernel->serviceContainer['product_manager'];
+$emailService = $CI->kernel->serviceContainer['email_notification'];
+$configLoader = $CI->kernel->serviceContainer['config_loader'];
+$viewParser = new \CI_Parser();
 
-$configDatabase = require dirname(__FILE__). '/../config/param/database.php';
+use EasyShop\Script\ScriptBaseClass as ScriptBaseClass;
 
-$dbh = new PDO("mysql:host=".$configDatabase['host'].";dbname=".$configDatabase['dbname'],
-                $configDatabase['user'],
-                $configDatabase['password']);
+class UpdateSearchKeyword extends ScriptBaseClass
+{
+    private $connection;
+    private $productManager;
 
-$sql = "SELECT id_product FROM es_product";
-$counter = 0;
-echo PHP_EOL .'Scanning of data started ('.date('M-d-Y h:i:s A').')'.PHP_EOL;
-echo PHP_EOL;
+    /**
+     * Constructor
+     * @param string                                   $hostName
+     * @param string                                   $dbUsername
+     * @param string                                   $dbPassword
+     * @param EasyShop\Notifications\EmailNotification $emailService
+     * @param EasyShop\ConfigLoader\ConfigLoader       $configLoader
+     * @param \CI_Parser                               $viewParser
+     * @param EasyShop\Product\ProductManager          $productManager
+     */
+    public function __construct(
+        $hostName,
+        $dbUsername,
+        $dbPassword,
+        $emailService,
+        $configLoader,
+        $viewParser,
+        $productManager
+    ) {
+        parent::__construct($emailService, $configLoader, $viewParser);
+        $this->connection = new PDO(
+            $hostName,
+            $dbUsername,
+            $dbPassword
+        );
+        $this->productManager = $productManager;
+    }
 
-foreach ($dbh->query($sql) as $row) {
-    $productManager->generateSearchKeywords($row['id_product']);
-    echo $row['id_product'] . ' DONE'.PHP_EOL;
-    $counter++;
+    /**
+     * Execute script
+     */
+    public function execute()
+    {
+        $productds = $this->getAllProducts();
+
+        echo PHP_EOL .'Scanning of data started ('.date('M-d-Y h:i:s A').')'.PHP_EOL;
+        echo PHP_EOL;
+
+        foreach ($productds as $product) {
+            $this->productManager->generateSearchKeywords($product['id_product']);
+            echo $product['id_product'] . ' DONE'.PHP_EOL;
+        }
+
+        echo PHP_EOL .'Scanning of data ended ('.date('M-d-Y h:i:s A').')'.PHP_EOL;
+        echo PHP_EOL.count($productds).' ROWS AFFECTED!'.PHP_EOL;
+    }
+
+    private function getAllProducts()
+    {
+        $getProductsQuery = "
+            SELECT id_product FROM es_product
+        ";
+
+        $getProducts = $this->connection->prepare($getProductsQuery);
+        $getProducts->execute();
+        $products = $getProducts->fetchAll(PDO::FETCH_ASSOC);
+
+        return $products;
+    }
 }
 
-echo PHP_EOL .'Scanning of data ended ('.date('M-d-Y h:i:s A').')'.PHP_EOL;
-echo PHP_EOL.$counter.' ROWS AFFECTED!'.PHP_EOL;
+$updateSearchKeyword  = new UpdateSearchKeyword(
+    $CI->db->hostname,
+    $CI->db->username,
+    $CI->db->password,
+    $emailService,
+    $configLoader,
+    $viewParser,
+    $productManager
+);
 
-$dbh = null;
-?>
+$updateSearchKeyword->execute();
