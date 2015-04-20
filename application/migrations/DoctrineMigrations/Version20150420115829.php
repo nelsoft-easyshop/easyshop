65e65dfc2d7fcd79dd780e546aae893af03442b1
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-20 12:36:13 +0800

    #4461 add down migration sql

--- application/migrations/DoctrineMigrations/Version20150420115829.php
+++ application/migrations/DoctrineMigrations/Version20150420115829.php
@@ -44,5 +44,116 @@ class Version20150420115829 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='2'
+            AND `date_d` = '2015-02-19'
+            AND `memo` = 'Chinese New Year';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-02-25'
+            AND `memo` = 'EDSA Revolution Anniversary';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-04-02'
+            AND `memo` = 'Maundy Thursday';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-04-03'
+            AND `memo` = 'Good Friday';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='2' 
+            AND `date_d` = '2015-04-04'
+            AND `memo` = 'Black Saturday';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-04-09'
+            AND `memo` = 'Day of Valor';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-05-01'
+            AND `memo` = 'Labor Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-06-12'
+            AND `memo` = 'Independence Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='2' 
+            AND `date_d` = '2015-08-21'
+            AND `memo` = 'Ninoy Aquino Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-08-31'
+            AND `memo` = 'National Heroes Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='2' 
+            AND `date_d` = '2015-11-01'
+            AND `memo` = 'All Saints Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-11-30'
+            AND `memo` = 'Bonifacio Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-12-24'
+            AND `memo` = 'Additional special non-working day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-12-25'
+            AND `memo` = 'Christmas Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='1' 
+            AND `date_d` = '2015-12-30'
+            AND `memo` = 'Rizal Day';
+        ");
+
+        $this->addSql("
+            DELETE FROM `es_holidaydetails` WHERE 
+            `type`='2' 
+            AND `date_d` = '2015-12-31'
+            AND `memo` = 'Last Day of the Year';
+        ");
     }
 }
