bc27548a78556b45afd176841072cea0c571cf18
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-20 15:40:27 +0800

    #4421 convert to spaces

--- web/assets/js/src/static.js
+++ web/assets/js/src/static.js
@@ -1,34 +1,24 @@
 (function ($) {
-	$(window).on("load", function(){
+    $(window).on("load", function(){
+        var tabSrc= $( document ).find(".tab-head-container a.active").attr('src');
+        $("#"+tabSrc).show();
+        $(".tab-head-container a").click(function(){
+            var $this = $(this);
+            var tabLink = $this.attr('src');
+            var tabName = $(document).find("#"+tabLink);
 
-		//default
-		var tabSrc= $( document ).find(".tab-head-container a.active").attr('src');
-		$("#"+tabSrc).show();
-
-		/*$(".tab-head-container li").click(function(){
-			$(this).parent().prepend(this);
-		});*/
-
-		$(".tab-head-container a").click(function(){
-			var tabLink = $(this).attr('src');
-			var tabName = $(document).find("#"+tabLink);
-
-			$(".tab-head-container a").not($(this)).removeClass("active");
-			$(this).addClass("active");
-			$(".tab-container").not(tabName).hide();
-			tabName.show();
-		});
-
-
-
-		$(".accordion-head").click(function(){
-			var accordionBody = $(this).parents(".accordion-item-container").find(".accordion-body")
-			$(".accordion-head").not(this).removeClass("toggled");
-			$(".accordion-body").not(accordionBody).slideUp("fast");
-			$(this).toggleClass("toggled");
-			accordionBody.slideToggle("fast");
-		});
-
-
-	});
+            $(".tab-head-container a").not($this).removeClass("active");
+            $this.addClass("active");
+            $(".tab-container").not(tabName).hide();
+            tabName.show();
+        });
+        $(".accordion-head").click(function(){
+            var $this = $(this);
+            var accordionBody = $this.parents(".accordion-item-container").find(".accordion-body")
+            $(".accordion-head").not($this).removeClass("toggled");
+            $(".accordion-body").not(accordionBody).slideUp("fast");
+            $this.toggleClass("toggled");
+            accordionBody.slideToggle("fast");
+        });
+    });
 })(jQuery);
