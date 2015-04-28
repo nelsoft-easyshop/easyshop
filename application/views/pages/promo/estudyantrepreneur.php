bbc6a2f360ddfa52c01edc07e5ad9a9bb86cba85
Sam <samgavinio@easyshop.ph>
2015-04-28 20:36:04 +0800

    #4564 add missing type annotation

--- application/views/pages/promo/estudyantrepreneur.php
+++ application/views/pages/promo/estudyantrepreneur.php
@@ -249,7 +249,7 @@
         <script type="text/javascript" src="/assets/js/src/vendor/jquery-1.9.1.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
         <script type="text/javascript" src="/assets/js/src/promo/estudyantrepreneur.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
         <script type="text/javascript" src="/assets/js/src/plugins.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
-        <script src="/assets/js/src/vendor/modernizr-2.6.2.min.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
+        <script type="text/javascript" src="/assets/js/src/vendor/modernizr-2.6.2.min.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
         <script type="text/javascript" src="/assets/js/src/promo/christmas-promo.js?ver=<?php echo ES_FILE_VERSION ?>"></script>
     <?php else: ?>
         <script src="/assets/js/min/easyshop.estudyantrepreneur-promo.js?ver=<?php echo ES_FILE_VERSION ?>" type="text/javascript"></script>
