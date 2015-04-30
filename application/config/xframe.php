2108d522ef18f3f57ad809a7dda260d20f2e48a5
Sam <samgavinio@easyshop.ph>
2015-04-30 15:50:12 +0800

    #4599 move xframe attachment code to  hook

---
+++ application/config/xframe.php
@@ -0,0 +1,17 @@
+<?php  
+
+if ( ! defined('BASEPATH')){
+    exit('No direct script access allowed');
+}
+
+$config = [
+    'bypassURI' => [
+    ],
+    'bypassFirstSegment' => [
+        'easywidget',
+    ],
+];
+        
+/* End of file csrf.php */
+/* Location: ./application/config/xframe.php */
+
