b00b15a570801ff837ad47f3982c956964d2dff7
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-04-30 13:17:27 +0800

    #4514 comment reference

--- web/assets/js/angular/directives/preloadResource.js
+++ web/assets/js/angular/directives/preloadResource.js
@@ -1,3 +1,11 @@
+
+/**
+ * Preload Resource Data into AngularJS
+ *
+ * https://robots.thoughtbot.com/preload-resource-data-into-angularjs
+ * Example: <div ng-cloak preload-resource="foo"></div>
+ * you can now access value foo by using $scope.preloadResource in your controller
+ */
 app.directive("preloadResource", function() {
     return {
         link: function(scope, element, attrs) {
