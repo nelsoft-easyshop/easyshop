3886fb925ab521f257e1c5ea66505b8cc42167ca
carlduazo <justine.duazo@easyshop.ph>
2015-05-04 15:35:19 +0800

    #4595 Added call to action markup

---
+++ application/views/pages/home/sell-an-item-call-to-action.php
@@ -0,0 +1,18 @@
+<div class="call-to-action-sell-container">
+    <a href="/sell/step1" title="Sell an item">
+        <div class="call-to-action-sell-wrapper">
+            <div class="call-to-action-sell-text">
+                Sell An Item
+            </div>
+            <div class="top-filler">
+
+            </div>
+            <div class="base">
+
+            </div>
+            <div class="icon-base">
+                <i class="icon-item"></i>
+            </div>
+        </div>
+    </a>
+</div>
