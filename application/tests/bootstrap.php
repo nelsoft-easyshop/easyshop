09cfa4c2dffd8ca9ddd78b247c219ef45e2322dc
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-05-05 14:33:59 +0800

    #4624 add new line

--- application/tests/bootstrap.php
+++ application/tests/bootstrap.php
@@ -6,13 +6,14 @@
  *---------------------------------------------------------------
  *
  * This will "override" later functions meant to be defined
- * in core\Common.php, so they throw erros instead of output strings
+ * in core\Common.php, so they throw errors instead of output strings
  */
 function show_error($message, $status_code = 500, $heading = 'An Error Was Encountered')
 {
     throw new PHPUnit_Framework_Exception($message, $status_code);
 }
-function show_404($page = '', $log_error = TRUE)
+
+function show_404($page = '', $log_error = true)
 {
     throw new PHPUnit_Framework_Exception($page, 404);
 }
@@ -32,7 +33,7 @@ ob_end_clean();
 
 /*
  * This will autoload controllers inside subfolders
- */ 
+ */
 spl_autoload_register(function ($class) {
     foreach (glob(APPPATH.'controllers/**/'.strtolower($class).'.php') as $controller) {
         require_once $controller;
