8dc6bdb4619c6b229ef67da86fc065cd8bf499ef
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-05-06 14:53:06 +0800

    #4625 add migration file ofr index

---
+++ application/migrations/DoctrineMigrations/Version20150506144943.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\Version;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150506144943 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_member` 
+            ADD FULLTEXT INDEX `ft_username_idx` (`username` ASC);
+        ");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            ALTER TABLE `es_member` 
+            DROP INDEX `ft_username_idx` ;
+        ");
+    }
+}
