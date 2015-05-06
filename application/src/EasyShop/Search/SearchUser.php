1a80d149353da75891293571641bddcb4153e941
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-05-06 14:29:10 +0800

    #4625 add fallback search if sphinx is disables

--- application/src/EasyShop/Search/SearchUser.php
+++ application/src/EasyShop/Search/SearchUser.php
@@ -10,12 +10,6 @@ class SearchUser
     const PER_PAGE = 30;
 
     /**
-     * Number of match matches for sphinx conf
-     */
-    const SPHINX_MATCH_MATCHES = 100000;
-
-
-    /**
      * Entity Manager instance
      *
      * @var Doctrine\ORM\EntityManager
@@ -37,17 +31,25 @@ class SearchUser
     private $userManager;
 
     /**
+     * Config Loader instance
+     * @var EasyShop\ConfigLoader\ConfigLoader
+     */
+    private $configLoader;
+
+    /**
      * Constructor. Retrieves Entity Manager instance
      *
      */
     public function __construct(
         $em,
         $sphinxClient,
-        $userManager
+        $userManager,
+        $configLoader
     ) {
         $this->em = $em;
         $this->sphinxClient = $sphinxClient;
         $this->userManager = $userManager;
+        $this->configLoader = $configLoader;
     }
 
     /**
@@ -58,18 +60,31 @@ class SearchUser
     private function filterBySearchString($queryString)
     {
         $ids = [];
+        $sphinxMatchMatches = $this->configLoader->getItem('search','sphinx_match_matches');
         $this->sphinxClient->SetMatchMode('SPH_MATCH_ANY');
         $this->sphinxClient->SetFieldWeights([
             'store_name' => 100,
         ]);
-        $this->sphinxClient->setLimits(0, self::SPHINX_MATCH_MATCHES, self::SPHINX_MATCH_MATCHES);
+        $this->sphinxClient->setLimits(0, $sphinxMatchMatches, $sphinxMatchMatches);
         $this->sphinxClient->AddQuery($queryString.'*', 'users');
         $sphinxResult = $this->sphinxClient->RunQueries();
 
         if ($sphinxResult === false) {
-            // TO DO Fallback search using mysql
+            $clearString = str_replace('"', '', preg_replace('!\s+!', ' ', $queryString));
+            if (trim($clearString) !== "") {
+                $explodedStringWithRegEx = explode(' ', trim(preg_replace('/[^A-Za-z0-9\ ]/', '', $clearString))); 
+                $wildCardString = !implode('* +', $explodedStringWithRegEx)
+                                  ? "" 
+                                  : '+'.implode('* +', $explodedStringWithRegEx) .'*';
+                $searchString = str_replace("+*", "", $wildCardString);
+                $users = $this->em->getRepository('EasyShop\Entities\EsMember')
+                                         ->searchUser($searchString); 
+                foreach ($users as $user) {
+                    $ids[] = $user['idMember']; 
+                }
+            }
         }
-        else if(isset($sphinxResult[0]['matches'])) {
+        elseif (isset($sphinxResult[0]['matches'])) { 
             foreach ($sphinxResult[0]['matches'] as $memberId => $member) {
                 $ids[] = $memberId;
             }
@@ -88,7 +103,7 @@ class SearchUser
         $queryString = $parameters['q_str'];
         $pageNumber = isset($parameters['page']) && $parameters['page']?trim($parameters['page']):false;
         $perPage = isset($parameters['limit']) ? $parameters['limit'] : self::PER_PAGE;
-        $offset = (int) bcmul($pageNumber, $perPage);
+        $offset = bcmul($pageNumber, $perPage);
         $memberIds = $this->filterBySearchString($queryString);
         $paginatedMemberIds = array_slice($memberIds, $offset, $perPage);
         $members = $this->em->getRepository('EasyShop\Entities\EsMember')
