29441e612cde5081569fa9f60c8283e15986ebdb
ryanannatuvasquez <ryanannatuvasquez@easyshop.ph>
2015-05-13 14:45:20 +0800

    #4696 update image

--- web/assets/js/src/vendor_header.js
+++ web/assets/js/src/vendor_header.js
@@ -379,7 +379,9 @@ var jsonCity = jQuery.parseJSON($('#json_city').val());
                     }
                     else if(formAction === 'upload_img'){
                         var avatarImage = $('img.avatar-image');
+                        var smallAvatar = $('img#vendor-profile-avatar');
                         avatarImage.attr('src',xhrResponse.image);
+                        smallAvatar.attr('src',xhrResponse.smallImage);
                     }
                     $.modal.close();
                     $('#banner-cancel-changes').trigger('click');
