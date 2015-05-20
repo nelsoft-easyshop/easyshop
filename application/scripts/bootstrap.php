14c2db5f6c1ed9119e6eb83c9a6008387ea2cf45
Sam <samgavinio@easyshop.ph>
2015-05-20 12:55:38 +0800

    #4752 set  values for CLI

--- application/scripts/bootstrap.php
+++ application/scripts/bootstrap.php
@@ -3,6 +3,13 @@
 include_once  __DIR__.'/../src/EasyShop/CLI/CLIHelper.php';
 include_once __DIR__ . '/../../vendor/autoload.php';
 
+$_SERVER['REMOTE_ADDR'] = 'CLI';
+$_SERVER['HTTP_HOST'] = 'CLI';
+$_SERVER['REQUEST_URI'] = 'CLI';
+$_SERVER['REQUEST_URI'] = 'CLI';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['HTTP_USER_AGENT'] = 'CLI';
+
 ob_start();
 include_once __DIR__ . '/../../web/index.php';
 ob_end_clean(); 
