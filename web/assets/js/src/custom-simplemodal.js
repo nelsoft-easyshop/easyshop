87a69b34b464656485c9f3225bad0d93f2efe259
carlduazo <justine.duazo@easyshop.ph>
2015-05-22 16:10:03 +0800

    #4761 Modified custom simplemodal function

--- web/assets/js/src/custom-simplemodal.js
+++ web/assets/js/src/custom-simplemodal.js
@@ -1,13 +1,14 @@
 function getMarginTop()
 {
-	$(this).bind("click resize",function(){
+	$(this).bind("myCustom click resize",function(){
 	    var windowHeight = $(window).outerHeight(); //Height of window
 	    var modalContentHeight = $("#simplemodal-data").outerHeight(); //Height modal content
 	    var remainingVerticalSpace = windowHeight - modalContentHeight; //Remaining vertical space of the window when modal is present
 	    var halfOfVerticalSpace = remainingVerticalSpace/2; //Half of the vertical space that will represent as the margin-top of the modal to center its vertical alignment
 
-	    $(".simplemodal-container").css("top", halfOfVerticalSpace+"px"); 
-	});
+	    $(".simplemodal-container").css("top", halfOfVerticalSpace+"px").end(); 
+	}).trigger("myCustom");
+
 }
 
 
